0f9eb84103a5da90b0d501b334511a67272f578c
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2012-08-14 08:58:01 -0500

    Added getPeople function, this is the file that gets all the data. It is a subset of the getTaskDetails function, just people only this time.

---
+++ web/services/RIS/getPeopleDetails.php
@@ -0,0 +1,37 @@
+<?php
+// Module: getPeopleDetails.php
+// Author(s): Michael van den Eijnden
+// Last Updated: 14 August 2012
+// Parameters: None
+// Returns: xml rest request
+// Purpose: Entry point in REST (using Slim) to return XML data for People Data
+
+//error_reporting(-1);
+
+require 'Slim/Slim.php';
+require 'getPeopleData.php';
+
+$debug = false;
+
+//With default settings
+$app = new Slim();
+
+if (!$debug)
+{
+	$app->error();
+}
+
+//Default function
+$app->get('/', function () use ($app) {
+    global $debug;
+	$allGetParams = $app->request()->get();
+	$results = getData($allGetParams); //Has to return TRUE or no XML.
+	$response = $app->response();
+	$response['Content-Type'] = 'application/xml';
+	$response['X-Powered-By'] = 'Slim';
+	//}
+});
+
+$app->run();
+
+?>
