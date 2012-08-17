955049e95c0b48e8ce2be26cb7603587cf05bd2d
Jew-Lee I. Lann <julie.lann@tamucc.edu>
2012-08-17 13:03:30 -0500

    Error connect

--- web/applications/dif/index.php
+++ web/applications/dif/index.php
@@ -90,7 +90,7 @@ em { font-weight: bold; padding-right: 1em; vertical-align: top; color:#FF0000;}
 #                    CONNECTION TO POSTGRES                        #
 ####################################################################
 $connection = pg_connect("host=172.22.10.206 port=5432 dbname=gomri user=postgres password=postgres")  
-or die ("Something went wrong --> " . pg_last_error($connection)); 
+or die ("ERROR:: " . pg_last_error($connection)); 
 if (!$connection) { die("Error in connection: " . pg_last_error()); } 
 ####################################################################
 #                    Check ID                        #
