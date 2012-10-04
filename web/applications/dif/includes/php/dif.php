0d8fc2ffcd5f292e174344d55cf486c182cb9edc
James C. Davis <James.Davis@tamucc.edu>
2012-10-04 17:48:08 -0500

    LDAP attributes return an array so need [0] for first attribute

--- web/applications/dif/includes/php/dif.php
+++ web/applications/dif/includes/php/dif.php
@@ -21,8 +21,8 @@ if (isset($uid)) {
     if (count($userDNs) > 0) {
         $attributes = getAttributes($ldap,$userDN,array('givenName','sn'));
         if (count($attributes) > 0) {
-            if (array_key_exists('givenName',$attributes)) $firstName = $attributes['givenName'];
-            if (array_key_exists('sn',$attributes)) $lastName = $attributes['sn'];
+            if (array_key_exists('givenName',$attributes)) $firstName = $attributes['givenName'][0];
+            if (array_key_exists('sn',$attributes)) $lastName = $attributes['sn'][0];
         }
     }
 }
