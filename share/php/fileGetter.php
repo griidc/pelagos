8d234f7e1eac73009428026cc7b4a4c4359157ad
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2012-10-10 15:50:09 -0500

    Moved from Enargite to Quartz.

---
+++ share/php/fileGetter.php
@@ -0,0 +1,70 @@
+<?php
+
+$file = preg_replace('/^\/[^\/]+$/','',$_SERVER['REQUEST_URI']);
+$file = preg_replace('/^\/[^\/]+\//','',$file);
+$file = str_replace('//','/',$file);
+$file = str_replace('../','',$file);
+if (preg_match('/^includes\//',$file))
+{
+    $info = finfo_open(FILEINFO_MIME_TYPE);
+    $mime = finfo_file($info, $file);
+
+    if ($mime === false)
+    {
+    header("HTTP/1.0 403 Not Found");
+        flush();
+        ob_clean();
+        exit;
+    }
+    header('Content-Length: ' . filesize($file));
+    header('Content-Disposition: inline; filename=' . basename($file));
+    header('Content-Transfer-Encoding: binary');
+    header('Content-type: '.$mime);
+    flush();
+    ob_clean();
+    readfile ($file);
+    exit;
+}
+elseif ($file != '')
+{
+    header("HTTP/1.0 403 Not Found");
+    flush();
+    ob_clean();
+    exit;
+}
+
+if ($_GET) 
+{
+    if (isset($_GET['getfile'])) 
+    {
+        $info = finfo_open(FILEINFO_MIME_TYPE);
+        $file = getcwd() .'/includes/'. $_GET['getfile'];
+        $file = str_replace('//','/',$file);
+        $file = str_replace('../','',$file);
+        $mime = finfo_file($info, $file);
+                
+        if ($mime === false)
+        {
+            header("HTTP/1.0 403 Not Found");
+            flush();
+            ob_clean();
+            exit;
+        }
+        header('Content-Length: ' . filesize($file));
+		header('Content-Disposition: inline; filename=' . basename($file));
+		header('Content-Transfer-Encoding: binary');
+        header('Content-type: '.$mime);
+        flush();
+        ob_clean();
+        readfile ($file);
+        exit;
+    }
+}
+
+if (!preg_match('/\?|\/$/',$_SERVER['REQUEST_URI']))
+{
+    $newLocation = 'Location:'.$_SERVER['REQUEST_URI']. '/';
+    header($newLocation);
+}
+
+?>
