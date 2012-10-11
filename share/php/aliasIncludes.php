33b511995fc421a7c3dc5fa23c95758480bc6f60
James C. Davis <James.Davis@tamucc.edu>
2012-10-11 11:06:02 -0500

    Improved aliasIncludes to allow pathinfo and redirect when querystring present

--- share/php/aliasIncludes.php
+++ share/php/aliasIncludes.php
@@ -11,7 +11,7 @@ if (preg_match('/^includes\//',$file))
 
     if ($mime === false)
     {
-    header("HTTP/1.0 403 Not Found");
+        header("HTTP/1.0 403 Not Found");
         flush();
         ob_clean();
         exit;
@@ -25,46 +25,14 @@ if (preg_match('/^includes\//',$file))
     readfile ($file);
     exit;
 }
-elseif ($file != '')
-{
-    header("HTTP/1.0 403 Not Found");
-    flush();
-    ob_clean();
-    exit;
-}
 
-if ($_GET) 
-{
-    if (isset($_GET['getfile'])) 
-    {
-        $info = finfo_open(FILEINFO_MIME_TYPE);
-        $file = getcwd() .'/includes/'. $_GET['getfile'];
-        $file = str_replace('//','/',$file);
-        $file = str_replace('../','',$file);
-        $mime = finfo_file($info, $file);
-                
-        if ($mime === false)
-        {
-            header("HTTP/1.0 403 Not Found");
-            flush();
-            ob_clean();
-            exit;
-        }
-        header('Content-Length: ' . filesize($file));
-		header('Content-Disposition: inline; filename=' . basename($file));
-		header('Content-Transfer-Encoding: binary');
-        header('Content-type: '.$mime);
-        flush();
-        ob_clean();
-        readfile ($file);
-        exit;
-    }
-}
+$URI = preg_split('/\?/',$_SERVER['REQUEST_URI']);
 
-if (!preg_match('/\?|\/$/',$_SERVER['REQUEST_URI']))
+if (!preg_match('/\/$/',$URI[0]))
 {
-    $newLocation = 'Location:'.$_SERVER['REQUEST_URI']. '/';
-    header($newLocation);
+    $newLocation = "$URI[0]/";
+    if (isset($URI[1]) and !empty($URI[1])) $newLocation .= "?$URI[1]";
+    header("Location: $newLocation");
 }
 
 ?>
