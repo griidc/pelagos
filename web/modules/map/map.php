bb3a30b4372e2ca6b0fd2c1fa55d4690999ca979
Julie Irena Lann <Julie.Lann@tamucc.edu>
2012-12-11 10:51:11 -0600

    version 1 of the map feature.

---
+++ web/modules/map/map.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <meta name = "description" content = "GRIIDC Map"/>
+        <title>GRIIDC Map</title>
+        <link rel="stylesheet" type="text/css" href="includes/css/map.css">
+	   <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+       <?PHP include('includes/js/map.js'); ?>
+	   <?PHP include('/includes/qTip2/jquery.qtip.min.css'); ?>
+       <script type="text/javascript" src="http://proteus.tamucc.edu/sites/all/modules/jquery_update/replace/jquery/jquery.min.js?v=1.5.2"></script>
+       <script type="text/javascript" src="https://proteus.tamucc.edu/includes/qTip2/jquery.qtip.min.js "></script>
+       <link rel="stylesheet" type="text/css" href="includes/css/jquery.qtip.css" />
+	   <script langauge="javascript">
+            function post_value(){
+               opener.document.ed.geoloc.value = document.frm.mc.value;
+               self.close();
+            }
+       </script>
+      <?PHP 
+  	   if (!file_exists('config.php')) {
+          echo 'Error: config.php is missing. Please see config.php.example for an example config file.';
+          exit;
+      }
+      require_once 'config.php';
+      $connection = pg_connect(GOMRI_DB_CONN_STRING) or die ("ERROR: " . pg_last_error($connection)); 
+      if (!$connection) { die("Error in connection: " . pg_last_error()); } 
+      $result3 = pg_exec($connection, "SELECT comments FROM form_info WHERE id=27");
+      if (!$result3) { die("Error in SQL query: " . pg_last_error()); } 
+      while($row = pg_fetch_row($result3)){$tip=$row[0];}
+	  #echo "$tip<-------------------";
+      #$tip = "<p>This user interactive map is to assist in defining coordinates of the geographical or study area. The control panel on the upper-left corner allows for zooming and panning. The drop-down on the upper-right is to toggle map types to use and the panel below the map contains tools to generate the coordinates. The first button is used to create polygon, the second is to get the coordinates of a point in the map and the third button is to delete inputs and start over.</p><p>Click the Submit button to complete the process.</p>";
+      echo"	
+       <script type=\"text/javascript\">
+      $(document).ready(function()
+      {
+      $.fn.qtip.defaults = $.extend(true, {}, $.fn.qtip.defaults, {
+                  position: {
+                          adjust: {
+                          method: \"flip flip\"
+                      },
+                          my: \"middle left\",
+                          at: \"middle right\",
+                          viewport: $(window)
+                          },
+                          show: {
+                          event: \"mouseenter focus\",
+                          solo: true
+                          },
+                          hide: {
+                          event: \"mouseleave blur\",
+                          delay: 100,
+                          fixed: true
+                          },
+                          style: {
+                      classes: \"ui-tooltip-shadow ui-tooltip-tipped\"
+                      }
+                  });
+
+      	$('#something').qtip({
+            	content: {
+      		text: '"."$tip"."'
+      	}
+      });
+     });
+ </script>";
+	   ?>
+ </head>
+<body id="body" onload="initmap()">
+   <div id="top"> 
+       <div id="map_canvas"></div> 
+   </div>
+   <div id="presenter2">
+       <form style="margin-right:5px" action="#" onsubmit="showAddress(this.address.value); return false">
+           <input type="text"  class="input"  size="50" name="address" value="Gulf of Mexico" />
+           <input type="submit" value="Search" class="button"  style="padding:7px;border:4px;"/>
+       </form>
+   </div>
+   <div id="zoom"> 
+      <form style="float:left;text-align:right;" action="#">
+         <input type="text" size="5" name="myzoom" id="myzoom" value="07" style="width:15px;" />
+      </form>
+   </div>
+   <div id="presenter"> 
+     <table border=0><tr><td>
+       <div class="topbutton cleair">
+          <table border=0 width="100%"><tr><td><div style="padding:7px 0px 0px 7px;"><strong>Tools:</strong></div></td><td><div style="float:right"><img id="something" src="/dif/images/info.png"></div></td></tr></table>
+           <form id="tools" style="padding:0px;" action="./" method="post" onsubmit="return false">
+		   <table border=0 width="80%"><tr><td>
+			  <div class="topbutton"><input type="image" src="images/polygon_icon.png" onclick="toolID=parseInt(this.value);setTool();" value="2"/></div></td><td>
+					 <div class="topbutton"><input type="image" src="images/marker_icon.png"  onclick="toolID=parseInt(this.value);setTool();" value="5"/></div></td><td>
+					 <div class="topbutton"><input type="image" src="images/clear_all.png"  onclick="toolID=parseInt(this.value);setTool();getfocus(this.value);" alt="Clear Screen" value="5"/></div></td></tr></table>
+           </form>   
+       </div>  
+     </td><td>
+        <form name="frm" style="float:right;text-align:right;padding-top:0px;" action="#"> 
+        <textarea name="mc" id="coords1"  maxlength="20" cols="57" rows="4"> </textarea>
+     </td><td>
+         <select id="over" style="width:180px; border:0px solid #000000;">
+           <option>LngLat mousemove</option>
+           <option  selected="selected">LatLng mousemove</option>
+        </select>
+		<br /><hr /><br />
+        <input type=button value='Submit' onclick="post_value();"> 
+	    
+        </form>
+	</td></tr></table>
+    </div>
+    <div id="bottom"></div>
+</div>
+</body>
+</html>
+
