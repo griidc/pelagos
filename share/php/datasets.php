152d92f928cbf2edf52938e2cb0709ad7a8cdb65
James C. Davis <James.Davis@tamucc.edu>
2013-03-18 16:25:37 -0500

    Changed dataset functions to take a search parameter and rank and order search results based on how many search words are found in the title and abstract.

--- share/php/datasets.php
+++ share/php/datasets.php
@@ -20,65 +20,65 @@ $GLOBALS['NULL_MAP'] = array(
 if (!defined('FILTER_REG')) define('FILTER_REG','/^(.*?)\s*(>=|<=|>|<|!=|=)\s*(.*?)$/');
 
 $GLOBALS['REGISTRY_FIELDS'] = array(
-    "registry_id",
-    "data_server_type",
-    "url_data",
-    "url_metadata",
-    "data_source_pull",
-    "doi",
-    "username",
-    "password",
-    "availability_date",
-    "access_status",
-    "access_period",
-    "access_period_start",
-    "access_period_weekdays",
-    "dataset_poc_name",
-    "dataset_poc_email",
-    "submittimestamp",
-    "userid",
-    "authentication",
-    "generatedoi",
-    "dataset_download_start_datetime",
-    "dataset_download_size",
-    "dataset_download_end_datetime",
-    "dataset_filename",
-    "dataset_uuid",
-    "dataset_metadata metadata_filename",
-    "dataset_download_error_log",
-    "dataset_download_status",
-    "to_char(submittimestamp,'YYYY') as year"
+    "r.registry_id",
+    "r.data_server_type",
+    "r.url_data",
+    "r.url_metadata",
+    "r.data_source_pull",
+    "r.doi",
+    "r.username",
+    "r.password",
+    "r.availability_date",
+    "r.access_status",
+    "r.access_period",
+    "r.access_period_start",
+    "r.access_period_weekdays",
+    "r.dataset_poc_name",
+    "r.dataset_poc_email",
+    "r.submittimestamp",
+    "r.userid",
+    "r.authentication",
+    "r.generatedoi",
+    "r.dataset_download_start_datetime",
+    "r.dataset_download_size",
+    "r.dataset_download_end_datetime",
+    "r.dataset_filename",
+    "r.dataset_uuid",
+    "r.dataset_metadata metadata_filename",
+    "r.dataset_download_error_log",
+    "r.dataset_download_status",
+    "to_char(r.submittimestamp,'YYYY') as year"
 );
 
 $GLOBALS['DIF_FIELDS'] = array(
-    "dataset_uid",
-    "task_uid",
-    "project_id",
-    "dataset_type",
-    "dataset_for",
-    "size",
-    "observation",
-    "approach",
-    "start_date",
-    "end_date",
-    "geo_location",
-    "historic_links",
-    "meta_editor",
-    "meta_standards",
-    "point",
-    "national",
-    "ethical",
-    "remarks",
-    "primary_poc",
-    "secondary_poc",
-    "logname",
-    "status",
-    "datafor"
+    "d.dataset_uid",
+    "d.task_uid",
+    "d.project_id",
+    "d.dataset_type",
+    "d.dataset_for",
+    "d.size",
+    "d.observation",
+    "d.approach",
+    "d.start_date",
+    "d.end_date",
+    "d.geo_location",
+    "d.historic_links",
+    "d.meta_editor",
+    "d.meta_standards",
+    "d.point",
+    "d.national",
+    "d.ethical",
+    "d.remarks",
+    "d.primary_poc",
+    "d.secondary_poc",
+    "d.logname",
+    "d.status",
+    "d.datafor"
 );
 
 $GLOBALS['REGISTRY_OVERRIDE_FIELDS'] = array(
-    "CASE WHEN dataset_title IS NULL THEN title ELSE dataset_title END AS title",
-    "CASE WHEN dataset_abstract IS NULL THEN abstract ELSE dataset_abstract END AS abstract"
+    "CASE WHEN r.dataset_title IS NULL THEN title ELSE r.dataset_title END AS title",
+    "CASE WHEN r.dataset_abstract IS NULL THEN abstract ELSE r.dataset_abstract END AS abstract"
 );
 
 $GLOBALS['IDENTIFIED_FROM'] = 'FROM datasets d
@@ -103,27 +103,93 @@ $GLOBALS['REGISTERED_FROM'] = 'FROM registry r
                                LEFT JOIN datasets d
                                ON d.dataset_udi = r.dataset_udi';
 
-function count_identified_datasets($dbh, $filters = array()) {
+$GLOBALS['IDENTIFIED_SEARCH_RANK'] = "
+JOIN
+(
+    SELECT d2.dataset_uid, COUNT(search_word) AS search_rank
+    FROM
+    (
+        SELECT
+            dataset_uid,
+            title || ' ' || abstract AS search_field
+        FROM datasets d3
+    ) AS d2
+
+    JOIN search_temp ON d2.search_field ~* ('\\y' || search_temp.search_word || '\\y')
+    GROUP BY d2.dataset_uid
+
+) ranked ON ranked.dataset_uid = d.dataset_uid";
+
+$GLOBALS['REGISTRY_SEARCH_RANK'] = "
+JOIN
+(
+    SELECT r2.registry_id, COUNT(search_word) AS search_rank
+    FROM
+    (
+        SELECT
+            registry_id,
+            dataset_title || ' ' || dataset_abstract AS search_field
+        FROM registry r3
+        INNER JOIN
+        (
+            SELECT MAX(registry_id) AS MaxID
+            FROM registry
+            GROUP BY substr(registry_id,1,16)
+        ) m
+        ON r3.registry_id = m.MaxID
+
+    ) AS r2
+
+    JOIN search_temp ON r2.search_field ~* ('\\y' || search_temp.search_word || '\\y')
+    GROUP BY r2.registry_id
+
+) ranked ON ranked.registry_id = r.registry_id";
+
+function count_identified_datasets($dbh, $filters = array(), $search = '') {
+    create_search_temp($dbh,$search);
+
     $SELECT = 'SELECT COUNT(d.dataset_udi)';
 
     $WHERE = build_where($filters);
 
-    $stmt = $dbh->prepare("$SELECT $GLOBALS[IDENTIFIED_FROM] $WHERE;");
-    $stmt->execute();
-    return $stmt->fetchColumn();
+    $stmt = $dbh->prepare("$SELECT $GLOBALS[IDENTIFIED_FROM] $GLOBALS[IDENTIFIED_SEARCH_RANK] $WHERE;");
+
+    if (!$stmt->execute()) {
+        $arr = $stmt->errorInfo();
+        print_r($arr);
+    }
+
+    $retval = $stmt->fetchColumn();
+
+    drop_search_temp($dbh);
+
+    return $retval;
 }
 
-function count_registered_datasets($dbh, $filters = array()) {
-    $SELECT = 'SELECT COUNT(registry_id)';
+function count_registered_datasets($dbh, $filters = array(), $search = '') {
+    create_search_temp($dbh,$search);
+
+    $SELECT = 'SELECT COUNT(r.registry_id)';
 
     $WHERE = build_where($filters,true);
 
-    $stmt = $dbh->prepare("$SELECT $GLOBALS[REGISTERED_FROM] $WHERE;");
-    $stmt->execute();
-    return $stmt->fetchColumn();
+    $stmt = $dbh->prepare("$SELECT $GLOBALS[REGISTERED_FROM] $GLOBALS[REGISTRY_SEARCH_RANK] $WHERE;");
+
+    if (!$stmt->execute()) {
+        $arr = $stmt->errorInfo();
+        print_r($arr);
+    }
+
+    $retval = $stmt->fetchColumn();
+
+    drop_search_temp($dbh);
+
+    return $retval;
 }
 
-function get_identified_datasets($dbh, $filters = array(), $order_by = 'udi') {
+function get_identified_datasets($dbh, $filters = array(), $search = '', $order_by = 'udi') {
+    create_search_temp($dbh,$search);
+
     $SELECT = 'SELECT ' . 
               implode(',',$GLOBALS['DIF_FIELDS']) . ',' .
               implode(',',$GLOBALS['REGISTRY_FIELDS']) . ',' .
@@ -132,23 +198,43 @@ function get_identified_datasets($dbh, $filters = array(), $order_by = 'udi') {
 
     $WHERE = build_where($filters);
 
-    $stmt = $dbh->prepare("$SELECT $GLOBALS[IDENTIFIED_FROM] $WHERE ORDER BY $order_by;");
-    $stmt->execute();
-    return $stmt->fetchAll();
+    $stmt = $dbh->prepare("$SELECT $GLOBALS[IDENTIFIED_FROM] $GLOBALS[IDENTIFIED_SEARCH_RANK] $WHERE ORDER BY search_rank DESC, $order_by;");
+
+    if (!$stmt->execute()) {
+        $arr = $stmt->errorInfo();
+        print_r($arr);
+    }
+
+    $retval = $stmt->fetchAll();
+
+    drop_search_temp($dbh);
+
+    return $retval;
 }
 
-function get_registered_datasets($dbh, $filters = array(), $order_by = 'registry_id') {
+function get_registered_datasets($dbh, $filters = array(), $search = '', $order_by = 'registry_id') {
+    create_search_temp($dbh,$search);
+
     $SELECT = 'SELECT ' . 
               implode(',',$GLOBALS['REGISTRY_FIELDS']) . ',' .
               implode(',',$GLOBALS['DIF_FIELDS']) . ',' .
               implode(',',$GLOBALS['REGISTRY_OVERRIDE_FIELDS']) . ',' .
-              "CASE WHEN r.dataset_udi IS NULL THEN substr(registry_id,1,16) ELSE r.dataset_udi END AS udi";
+              "CASE WHEN r.dataset_udi IS NULL THEN substr(r.registry_id,1,16) ELSE r.dataset_udi END AS udi";
 
     $WHERE = build_where($filters,true);
 
-    $stmt = $dbh->prepare("$SELECT $GLOBALS[REGISTERED_FROM] $WHERE ORDER BY $order_by;");
-    $stmt->execute();
-    return $stmt->fetchAll();
+    $stmt = $dbh->prepare("$SELECT $GLOBALS[REGISTERED_FROM] $GLOBALS[REGISTRY_SEARCH_RANK] $WHERE ORDER BY search_rank DESC, $order_by;");
+
+    if (!$stmt->execute()) {
+        $arr = $stmt->errorInfo();
+        print_r($arr);
+    }
+
+    $retval = $stmt->fetchAll();
+
+    drop_search_temp($dbh);
+
+    return $retval;
 }
 
 function build_where($filters,$registered = false) {
@@ -159,13 +245,13 @@ function build_where($filters,$registered = false) {
         if (preg_match(FILTER_REG,$filter,$matches)) {
             switch (strtolower($matches[1])) {
                 case 'registry_id':
-                    $WHERE .= " AND registry_id " . $GLOBALS['PGSQL_LIKE_MAP'][$matches[2]] . " '$matches[3]'";
+                    $WHERE .= " AND r.registry_id " . $GLOBALS['PGSQL_LIKE_MAP'][$matches[2]] . " '$matches[3]'";
                     break;
                 case 'registry_ids':
                     $registry_ids = preg_split('/,/',$matches[3]);
                     $registry_id_filters = array();
                     foreach ($registry_ids as $registry_id) {
-                        $registry_id_filters[] = "registry_id " . $GLOBALS['PGSQL_LIKE_MAP'][$matches[2]] . " '$registry_id%'";
+                        $registry_id_filters[] = "r.registry_id " . $GLOBALS['PGSQL_LIKE_MAP'][$matches[2]] . " '$registry_id%'";
                     }
                     if ($matches[2] == '!=') { $glue = ' AND '; }
                     else { $glue = ' OR '; }
@@ -202,7 +288,7 @@ function build_where($filters,$registered = false) {
                     break;
                 case 'title':
                     if ($registered) {
-                        $WHERE .= " AND dataset_title " . $GLOBALS['PGSQL_LIKE_MAP'][$matches[2]] . " '$matches[3]'";
+                        $WHERE .= " AND r.dataset_title " . $GLOBALS['PGSQL_LIKE_MAP'][$matches[2]] . " '$matches[3]'";
                     }
                     else {
                         $WHERE .= " AND title " . $GLOBALS['PGSQL_LIKE_MAP'][$matches[2]] . " '$matches[3]'";
@@ -210,7 +296,7 @@ function build_where($filters,$registered = false) {
                     break;
                 case 'abstract':
                     if ($registered) {
-                        $WHERE .= " AND dataset_abstract " . $GLOBALS['PGSQL_LIKE_MAP'][$matches[2]] . " '$matches[3]'";
+                        $WHERE .= " AND r.dataset_abstract " . $GLOBALS['PGSQL_LIKE_MAP'][$matches[2]] . " '$matches[3]'";
                     }
                     else {
                         $WHERE .= " AND abstract " . $GLOBALS['PGSQL_LIKE_MAP'][$matches[2]] . " '$matches[3]'";
@@ -220,10 +306,15 @@ function build_where($filters,$registered = false) {
                     $WHERE .= " AND status $matches[2] $matches[3]";
                     break;
                 case 'dataset_download_status':
-                    $WHERE .= " AND dataset_download_status $matches[2] '$matches[3]'";
+                    if ($matches[2] == '!=') {
+                        $WHERE .= " AND (r.dataset_download_status IS NULL OR r.dataset_download_status != '$matches[3]')";
+                    }
+                    else {
+                        $WHERE .= " AND (r.dataset_download_status IS NOT NULL AND r.dataset_download_status = '$matches[3]')";
+                    }
                     break;
                 case 'registered':
-                    $WHERE .= " AND registry_id " . $GLOBALS['IS_MAP'][$matches[2]] . ' ' . $GLOBALS['NULL_MAP'][$matches[3]];
+                    $WHERE .= " AND r.registry_id " . $GLOBALS['IS_MAP'][$matches[2]] . ' ' . $GLOBALS['NULL_MAP'][$matches[3]];
                     break;
                 case 'filter':
                     $WHERE .= " AND (";
@@ -242,6 +333,22 @@ function build_where($filters,$registered = false) {
     }
 
     return $WHERE;
+}
+
+function create_search_temp($dbh,$search) {
+    $stmt = $dbh->prepare("CREATE TEMP TABLE search_temp AS SELECT search_word FROM regexp_split_to_table('$search', '\\s+') AS search_word;");
+    if (!$stmt->execute()) {
+        $arr = $stmt->errorInfo();
+        print_r($arr);
+    }
+}
+
+function drop_search_temp($dbh) {
+    $stmt = $dbh->prepare("DROP TABLE search_temp;");
+    if (!$stmt->execute()) {
+        $arr = $stmt->errorInfo();
+        print_r($arr);
+    }
 }
 
 ?>
