6916f482739e31e058626379b72a006295cd807e
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2014-01-20 15:13:10 -0600

    Added status of dataset etc... on dataland page.
    PELAGOS-589

--- web/applications/dataland/details.php
+++ web/applications/dataland/details.php
@@ -6,8 +6,10 @@ include_once '/usr/local/share/GRIIDC/php/aliasIncludes.php';
 
 drupal_add_css('/dif/includes/css/overwrite.css',array('type'=>'external'));
 drupal_add_js('/includes/jquery-validation/jquery.validate.js',array('type'=>'external'));
+//drupal_add_js('/includes/qTip2/jquery.qtip.min.js',array('type'=>'external'));
 
 drupal_add_css('/data/includes/css/xmlverbatim.css',array('type'=>'external'));
+drupal_add_css('/includes/qTip2/jquery.qtip.min.css',array('type'=>'external'));
 
 drupal_add_library('system', 'ui.tabs');
 drupal_add_library('system', 'ui.button');
@@ -17,6 +19,10 @@ drupal_add_js('/includes/openlayers/lib/OpenLayers.js',array('type'=>'external')
 drupal_add_js('//maps.google.com/maps/api/js?v=3&sensor=false',array('type'=>'external'));
 drupal_add_js('/includes/geoviz/geoviz.js',array('type'=>'external'));
 
+$GLOBALS['config'] = parse_ini_file('config.ini',true);
+
+require_once 'Twig_Extensions_GRIIDC.php';
+
 include_once '/usr/local/share/GRIIDC/php/pdo.php';
 
 require_once '/usr/share/pear/Twig/Autoloader.php';
@@ -24,6 +30,7 @@ Twig_Autoloader::register();
 
 $loader = new Twig_Loader_Filesystem('./templates');
 $twig = new Twig_Environment($loader,array('autoescape' => false));
+$twig->addExtension(new Twig_Extensions_GRIIDC());
 
 $udi='';
 $dsscript='';
@@ -53,7 +60,29 @@ if ($udi <> '')
 	$pconn = pdoDBConnect($dbconnstr);
 
 	$pquery = "
-	SELECT * , ST_AsText(metadata.geom) AS \"the_geom\"
+	SELECT * , ST_AsText(metadata.geom) AS \"the_geom\",
+	CASE WHEN registry.dataset_title IS NULL THEN title ELSE registry.dataset_title END AS title,
+	CASE WHEN status = 2 THEN 1 WHEN status = 1 THEN 2 ELSE 0 END AS identified,
+	CASE WHEN status = 2 THEN 1 WHEN status = 1 THEN 2 ELSE 0 END AS identified,
+	CASE WHEN registry.registry_id IS NULL THEN 0 ELSE 1 END AS registered,
+	CASE WHEN metadata_dl_status IS NULL OR
+		metadata_dl_status != 'Completed' OR
+		url_metadata IS NULL OR
+		url_metadata NOT SIMILAR TO '(file://)?/sftp/data/%.met'
+	THEN 0
+	WHEN metadata_status = 'Accepted'
+		THEN 1
+	ELSE 2
+	END AS metadata,
+	CASE WHEN dataset_download_status IS NULL OR
+		dataset_download_status != 'done' OR
+		url_data IS NULL OR
+		(data_source_pull = 'f' AND url_data NOT SIMILAR TO '(file://)?/sftp/data/%.dat')
+	THEN 0
+	WHEN access_status = 'None'
+	THEN 1
+	ELSE 2
+	END AS available
 	FROM registry
 	LEFT OUTER JOIN datasets ON substr(registry.registry_id,0,17) = datasets.dataset_udi
 	LEFT OUTER JOIN metadata on registry.registry_id = metadata.registry_id
@@ -64,7 +93,7 @@ if ($udi <> '')
 	";
 
 	$prow = pdoDBQuery($pconn,$pquery);
-	//echo "<pre>";
+	//echo $pquery;
 	//var_dump($prow);
 
 	//echo "</pre>";
@@ -118,9 +147,9 @@ if ($udi <> '')
 
 	$mprow = pdoDBQuery($mconn,$mquery);
 
-	//echo "<pre>";
-	//var_dump($mprow);
-	//echo "</pre>";
+	// echo "<pre>";
+	// var_dump($prow);
+	// echo "</pre>";
 }
 function transform($xml, $xsl) { 
 	if ($xml <> "" AND $xml != null)
@@ -171,10 +200,14 @@ function transform($xml, $xsl) {
 (function ($) {
 	$(function() {
 		
-		$("#olmap").width($(document).width()/2.5);
-		$("#olmap").height($("#olmap").width()/4*3);
+		resizeMap();
+		
+		$( window ).resize(function()
+		{
+			resizeMap();
+		});
 				
-		$("#rawxml").width($(document).width()-650);
+		$("#rawxml").width($(document).width()*.90);
 	
 		$("#tabs").tabs({ heightStyle: "content" });
 		
@@ -239,22 +272,39 @@ function transform($xml, $xsl) {
 		
 	});
 	
+	function resizeMap()
+	{
+		$("#olmap").width($(document).width()*.40);
+		mapscreenhgt = $("#olmap").width()/4*3;
+		summaryhgt = $("#summary").height()
+		if (mapscreenhgt > summaryhgt)
+		{
+			$("#olmap").height(mapscreenhgt)
+		}
+		else
+		{
+			$("#olmap").height(summaryhgt)
+		}
+	};
+	
 	$(document).on('imready', function(e) {
 		<?php echo $dsscript;?>
-		console.log("added");
+		//console.log("added");
 	});
 })(jQuery);
 </script>
   
 <table border="0" width="100%">
 <tr>
-<td width="30%">
+<td width="40%">
 
 <div id="olmap" styale="width: 640px;height: 480px;"></div>
 
 </td>
-<td style="padding:10px;" width="70%" valign="top">
-<?php echo $twig->render('summary.html', array('pdata' => $prow,'mdata' => $mrow,'mpdata' => $mprow)); ?>
+<td style="padding:10px;" width="60%" valign="top">
+<div id="summary">
+<?php  echo $twig->render('summary.html', array('pdata' => $prow,'mdata' => $mrow,'mpdata' => $mprow)); ?>
+</div>
 </td>
 </tr>
 </table>
@@ -271,6 +321,7 @@ function transform($xml, $xsl) {
 			-->
 		</ul>
 		<div class="tabb" id="tabs-1">
+		
 			<?php echo $twig->render('details.html', array('pdata' => $prow,'mdata' => $mrow,'mpdata' => $mprow)); ?>
 		</div>
 		<div class="tabb" id="tabs-2" style="overflow:auto;word-wrap:break-word;height:100%;">
