4af05dee23c7be3a525346ff6f44a35f938aca57
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2014-01-29 13:42:59 -0600

    Updated changes for Metamap, tooltips, and help text.

--- web/applications/metadata-editor/includes/metamap.php
+++ web/applications/metadata-editor/includes/metamap.php
@@ -1,15 +1,42 @@
-<title>GeoViz Template</title>
-<link type="text/css" rel="stylesheet" href="//code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
-<script type="text/javascript" src="//code.jquery.com/jquery-1.9.1.js"></script>
+<title>GeoViz Mapping Tool</title>
+<link type="text/css" rel="stylesheet" href="/sites/all/modules/jquery_update/replace/ui/themes/base/minified/jquery.ui.theme.min.css" />
+<link type="text/css" rel="stylesheet" href="/includes/qTip2/jquery.qtip.min.css" />
+<script type="text/javascript" src="//code.jquery.com/jquery-1.8.2.js"></script>
 <script type="text/javascript" src="//code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
 <script type="text/javascript" src="/includes/openlayers/lib/OpenLayers.js"></script>
 <script type="text/javascript" src="//maps.google.com/maps/api/js?v=3&sensor=false"></script>
+<script type="text/javascript" src="/includes/qTip2/jquery.qtip.min.js"></script>
 
-<script src="/includes/geoviz/geoviz.js"></script>
+
+<!--<script src="/includes/geoviz/geoviz.js"></script>-->
+<script src="/~mvandeneijnden/map/geoviz.js"></script>
 
 <script>
 	$(document).ready(function() 
 	{
+		$.fn.qtip.defaults = $.extend(true, {}, $.fn.qtip.defaults, {
+            show: {
+                event: "mouseenter focus",
+                solo: true
+            },
+            hide: {
+                event: "mouseleave blur",
+                delay: 100,
+                fixed: true
+            },
+            style: {
+                classes: "ui-tooltip-shadow ui-tooltip-tipped"
+            },
+			position: {
+                adjust: {
+                    method: "flip flip"
+                },
+                my: "top left",
+                at: "bottom right",
+                viewport: $(window)
+			}
+        });
+		
 		initMap('olmap',{'onlyOneFeature':true,'allowModify':true,'allowDelete':true});
 		initToolbar('maptoolbar',{'showDrawTools':false,'showExit':true});
 	});
