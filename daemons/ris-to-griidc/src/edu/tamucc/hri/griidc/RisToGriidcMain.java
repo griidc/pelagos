b190159feda7c6a0cd0c321b93f1a57edfaa2a70
Joe Holland <joe.holland@tamucc.edu>
2014-02-05 11:35:11 -0600

    RISIMP-1  the main driver and command line executable

--- daemons/ris-to-griidc/src/edu/tamucc/hri/griidc/RisToGriidcMain.java
+++ daemons/ris-to-griidc/src/edu/tamucc/hri/griidc/RisToGriidcMain.java
@@ -12,33 +12,51 @@ import edu.tamucc.hri.griidc.support.RisInstDeptPeopleErrorCollection;
 
 public class RisToGriidcMain {
 
-	public InstitutionSynchronizer instSynker = new InstitutionSynchronizer();
-	public DepartmentSynchronizer deptSynker = new DepartmentSynchronizer();
-	public PersonSynchronizer personSynker = new PersonSynchronizer();
+	private InstitutionSynchronizer instSynker = new InstitutionSynchronizer();
+	private DepartmentSynchronizer deptSynker = new DepartmentSynchronizer();
+	private PersonSynchronizer personSynker = new PersonSynchronizer();
+	private FundingEnvelopeSynchronizer fundingEnvelopeSynker = new FundingEnvelopeSynchronizer();
+	private ProjectSynchronizer projectSynker = new ProjectSynchronizer();
 
-	
+	private static boolean RunEverything = true;
+
 	public RisToGriidcMain() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
 		RisToGriidcMain risToGriidcMain = new RisToGriidcMain();
+		System.out.println("-- Start risToGriidcMain --");
+		InstitutionSynchronizer.setDebug(false);
+		DepartmentSynchronizer.setDebug(false);
+		PersonSynchronizer.setDebug(false);
+		EmailSynchronizer.setDebug(false);
+		FundingEnvelopeSynchronizer.setDebug(false);
+		ProjectSynchronizer.setDebug(false);
+
+		RisInstDeptPeopleErrorCollection risInstitutionWithErrors = null;
 		try {
-			RisInstDeptPeopleErrorCollection risInstitutionWithErrors = null;
-			InstitutionSynchronizer.setDebug(false);
-			DepartmentSynchronizer.setDebug(false);
-			PersonSynchronizer.setDebug(false);
-			EmailSynchronizer.setDebug(false);
-			risInstitutionWithErrors = risToGriidcMain.instSynker.syncGriidcInstitutionFromRisInstitution();
-			risInstitutionWithErrors = risToGriidcMain.deptSynker.syncGriidcDepartmentFromRisDepartment(risInstitutionWithErrors);
-			risInstitutionWithErrors = risToGriidcMain.personSynker.syncGriidcPersonFromRisPeople(risInstitutionWithErrors);
-			
+			risInstitutionWithErrors = risToGriidcMain.instSynker
+					.syncGriidcInstitutionFromRisInstitution();
+			risInstitutionWithErrors = risToGriidcMain.deptSynker
+					.syncGriidcDepartmentFromRisDepartment(risInstitutionWithErrors);
+			risInstitutionWithErrors = risToGriidcMain.personSynker
+					.syncGriidcPersonFromRisPeople(risInstitutionWithErrors);
+			risToGriidcMain.fundingEnvelopeSynker
+					.syncGriidcFundingEnvelopeFromRisFundingSource();
+			risToGriidcMain.projectSynker.syncGriidcProjectFromRisPrograms();
+
 			MiscUtils.closePrimaryLogFile();
 			MiscUtils.closeRisErrorLogFile();
+
 			risToGriidcMain.report();
 			risToGriidcMain.emailLogs();
-			MiscUtils.writeStringToFile("InstDeptPeopleDetail.txt",risInstitutionWithErrors.toString());
+			MiscUtils.writeStringToFile("InstDeptPeopleDetail.txt",
+					risInstitutionWithErrors.toString());
+
 			System.out.println("END of risToGriidcMain");
+			System.out.println(MiscUtils.getProjectNumberFundingCycleCache()
+					.toString());
 
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -75,64 +93,106 @@ public class RisToGriidcMain {
 		emailer.sendEmail(from, tos, subject, msg);
 	}
 
-	
-	public void report() throws FileNotFoundException, PropertyNotFoundException {
+	public void report() throws FileNotFoundException,
+			PropertyNotFoundException {
+
+		String pFormat = "%-50s %10d%n";
 
 		System.out.println("RisToGriidcMain finished");
-		
-		System.out.println("************************************************");
-		System.out.println("Read " + instSynker.getRisInstitutionCount()
-				+ " RIS Institutions records");
-		System.out.println("RISS institutions            errors: "
-				+ instSynker.getRisInstitutionsErrors());
-		System.out.println("RISS institutions           skipped: "
-				+ instSynker.getRisInstitutionsSkipped());
-		System.out.println("GRIIDC Institutions           added: "
-				+ instSynker.getGriidcInstitutionsAdded());
-		System.out.println("GRIIDC Institutions        modified: "
-				+ instSynker.getGriidcInstitutionsModified());
-		System.out.println("GRIIDC Institutions      duplicates: "
-				+ instSynker.getGriidcInstitutionsDuplicates());
-		System.out.println("************************************************");
-		System.out.println("Read " + deptSynker.getRisDepartmentCount() + 
-				" RIS Department records");
-		System.out.println("RISS departments            errors: "
-				+ deptSynker.getRisDepartmentErrors());
-		System.out.println("RISS departments           skipped: "
-				+ deptSynker.getRisDepartmentRecordsSkipped());
-		System.out.println("GRIIDC departments           added: "
-				+ deptSynker.getGriidcDepartmentsAdded());
-		System.out.println("GRIIDC departments        modified: "
-				+ deptSynker.getGriidcDepartmentsModified());
-		System.out.println("GRIIDC departments      duplicates: "
-				+ deptSynker.getGriidcDepartmentsDuplicates());
-		System.out.println("************************************************");
-		System.out.println("Read " + personSynker.getRisPeopleCount() + 
-				" RIS People records");
-		System.out.println("RISS People            errors: "
-				+ personSynker.getRisPeopleErrors());
-		System.out.println("RISS People           skipped: "
-				+ personSynker.getRisPeopleRecordsSkipped());
-		System.out.println("GRIIDC Person           added: "
-				+ personSynker.getGriidcPersonsAdded());
-		System.out.println("GRIIDC Person        modified: "
-				+ personSynker.getGriidcPersonsModified());
-		System.out.println("GRIIDC Person      duplicates: "
-				+ personSynker.getGriidcPersonsDuplicates());
-		System.out.println("************************************************");
-		System.out.println("Added " + personSynker.getTelephoneRecordsAdded() + 
-				" GRIIDC Telephone records");
-		System.out.println("RISS People telephone number errors: "
-				+ personSynker.getRisTelephoneErrors());
-		
-		System.out.println("************************************************");
-		
+
+		System.out
+				.println("*************************************************************");
+		System.out.printf(pFormat, "RIS Institutions records read:",
+				instSynker.getRisRecordCount());
+		System.out.printf(pFormat, "RISS Institutions errors:",
+				instSynker.getRisRecordErrors());
+		System.out.printf(pFormat, "RISS Institutions skipped:",
+				instSynker.getRisRecordsSkipped());
+		System.out
+				.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+		System.out.printf(pFormat, "GRIIDC Institutions added:",
+				instSynker.getGriidcRecordsAdded());
+		System.out.printf(pFormat, "GRIIDC Institutions modified:",
+				instSynker.getGriidcRecordsModified());
+		System.out.printf(pFormat, "GRIIDC Institutions duplicates:",
+				instSynker.getGriidcRecordDuplicates());
+		System.out
+				.println("*************************************************************");
+		System.out.printf(pFormat, "RIS Departments records read:",
+				deptSynker.getRisRecordCount());
+		System.out.printf(pFormat, "RISS Departments errors:",
+				deptSynker.getRisRecordErrors());
+		System.out.printf(pFormat, "RISS Departments skipped:",
+				deptSynker.getRisRecordsSkipped());
+		System.out
+				.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+		System.out.printf(pFormat, "GRIIDC Department added:",
+				deptSynker.getGriidcRecordsAdded());
+		System.out.printf(pFormat, "GRIIDC Department modified:",
+				deptSynker.getGriidcRecordsModified());
+		System.out.printf(pFormat, "GRIIDC Department duplicates:",
+				deptSynker.getGriidcRecordDuplicates());
+		System.out
+				.println("*************************************************************");
+		System.out.printf(pFormat, "RIS People records read:",
+				personSynker.getRisRecordCount());
+		System.out.printf(pFormat, "RISS People errors:",
+				personSynker.getRisRecordErrors());
+		System.out.printf(pFormat, "RISS People skipped:",
+				personSynker.getRisRecordsSkipped());
+		System.out.printf(pFormat, "RISS People telephone number errors:",
+				personSynker.getRisTelephoneErrors());
+		System.out
+				.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+		System.out.printf(pFormat, "GRIIDC Telephone records added ",
+				personSynker.getTelephoneRecordsAdded());
+		System.out.printf(pFormat, "GRIIDC Person added:",
+				personSynker.getGriidcRecordsAdded());
+		System.out.printf(pFormat, "GRIIDC Person modified:",
+				personSynker.getGriidcRecordsModified());
+		System.out.printf(pFormat, "GRIIDC Person duplicates:",
+				personSynker.getGriidcRecordDuplicates());
+		System.out
+				.println("*************************************************************");
+		System.out.printf(pFormat, "RIS Funding Source records read:",
+				fundingEnvelopeSynker.getRisRecordCount());
+		System.out.printf(pFormat, "RISS Funding Source errors:",
+				fundingEnvelopeSynker.getRisRecordErrors());
+		System.out.printf(pFormat, "RISS Funding Source skipped:",
+				fundingEnvelopeSynker.getRisRecordsSkipped());
+		System.out
+				.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+		System.out.printf(pFormat, "GRIIDC Funding Envelope records added:",
+				fundingEnvelopeSynker.getGriidcRecordsAdded());
+		System.out.printf(pFormat, "GRIIDC Funding Envelope records modified:",
+				fundingEnvelopeSynker.getGriidcRecordsModified());
+		System.out.printf(pFormat, "GRIIDC Funding Envelope duplicates:",
+				fundingEnvelopeSynker.getGriidcRecordDuplicates());
+		System.out
+				.println("*************************************************************");
+		System.out.printf(pFormat, "RIS Programs records read:",
+				projectSynker.getRisRecordCount());
+		System.out.printf(pFormat, "RISS Programs errors:",
+				projectSynker.getRisRecordErrors());
+		System.out.printf(pFormat, "RISS Programs skipped:",
+				projectSynker.getRisRecordsSkipped());
+		System.out
+				.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+		System.out.printf(pFormat, "GRIIDC Project records added:",
+				projectSynker.getGriidcRecordsAdded());
+		System.out.printf(pFormat, "GRIIDC Project records modified:",
+				projectSynker.getGriidcRecordsModified());
+		System.out.printf(pFormat, "GRIIDC Project records duplicates:",
+				projectSynker.getGriidcRecordDuplicates());
+		System.out
+				.println("*************************************************************");
+
 		System.out.println("Activity reported to log file: "
 				+ MiscUtils.getPrimaryLogFileName());
 		System.out.println(MiscUtils.getRisErrorLogCount()
 				+ " RIS Data Errors reported to log file: "
 				+ MiscUtils.getRisErrorLogFileName());
-		
+
 	}
 
 }
