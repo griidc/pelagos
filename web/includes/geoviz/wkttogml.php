e275182c79fb8d0eeddbb250097ea2844052dc4d
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2014-07-09 10:43:32 -0500

    Added GML ID feature.

--- web/includes/geoviz/wkttogml.php
+++ web/includes/geoviz/wkttogml.php
@@ -5,6 +5,65 @@
 
 include_once '/usr/local/share/GRIIDC/php/pdo.php';
 
+function addGMLID($gml)
+{
+    
+    $doc = new DomDocument('1.0','UTF-8');
+    $doc->loadXML($gml,LIBXML_NOERROR);
+    
+    
+    foreach ($doc->childNodes as $node)
+    {
+        $topNode = $node->nodeName;
+        switch ($topNode)
+        {
+            case 'gml:Polygon':
+            $node->setAttribute('gml:id','Polygon1');
+            break;
+            case 'gml:Curve':
+            $node->setAttribute('gml:id','Curve1');
+            break;
+            case 'gml:Point':
+            $node->setAttribute('gml:id','Point1');
+            break;
+            case 'gml:MultiPoint':
+            $node->setAttribute('gml:id','Multipoint1');
+            $i=0;
+            foreach ($node->childNodes as $child)
+            {
+                $i++;
+                $child->firstChild->setAttribute('gml:id',"Point$i");
+            }
+            break;
+            case 'gml:MultiCurve':
+            $node->setAttribute('gml:id','MultiCurve1');
+            $i=0;
+            foreach ($node->childNodes as $child)
+            {
+                $i++;
+                $child->firstChild->setAttribute('gml:id',"Curve$i");
+            }
+            break;
+            case 'gml:MultiSurface':
+            $node->setAttribute('gml:id','MultiSurface');
+            $i=0;
+            foreach ($node->childNodes as $child)
+            {
+                $i++;
+                $child->firstChild->setAttribute('gml:id',"Polygon$i");
+            }
+            break;
+        }
+    }
+    
+    $gml = $doc->saveXML();
+    $cleanXML = new SimpleXMLElement($gml,LIBXML_NOERROR);
+    $dom = dom_import_simplexml($cleanXML);
+    $gml = $dom->ownerDocument->saveXML($dom->ownerDocument->documentElement);
+    return $gml;
+    
+}
+
 $wkt='';
 $gml='';
 
@@ -31,7 +90,9 @@ $row = pdoDBQuery($conn,$query);
 $gml = $row[0]["st_asgml"];
 
 //echo "{\"featureid\":\"$featureid\",\"reason\":\"$reason\"}";
-echo $gml;
+
+
+echo addGMLID($gml);;
 
 ?>
 
