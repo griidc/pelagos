ca37a9b91d985c4916c1a9bc06e76b0979f2fe8e
Michael Scott Williamson <michael.williamson@tamucc.edu>
2014-07-24 13:56:25 -0500

    Corrected location of geoviz library call

--- web/applications/data-discovery/index.php
+++ web/applications/data-discovery/index.php
@@ -80,7 +80,7 @@ $app->get('/css/:name.css', function ($name) use ($app) {
 $app->get('/', function () use ($app) {
     drupal_add_js('/includes/openlayers/lib/OpenLayers.js',array('type'=>'external'));
     drupal_add_js('//maps.google.com/maps/api/js?v=3&sensor=false',array('type'=>'external'));
-    drupal_add_js('/geoviz/js/geoviz.js','external');
+    drupal_add_js('/includes/geoviz/geoviz.js','external');
     $stash=index($app);
     # for now, only do this for guestAuthUser people, GoMRI auto-download is handled elsewhere.
     if( (isset($_COOKIE['dl_attempt_udi_cookie'])) and user_is_logged_in_somehow() ) {
