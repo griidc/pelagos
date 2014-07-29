b641690eabf0ece7fbe8a0ddc43043ba65608852
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2014-07-29 15:25:33 -0500

    Removed GML template comment code.
    PELAGOS-806

--- web/applications/metadata-editor/EX_BoundingPolygon.php
+++ web/applications/metadata-editor/EX_BoundingPolygon.php
@@ -51,16 +51,3 @@ class EX_BoundingPolygon
 }
 
 ?>
-<!--
-<gmd:EX_BoundingPolygon id="boundingPolygon"> 
-    <gmd:polygon> 
-        <gml:Polygon> 
-        <gml:interior> 
-            <gml:LinearRing> 
-                <gml:coordinates decimal=" 156.86274,71.34815 -156.87389,71.33893 -156.88004,71.33883 -156.89144,71.33259 -156.89982,71.33182 156.86274,71.34815 "/> 
-            </gml:LinearRing> 
-        </gml:interior> 
-    </gml:Polygon> 
-    </gmd:polygon> 
-</gmd:EX_BoundingPolygon>
--->
