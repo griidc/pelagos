7e3f9df350c0a0e40a47e586dc75e283d0a8b3f7
James C. Davis <James.Davis@tamucc.edu>
2014-08-05 15:01:43 -0500

    Added note about password expiration to account creation confirmation email.

--- web/applications/account/index.php
+++ web/applications/account/index.php
@@ -484,7 +484,10 @@ $app->post('/approve/create', $GLOBALS['AUTH_FOR_ROLE']('admin'), function () us
             $toAddress = $ldif['person']['mail']['value'];
             $fromAddress = 'GRIIDC Account Request <griidc@gomri.org>';
             $subject = "GRIIDC Account Request Approved: $uid";
-            $message = "Your account request has been approved.\n\nYour username is: $uid\n\nYou may now use this username and the password you provided to log in to GRIIDC services.";
+            $message = "Your account request has been approved.";
+            $message .= "\n\nYour username is: $uid";
+            $message .= "\n\nYou may now use this username and the password you provided to log in to GRIIDC services.";
+            $message .= "\n\nNote: You must change your password every 90 days or it will expire. You will begin receiving password expiration warnings one week before your password expires.";
 
             mail($toAddress,$subject,$message,"From: $fromAddress");
 
