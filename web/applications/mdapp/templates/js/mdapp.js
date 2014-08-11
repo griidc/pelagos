ec55b8a0e4940d3d3ae582a2fa6b88c7d4736657
Michael Scott Williamson <michael.williamson@tamucc.edu>
2014-08-11 15:14:47 -0500

    Altered datatables pagination control to support "all"

--- web/applications/mdapp/templates/js/mdapp.js
+++ web/applications/mdapp/templates/js/mdapp.js
@@ -13,7 +13,8 @@ $(document).ready( function () {
 
     $('table.display').dataTable( {
         "paging": true,
-        "jQueryUI": true
+        "jQueryUI": true,
+        "lengthMenu": [ [-1, 25, 50, 75, 100], ["All", 25, 50, 75, 100] ]
     } );
 } );
 
