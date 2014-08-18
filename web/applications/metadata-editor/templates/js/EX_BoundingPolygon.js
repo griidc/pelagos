5f5732568efae902460ad70667dab4db7e5ae504
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2014-08-18 10:30:33 -0500

    Name change for mini map.

--- web/applications/metadata-editor/templates/js/EX_BoundingPolygon.js
+++ web/applications/metadata-editor/templates/js/EX_BoundingPolygon.js
@@ -1,5 +1,5 @@
 
-    geowizard = new MapWizard({"divSmallMap":"difMap","divSpatial":"spatial","divNonSpatial":"nonspatial","divSpatialWizard":"spatwizbtn","gmlField":"BPL1_{{instanceName}}","descField":"EX1_{{instanceName}}","spatialFunction":"changeExtent{{instanceName}}"});
+    geowizard = new MapWizard({"divSmallMap":"smlMDEMap","divSpatial":"spatial","divNonSpatial":"nonspatial","divSpatialWizard":"spatwizbtn","gmlField":"BPL1_{{instanceName}}","descField":"EX1_{{instanceName}}","spatialFunction":"changeExtent{{instanceName}}"});
 	
 	$("#dtabs-4").on('active', function() {
 		console.log('tabs4-click');
