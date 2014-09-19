2ea4e88c43a0133cf90c28c7262adb42f8c161c2
James C. Davis <James.Davis@tamucc.edu>
2014-09-19 13:14:47 -0500

    Expect first parameter to be a Pelagos::Logger object instead of a file handle

--- share/perl/lib/Pelagos/DataStore.pm
+++ share/perl/lib/Pelagos/DataStore.pm
@@ -16,7 +16,7 @@ sub check_data_store_directory {
     my ($log,$config,$udi,$custodian_uid,$custodian_gid) = @_;
     # check if data store directory exists for this udi
     if (! -d "$config->{paths}->{data_store}/$udi") {
-        writelog($log,"[$udi] creating data store directory $config->{paths}->{data_store}/$udi");
+        $log->write("[$udi] creating data store directory $config->{paths}->{data_store}/$udi");
         # make directory
         mkdir("$config->{paths}->{data_store}/$udi",'0750') or die "Error: could not create directory: $config->{paths}->{data_store}/$udi ($!)\n";
         # make custodian the owner
@@ -30,7 +30,7 @@ sub check_download_directory {
     my ($log,$config,$udi,$apache_uid,$apache_gid,$r) = @_;
     # check if download directory exists for this udi
     if (! -d "$config->{paths}->{download}/$udi") {
-        writelog($log,"[$udi] creating download directory $config->{paths}->{download}/$udi");
+        $log->write($log,"[$udi] creating download directory $config->{paths}->{download}/$udi");
         # determine download directory permissions
         my $dl_dir_perms = '0750';
         $dl_dir_perms = '0751' if $r->{'access_status'} eq 'None';
