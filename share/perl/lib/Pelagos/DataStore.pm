e58f6aef54797541a23c6495833718690f2b5e00
James C. Davis <James.Davis@tamucc.edu>
2014-09-22 13:29:23 -0500

    Changed check_data_store_directory and check_download_directory sot hat second parameter is just the directory path (not the whole config hash).

--- share/perl/lib/Pelagos/DataStore.pm
+++ share/perl/lib/Pelagos/DataStore.pm
@@ -13,35 +13,35 @@ require Exporter;
 @EXPORT = qw(check_data_store_directory check_download_directory);
 
 sub check_data_store_directory {
-    my ($log,$config,$udi) = @_;
+    my ($log,$data_store_path,$udi) = @_;
     my (undef,undef,$custodian_uid,$custodian_gid) = getpwnam('custodian');
     # check if data store directory exists for this udi
-    if (! -d "$config->{paths}->{data_store}/$udi") {
-        $log->write("[$udi] creating data store directory $config->{paths}->{data_store}/$udi");
+    if (! -d "$data_store_path/$udi") {
+        $log->write("[$udi] creating data store directory $data_store_path/$udi");
         # make directory
-        mkdir("$config->{paths}->{data_store}/$udi",'0750') or die "Error: could not create directory: $config->{paths}->{data_store}/$udi ($!)\n";
+        mkdir("$data_store_path/$udi",'0750') or die "Error: could not create directory: $data_store_path/$udi ($!)\n";
         # make custodian the owner
-        chown($custodian_uid,$custodian_gid,"$config->{paths}->{data_store}/$udi");
+        chown($custodian_uid,$custodian_gid,"$data_store_path/$udi");
         # give apache access
-        system("setfacl -m u:apache:--x $config->{paths}->{data_store}/$udi");
+        system("setfacl -m u:apache:--x $data_store_path/$udi");
     }
 }
 
 sub check_download_directory {
-    my ($log,$config,$udi,$access_status) = @_;
+    my ($log,$download_directory_path,$udi,$access_status) = @_;
     my (undef,undef,$apache_uid,$apache_gid) = getpwnam('apache');
     # check if download directory exists for this udi
-    if (! -d "$config->{paths}->{download}/$udi") {
-        $log->write($log,"[$udi] creating download directory $config->{paths}->{download}/$udi");
+    if (! -d "$download_directory_path/$udi") {
+        $log->write($log,"[$udi] creating download directory $download_directory_path/$udi");
         # determine download directory permissions
         my $dl_dir_perms = '0750';
         $dl_dir_perms = '0751' if $access_status eq 'None';
         # make directory
-        mkdir("$config->{paths}->{download}/$udi",$dl_dir_perms) or die "Error: could not create directory: $config->{paths}->{download}/$udi ($!)\n";
+        mkdir("$download_directory_path/$udi",$dl_dir_perms) or die "Error: could not create directory: $download_directory_path/$udi ($!)\n";
         # make apache the owner
-        chown($apache_uid,$apache_gid,"$config->{paths}->{download}/$udi");
+        chown($apache_uid,$apache_gid,"$download_directory_path/$udi");
         # give custodian access
-        system("setfacl -m u:custodian:rwx $config->{paths}->{download}/$udi");
+        system("setfacl -m u:custodian:rwx $download_directory_path/$udi");
     }
 }
 
