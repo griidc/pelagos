ef4c572fd7bd039f172158c2ff03968d922a285e
James C. Davis <James.Davis@tamucc.edu>
2014-09-29 16:56:07 -0500

    Use octal numbers instead of strings for directory permissions.

--- share/perl/lib/Pelagos/DataStore.pm
+++ share/perl/lib/Pelagos/DataStore.pm
@@ -19,7 +19,7 @@ sub check_data_store_directory {
     if (! -d "$data_store_path/$udi") {
         $log->write("[$udi] creating data store directory $data_store_path/$udi");
         # make directory
-        mkdir("$data_store_path/$udi",'0750') or die "Error: could not create directory: $data_store_path/$udi ($!)\n";
+        mkdir("$data_store_path/$udi",0750) or die "Error: could not create directory: $data_store_path/$udi ($!)\n";
         # make custodian the owner
         chown($custodian_uid,$custodian_gid,"$data_store_path/$udi");
         # give apache access
@@ -34,8 +34,8 @@ sub check_download_directory {
     if (! -d "$download_directory_path/$udi") {
         $log->write($log,"[$udi] creating download directory $download_directory_path/$udi");
         # determine download directory permissions
-        my $dl_dir_perms = '0750';
-        $dl_dir_perms = '0751' if $access_status eq 'None';
+        my $dl_dir_perms = 0750;
+        $dl_dir_perms = 0751 if $access_status eq 'None';
         # make directory
         mkdir("$download_directory_path/$udi",$dl_dir_perms) or die "Error: could not create directory: $download_directory_path/$udi ($!)\n";
         # make apache the owner
