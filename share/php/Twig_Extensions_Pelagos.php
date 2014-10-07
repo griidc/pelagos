f6d72078eb23d1b136c63240353008f310743be1
James C. Davis <James.Davis@tamucc.edu>
2014-10-07 13:52:05 -0500

    Add evaluate filter

--- share/php/Twig_Extensions_Pelagos.php
+++ share/php/Twig_Extensions_Pelagos.php
@@ -17,7 +17,14 @@ class Twig_Extensions_Pelagos extends \Twig_Extension {
             'removeYR1BG' => new \Twig_Filter_Method($this,'removeYR1BG'),
             'statusToImg' => new \Twig_Filter_Method($this,'statusToImg'),
             'statusToTitle' => new \Twig_Filter_Method($this,'statusToTitle'),
-            'trimws' => new \Twig_Filter_Method($this,'trimws')
+            'trimws' => new \Twig_Filter_Method($this,'trimws'),
+            'evaluate' => new \Twig_Filter_Method($this, 'evaluate', array(
+                'needs_environment' => true,
+                'needs_context' => true,
+                'is_safe' => array(
+                    'evaluate' => true
+                )
+            ))
         );
     }
 
@@ -29,12 +36,24 @@ class Twig_Extensions_Pelagos extends \Twig_Extension {
         return $GLOBALS['config']['status_icons'][$status];
     }
 
+    public function statusToTitle($status_type) {
+        return $GLOBALS['config']['status_titles'][$status_type];
+    }
+
     public function trimws($string) {
         return preg_replace('/^\s+|\s+$/','',$string);
     }
 
-    public function statusToTitle($status_type) {
-        return $GLOBALS['config']['status_titles'][$status_type];
+    public function evaluate( \Twig_Environment $environment, $context, $string ) {
+        $loader = $environment->getLoader( );
+        $parsed = $this->parseString( $environment, $context, $string );
+        $environment->setLoader( $loader );
+        return $parsed;
+    }
+
+    protected function parseString( \Twig_Environment $environment, $context, $string ) {
+        $environment->setLoader( new \Twig_Loader_String( ) );
+        return $environment->render( $string, $context );
     }
 }
 
