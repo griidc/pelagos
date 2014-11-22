3a01dfbb8069f0e59893559a792dc69f861b2035
James C. Davis <James.Davis@tamucc.edu>
2014-11-21 18:36:32 -0600

    Ensure necessary libraries are loaded in DataManagers.php

--- share/php/DataManagers.php
+++ share/php/DataManagers.php
@@ -2,6 +2,12 @@
 
 function getDMsFromUser($uid)
 {
+    if (!function_exists('getRCsFromUser')) {
+        require 'ResearchConsortia.php';
+    }
+    if (!function_exists('getDMsFromRC')) {
+        require 'rpis.php';
+    }
     $dms = array();
     foreach (getRCsFromUser($uid) as $rc) {
         $dms = array_merge($dms, getDMsFromRC($rc));
@@ -11,5 +17,11 @@ function getDMsFromUser($uid)
 
 function getDMsFromUDI($udi)
 {
+    if (!function_exists('getRCFromUDI')) {
+        require 'ResearchConsortia.php';
+    }
+    if (!function_exists('getDMsFromRC')) {
+        require 'rpis.php';
+    }
     return getDMsFromRC(getRCFromUDI($udi));
 }
