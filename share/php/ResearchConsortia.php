b1b8e65e06fa4a58e412669160207b0e0864f604
James C. Davis <James.Davis@tamucc.edu>
2014-11-24 14:31:53 -0600

    Open RIS database connection and send DBH to getRCsFromRISUser

--- share/php/ResearchConsortia.php
+++ share/php/ResearchConsortia.php
@@ -30,9 +30,14 @@ if (!function_exists('getRCsFromUser')) {
     function getRCsFromUser($userId)
     {
         require_once 'RIS.php';
+        require_once 'db-utils.lib.php';
         #consult LDAP for $userId -> $RIS_user_ID
         $risUserId = getEmployeeNumberFromUID($userId);
-        $project_ids = getRCsFromRISUser($risUserId);
+        # open a database connetion to RIS
+        $RIS_DBH = OpenDB('RIS_RO');
+        $project_ids = getRCsFromRISUser($RIS_DBH, $risUserId);
+        # close database connection
+        $RIS_DBH = null;
         return $project_ids;
     }
 }
