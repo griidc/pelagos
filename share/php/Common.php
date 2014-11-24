bad72d12eebb70e8d6e7e40eb851a0d9957da293
Michael Scott Williamson <michael.williamson@tamucc.edu>
2014-11-24 13:59:36 -0600

    Bugfix on Common.php

--- share/php/Common.php
+++ share/php/Common.php
@@ -3,13 +3,13 @@ if(!function_exists('configMerge')) {
     function configMerge($originalConfigArray,$localConfigArray)
     {
         $newConfig = array();
-        foreach(array_keys($originalConfigArray) as $key) {
+        foreach(array_keys($localConfigArray) as $key) {
             if(isset($localConfigArray[$key])) {
                 $newConfig[$key] = array_merge($originalConfigArray[$key], $localConfigArray[$key]);
             } else {
                 $newConfig[$key]=$originalConfigArray[$key];
             }
         }
-        return $newConfig;
+        return array_merge($originalConfigArray,$newConfig);
     }
 }
