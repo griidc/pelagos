590f21394cbf29f9c265c79458f6d94a289b4d9b
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2014-11-25 17:33:42 -0600

    Updated INI with subject and added code to populate templates.
    PELAGOS-92

--- share/php/EventHandler.php
+++ share/php/EventHandler.php
@@ -1,5 +1,18 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+$user = array('FirstName'=>'Fred','LastName'=>'Flintstone','email'=>'fred@rockstone.net','userId'=>'fflintstone');
+
+$data = array('userId'=>'jweick','udi'=>'R1.x138.078:0019','user'=>$user);
+
+echo '<pre>';
+
+eventHappened('dif_saved_and_submitted',$data);
+
+echo '</pre>';
+
 function getMessageTemplate($Action)
 {
     $eventHandlerConfig  = parse_ini_file('./EventHandler.ini', true);
@@ -27,24 +40,49 @@ function expandTemplate($Template, $Data)
     $loader = new Twig_Loader_String();
     $twig = new Twig_Environment($loader);
 
-    return $twig->render($Template, $Data);
+    try {
+        return $twig->render($Template, $Data);
+    } catch (Exception $e) {
+        throw $e;
+    }
+}
+
+function getRCsByUserId($userId)
+{
+    require_once 'ResearchConsortia.php';
+    require_once 'RIS.php';
+    require_once 'DBUtils.php';
+    #get Person ID by userId
+    $personId = getEmployeeNumberFromUID($userId);
+    # open a database connetion to RIS
+    $RIS_DBH = openDB('RIS_RO');
+    #get RC's by Person ID
+    $rcByPersonID = getProjectDetails($RIS_DBH,array("peopleid=$personId"));
+    # close database connection
+    $RIS_DBH = null;
+    
+    return $rcByPersonID;
+}
+
+function getRCsByUDI($udi)
+{
+    require_once 'ResearchConsortia.php';
+    require_once 'RIS.php';
+    require_once 'DBUtils.php';
+    #get Project ID by UDI
+    $projectid = getRCFromUDI($udi);
+    # open a database connetion to RIS
+    $RIS_DBH = openDB('RIS_RO');
+    #get RC's by Person ID
+    $rcByUDI = getProjectDetails($RIS_DBH,array("projectid=$projectid"));
+    # close database connection
+    $RIS_DBH = null;
+    
+    return $rcByUDI;
 }
 
 function eventHappened($Action, $Data)
 {
-    #Placeholder
-
-    /*
-        $template = get_message_text_template_from_id($action_id)
-        $DMs = array()
-        $DMs += getDMsFromUser($data['user'])
-        $DMs += getDMsFromUDI($data['udi'])
-        loop($DMs) {
-        $to = $DM['email']
-        $message = expand_template($template,$data+$DM);
-        mail($to,$message)
-    */
-
     $messageData = getMessageTemplate($Action);
 
     $messageTemplate = $messageData['messageTemplate'];
@@ -71,10 +109,26 @@ function eventHappened($Action, $Data)
 
     foreach ($dataManagers as $dataManager) {
         $mailData = array();
+        
+        #need: rcbyuserid
+        #need: rcbyudi
+        
+        $rcByUserId = getRCsByUserId($Data['userId']);
+        $rcByUDI = getRCsByUDI($Data['udi']);
+        
+        //var_dump($rcByUserId);
+        //var_dump($rcByUDI);
+        
+        $mailData["data"] = $Data;
+        $mailData["dm"] = $dataManager;
+        $mailData["rcbyuserid"] = $rcByUserId;
+        $mailData["rcbyudi"] = $rcByUDI;
+        
+        //var_dump($dataManager);
 
-        $mailMessage  = expandTemplate($messageTemplate, array('firstname' => $dataManager['FirstName']));
+        $mailMessage  = expandTemplate($messageTemplate, $mailData);
 
-        require_once 'griidcMailer.php';
+        require_once 'stubs/griidcMailerStub.php';
         $eventMailer = new griidcMailer(false);
         $eventMailer->addToUser($dataManager['FirstName'], $dataManager['LastName'], $dataManager['Email']);
         $eventMailer->mailMessage = $mailMessage;
