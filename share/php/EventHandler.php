0ce84e23f4f927be07df315317a4076b8cd14d35
James C. Davis <James.Davis@tamucc.edu>
2014-11-25 20:59:00 -0600

    Removed debugging code and extraneous whitespace

--- share/php/EventHandler.php
+++ share/php/EventHandler.php
@@ -1,18 +1,5 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-
-$user = array('FirstName'=>'Fred','LastName'=>'Flintstone','email'=>'fred@rockstone.net','userId'=>'fflintstone');
-
-$data = array('userId'=>'jweick','udi'=>'R1.x138.078:0019','user'=>$user);
-
-echo '<pre>';
-
-eventHappened('dif_saved_and_submitted',$data);
-
-echo '</pre>';
-
 function getMessageTemplate($Action)
 {
     $eventHandlerConfig  = parse_ini_file('./EventHandler.ini', true);
@@ -52,15 +39,15 @@ function getRCsByUserId($userId)
     require_once 'ResearchConsortia.php';
     require_once 'RIS.php';
     require_once 'DBUtils.php';
-    #get Person ID by userId
+    # get Person ID by userId
     $personId = getEmployeeNumberFromUID($userId);
     # open a database connetion to RIS
     $RIS_DBH = openDB('RIS_RO');
-    #get RC's by Person ID
-    $rcByPersonID = getProjectDetails($RIS_DBH,array("peopleid=$personId"));
+    # get RC's by Person ID
+    $rcByPersonID = getProjectDetails($RIS_DBH, array("peopleid=$personId"));
     # close database connection
     $RIS_DBH = null;
-    
+
     return $rcByPersonID;
 }
 
@@ -69,15 +56,15 @@ function getRCsByUDI($udi)
     require_once 'ResearchConsortia.php';
     require_once 'RIS.php';
     require_once 'DBUtils.php';
-    #get Project ID by UDI
+    # get Project ID by UDI
     $projectid = getRCFromUDI($udi);
     # open a database connetion to RIS
     $RIS_DBH = openDB('RIS_RO');
-    #get RC's by Person ID
-    $rcByUDI = getProjectDetails($RIS_DBH,array("projectid=$projectid"));
+    # get RC's by Person ID
+    $rcByUDI = getProjectDetails($RIS_DBH, array("projectid=$projectid"));
     # close database connection
     $RIS_DBH = null;
-    
+
     return $rcByUDI;
 }
 
@@ -109,26 +96,18 @@ function eventHappened($Action, $Data)
 
     foreach ($dataManagers as $dataManager) {
         $mailData = array();
-        
-        #need: rcbyuserid
-        #need: rcbyudi
-        
+
         $rcByUserId = getRCsByUserId($Data['userId']);
         $rcByUDI = getRCsByUDI($Data['udi']);
-        
-        //var_dump($rcByUserId);
-        //var_dump($rcByUDI);
-        
+
         $mailData["data"] = $Data;
         $mailData["dm"] = $dataManager;
         $mailData["rcbyuserid"] = $rcByUserId;
         $mailData["rcbyudi"] = $rcByUDI;
-        
-        //var_dump($dataManager);
 
         $mailMessage  = expandTemplate($messageTemplate, $mailData);
 
-        require_once 'stubs/griidcMailerStub.php';
+        require_once 'griidcMailer.php';
         $eventMailer = new griidcMailer(false);
         $eventMailer->addToUser($dataManager['FirstName'], $dataManager['LastName'], $dataManager['Email']);
         $eventMailer->mailMessage = $mailMessage;
