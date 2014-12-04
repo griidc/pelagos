46ef42be33cc8dcae0f6b584c45c78ce410c89df
James C. Davis <James.Davis@tamucc.edu>
2014-12-04 14:20:14 -0600

    Replaced hard-coded triton.tamucc.edu with value from ldap.ini

--- web/services/RIS/getLDAPUserName.php
+++ web/services/RIS/getLDAPUserName.php
@@ -2,8 +2,10 @@
 function getFullNameFromDrupalUsername()
 {
 	global $user;
+    $config = parse_ini_file('/etc/opt/pelagos.ini', true);
+    $config = array_merge($config, parse_ini_file($config['paths']['conf'].'/ldap.ini', true));
 	$userId = $user->name;
-	$ldap = ldap_connect("ldap://triton.tamucc.edu");
+    $ldap = ldap_connect('ldap://'.$config['ldap']['server']);
 	$result = ldap_search($ldap, "ou=people,dc=griidc,dc=org", "(uid=$userId)", array('givenName','sn'));
 	$entries = ldap_get_entries($ldap, $result);
 	for ($i=0; $i<$entries['count']; $i++) 
