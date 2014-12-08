33ae830d0018b5f98a04652e6ab5ea80c8e67286
Michael Scott Williamson <michael.williamson@tamucc.edu>
2014-12-08 11:16:01 -0600

    Updated 2nd message with new verbage ( PELAGOS-33 )

--- web/applications/dif/index.php
+++ web/applications/dif/index.php
@@ -8,8 +8,8 @@ drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/spin.js/2.0.1/spin.min.js',array
 drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/datejs/1.0/date.min.js',array('type'=>'external'));
 drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/openlayers/2.13.1/OpenLayers.js',array('type'=>'external'));
 drupal_add_js('//maps.google.com/maps/api/js?v=3&sensor=false',array('type'=>'external'));
-drupal_add_js('/includes/geoviz/geoviz.js',array('type'=>'external'));
-drupal_add_js('/includes/geoviz/mapWizard.js',array('type'=>'external'));
+drupal_add_js('/mwilliamson/includes/geoviz/geoviz.js',array('type'=>'external'));
+drupal_add_js('/mwilliamson/includes/geoviz/mapWizard.js',array('type'=>'external'));
 drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/jstree/3.0.1/jstree.min.js',array('type'=>'external'));
 drupal_add_css('//cdnjs.cloudflare.com/ajax/libs/jstree/3.0.1/themes/default/style.min.css',array('type'=>'external'));
 drupal_add_css('includes/css/dif.css',array('type'=>'external'));
