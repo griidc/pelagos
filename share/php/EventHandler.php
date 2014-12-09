67f5afa732facc05b11019efec963dd8743a8cf8
James C. Davis <James.Davis@tamucc.edu>
2014-12-09 12:01:33 -0600

    Don't use hard path to Twig/Autoloader.php

--- share/php/EventHandler.php
+++ share/php/EventHandler.php
@@ -25,7 +25,7 @@ function getMessageTemplate($Action)
 
 function expandTemplate($Template, $Data)
 {
-    require_once '/usr/share/pear/Twig/Autoloader.php';
+    require_once 'Twig/Autoloader.php';
     Twig_Autoloader::register();
     $loader = new Twig_Loader_String();
     $twig = new Twig_Environment($loader);
