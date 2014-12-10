311b2865829183f0c1e386ccaa15def4ad9c4b65
James C. Davis <James.Davis@tamucc.edu>
2014-12-10 12:42:23 -0600

    Moved functions to lib/search.php

--- web/applications/data-discovery/lib/search.php
+++ web/applications/data-discovery/lib/search.php
@@ -89,4 +89,103 @@ function get_accepted_metadata($category) {
     return $data->fetchAll();
 }
 
+function index($app) {
+    $env = $app->environment();
+    drupal_add_js('/tree/js/tree.js',array('type'=>'external'));
+    drupal_add_js("$env[SCRIPT_NAME]/js/search.js",array('type'=>'external'));
+//    drupal_add_js("$env[SCRIPT_NAME]/js/package.js",array('type'=>'external'));
+    drupal_add_library('system', 'jquery.cookie');
+    drupal_add_css("$env[SCRIPT_NAME]/css/search.css",array('type'=>'external'));
+    drupal_add_css("$env[SCRIPT_NAME]/includes/css/scrollbars.css",array('type'=>'external'));
+    drupal_add_css("$env[SCRIPT_NAME]/includes/css/datasets.css",array('type'=>'external'));
+    drupal_add_css("$env[SCRIPT_NAME]/includes/css/downloads.css",array('type'=>'external'));
+    drupal_add_css("$env[SCRIPT_NAME]/includes/css/dataset_details.css",array('type'=>'external'));
+    drupal_add_css("$env[SCRIPT_NAME]/includes/css/dataset_download.css",array('type'=>'external'));
+    drupal_add_css("$env[SCRIPT_NAME]/includes/css/logins.css",array('type'=>'external'));
+    if (array_key_exists('treePaneCollapsed',$GLOBALS['config']['DataDiscovery'])) {
+        $stash['treePaneCollapsed'] = $GLOBALS['config']['DataDiscovery']['treePaneCollapsed'];
+    }
+    else {
+        $stash['treePaneCollapsed'] = 0;
+    }
+    $stash['defaultFilter'] = $app->request()->get('filter');
+    return $stash;
+}
+
+function getHashes($udi) {
+    $sql = "select fs_md5_hash, fs_sha1_hash, fs_sha256_hash from registry_view where
+            dataset_udi = ?";
+    $dbms = OpenDB("GOMRI_RO");
+    $data = $dbms->prepare($sql);
+    $data->execute(array($udi));
+    $raw_data = $data->fetch();
+    $md5    = '';
+    $sha1   = '';
+    $sha256 = '';
+    if ($raw_data) {
+        $md5    = $raw_data[0];
+        $sha1   = $raw_data[1];
+        $sha256 = $raw_data[2];
+    }
+    return "$md5|$sha1|$sha256";
+}
+
+function getApprovedMetadataUDIs() {
+    $hash = array();
+    $sql = "select dataset_udi from registry_view where
+            metadata_status = 'Accepted'";
+    $dbms = OpenDB("GOMRI_RO");
+    $data = $dbms->prepare($sql);
+    $data->execute();
+    while ($raw_data = $data->fetch()) {
+        $hash[$raw_data[0]]=1;
+    }
+    $dbms = null;
+    return $hash;
+}
+
+function isMetadataApproved($udi) {
+    $sql = "select count(*) from registry_view where
+            metadata_status = 'Accepted' and dataset_udi = ?;";
+    $dbms = OpenDB("GOMRI_RO");
+    $data = $dbms->prepare($sql);
+    $data->execute(array($udi));
+    while ($row = $data->fetch()) {
+        $count = $row[0];
+    }
+    return ($count>0);
+}
+
+function canHazGridFTP($user,$udi,$filename) {
+    if(!isset($user->name)) {
+        return 0;
+    }
+    $conf_path = $GLOBALS['config']['paths']['conf'];
+    $GLOBALS['ldap'] = parse_ini_file("$conf_path/ldap.ini",true);
+    $ldaphost = $GLOBALS['ldap']['ldap']['server'];
+    $baseDN = $GLOBALS['ldap']['ldap']['base_dn'];
+
+    $ldap = connectLDAP($ldaphost);
+    $dns = getDNs($ldap,$baseDN,"uid=$user->name");
+    $dn = $dns[0]['dn'];
+
+    $posix = userHasObjectClass($dn,"posixAccount");
+    $homedir = getHomeDir($user->name);
+
+    clearstatcache(); # needed because stat operations like file_exists() cache file state tests
+
+    $all_ok = 0;
+    if (
+            ($posix)
+            and (isset($homedir))
+            and ($homedir != null)
+            and is_writable($homedir."/download/" )
+            and (is_writable($homedir."/download/$udi" ) or (!file_exists($homedir."/download/$udi")))
+       ) {
+        $all_ok = 1;
+    }
+    return $all_ok;
+
+}
+
 ?>
