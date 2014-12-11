3ea13075c4a51d54e5a9bb9ef4184a1f7fea5f96
Michael Scott Williamson <michael.williamson@tamucc.edu>
2014-12-11 11:39:23 -0600

    removed Web-IP-Switcher from share/bash

--- share/bash/Web-IP-Switcher/private/Disable-212.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Shut down 172.22.10.212 interface
-/sbin/ifdown 'em1:0'
-
-# Prevent it from coming back online at boot
-/bin/echo '/etc/sysconfig/network-scripts/ifcfg-em1:0' | /usr/bin/xargs /bin/sed -i 's/ONBOOT=yes/ONBOOT=no/g'
