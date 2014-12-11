98575c19de299789010004030e1892a2bac5545c
Michael Scott Williamson <michael.williamson@tamucc.edu>
2014-12-11 11:32:30 -0600

    Added check for root user only on Web-IP-Switcher use, uncommented needed features

--- share/bash/Web-IP-Switcher/RemoveOfflineMessage.sh
+++ share/bash/Web-IP-Switcher/RemoveOfflineMessage.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 THISHOST=$(hostname)
+USERNAME=$(id -un)
+
+if [ "$USERNAME" != 'root' ]; then
+    echo "This script must be run from root."
+    exit 1
+fi
 
 if [ "$THISHOST" == "proteus.tamucc.edu" ]; then
     # De-activate 172.22.10.212 on proteus
     echo "De-activating Proteus's 172.22.10.212 interface"
-    #/sbin/ifdown 'ifcfg-em1:0'
+    /sbin/ifdown 'ifcfg-em1:0'
 
     # SSH into poseidon and activate 172.22.10.212
     echo "Enabling poseidon's 172.22.10.212 interface"
-    #/usr/bin/ssh -C /usr/local/bin/Web-IP-Switcher/private/Enable-212.sh ipmanager@poseidon.tamucc.edu
+    /usr/bin/ssh -C /usr/local/bin/Web-IP-Switcher/private/Enable-212.sh ipmanager@poseidon.tamucc.edu
 else 
     echo "This utility was meant to be run from proteus.tamucc.edu only."
 fi
--- share/bash/Web-IP-Switcher/ShowOfflineMessage.sh
+++ share/bash/Web-IP-Switcher/ShowOfflineMessage.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 THISHOST=$(hostname)
+USERNAME=$(id -un)
+
+if [ "$USERNAME" != 'root' ]; then
+    echo "This script must be run from root."
+    exit 1
+fi
 
 if [ "$THISHOST" == "proteus.tamucc.edu" ]; then
     # SSH into poseidon and shut down 212 IP address
     echo "Shutting down poseidon's 172.22.10.212 interface"
-    #/usr/bin/ssh -C /usr/local/bin/Web-IP-Switcher/private/Disable-212.sh ipmanager@poseidon.tamucc.edu
+    /usr/bin/ssh -C /usr/local/bin/Web-IP-Switcher/private/Disable-212.sh ipmanager@poseidon.tamucc.edu
 
     # Activate 172.22.10.212 on proteus
     echo "Activating Proteus's 172.22.10.212 interface"
-    #/sbin/ifup 'ifcfg-em1:0'
+    /sbin/ifup 'ifcfg-em1:0'
 
     # Refresh the ARP tables in connected networking hardware
     echo "Informing Network of IP host change"
-    #/usr/sbin/arping -c 1 -s 172.22.10.212 -I em1 172.22.255.255
+    /usr/sbin/arping -c 1 -s 172.22.10.212 -I em1 172.22.255.255
 else 
     echo "This utility was meant to be run from proteus.tamucc.edu only."
+    exit 1
 fi
