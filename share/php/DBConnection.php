22eb17e96f4371e5a09db1a323532872449423e6
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2015-01-26 13:11:59 -0600

    Removed unneeded function

--- share/php/DBConnection.php
+++ share/php/DBConnection.php
@@ -73,38 +73,6 @@ class DBConnection
         return $statementHandler->fetchAll();
     }
     
-    /**
-     * @param string $req : the query on which link the values
-     * @param array $array : associative array containing the values ??to bind
-     * @param array $typeArray : associative array with the desired value for its corresponding key in $array
-     * */
-    function bindArrayValue($req, $array, $typeArray = false)
-    {
-        if (is_object($req) && ($req instanceof PDOStatement)) {
-            foreach ($array as $key => $value) {
-                if ($typeArray) {
-                    $req->bindValue(":$key", $value, $typeArray[$key]);
-                } else {
-                    if (is_int($value)) {
-                        $param = PDO::PARAM_INT;
-                    } elseif (is_bool($value)) {
-                        $param = PDO::PARAM_BOOL;
-                    } elseif (is_null($value)) {
-                        $param = PDO::PARAM_NULL;
-                    } elseif (is_string($value)) {
-                        $param = PDO::PARAM_STR;
-                    } else {
-                        $param = false;
-                    }
-                    
-                    if ($param) {
-                        $req->bindValue(":$key", $value, $param);
-                    }
-                }
-            }
-        }
-    }
-    
     public function __destruct()
     {
         $this->pdoconnection = null;
