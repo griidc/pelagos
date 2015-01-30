32f190e044a89aae59e1a434383a01b0a48a487f
Michael Scott Williamson <michael.williamson@tamucc.edu>
2015-01-30 13:24:55 -0600

    minor psr2 cleanup

--- web/applications/dif/index.php
+++ web/applications/dif/index.php
@@ -4,17 +4,17 @@ $GLOBALS['pelagos']['title'] = 'Dataset Information Form (DIF)';
 
 include_once '../../../share/php/aliasIncludes.php';
 
-drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.11.1/jquery.validate.min.js',array('type'=>'external'));
-drupal_add_js('includes/js/dif.js',array('type'=>'external'));
-drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/spin.js/2.0.1/spin.min.js',array('type'=>'external'));
-drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/datejs/1.0/date.min.js',array('type'=>'external'));
-drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/openlayers/2.13.1/OpenLayers.js',array('type'=>'external'));
-drupal_add_js('//maps.google.com/maps/api/js?v=3&sensor=false',array('type'=>'external'));
-drupal_add_js('/includes/geoviz/geoviz.js',array('type'=>'external'));
-drupal_add_js('/includes/geoviz/mapWizard.js',array('type'=>'external'));
-drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/jstree/3.0.1/jstree.min.js',array('type'=>'external'));
-drupal_add_css('//cdnjs.cloudflare.com/ajax/libs/jstree/3.0.1/themes/default/style.min.css',array('type'=>'external'));
-drupal_add_css('includes/css/dif.css',array('type'=>'external'));
+drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.11.1/jquery.validate.min.js', array('type'=>'external'));
+drupal_add_js('includes/js/dif.js', array('type'=>'external'));
+drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/spin.js/2.0.1/spin.min.js', array('type'=>'external'));
+drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/datejs/1.0/date.min.js', array('type'=>'external'));
+drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/openlayers/2.13.1/OpenLayers.js', array('type'=>'external'));
+drupal_add_js('//maps.google.com/maps/api/js?v=3&sensor=false', array('type'=>'external'));
+drupal_add_js('/includes/geoviz/geoviz.js', array('type'=>'external'));
+drupal_add_js('/includes/geoviz/mapWizard.js', array('type'=>'external'));
+drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/jstree/3.0.1/jstree.min.js', array('type'=>'external'));
+drupal_add_css('//cdnjs.cloudflare.com/ajax/libs/jstree/3.0.1/themes/default/style.min.css', array('type'=>'external'));
+drupal_add_css('includes/css/dif.css', array('type'=>'external'));
 drupal_add_library('system', 'ui.datepicker');
 drupal_add_library('system', 'ui.widget');
 drupal_add_library('system', 'ui.dialog');
@@ -24,7 +24,7 @@ drupal_add_library('system', 'ui.autocomplete');
 
 include 'dif.php';
 if (getUID() == "") {
-    drupal_set_message('Please log in first!','warning',false);
+    drupal_set_message('Please log in first!', 'warning', false);
     echo '<h1>Please login in first to use this form!</h1>';
 } else {
     showDIFForm();
