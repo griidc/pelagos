e2b04aeda295b34f70b9782768b1d4a79c8ab8d5
James C. Davis <James.Davis@tamucc.edu>
2015-02-26 16:57:28 -0600

    Removed dead javascript

--- web/applications/stats/includes/js/stats.js
+++ web/applications/stats/includes/js/stats.js
@@ -1,35 +1,10 @@
-var countries;
-
-var firstResize = true;
-
 var $ = jQuery.noConflict();
-
-var winWidth = $(window).width();
-var winHeight = $(window).height();
-
-var drawn = { dataset_for: false, dataset_type: false, dataset_procedure: false };
 
 var flotConfig;
 
 var overviewSections;
 
 $(document).ready(function() {
-
-    winWidth = $(window).width();
-    winHeight = $(window).height();
-
-    $(window).resize(function() {
-        var winNewWidth = $(window).width();
-        var winNewHeight = $(window).height();
-        if (winNewWidth != winWidth || winNewHeight != winHeight) {
-            winWidth = winNewWidth;
-            winHeight = winNewHeight;
-            if (firstResize) {
-                firstResize = false;
-                return;
-            }
-        }
-    });
 
     overviewSections = {
         'summary-of-records': {
@@ -110,7 +85,3 @@ $(document).ready(function() {
     }
 
 });
-
-function labelFormatter(label, series) {
-    return "<div style='font-size:8pt; text-align:center; padding:2px; padding-left:5px; color:#555; background-color:transparent;'>" + label + "<br/>" + series.data[0][1] + " TB (" + Math.round(series.percent) + "%)</div>";
-}
