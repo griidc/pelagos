67d9abbfd4b3315d5d20f415b87c5dd455249337
James C. Davis <James.Davis@tamucc.edu>
2015-03-27 13:16:09 -0500

    Retrieve hash token the same way the account module does
    Use the same function the account management module does to retrieve
    person data (which includes the hash) so that a hash is returned that
    can be used for password reset.

--- web/applications/account/hash.php
+++ web/applications/account/hash.php
@@ -1,14 +1,22 @@
 <?php
 
+require_once 'lib/constants.php';
 require_once 'lib/account.php';
+require_once 'config.php';
+
+$GLOBALS['LDAP'] = ldap_connect('ldap://'.LDAP_HOST);
+
+# attempt to bind to LDAP
+if (!ldap_bind($GLOBALS['LDAP'], LDAP_BIND_DN, LDAP_BIND_PW)) {
+    print "Error binding to LDAP.\n";
+    exit;
+}
 
 if (count($argv) < 2) {
     print "Usage: php hash.php email_address\n";
     exit;
 }
 
-$RPIS = query_RPIS($argv[1]);
+$person = get_ldap_user("mail=$argv[1]");
 
-print $RPIS['hash'] . "\n";
-
-?>
+print $person['hash'] . "\n";
