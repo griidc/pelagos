d4696281ecd5f8b66ee1d21cbb3d86124ced7a90
James C. Davis <James.Davis@tamucc.edu>
2015-04-06 12:07:19 -0500

    Added a Pelagos Citation class.

---
+++ share/php/Pelagos/Citation.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Pelagos;
+
+class Citation
+{
+    private $id;
+    private $text;
+    private $style;
+    private $locale;
+    private $timestamp;
+    
+    public function __construct(
+        $id,
+        $text = null,
+        $style = null,
+        $locale = null,
+        $timestamp = null
+    ) {
+        $this->id = $id;
+        $this->text = $text;
+        $this->style = $style;
+        $this->locale = $locale;
+        $this->setTimeStamp($timestamp);
+    }
+
+    public function setTimeStamp($timestamp)
+    {
+        if ($timestamp === null) {
+            $this->timestamp = new \DateTime();
+        } else {
+            $this->timestamp = $timestamp;
+        }
+    }
+
+    public function asJSON()
+    {
+        return json_encode(
+            array(
+                'id' => $this->id,
+                'text' => $this->text,
+                'style' => $this->style,
+                'locale' => $this->locale,
+                'timestamp' => $this->timestamp,
+            ),
+            JSON_UNESCAPED_SLASHES
+        );
+    }
+}
