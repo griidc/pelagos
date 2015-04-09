a529fe4f9ad0b72b1abbff976c63b8d5ea15ac4f
Michael Scott Williamson <michael.williamson@tamucc.edu>
2015-04-09 11:03:25 -0500

    Improved error handling, changed method, changed link route
    Database errors are now caught and passed on, method for linking is
    now LINK, and the route to link no longer has "makelink" its URI.

--- web/services/publink/index.php
+++ web/services/publink/index.php
@@ -6,16 +6,48 @@ require_once "ldap.php";
 require_once "lib/Storage.php";
 require_once "lib/Publink.php";
 
-$comp->slim->get('/', function () {
+global $quit;
+$quit = false;
+
+$comp->slim->get('/', function () use ($comp) {
     $GLOBALS['pelagos']['title'] = 'Publink Service';
     print 'This service creates associations between datasets and publications.';
 });
 
-$comp->slim->map('/makelink/:udi/:doiShoulder/:doiBody(/)', function ($udi, $doiShoulder, $doiBody) use ($comp) {
+$comp->slim->map('/:udi/:doiShoulder/:doiBody(/)', function ($udi, $doiShoulder, $doiBody) use ($comp) {
+    global $user;
+    global $quit;
+    if (!isset($user->name)) {
+        $quit = true;
+        $HTTPStatus = new \Pelagos\HTTPStatus(401,'Login Required to use this feature.');
+        $comp->slim->response->headers->set('Content-Type', 'application/json');
+        $comp->slim->response->status($HTTPStatus->code);
+        $comp->slim->response->setBody($HTTPStatus->asJSON());
+        return;
+    }
     $doi = $doiShoulder.'/'.$doiBody;
     $Publink = new \Pelagos\Publink;
-    $Publink->createLink($udi,$doi,getEmployeeNumberFromUID($GLOBALS['_SESSION']['phpCAS']['user']));
+    try {
+        $Publink->createLink($udi,$doi,getEmployeeNumberFromUID($user->name));
+    } catch (\PDOException $ee) {
+        $quit = true;
+        $HTTPStatus = new \Pelagos\HTTPStatus(500,$ee);
+        $comp->slim->response->headers->set('Content-Type', 'application/json');
+        $comp->slim->response->status($HTTPStatus->code);
+        $comp->slim->response->setBody($HTTPStatus->asJSON());
+        return;
+    }
+    # if successful
+    $HTTPStatus = new \Pelagos\HTTPStatus(200,"A Link has been successfully created between dataset $udi and publication $doi.");
+    $comp->slim->response->headers->set('Content-Type', 'application/json');
+    $comp->slim->response->status($HTTPStatus->code);
+    $comp->slim->response->setBody($HTTPStatus->asJSON());
+
     $comp->quit();
 })->via('LINK');
 
 $comp->slim->run();
+
+if($quit) {
+    $comp->quit();
+}
