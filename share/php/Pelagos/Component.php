bd00f40f50c4b7f69ec6eb53ffca078f2a7418ff
James C. Davis <James.Davis@tamucc.edu>
2015-04-10 13:34:26 -0500

    Added Twig to \Pelagos\Component
    Also added methods:
    * addJS
    * addCSS
    * addLibrary

--- share/php/Pelagos/Component.php
+++ share/php/Pelagos/Component.php
@@ -6,11 +6,38 @@ class Component
 {
     public $slim;
 
-    public function __construct()
+    public function __construct($options = array())
     {
-        require 'Slim/Slim.php';
+        # load Twig
+        require_once 'Twig/Autoloader.php';
+        # load custom Twig extensions
+        # require_once 'Twig_Extensions_Pelagos.php';
+        require_once 'Slim/Slim.php';
         \Slim\Slim::registerAutoloader();
-        $this->slim = new \Slim\Slim();
+        if (!array_key_exists('Slim', $options) or !is_array($options['Slim'])) {
+            $options['Slim'] = array();
+        }
+        if (class_exists('Twig_Autoloader')) {
+            # load Twig Slim-View
+            require_once 'Slim-Views/Twig.php';
+            $options['Slim']['view'] = new \Slim\Views\Twig();
+        }
+        $this->slim = new \Slim\Slim($options['Slim']);
+    }
+
+    public function addJS($js_file)
+    {
+        drupal_add_js($GLOBALS['pelagos']['component_path'] . "/$js_file", array('type'=>'external'));
+    }
+
+    public function addCSS($css_file)
+    {
+        drupal_add_css($GLOBALS['pelagos']['component_path'] . "/$css_file", array('type'=>'external'));
+    }
+
+    public function addLibrary($library)
+    {
+        drupal_add_library('system', $library);
     }
 
     public function quit()
