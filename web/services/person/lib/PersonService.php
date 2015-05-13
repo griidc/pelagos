eca993360abce81fa21bb8f47e783de23c79c7ae
Michael Scott Williamson <michael.williamson@tamucc.edu>
2015-05-13 16:30:29 -0500

    Adding accidently untracked file

---
+++ web/services/person/lib/PersonService.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Pelagos\Component;
+class PersonService extends \Pelagos\Component
+{
+    public function giveDescription()
+    {
+        $GLOBALS['pelagos']['title'] = 'Person Webservice';
+        return $this->slim->render("html/index.html");
+    }
+
+    public function createPerson($entityManager, $firstName, $lastName, $email)
+    {
+        global $user;
+
+        $this->setQuitOnFinalize(true);
+
+        // First name, last name, and email have to exist, otherwise the route
+        // would not have been matched
+
+        // validate email format
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $this->setSlimResponseHTTPStatusJSON(
+                new \Pelagos\HTTPStatus(
+                    400,
+                    'Improperly formatted email address'
+                )
+            );
+            return;
+        }
+
+        // Check to see that user is logged in.
+        // THIS IS AN INSUFFICIENT SECURITY CHECK, THIS WILL
+        // HAVE TO BE TIED TO SOME SORT OF ACCESS LIST WHEN
+        // RELEASED.
+        if (!isset($user->name)) {
+            $this->setSlimResponseHTTPStatusJSON(
+                new \Pelagos\HTTPStatus(
+                    401,
+                    'Login Required to use this feature'
+                )
+            );
+            return;
+        }
+
+        $person = new \Pelagos\Entity\Person($firstName, $lastName, $email);
+
+        $entityManager->persist($person);
+
+        try {
+            $entityManager->flush();
+            $firstName = $person->getFirstName();
+            $lastName = $person->getLastName();
+            $email = $person->getEmailAddress();
+            $id = $person->getId();
+
+            $code = 200;
+            $msg = "A person has been successfully created $firstName $lastName ($email) with at ID of $id.";
+        } catch (\Exception $error) {
+            // Duplicate Error - 23505
+            if (preg_match('/SQLSTATE\[23505\]: Unique violation/', $error->getMessage())) {
+                $code = 409;
+                $msg = 'This record already exists in the database';
+            } else {
+                $code = 500;
+                $msg = "A general database error has occured." . $error->getMessage();
+            }
+        }
+        $this->setSlimResponseHTTPStatusJSON(new \Pelagos\HTTPStatus($code, $msg));
+        return;
+    }
+}
