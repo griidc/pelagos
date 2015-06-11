2169db29d25090a6fd6f12ae07a726cd06b2f4ba
James C. Davis <James.Davis@tamucc.edu>
2015-06-11 12:02:48 -0500

    Pull database error info from PDOException's errorInfo array
    This is preferable to parsing it out from the message.
    Also, pull out the driver-specific error code (and added a getter for it).

--- src/Pelagos/Exception/PersistenceException.php
+++ src/Pelagos/Exception/PersistenceException.php
@@ -13,8 +13,11 @@ class PersistenceException extends \Exception
     /** @var string The parameters sent to the database. */
     protected $databaseParams;
 
-    /** @var int The error code from the database. */
+    /** @var string The SQLSTATE error code from the database. */
     protected $databaseErrorCode;
+
+    /** @var int The error code from the database driver. */
+    protected $databaseErrorDriverCode;
 
     /** @var string The error message from the database. */
     protected $databaseErrorMessage;
@@ -24,24 +27,30 @@ class PersistenceException extends \Exception
 
     /**
      * Constructor for a PersistenceException.
-     * Pull out pieces from error message and populate properties.
+     * If previous exception's previous exception is a PDOException, pull out
+     * the database error codes, message, and hint from the PDOException.
+     * Attempt to extract query and parameters from error message.
      */
-    public function __construct($message = '', $code = 0, Exception $previous = null)
+    public function __construct($message = '', $code = 0, \Exception $previous = null)
     {
+        if (isset($previous) and is_a($previous->getPrevious(), '\PDOException')) {
+            $this->databaseErrorCode = $previous->getPrevious()->errorInfo[0];
+            $this->databaseErrorCodeDriver = $previous->getPrevious()->errorInfo[1];
+            $dbMessage = $previous->getPrevious()->errorInfo[2];
+            if (preg_match('/ERROR: +(.*?)(?:\n|$)/', $dbMessage, $matches)) {
+                $this->databaseErrorMessage = $matches[1];
+            } else {
+                $this->databaseErrorMessage = $dbMessage;
+            }
+            if (preg_match('/HINT: +(.*?)(?:\n|$)/', $dbMessage, $matches)) {
+                $this->databaseErrorHint = $matches[1];
+            }
+        }
         if (preg_match('/An exception occurred while executing \'([^\']+)\'/', $message, $matches)) {
             $this->databaseQuery = $matches[1];
         }
         if (preg_match('/with params \[([^\]]+)\]/', $message, $matches)) {
             $this->databaseParams = $matches[1];
-        }
-        if (preg_match('/SQLSTATE\[(\d+)\]/', $message, $matches)) {
-            $this->databaseErrorCode = $matches[1];
-        }
-        if (preg_match('/ERROR: +(.*?)(?:\'|\n|$)/', $message, $matches)) {
-            $this->databaseErrorMessage = $matches[1];
-        }
-        if (preg_match('/HINT: +(.*?)(?:\'|\n|$)/', $message, $matches)) {
-            $this->databaseErrorHint = $matches[1];
         }
         parent::__construct($message, $code, $previous);
     }
@@ -69,11 +78,21 @@ class PersistenceException extends \Exception
     /**
      * Getter for databaseErrorCode.
      *
-     * @return int The error code from the database
+     * @return string The SQLSTATE error code from the database.
      */
     public function getDatabaseErrorCode()
     {
         return $this->databaseErrorCode;
+    }
+
+    /**
+     * Getter for databaseErrorCodeDriver.
+     *
+     * @return string The driver-specific error code from the database.
+     */
+    public function getDatabaseErrorCodeDriver()
+    {
+        return $this->databaseErrorCodeDriver;
     }
 
     /**
