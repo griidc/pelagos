b7fa984c7a947a221ec6a8a7351ed8fdbb5a4f95
Michael Scott Williamson <michael.williamson@tamucc.edu>
2015-07-01 15:39:31 -0500

    Removing search on all columns except First Name, Last Name and Email.
    Adjusted datatable initialization to disable search on all columns
    except those requested be searchable.

--- web/applications/person-list/static/js/personList.js
+++ web/applications/person-list/static/js/personList.js
@@ -2,6 +2,8 @@ var $ = jQuery.noConflict();
 
 $(document).ready(function(){
     $('#personlist').DataTable({
-         "lengthMenu": [ [25, 50, 100, -1], [25, 50, 100, "Show All"] ]
+         "lengthMenu": [ [25, 50, 100, -1], [25, 50, 100, "Show All"] ],
+         "columnDefs": [ { "searchable": false, "targets": [ 0, 4, 5, 6, 7 ] }
+  ]
      });
 });
