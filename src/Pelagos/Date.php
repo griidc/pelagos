1a14425096f1efdef2c0d9d943c23b85976acc48
Joe Holland <joe.holland@tamucc.edu>
2015-09-15 09:59:45 -0500

    Added explicit outer scope to constructor arg DateTimeZone

--- src/Pelagos/Date.php
+++ src/Pelagos/Date.php
@@ -28,7 +28,7 @@ class Date extends DateTime  // this is \Pelagos\DateTime
      * @param null $time
      * @param DateTimeZone $timezone
      */
-    public function __construct($time = null, DateTimeZone $timezone = null)
+    public function __construct($time = null, \DateTimeZone $timezone = null)
     {
          parent::__construct($time, $timezone);
          $this->setFormat(Date::$DefaultFormat);
