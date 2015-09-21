620dbf5b1ee725cea90df9c03c622c9433c2fb1a
Michael Scott Williamson <michael.williamson@tamucc.edu>
2015-09-21 14:21:34 -0500

    Added ResearchGroup.js "template" for entity-list

---
+++ web/applications/entity-list/static/js/ResearchGroup.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    "use strict";
+
+    $(".entityTable").pelagosDataTable({
+        "ajax": pelagosBasePath + "/services/entity/ResearchGroup?properties=name,url,phoneNumber,deliveryPoint,city,administrativeArea,postalCode,creationTimeStamp,creator,modificationTimeStamp,modifier,description,emailAddress"
+        "columns": [
+            { "data": "name" },
+            { "data": "url" },
+            { "data": "phoneNumber" },
+            { "data": "deliveryPoint" },
+            { "data": "city" },
+            { "data": "administrativeArea" },
+            { "data": "postalCode" },
+            { "data": "country" },
+            { "data": "creationTimeStamp" },
+            { "data": "creator" },
+            { "data": "modificationTimeStamp" },
+            { "data": "modifier" }
+            { "data": "description" },
+            { "data": "emailAddress" },
+        ],
+        "headers": [
+            "Name",
+            "URL",
+            "Phone Number",
+            "Address",
+            "City",
+            "State",
+            "Zip",
+            "Country",
+            "Created On",
+            "Creator",
+            "Last Modified",
+            "Modifier By"
+            "Description",
+            "Email Address",
+        ]
+    });
+});
