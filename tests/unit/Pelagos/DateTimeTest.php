c1c4b7e1975476b3247b9f60d8b4a6a659210324
Joe Holland <joe.holland@tamucc.edu>
2015-09-23 15:06:13 -0500

    added space after comma in function call in both DateTest.php and DateTimeTest.php to fix psr2 error.

--- tests/unit/Pelagos/DateTimeTest.php
+++ tests/unit/Pelagos/DateTimeTest.php
@@ -38,7 +38,7 @@ class DateTimeTest extends \PHPUnit_Framework_TestCase
 
         $referenceDateTime->setFormat($referenceFormat);
 
-        $this->assertEquals($referenceFormatResult,(string)$referenceDateTime);
+        $this->assertEquals($referenceFormatResult, (string)$referenceDateTime);
     }
 
     /**
