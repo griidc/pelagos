e783096338e09edf01c9f9869e167557682271c8
James C. Davis <James.Davis@tamucc.edu>
2015-10-02 09:18:32 -0500

    Allow dependency injection of an EntityService to the EntityWebService

--- src/Pelagos/Component/EntityWebService.php
+++ src/Pelagos/Component/EntityWebService.php
@@ -45,14 +45,20 @@ class EntityWebService extends \Pelagos\Component
      *
      * @access public
      */
-    public function __construct(\Slim\Slim $slim)
+    public function __construct(\Slim\Slim $slim, EntityService $entityService = null)
     {
         // Call constructor for \Pelagos\Component
         parent::__construct();
         // Save the Slim instance
         $this->slim = $slim;
-        // Create an EntityService instance
-        $this->entityService = new EntityService($this->getEntityManager());
+        // If an Entity Service has been provided
+        if (isset($entityService)) {
+            // Save the EntityService instance
+            $this->entityService = $entityService;
+        } else {
+            // Create an EntityService instance
+            $this->entityService = new EntityService($this->getEntityManager());
+        }
     }
 
     /**
