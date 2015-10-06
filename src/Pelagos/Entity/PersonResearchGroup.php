7f5a149cf0edf69eedf9cfeb3c2c68bb075a1c1d
James C. Davis <James.Davis@tamucc.edu>
2015-10-06 11:28:27 -0500

    Corrected parameter type in DocBlock for setLabel

--- src/Pelagos/Entity/PersonResearchGroup.php
+++ src/Pelagos/Entity/PersonResearchGroup.php
@@ -165,7 +165,7 @@ class PersonResearchGroup extends Entity
     /**
      * Setter for Label.
      *
-     * @param mixed $label The Label for this association.
+     * @param string $label The Label for this association.
      *
      * @return void
      */
