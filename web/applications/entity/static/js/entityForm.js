6a7af3e4666eb3ed4ad704cc3e01c51e0db980fd
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2015-10-07 11:41:58 -0500

    Revert "Merge branch 'feature/PELAGOS-1628-extend-entity-application-to' into feature/PELAGOS-1616-create-personresearchgroup-entity"
    This reverts commit f98071071362c016739f06047cefc6db010e5b3c, reversing
    changes made to 71549a655a1d0e384d56e90be885f24b16385be7.
    
    Conflicts:
    	web/applications/entity/static/js/entityForm.js

--- web/applications/entity/static/js/entityForm.js
+++ web/applications/entity/static/js/entityForm.js
@@ -28,8 +28,6 @@
         return this.each(function() {
             //plug-in
 
-            var thisForm = this;
-
             //make sure this is of type form
             if (!$(this).is("form")) {
                 return false;
@@ -64,12 +62,7 @@
 
             var formValidator = $(this).validate({
                 submitHandler: function(form) {
-                    if (entityId === "") {
-                        updateEntity(form, "Create");
-                    } else {
-                        updateEntity(form, "Update");
-                    }
-
+                    updateEntity(form);
                 }
             });
 
@@ -82,34 +75,7 @@
 
             $(".entityFormButton").css("visibility", "hidden").button();
 
-            var innerform = "<div class=\"innerForm\">" +
-                            "<img class=\"editimg\" src=\"" +
-                            pelagosComponentPath +
-                            "/static/images/application_edit.png\">";
-            if ($(this).hasAttr("deletable")) {
-                 innerform += "<img class=\"deleteimg\" src=\"" +
-                 pelagosComponentPath +
-                 "/static/images/delete.png\">";
-            }
-            innerform += "</div>";
-
-            $(this).append(innerform);
-
-            $(".deleteimg", this).button().click(function (event) {
-                event.stopPropagation();
-
-                $.when(showConfirmation("Delete Entity", "Are you sure?")).done(function() {
-                    $.when(updateEntity($(thisForm), "Delete")).done(function() {
-                        $(".entityWrapper").has(thisForm)
-                        .animate({ height: "toggle", opacity: "toggle" }, "slow", function() {
-                            $(this).slideUp("fast", function() {
-                                $(this)
-                                .remove();
-                            });
-                        });
-                    });
-                });
-            });
+            $(".entityWrapper").has(this).append("<div class=\"innerForm\"><div>");
 
             $(this).on("keyup change", function () {
                 if ($(".entityWrapper").has(this).hasClass("active")) {
@@ -125,7 +91,9 @@
 
                     $("input:visible,textarea,select", this).each(function() {
                         $(this).attr("disabled", false);
-                        if (!$(this).hasAttr("dontvalidate")) {
+
+                        var dontvalidate = $(this).attr("dontvalidate");
+                        if (typeof dontvalidate === typeof undefined || dontvalidate === false) {
                             $(this).rules("add", {
                                 remote: {
                                     url: url
@@ -133,10 +101,8 @@
                             });
                         }
                     });
-                    $(".innerForm", this).hide();
-                    $(".entityFormButton,.showOnEdit", this)
-                    .css({opacity: 0.0, visibility: "visible"})
-                    .animate({opacity: 1.0});
+                    $(".innerForm", this).remove();
+                    $(".entityFormButton,.showOnEdit", this).css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0});
                     $("button", this).button("enable");
                 }
             });
@@ -150,12 +116,10 @@
                     .rules("remove");
                 });
                 $(".entityWrapper").has(this)
-                .removeClass("active")
-                .find(".innerForm", this).show();
+                .append("<div class=\"innerForm\"><div>")
+                .removeClass("active");
 
-                $(".entityFormButton,.showOnEdit", this)
-                .css({opacity: 1.0, visibility: "visible" })
-                .animate({opacity: 0.0});
+                $(".entityFormButton,.showOnEdit", this).css({opacity: 1.0, visibility: "visible" }).animate({opacity: 0.0});
                 $(this).prop("unsavedChanges", false);
 
                 $("button", this).button("disable");
@@ -165,11 +129,6 @@
                 $(".entityWrapper").has(this).click();
             }
         });
-    };
-
-    $.fn.hasAttr = function(attribute) {
-        var hasAttribute = $(this).attr(attribute);
-        return (typeof hasAttribute === typeof undefined || hasAttribute === false) ? false : true;
     };
 
     $.fn.fillForm = function(Data) {
@@ -253,107 +212,74 @@
         });
     }
 
-    function updateEntity(form, action)
+    function updateEntity(form)
     {
         var data = new FormData(form);
         var entityType = $(form).attr("entityType");
         var entityId = $(form).find("[name=\"id\"]").val();
         var url;
-
+        var title = "";
+        var message = "";
         var type;
         var returnCode;
-
-        switch (action)
-        {
-            case "Create":
-                url = pelagosBasePath + "/services/entity/" + entityType;
-                type = "POST";
-                returnCode = 201;
-                break;
-            case "Update":
-                url = pelagosBasePath + "/services/entity/" + entityType + "/" + entityId;
-                type = "PUT";
-                returnCode = 200;
-                break;
-            case "Delete":
-                url = pelagosBasePath + "/services/entity/" + entityType + "/" + entityId;
-                type = "DELETE";
-                returnCode = 200;
-                break;
-            default:
-                url = pelagosBasePath + "/services/entity/" + entityType + "/" + entityId;
-                type = "GET";
-                returnCode = 200;
-                break;
+        if (entityId === "") {
+            url = pelagosBasePath + "/services/entity/" + entityType;
+            type = "POST";
+            returnCode = 201;
+        } else {
+            url = pelagosBasePath + "/services/entity/" + entityType + "/" + entityId;
+            type = "PUT";
+            returnCode = 200;
         }
-
-        return $.Deferred(function() {
-            var title = "";
-            var message = "";
-            var mainPromise = this;
-            var myAjax = $.ajax({
-                type: type,
-                data: data,
-                url: url,
-                // Optionally enforce JSON return, in case a status 200 happens, but no JSON returns
-                //dataType: 'json',
-                cache: false,
-                contentType: false,
-                processData: false,
-                success: function(json) {
-                    if (json.code === returnCode) {
-                        title = "Success!";
-                        message = json.message;
-                        $(form).fillForm(json.data);
-                        //$(form).trigger("saved");
-                        $(form).trigger("reset");
-                    } else {
-                        title = "Error!";
-                        message = "Something went wrong!<br>Didn't receive the correct success message!";
-						myAjax.reject();
-                    }
-                },
-                error: function(response) {
-                    var json = response.responseJSON;
-                    if (typeof response.responseJSON === "undefined") {
-                        json = {};
-                        json.code = response.status;
-                        json.message = response.statusText;
-                    }
-                    title = "Error!";
-                    message = json.message;
-                }
-            })
-            .then(function() {
-                return $.Deferred(function() {
-                    var notyPromise = this;
-                    $("#notycontainer", form)
-                    .noty({
-                        layout: "top",
-                        text: message,
-                        theme: "relax",
-                        animation: {
-                            open: "animated bounceIn", // Animate.css class names
-                            close: "animated fadeOut", // Animate.css class names
-                            easing: "swing", // unavailable - no need
-                            speed: 500 // unavailable - no need
-                        },
-                        timeout: 3000,
-                        callback: {
-                            afterClose: function() {
-                                notyPromise.resolve();
-                            }
-                        }
-                    });
+        $.ajax({
+            type: type,
+            data: data,
+            url: url,
+            // Optionally enforce JSON return, in case a status 200 happens, but no JSON returns
+            //dataType: 'json'
+            cache: false,
+            contentType: false,
+            processData: false
+        })
+        .done(function(json) {
+            if (json.code === returnCode) {
+                title = "Success!";
+                message = json.message;
+                $(form).fillForm(json.data);
+                //$(form).trigger("saved");
+                $(form).trigger("reset");
+            } else {
+                title = "Error!";
+                message = "Something went wrong!<br>Didn't receive the correct success message!";
+            }
+        })
+        .fail(function(response) {
+            var json = response.responseJSON;
+            if (typeof response.responseJSON === "undefined") {
+                json = {};
+                json.code = response.status;
+                json.message = response.statusText;
+            }
+            title = "Error!";
+            message = json.message;
+        })
+        .always(function(json) {
+            if (json.code !== returnCode) {
+                showDialog(title, message);
+            } else {
+                $("#notycontainer", form).noty({
+                    layout: "top",
+                    text: message,
+                    theme: "relax",
+                    animation: {
+                        open: "animated bounceIn", // Animate.css class names
+                        close: "animated fadeOut", // Animate.css class names
+                        easing: "swing", // unavailable - no need
+                        speed: 500 // unavailable - no need
+                    },
+                    timeout: 3000
                 });
-            })
-			.fail(function() {
-				showDialog(title, message);
-				mainPromise.reject();
-			})
-            .done(function() {
-                return mainPromise.resolve();
-            });
+            }
         });
     }
 }(jQuery));
