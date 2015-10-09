fccddf283aeb35c214267a0cb874b5ff57de247a
James C. Davis <James.Davis@tamucc.edu>
2015-10-09 12:41:27 -0500

    Extended sortBy to allow sorting by properties of properties that are entity references

--- src/Pelagos/TwigEntityExtensions.php
+++ src/Pelagos/TwigEntityExtensions.php
@@ -50,22 +50,52 @@ class TwigEntityExtensions extends \Twig_Extension
             $entityList,
             function ($a, $b) use ($properties) {
                 while (count($properties) > 0) {
-                    $property = array_shift($properties);
-                    if (!$a->propertyExists($property) or !$b->propertyExists($property)) {
-                        // If $property doesn't exist on either side, we can't sort.
-                        return 0;
+                    // For each sort criteria, reset 'a' and 'b' entity back to original.
+                    $aEntity = $a;
+                    $bEntity = $b;
+                    // Pull a property descriptor of the front of the properties array.
+                    $propertyDescriptor = array_shift($properties);
+                    // Break property descriptor into pieces.
+                    $propertyChain = explode('.', $propertyDescriptor);
+                    // Go through each property in the chain.
+                    while (count($propertyChain) > 0) {
+                        // Pull a property off the front of the chain.
+                        $property = array_shift($propertyChain);
+                        // Make sure the the property exists on both sides.
+                        if (!$aEntity->propertyExists($property) or !$bEntity->propertyExists($property)) {
+                            // If $property doesn't exist on either side, we can't sort.
+                            return 0;
+                        }
+                        // Get the properties list from both sides.
+                        $aProperties = $aEntity->getProperties();
+                        $bProperties = $bEntity->getProperties();
+                        // Make sure the property has a getter on both sides.
+                        if (!array_key_exists($property, $aProperties) or
+                            !array_key_exists($property, $bProperties) or
+                            !array_key_exists('getter', $aProperties[$property]) or
+                            !array_key_exists('getter', $bProperties[$property])) {
+                            // If $property doesn't have a getter specified on either side, we can't sort.
+                            return 0;
+                        }
+                        // Get values for the property from both sides via the getter.
+                        $aVal = $aEntity->$aProperties[$property]['getter']();
+                        $bVal = $bEntity->$bProperties[$property]['getter']();
+                        // If the property value from 'a' is a reference to another entity.
+                        if (gettype($aVal) == 'object' and $aVal instanceof \Pelagos\Entity\Entity) {
+                            // Put it in to $aEntity to allow further processing.
+                            $aEntity = $aVal;
+                            // Set $aVal to the entity's id in case we are comparing whole entities.
+                            $aVal = $aVal->getId();
+                        }
+                        // If the property value from 'b' is a reference to another entity.
+                        if (gettype($bVal) == 'object' and $bVal instanceof \Pelagos\Entity\Entity) {
+                            // Put it in to $bEntity to allow further processing.
+                            $bEntity = $bVal;
+                            // Set $bVal to the entity's id in case we are comparing whole entities.
+                            $bVal = $bVal->getId();
+                        }
                     }
-                    $aProperties = $a->getProperties();
-                    $bProperties = $b->getProperties();
-                    if (!array_key_exists($property, $aProperties) or
-                        !array_key_exists($property, $bProperties) or
-                        !array_key_exists('getter', $aProperties[$property]) or
-                        !array_key_exists('getter', $bProperties[$property])) {
-                        // If $property doesn't have a getter specified on either side, we can't sort.
-                        return 0;
-                    }
-                    $aVal = $a->$aProperties[$property]['getter']();
-                    $bVal = $b->$bProperties[$property]['getter']();
+
                     if (gettype($aVal) == 'string' and gettype($bVal) == 'string') {
                         // It the values on both sides are strings, use a string comparison.
                         $cmp = strcmp($aVal, $bVal);
