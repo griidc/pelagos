d42814d9f106d66c6bbb1c386a771ad096b2e6be
Michael Scott Williamson <michael.williamson@tamucc.edu>
2015-11-09 13:22:40 -0600

    Moved constants into class.

--- src/Pelagos/DoctrineExtensions/DBAL/Types/TokenType.php
+++ src/Pelagos/DoctrineExtensions/DBAL/Types/TokenType.php
@@ -1,12 +1,12 @@
 <?php
 namespace Pelagos\DBAL;
 
-const ENUM_TOKEN_TYPE = 'token_use_type';
-const CREATE_TOKEN = 'CREATE_ACCOUNT';
-const RESET_TOKEN = 'RESET_ACCOUNT';
-
 class TokenType extends EnumType
 {
-    protected $name = ENUM_TOKEN_TYPE;
-    protected $values = array(CREATE_TOKEN, RESET_TOKEN);
+    const ENUM_TOKEN_TYPE = 'token_use_type';
+    const CREATE_TOKEN = 'CREATE_ACCOUNT';
+    const RESET_TOKEN = 'RESET_ACCOUNT';
+
+    protected $name = self::ENUM_TOKEN_TYPE;
+    protected $values = array(self::CREATE_TOKEN, self::RESET_TOKEN);
 }
