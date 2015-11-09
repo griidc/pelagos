a37cfd2524da1bb8ad542a07ffebd522d26e5e0a
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2015-11-09 13:24:21 -0600

    Email function using swiftmailer getting PersonToken and handling it.

--- src/Pelagos/Component/EntityApplication/AccountApplication.php
+++ src/Pelagos/Component/EntityApplication/AccountApplication.php
@@ -10,9 +10,20 @@ use \Pelagos\Service\EntityService;
  */
 class AccountApplication extends \Pelagos\Component\EntityApplication
 {
+    /**
+     * The instance of \Slim\Slim used by this application service.
+     *
+     * @var \Twig $twig
+     *
+     * @access private
+     */
+    private $twig;
+
     public function __construct(\Slim\Slim $slim)
     {
         parent::__construct($slim);
+
+        $this->twig = new \Twig_Environment(new \Twig_Loader_Filesystem('./templates'));
     }
 
     public function handleEntity($entityType)
@@ -38,14 +49,81 @@ class AccountApplication extends \Pelagos\Component\EntityApplication
 
         $entity = $entityService->getBy('Person', $this->slim->request->params());
 
-        if ($entity) {
-            // Instantiate Token Here
+        $knownEmail = false;
+
+        foreach ($entity as $Person) {
+            // Get PersonToken
+            $PersonToken = $Person->getToken();
+
+            // if $Person has Token, remove Token
+            if ($PersonToken) {
+                try {
+                    $this->getEntityService()->delete($PersonToken);
+                } catch (\Exception $e) {
+                    echo 'An error has occured: ' . $e->getMessage();
+                    $this->quit();
+                }
+            }
+
+            // Create new PersonToken
+            $PersonToken = new \Pelagos\PersonToken($Person);
+
+            // Persist PersonToken
+            $PersonToken->setPerson($Person);
+            $PersonToken = $this->entityService->persist($PersonToken);
+
+            $mailData = array(
+                'Person' => $Person,
+            );
+
+            $template = $this->twig->loadTemplate('accountConfirmation.email.html.twig');
+
+            $email = array(
+                'toEmail'  => $Person->getEmailAddress(),
+                'toName'   => $Person->getFirstName() . ' ' . $Person->getLastName(),
+                'subject'  => $template->renderBlock('subject', $mailData),
+                'bodyHTML' => $template->renderBlock('body_html', $mailData),
+                'bodyText' => $template->renderBlock('body_text', $mailData),
+            );
+
+            $this->sendMail($email);
+
+            $knownEmail = true;
         }
 
         $twigData = array(
-            "entity" => $entity
+            "knownEmail" => $knownEmail
         );
 
         $this->slim->render('AccountRequestResponse.html', $twigData);
+    }
+
+    /**
+     * A swift mailer function to send e-mail.
+     *
+     * @param $email Array An array of parameters used to send e-mail.
+     *
+     * @access private
+     */
+    private function sendMail($email)
+    {
+        // Hurray a Transport, we're saved!
+        $transport = \Swift_MailTransport::newInstance();
+
+        // Create the Mailer using your created Transport
+        $mailer = \Swift_Mailer::newInstance($transport);
+
+        // Create a message
+        $message = \Swift_Message::newInstance('Wonderful Subject')
+            ->setFrom(array('griidc@gomri.org' => 'GRIIDC'))
+            ->setTo(array($email['toEmail'] => $email['toName']))
+            ->setSubject($email['subject'])
+            ->setBody($email['bodyText'], 'text/plain')
+            ->addPart($email['bodyHTML'], 'text/html')
+            ;
+
+        // Send the message
+        return $mailer->send($message);
+
     }
 }
