15ee9efdb0cb1779606fdea611087c9a3ee661a0
Michael Scott Williamson <michael.williamson@tamucc.edu>
2015-11-10 14:38:28 -0600

    Added docblocks to TokenType and additional error checking for enum membership.

--- src/Pelagos/DoctrineExtensions/DBAL/Types/TokenType.php
+++ src/Pelagos/DoctrineExtensions/DBAL/Types/TokenType.php
@@ -1,12 +1,71 @@
 <?php
-namespace Pelagos\DBAL;
+namespace Pelagos\DoctrineExtensions\DBAL\Types;
 
+/**
+ * TokenType class implementation of EnumeratedType base class.
+ *
+ * @see EnumType
+ */
 class TokenType extends EnumType
 {
+   /**
+    * Enumeration's name.
+    *
+    * @var string ENUM_TOKEN_TYPE
+    */
     const ENUM_TOKEN_TYPE = 'token_use_type';
+
+   /**
+    * Enumeration value.
+    *
+    * @var string CREATE_TOKEN
+    */
     const CREATE_TOKEN = 'CREATE_ACCOUNT';
+
+   /**
+    * Enumeration's value.
+    *
+    * @var string RESET_ACCOUNT
+    */
     const RESET_TOKEN = 'RESET_ACCOUNT';
 
+   /**
+    * Enumeration's name as used by this class.
+    *
+    * @var string name
+    */
     protected $name = self::ENUM_TOKEN_TYPE;
+
+   /**
+    * Enumeration's values.
+    *
+    * @var array
+    */
     protected $values = array(self::CREATE_TOKEN, self::RESET_TOKEN);
+
+    /**
+     * Converts a value from its PHP representation to its database representation of this type.
+     *
+     * @param mixed            $value    The value to convert.
+     * @param AbstractPlatform $platform The currently used database platform.
+     *
+     * @throws \InvalidArgumentException When attempting convert a non-enumerated value.
+     * @throws DBALException::notSupported When attempting to convert from anything
+     *         other than a TokenType type.
+     *
+     * @return string The database representation of the value.
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        // prevent coding standards violation
+        $griidcDummy = $platform;
+
+        if (gettype($value !== 'object') or get_class($value) !== ENUM_TOKEN_TYPE) {
+            throw DBALException::notSupported('convert from non TokenType');
+        }
+        if (!in_array($value, $this->values)) {
+            throw new InvalidArgumentException("Invalid '" . $this->name . "' value.");
+        }
+        return $value;
+    }
 }
