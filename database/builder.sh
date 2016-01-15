7596baf972850560086fa19813beef680acd05ff
Michael Scott Williamson <mwilliamson@hri-310-90e794.fw.tamucc.edu>
2016-01-15 13:48:32 -0600

    Made builder.sh script warn and require switch to actually run. Adjusted schema perms.

--- database/builder.sh
+++ database/builder.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
-PelagosDBScript=($(find . -wholename "*[0-9]*/[0-9]*.sql" | sort | sed "s/^\.\///"))
+if [[ $1 =~ "-f" ]]
+then
+    PelagosDBScript=($(find . -wholename "*[0-9]*/[0-9]*.sql" | sort | sed "s/^\.\///"))
 
-for sql in "${PelagosDBScript[@]}"
-do
-    echo "${sql}"
-    psql -U postgres gomri < "${sql}"
-    echo
-done
+    for sql in "${PelagosDBScript[@]}"
+    do
+        echo "${sql}"
+        psql -U postgres gomri < "${sql}"
+        echo
+    done
+else
+    echo $1
+    echo "This is a destructive database building script."
+    echo "Run this script with -f switch to really mean it."
+    echo "You WILL lose existing data."
+fi
