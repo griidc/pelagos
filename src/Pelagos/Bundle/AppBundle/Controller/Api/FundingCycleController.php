6dceae3890d15f64a033b6f5875f1638de112290
James C. Davis <James.Davis@tamucc.edu>
2016-01-28 16:14:56 -0600

    Added funding_cycles to API.

---
+++ src/Pelagos/Bundle/AppBundle/Controller/Api/FundingCycleController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Pelagos\Bundle\AppBundle\Controller\Api;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\FormInterface;
+
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use FOS\RestBundle\Util\Codes;
+use FOS\RestBundle\Controller\Annotations;
+
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+use Pelagos\Entity\FundingCycle;
+use Pelagos\Bundle\AppBundle\Form\FundingCycleType;
+
+/**
+ * The FundingCycle api controller.
+ */
+class FundingCycleController extends EntityController
+{
+    /**
+     * Get all research groups.
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   section = "Funding Cycles",
+     *   parameters = {
+     *     {"name"="someProperty", "dataType"="string", "required"=false, "description"="Filter by someProperty"}
+     *   },
+     *   output = "array<Pelagos\Entity\FundingCycle>",
+     *   statusCodes = {
+     *     200 = "Returned when successful",
+     *   }
+     * )
+     *
+     * @Annotations\Get("")
+     *
+     * @Annotations\View(serializerEnableMaxDepthChecks = true)
+     *
+     * @return array
+     */
+    public function getCollectionAction(Request $request)
+    {
+        return $this->handleGetCollection(FundingCycle::class, $request);
+    }
+
+    /**
+     * Get a single research group for a given id.
+     *
+     * @param integer $id The id of the research group to return.
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   section = "Funding Cycles",
+     *   output = "Pelagos\Entity\FundingCycle",
+     *   statusCodes = {
+     *     200 = "Returned when successful",
+     *     404 = "Returned when the research group is not found"
+     *   }
+     * )
+     *
+     * @Annotations\View(serializerEnableMaxDepthChecks = true)
+     *
+     * @return FundingCycle
+     */
+    public function getAction($id)
+    {
+        return $this->handleGetOne(FundingCycle::class, $id);
+    }
+
+    /**
+     * Create a new research group from the submitted data.
+     *
+     * @param Request $request The request object.
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   section = "Funding Cycles",
+     *   input = "FundingCycleType",
+     *   statusCodes = {
+     *     201 = "Returned when successful",
+     *     400 = "Returned when the form has errors"
+     *   }
+     * )
+     *
+     * @Annotations\View(
+     *   statusCode = Codes::HTTP_CREATED,
+     *   serializerEnableMaxDepthChecks = true
+     * )
+     *
+     * @return FundingCycle|FormInterface
+     */
+    public function postAction(Request $request)
+    {
+        return $this->handlePost(FundingCycleType::class, FundingCycle::class, $request);
+    }
+}
