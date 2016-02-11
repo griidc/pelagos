39e7b088e8a32f575f3c0dd1b3119c56b9c9f83e
Joe Holland <joe.holland@tamucc.edu>
2016-02-11 14:42:04 -0600

    Refactored common functions and state from ResearchGroupRole, FundingOrganizationRole and DataRepositoryRole into an abstract base class AbstractRole. Modified each of the concrete Role classes to extend AbstractRole.

---
+++ src/Pelagos/Entity/AbstractRole.php
@@ -0,0 +1,111 @@
+<?php
+
+
+namespace Pelagos\Entity;
+
+/**
+ * Class AbstractRole A base class for the representation of typed Role classes.
+ *
+ * Classes of the type XxxRole can extends this base class.
+ *
+ * @package Pelagos\Entity
+ */
+abstract class AbstractRole extends Entity implements RoleInterface
+{
+    /**
+     * Static array containing a list of the properties and their attributes.
+     *
+     * @var array $properties
+     *
+     * @Exclude
+     */
+    protected static $properties = array(
+        'name' => array(
+            'type' => 'string',
+            'getter' => 'getName',
+            'setter' => 'setName',
+        ),
+        'weight' => array(
+            'type' => 'integer',
+            'getter' => 'getWeight',
+            'setter' => 'setWeight',
+        ),
+    );
+
+    /**
+     * The name of this role.
+     *
+     * @var string
+     *
+     * @Assert\NotBlank(
+     *     message="Name is required"
+     * )
+     * @Assert\NoAngleBrackets(
+     *     message="Name cannot contain angle brackets (< or >)"
+     * )
+     */
+    protected $name;
+
+    /**
+     * The weight associated with this role.
+     *
+     * @var integer
+     *
+     * @Assert\NotBlank(
+     *     message="Weight is required"
+     * )
+     */
+    protected $weight;
+
+    /**
+     * Setter for Name.
+     *
+     * @param string $name The name of this role.
+     *
+     * @return void
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Getter for Name.
+     *
+     * @return string The name of this role.
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Setter for Weight.
+     *
+     * @param integer $weight The weight associated with this role.
+     *
+     * @throws \InvalidArgumentException When provided weight is not an integer or integer string.
+     *
+     * @return void
+     */
+    public function setWeight($weight)
+    {
+        if (is_int($weight) and $weight > 0 or
+            is_string($weight) and ctype_digit($weight) and (integer) $weight > 0
+        ) {
+            $this->weight = (integer) $weight;
+        } else {
+            throw new \InvalidArgumentException('Weight must be a positive integer');
+        }
+    }
+
+    /**
+     * Getter for Weight.
+     *
+     * @return integer The weight associated with this role.
+     */
+    public function getWeight()
+    {
+        return $this->weight;
+    }
+}
