8360af842ac947f3369c6f33a3a3234c5b59b4d0
Michael Scott Williamson <michael.williamson@tamucc.edu>
2016-02-22 14:56:24 -0600

    Added back file mistakingly renamed.

---
+++ src/Pelagos/Bundle/AppBundle/Resources/public/js/ResearchGroup.js
@@ -0,0 +1,26 @@
+$(document).ready(function()
+{
+    "use strict";
+    $(".entityForm[entityType=\"ResearchGroup\"] [name=\"logo\"]").on("logoChanged", function ()
+    {
+        if ($(this).attr("mimeType") !== "application/x-empty") {
+            $("#researchGroupLogo").html("<img src=\"data:" + $(this).attr("mimeType") + ";base64," + $(this).attr("base64") + "\">");
+        }
+    });
+
+    $(".entityForm[entityType=\"PersonResearchGroup\"]").on("entityDelete", function (event, deleteId)
+    {
+        $("#leadership tr[PersonResearchGroupId=\"" + deleteId + "\"]")
+        .animate({ height: "toggle", opacity: "toggle" }, "slow", function() {
+            $(this).slideUp("fast", function() {
+                $(this)
+                .remove();
+            });
+        });
+
+    });
+
+    $("#tabs")
+        .tabs({ heightStyle: "content" })
+        .tabs("disable", 1);
+});
