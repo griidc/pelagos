cbc3809c1a3a183ace36f695e2cb153fe62485f1
Michael Scott Williamson <michael.williamson@tamucc.edu>
2016-02-22 14:17:21 -0600

    Removed unused variable.

--- src/Pelagos/Bundle/AppBundle/Controller/ListController.php
+++ src/Pelagos/Bundle/AppBundle/Controller/ListController.php
@@ -6,8 +6,6 @@ use Pelagos\Bundle\AppBundle\Form\ResearchGroupType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-#use Symfony\Component\HttpFoundation\Response;
-#use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 /**
  * The default controller for the Pelagos UI App Bundle.
@@ -23,10 +21,8 @@ class ListController extends Controller
      */
     public function researchGroupListAction(Request $request)
     {
-        //return new Response(var_dump($request));
         $ui = array();
         $ui['entityType'] = 'ResearchGroup';
-        $ui['isUserLoggedIn'] = true;
         return $this->render('PelagosAppBundle:template:UI-ResearchGroupList.html.twig', $ui);
     }
 }
