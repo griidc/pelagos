f399c6e76d2756b9e61e9a50c02d0189718a06b2
Michael Scott Williamson <michael.williamson@tamucc.edu>
2016-03-09 10:18:38 -0600

    Changed logging message to more properer English.

--- web/applications/dataset-registry/registry.php
+++ web/applications/dataset-registry/registry.php
@@ -294,7 +294,7 @@ if ($_POST) {
                             move_uploaded_file($_FILES["metadatafile"]["tmp_name"],"$dest_dir/" . $_FILES["metadatafile"]["name"]);
                             $metadata_file_path = "file://$dest_dir/" . $_FILES["metadatafile"]["name"];
                             $registry_vals['metadata_dl_status'] = 'None';
-                            $message = "$drupaluser->name has been registered new metadata via direct upload for ".addslashes($_POST['dataset_udi']);
+                            $message = "$drupaluser->name has registered new metadata via direct upload for ".addslashes($_POST['dataset_udi']);
                             writeLog($message,$mdapp_logfile);
                         }
                     }
@@ -305,7 +305,7 @@ if ($_POST) {
                     if (array_key_exists('sftp_force_metadata_download',$_POST) and !empty($_POST['sftp_force_metadata_download'])) {
                         $registry_vals['metadata_dl_status'] = 'None';
                     }
-                    $message = "$drupaluser->name has been registered new metadata via SFTP/GridFTP upload for ".addslashes($_POST['dataset_udi']);
+                    $message = "$drupaluser->name has registered new metadata via SFTP/GridFTP upload for ".addslashes($_POST['dataset_udi']);
                     writeLog($message,$mdapp_logfile);
                     break;
                 case 'HTTP':
@@ -328,7 +328,7 @@ if ($_POST) {
                         // Only make this update if either the Uri to the metadata file is different from what
                         // is on record, or if the 'force' option has been selected by the user.
                         if (($forceMetadataDownload == true) or ($_POST['url_metadata_http'] != $storedUrl)) {
-                            $message = "$drupaluser->name has been registered new metadata via HTTP pull for ".addslashes($_POST['dataset_udi']);
+                            $message = "$drupaluser->name has registered new metadata via HTTP pull for ".addslashes($_POST['dataset_udi']);
                             writeLog($message,$mdapp_logfile);
                         }
                     }
