581cea8d9b60c194392892125cdc56f85b8b105c
James C. Davis <James.Davis@tamucc.edu>
2016-03-14 11:48:24 -0500

    Brought up to coding standards.

--- web/applications/dataset-summary/index.php
+++ web/applications/dataset-summary/index.php
@@ -1,50 +1,49 @@
 <?php
-// @codingStandardsIgnoreFile
 
-require_once __DIR__.'/../../../vendor/autoload.php';
+require_once __DIR__ . '/../../../vendor/autoload.php';
 
 $GLOBALS['pelagos']['title'] = 'Dataset Summary';
 
-set_include_path("../../../share/php" . PATH_SEPARATOR . get_include_path());
+set_include_path('../../../share/php' . PATH_SEPARATOR . get_include_path());
 
-# load global pelagos config
+// load global pelagos config
 $GLOBALS['config'] = parse_ini_file('/etc/opt/pelagos.ini', true);
-$GLOBALS['db'] = parse_ini_file($GLOBALS['config']['paths']['conf'].'/db.ini', true);
+$GLOBALS['db'] = parse_ini_file($GLOBALS['config']['paths']['conf'] . '/db.ini', true);
 
-# load Drupal functions
+// load Drupal functions
 require_once 'drupal.php';
-# load database utilities
+// load database utilities
 require_once 'DBUtils.php';
 
-# initialize Slim
+// initialize Slim
 $app = new \Slim\Slim(array('view' => new \Slim\Views\Twig()));
 
-# add custom Twig extensions
+// add custom Twig extensions
 $app->view->parserExtensions = array(
     new \Pelagos\TwigExtensions()
 );
 
 $app->get('/:udi', function ($udi) use ($app) {
-    # Regexp check this flaming chainsaw juggling sword-swallowing dangerous beast!
+    // Regexp check this flaming chainsaw juggling sword-swallowing dangerous beast!
     if (preg_match('/^[A-Z][0-6]\.x[0-9]{3}\.[0-9]{3}:[0-9]{4}$/', $udi) == 1) {
         $winUdi = preg_replace('/:/', '-', $udi);
 
-        # Remove stale old ZIP so it doesn't get appended to.
+        // Remove stale old ZIP so it doesn't get appended to.
         if (file_exists("/var/tmp/$winUdi.zip")) {
             unlink("/var/tmp/$winUdi.zip");
         }
 
-        # Create directory for dumps.
+        // Create directory for dumps.
         exec("mkdir /var/tmp/$winUdi");
 
-        # Make unique between runs.
+        // Make unique between runs.
         $pid = getmypid();
 
         $tableQueries = array(
             'datasets' => "SELECT * FROM datasets WHERE dataset_udi = '$udi'",
             'registry' => "SELECT * FROM registry WHERE registry_id like '%$udi%' ORDER BY registry_id DESC",
             'metadata' => "SELECT * FROM metadata WHERE registry_id like '%$udi%' ORDER BY registry_id DESC",
-            'dataset2publication_link_table' => "SELECT * FROM dataset2publication_link_table WHERE dataset_udi  = '$udi'",
+            'dataset2publication_link_table' => "SELECT * FROM dataset2publication_link_table WHERE dataset_udi = '$udi'",
             'alt_datasets' => "SELECT * FROM alt_datasets WHERE primary_udi = '$udi'",
             'doi_regs' => "SELECT * FROM doi_regs WHERE url LIKE '%$udi%'",
         );
@@ -55,17 +54,17 @@ $app->get('/:udi', function ($udi) use ($app) {
             $query = "\COPY ($query) TO '$resultFilename' WITH csv header";
             file_put_contents($queryFilename, $query);
             sleep(1);
-            # in production, we should create a separate read/only account for this pgpass auth to work safer.
+            // in production, we should create a separate read/only account for this pgpass auth to work safer.
             exec("/usr/bin/psql -U gomri_user gomri < $queryFilename");
             unlink($queryFilename);
         }
     } else {
-        print "not a valid udi";
+        echo 'not a valid udi';
     }
     exec("zip -r /var/tmp/$winUdi.zip /var/tmp/$winUdi");
     exec("rm -rf /var/tmp/$winUdi");
 
-    # send zip to browser
+    // send zip to browser
     header('Content-Type: application/zip');
     header("Content-Disposition: attachment; filename=$winUdi.zip");
     readfile("/var/tmp/$winUdi.zip");
