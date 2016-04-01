ee3ccaeb5220ca88ec661c71992a4fe9d16d5134
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2016-04-01 12:44:06 -0500

    Forgotten jms serializer is password

--- src/Pelagos/Entity/Password.php
+++ src/Pelagos/Entity/Password.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 use Symfony\Component\Validator\Constraints as Assert;
 
+use JMS\Serializer\Annotation as Serializer;
 
 use Pelagos\Exception\PasswordException;
 
