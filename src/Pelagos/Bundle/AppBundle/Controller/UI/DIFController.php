fda8b603769e5388540f2b4bd34a8e4b36cbc04f
James C. Davis <James.Davis@tamucc.edu>
2016-04-14 11:04:29 -0500

    Added start of DIF controller

---
+++ src/Pelagos/Bundle/AppBundle/Controller/UI/DIFController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Pelagos\Bundle\AppBundle\Controller\UI;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Pelagos\Bundle\AppBundle\Form\DIFType;
+
+use Pelagos\Entity\DIF;
+
+/**
+ * The DIF controller for the Pelagos UI App Bundle.
+ *
+ * @Route("/dif")
+ */
+class DIFController extends UIController
+{
+    /**
+     * The default action for the DIF.
+     *
+     * @param string $id The id of the DIF to load.
+     *
+     * @Route("/{id}")
+     *
+     * @return Response A Response instance.
+     */
+    public function defaultAction(Request $request, $id = null)
+    {
+        $dif = new DIF;
+        $form = $this->get('form.factory')->createNamed(null, DIFType::class, $dif);
+
+        return $this->render(
+            'PelagosAppBundle:DIF:difForm.html.twig',
+            array('form' => $form->createView())
+        );
+    }
+}
