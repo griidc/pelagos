9e06d8d46c924377fc8c1cfc5a01b80bbf192a3c
Michael Scott Williamson <michael.williamson@tamucc.edu>
2016-04-14 09:39:44 -0500

    Added use statement for HiddenType

--- src/Pelagos/Bundle/AppBundle/Form/DIFType.php
+++ src/Pelagos/Bundle/AppBundle/Form/DIFType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
