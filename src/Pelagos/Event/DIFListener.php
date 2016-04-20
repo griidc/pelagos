9c2801e19932d040c63e5d10c880c10d2176de0a
Michael Scott Williamson <michael.williamson@tamucc.edu>
2016-04-20 13:17:11 -0500

    Added docblocks for DIFListener.

--- src/Pelagos/Event/DIFListener.php
+++ src/Pelagos/Event/DIFListener.php
@@ -2,28 +2,83 @@
 namespace Pelagos\Event;
 
 use Pelagos\Entity\Account;
+use Pelagos\Entity\Person;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 use Pelagos\Bundle\AppBundle\DataFixtures\ORM\DataRepositoryRoles;
 
+/**
+ * Listener class for DIF-related events.
+ */
 class DIFListener
 {
+    /**
+     * The twig templating engine instance.
+     *
+     * @var \Twig_Environment $twig
+     */
     protected $twig;
+
+    /**
+     * The swiftmailer instance.
+     *
+     * @var \Swift_Mailere $mailer
+     */
     protected $mailer;
+
+    /**
+     * Person entity for the logged-in user.
+     *
+     * @var Person $currentUser
+     */
     protected $currentUser;
+
+    /**
+     * The symfony-managed token object to traverse to current user Person.
+     *
+     * @var TokenStorage $tokenStorage.
+     */
     protected $tokenStorage;
+
+    /**
+     * An array holding email from name/email information.
+     *
+     * @var Array $from
+     */
     protected $from;
 
-    public function __construct(\Twig_Environment $twig, \Swift_Mailer $mailer, TokenStorage $tokenStorage, $fromAddress, $fromName)
-    {
+    /**
+     * This is the class constructor to handle dependency injections.
+     *
+     * @param \Twig_Environment $twig         Twig engine.
+     * @param \Swift_Mailer     $mailer       Email handling library.
+     * @param TokenStorage      $tokenStorage Symfony's token object.
+     * @param string            $fromAddress  Sender's email address.
+     * @param string            $fromName     Sender's name to include in email.
+     */
+    public function __construct(
+        \Twig_Environment $twig,
+        \Swift_Mailer $mailer,
+        TokenStorage $tokenStorage,
+        $fromAddress,
+        $fromName
+    ) {
         $this->twig = $twig;
         $this->mailer = $mailer;
         $this->tokenStorage = $tokenStorage;
         $this->from = array($fromAddress => $fromName);
     }
 
+    /**
+     * Method to send an email on submit event.
+     *
+     * @param DIFEvent $event Event being acted upon.
+     *
+     * @return void
+     */
     public function onSubmitted(DIFEvent $event)
     {
-        $currentUser = $this->tokenStorage->getToken()->getUser()->getPerson(); // getUser() is Account's method.
+        // Token's getUser returns an account, not a person directly.
+        $currentUser = $this->tokenStorage->getToken()->getUser()->getPerson();
 
         // email DRPM(s)
         $drpms = $this->getDRPMs($event->getDIF());
@@ -36,27 +91,65 @@ class DIFListener
 
     }
 
+    /**
+     * Method to send an email on approval event.
+     *
+     * @param DIFEvent $event Event being acted upon.
+     *
+     * @return void
+     */
     public function onApproved(DIFEvent $event)
     {
         $dif = $event->getDIF();
     }
 
+    /**
+     * Method to send an email on a rejected event.
+     *
+     * @param DIFEvent $event Event being acted upon.
+     *
+     * @return void
+     */
     public function onRejected(DIFEvent $event)
     {
         $dif = $event->getDIF();
     }
 
+    /**
+     * Method to send an email on an Unlocking event.
+     *
+     * @param DIFEvent $event Event being acted upon.
+     *
+     * @return void
+     */
     public function onUnlocked(DIFEvent $event)
     {
         $dif = $event->getDIF();
     }
 
+    /**
+     * Method to send an email on unlock request event.
+     *
+     * @param DIFEvent $event Event being acted upon.
+     *
+     * @return void
+     */
     public function onUnlockRequested(DIFEvent $event)
     {
         $dif = $event->getDIF();
     }
 
-    protected function sendMailMsg($peopleObjs, $twigTemplate, $udi, $subject)
+    /**
+     * Method to build and send an email.
+     *
+     * @param array          $peopleObjs   An array of recepient Persons.
+     * @param \Twig_Template $twigTemplate A twig template.
+     * @param string         $udi          UDI of a dataset to include in email message.
+     * @param string         $subject      Subjet for email message.
+     *
+     * @return void
+     */
+    protected function sendMailMsg(array $peopleObjs, \Twig_Template $twigTemplate, $udi, $subject)
     {
         $mailData['udi'] = $udi;
         foreach ($peopleObjs as $person) {
@@ -71,10 +164,24 @@ class DIFListener
         }
     }
 
-    private function getDRPMs($dif)
+    /**
+     * Internal method to resolve DRPMs from a dif.
+     *
+     * @param DIF $dif A DIF entity.
+     *
+     * @return Array of Persons having DRPM status.
+     */
+    private function getDRPMs(DIF $dif)
     {
         $recepientPeople = array();
-        foreach($dif->getResearchGroup()->getFundingCycle()->getFundingOrganization()->getDataRepository()->getPersonDataRepositories() as $pdr) {
+        $personDataRepositories = $dif->getResearchGroup()
+                                      ->getFundingCycle()
+                                      ->getFundingCycle()
+                                      ->getFundingOrganization()
+                                      ->getDataRepository()
+                                      ->getPersonDataRepositories();
+
+        foreach ($personDataRepositories as $pdr) {
             if ($pdr->getRole()->getName() == DataRepositoryRoles::MANAGER) {
                 $recepientPeople[] = $pdr->getPerson();
             }
@@ -82,4 +189,3 @@ class DIFListener
         return $recepientPeople;
     }
 }
-
