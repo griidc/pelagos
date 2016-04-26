dfcd4f1f2492038b3ca9e0c69363cc29e6213fb4
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2016-04-26 16:23:02 -0500

    Modification the controller. Added post route, and form validation.

--- src/Pelagos/Bundle/AppBundle/Controller/UI/DatasetSubmissionController.php
+++ src/Pelagos/Bundle/AppBundle/Controller/UI/DatasetSubmissionController.php
@@ -3,6 +3,7 @@
 namespace Pelagos\Bundle\AppBundle\Controller\UI;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,14 +26,57 @@ class DatasetSubmissionController extends UIController
      * @param string|null $id      The id of the Dataset to load.
      *
      * @Route("/{id}")
+     * 
+     * @Method("GET")
      *
      * @return Response A Response instance.
      */
     public function defaultAction(Request $request, $id = null)
     {
         $dataset = new DatasetSubmission;
-        $form = $this->get('form.factory')->createNamed(null, DatasetSubmissionType::class, $dataset);
+        $form = $this->get('form.factory')->createNamed(
+            null, 
+            DatasetSubmissionType::class, 
+            $dataset
+        );
 
+        return $this->render(
+            'PelagosAppBundle:DatasetSubmission:index.html.twig',
+            array('form' => $form->createView())
+        );
+    }
+    
+    /**
+     * The default action for Dataset Submission.
+        *
+     * @param Request     $request The Symfony request object.
+     * @param string|null $id      The id of the Dataset to load.
+     *
+     * @Route("/{id}")
+     *
+     * @Method("POST")
+     *
+     * @return Response A Response instance.
+     */
+    public function postAction(Request $request, $id = null)
+    {
+        $dataset = new DatasetSubmission;
+        $form = $this->get('form.factory')->createNamed(
+        null, 
+        DatasetSubmissionType::class, 
+        $dataset
+        );
+        
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            // ... perform some action, such as saving the task to the database
+            
+            echo 'works!';
+            
+            $this->container->get('pelagos.entity.handler')->create($dataset);
+        }
+        
         return $this->render(
             'PelagosAppBundle:DatasetSubmission:index.html.twig',
             array('form' => $form->createView())
