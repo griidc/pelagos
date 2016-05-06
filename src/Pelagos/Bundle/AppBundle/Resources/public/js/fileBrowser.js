14107884a78d49dff228dc2e333080a101953ccc
James C. Davis <James.Davis@tamucc.edu>
2016-05-06 16:21:02 -0500

    Allow use of HTML 5 data attributes to configure fileBrowser.

--- src/Pelagos/Bundle/AppBundle/Resources/public/js/fileBrowser.js
+++ src/Pelagos/Bundle/AppBundle/Resources/public/js/fileBrowser.js
@@ -4,18 +4,18 @@
             type: ""
         }, options);
 
-        var loadFiles = function(overlay, cwd, fileListing, subDirectory) {
-            cwd.html("");
-            fileListing.html("");
-            var url = settings.url;
+        var loadFiles = function(localSettings, elements, subDirectory) {
+            elements.cwd.html("");
+            elements.fileListing.html("");
+            var url = localSettings.url;
             if (typeof subDirectory !== "undefined") {
                 url += "?subDirectory=" + subDirectory;
             }
             $.get(url, function(data) {
-                cwd.append(
+                elements.cwd.append(
                     $("<span/>").html(data.basePath)
                     .click(function() {
-                        loadFiles(overlay, cwd, fileListing);
+                        loadFiles(localSettings, elements);
                     })
                 );
                 if (typeof subDirectory !== "undefined") {
@@ -27,37 +27,37 @@
                         } else {
                             subDir += "/" + subDirectories[i]
                         }
-                        cwd.append(
+                        elements.cwd.append(
                             $("<span/>").html("/" + subDirectories[i])
                             .click(
                                 (function(sd) {
                                     return function() {
-                                        loadFiles(overlay, cwd, fileListing, sd);
+                                        loadFiles(localSettings, elements, sd);
                                     }
                                 }(subDir))
                             )
                         );
                     }
-                    fileListing.append(
+                    elements.fileListing.append(
                         $("<div/>").addClass("fileBrowserDirectory")
                             .append($("<div/>").addClass("fileBrowserProperty fileBrowserName").html(".."))
                             .click(function() {
                                 if (subDirectory.lastIndexOf("/") > -1) {
-                                    loadFiles(overlay, cwd, fileListing, subDirectory.substring(0, subDirectory.lastIndexOf("/")));
+                                    loadFiles(localSettings, elements, subDirectory.substring(0, subDirectory.lastIndexOf("/")));
                                 } else {
-                                    loadFiles(overlay, cwd, fileListing);
+                                    loadFiles(localSettings, elements);
                                 }
                             })
                     );
                 }
                 for (var i=0; i<data.directories.length; i++) {
-                    fileListing.append(
+                    elements.fileListing.append(
                         $("<div/>").addClass("fileBrowserDirectory")
                             .append($("<div/>").addClass("fileBrowserProperty fileBrowserName").html(data.directories[i].name))
                             .click(
                                 (function(j) {
                                     return function() {
-                                       loadFiles(overlay, cwd, fileListing, data.directories[j].path);
+                                       loadFiles(localSettings, elements, data.directories[j].path);
                                     }
                                 }(i))
                             )
@@ -70,7 +70,7 @@
                         var unitIndex = Math.floor(Math.log(data.files[i].size) / Math.log(1000));
                         fileSize = (data.files[i].size / Math.pow(1000, unitIndex)).toFixed(2) * 1 + " " + ["B", "kB", "MB", "GB", "TB"][unitIndex];
                     }
-                    fileListing.append(
+                    elements.fileListing.append(
                         $("<div/>").addClass("fileBrowserFile")
                             .append($("<div/>").addClass("fileBrowserProperty fileBrowserName").html(data.files[i].name))
                             .append($("<div/>").addClass("fileBrowserProperty fileBrowserMod").html(data.files[i].mtime))
@@ -78,8 +78,8 @@
                             .click(
                                 (function(j) {
                                     return function() {
-                                        $(settings.target).val(data.basePath + "/" + data.files[j].path);
-                                        overlay.hide();
+                                        $(localSettings.target).val(data.basePath + "/" + data.files[j].path);
+                                        elements.overlay.hide();
                                     }
                                 }(i))
                             )
@@ -90,49 +90,61 @@
 
         return this.each(function() {
 
-            var overlay = $("<div/>").addClass("fileBrowserOverlay");
+            var localSettings = $.extend({}, settings);
 
-            var title = $("<div/>").addClass("fileBrowserTitle")
-                .html("Select " + settings.type + " file:");
+            var object = this;
 
-            var cwd = $("<span/>").addClass("fileBrowserCwd");
+            $.each([ "url", "target", "type" ], function (index, value) {
+                if (typeof $(object).data(value) !== "undefined") {
+                    localSettings[value] = $(object).data(value);
+                }
+            });
 
-            var cwdBox = $("<div/>").addClass("fileBrowserCwdBox")
+            var elements = {};
+
+            elements.overlay = $("<div/>").addClass("fileBrowserOverlay");
+
+            elements.title = $("<div/>").addClass("fileBrowserTitle")
+                .html("Select " + localSettings.type + " file:");
+
+            elements.cwd = $("<span/>").addClass("fileBrowserCwd");
+
+            elements.cwdBox = $("<div/>").addClass("fileBrowserCwdBox")
                 .append("<strong>Directory: </strong>")
-                .append(cwd);
+                .append(elements.cwd);
 
-            var header = $("<div/>").addClass("fileBrowserHeader")
+            elements.header = $("<div/>").addClass("fileBrowserHeader")
                 .append($("<div/>").addClass("fileBrowserProperty fileBrowserName").html("Name"))
                 .append($("<div/>").addClass("fileBrowserProperty fileBrowserMod").html("Date modified"))
                 .append($("<div/>").addClass("fileBrowserProperty fileBrowserSize").html("Size"));
 
-            var fileListing = $("<div/>").addClass("fileBrowserFileListing");
+            elements.fileListing = $("<div/>").addClass("fileBrowserFileListing");
 
-            var fileBox = $("<div/>").addClass("fileBrowserFileBox")
-                .append(header).append(fileListing);
+            elements.fileBox = $("<div/>").addClass("fileBrowserFileBox")
+                .append(elements.header).append(elements.fileListing);
 
-            var cancel = $("<div/>").addClass("fileBrowserCancel")
+            elements.cancel = $("<div/>").addClass("fileBrowserCancel")
                 .append(
                     $("<button>Cancel</button>").click(function() {
-                        overlay.hide();
+                        elements.overlay.hide();
                     })
                 );
 
-            overlay.append(
+            elements.overlay.append(
                 $("<div/>").addClass("fileBrowserWrapper").append(
                     $("<div/>").addClass("fileBrowserContent")
-                        .append(title)
-                        .append(cwdBox)
-                        .append(fileBox)
-                        .append(cancel)
+                        .append(elements.title)
+                        .append(elements.cwdBox)
+                        .append(elements.fileBox)
+                        .append(elements.cancel)
                 )
             );
 
-            $(this).after(overlay);
+            $(this).after(elements.overlay);
 
             $(this).click(function() {
-                overlay.show();
-                loadFiles(overlay, cwd, fileListing);
+                elements.overlay.show();
+                loadFiles(localSettings, elements);
             });
         });
     };
