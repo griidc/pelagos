35d5ac6d2a7c773d1ffce5198f17c02cceaa9e30
Michael Scott Williamson <michael.williamson@tamucc.edu>
2016-05-12 11:46:36 -0500

    Added in error-handling.

--- src/Pelagos/Bundle/AppBundle/Command/CreateHomedirCommand.php
+++ src/Pelagos/Bundle/AppBundle/Command/CreateHomedirCommand.php
@@ -5,6 +5,7 @@ namespace Pelagos\Bundle\AppBundle\Command;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Ldap\Exception\LdapException;
 
 use Pelagos\Entity\Account;
 use Pelagos\Entity\Person;
@@ -41,9 +42,6 @@ class CreateHomedirCommand extends ContainerAwareCommand
      * @param InputInterface  $input  An Input Interface instance we don't use.
      * @param OutputInterface $output An OutputInterface instance.
      *
-     * @throws \Exception If a directory to be created already exists.
-     * @throws \Exception If a non-account is encountered.
-     *
      * @return integer Return 0 on success, or an error code otherwise.
      */
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -63,14 +61,22 @@ class CreateHomedirCommand extends ContainerAwareCommand
             ->select('account')
             ->from(Account::class, 'account')
             ->where(
+                $queryBuilder->expr()->eq('account.posix', $queryBuilder->expr()->literal(true))
+            )
+            ->andWhere(
                 $queryBuilder->expr()->like('account.homeDirectory', ':pattern')
             )
             ->setParameter('pattern', '/dev/null/%')
             ->getQuery()
             ->getResult();
 
+        $totalErrors = 0;
         $accounts = $result;
         foreach ($accounts as $account) {
+
+            $errorString = '';
+            $errorCount = 0;
+
             if ($account instanceof Account) {
 
                 // Get username for the last part of homedir.
@@ -78,40 +84,105 @@ class CreateHomedirCommand extends ContainerAwareCommand
                 $homeDir = "$prefix/$username";
 
                 // Set correct path in the model.
-                $output->writeln('Updating database for: ' . $username . '.');
                 $account->setHomeDirectory($homeDir);
-                $entityManager->persist($account);
-                $entityManager->flush();
 
                 // adjust LDAP definition
-                // Get Person associated with this Account.
                 $accountOwnerPerson = $account->getPerson();
-
-                // Update LDAP with this modified Account (via Person).
-                $output->writeln('Updating LDAP for: ' . $username . '.');
-                $this->getContainer()->get('pelagos.ldap')->updatePerson($accountOwnerPerson);
-
-                // Create home directory on server.
-                if (is_dir($homeDir)) {
-                    throw new \Exception("Directory for $username already exists.");
+                try {
+                    $this->getContainer()->get('pelagos.ldap')->updatePerson($accountOwnerPerson);
+                    $output->writeln("Updating LDAP for $username.");
+                } catch (LdapException $e) {
+                    $errorCount++;
+                    $errorString .= "Failed updating LDAP for $username.";
                 }
-                $output->writeln('Creating homedir for user ' . $username . ': ' . "$prefix/$username\n");
-                // Create home directory.
-                mkdir("$homeDir", 0750, false);
-                exec("/usr/bin/setfacl -m u:apache:r-x $homeDir");
-                exec("/usr/bin/setfacl -m u:$username:r-x $homeDir");
-                // create incoming directory.
-                mkdir("$homeDir/incoming", 0750, false);
-                exec("/usr/bin/setfacl -m u:apache:rwx $homeDir/incoming");
+
+                // Check to see if a directory already exists.
+                if (is_dir($homeDir)) {
+                    $errorCount++;
+                    $errorString .= "Homedir already exists: $homeDir.\n";
+                }
+
+                $output->writeln("Creating homedir structure for user: $homeDir\n");
+
+                // Create home directory, owned by script-running system user (pelagos).
+                if (false == mkdir("$homeDir", 0750, false)) {
+                    $errorCount++;
+                    $errorString .= "Could not create homedir: $homeDir.\n";
+                }
+
+                // Give Apache r-x on homedir with FACL.
+                $outputLines = array();
+                exec("/usr/bin/setfacl -m u:apache:r-x $homeDir", $outputLines, $returnValue);
+                if ($returnValue != 0) {
+                    $errorCount++;
+                    $errorString .= "Error setting facl (u:apache:r-x) on $homeDir.\n";
+                }
+
+                // Give user r-x on homedir with FACL.
+                $outputLines = array();
+                exec("/usr/bin/setfacl -m u:$username:r-x $homeDir", $outputLines, $returnValue);
+                if ($returnValue != 0) {
+                    $errorCount++;
+                    $errorString .= "Error setting facl (u:$username:r-x) on $homeDir.\n";
+                }
+
+                // Create incoming directory, owned by script-running system user (pelagos).
+                if (false == mkdir("$homeDir/incoming", 0750, false)) {
+                    $errorCount++;
+                    $errorString .= "Could not create directory: $homeDir/incoming.\n";
+                }
+
+                // Give Apache rwx on homedir/incoming with FACL.
+                $outputLines = array();
+                exec("/usr/bin/setfacl -m u:apache:rwx $homeDir/incoming", $outputLines, $returnValue);
+                if ($returnValue != 0) {
+                    $errorCount++;
+                    $errorString .= "Error setting facl (u:apache:rwx) on $homeDir/incoming.\n";
+                }
+
+                // Give user rwx on homedir/incoming with FACL.
                 exec("/usr/bin/setfacl -m u:$username:rwx $homeDir/incoming");
-                // Create download directory
-                mkdir("$homeDir/download", 0750, false);
-                exec("/usr/bin/setfacl -m u:apache:rwx $homeDir/download");
-                exec("/usr/bin/setfacl -m u:$username:r-x $homeDir/download");
+                if ($returnValue != 0) {
+                    $errorCount++;
+                    $errorString .= "Error setting facl (u:$username:rwx) on $homeDir/incoming.\n";
+                }
+
+                // Create download directory, owned by script-running system user (pelagos).
+                if (false == mkdir("$homeDir/download", 0750, false)) {
+                    $errorCount++;
+                    $errorString .= "Could not create $homeDir/download.\n";
+                }
+
+                // Give Apache rwx on homedir/download with FACL.
+                $outputLines = array();
+                exec("/usr/bin/setfacl -m u:apache:rwx $homeDir/download", $outputLines, $returnValue);
+                if ($returnValue != 0) {
+                    $errorCount++;
+                    $errorString .= "Error setting facl (u:apache:rwx) on $homeDir/download.\n";
+                }
+
+                // Give user r-x on homedir/download with FACL.
+                $outputLines = array();
+                exec("/usr/bin/setfacl -m u:$username:r-x $homeDir/download", $outputLines, $returnValue);
+                if ($returnValue != 0) {
+                    $errorCount++;
+                    $errorString .= "Error setting facl (u:$username:r-x) on $homeDir/download.\n";
+                }
+
+                // Persist changes to Account if everything has worked up to this point.
+                if (0 == $errorCount) {
+                    $output->writeln('Updating database for: ' . $username . '.');
+                    $entityManager->persist($account);
+                    $entityManager->flush();
+                } else {
+                    print $errorString;
+                }
             } else {
-                throw new \Exception('Expected account, got something else.');
+                    $output->writeln("Encountered unexpected non-Account object.\n");
             }
+            $totalErrors += $errorCount;
         }
-        return 0;
+        // Exit with error code 1 if there were any errors.
+        return ($totalErrors != 0);
     }
 }
