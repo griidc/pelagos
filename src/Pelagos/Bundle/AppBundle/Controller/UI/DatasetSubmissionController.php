e338a44d6689874e58272bf4856c1543d7007033
James C. Davis <James.Davis@tamucc.edu>
2016-05-16 15:04:36 -0500

    Only try to pull data from dataset submission if we have one.

--- src/Pelagos/Bundle/AppBundle/Controller/UI/DatasetSubmissionController.php
+++ src/Pelagos/Bundle/AppBundle/Controller/UI/DatasetSubmissionController.php
@@ -131,36 +131,38 @@ class DatasetSubmissionController extends UIController
             )
         );
 
-        switch ($datasetSubmission->getDatasetFileTransferType()) {
-            case DatasetSubmission::TRANSFER_TYPE_UPLOAD:
-                $form->get('datasetFileUpload')->setData(
-                    preg_replace('#^file://#', '', $datasetSubmission->getDatasetFileUri())
-                );
-                break;
-            case DatasetSubmission::TRANSFER_TYPE_SFTP:
-                $form->get('datasetFilePath')->setData(
-                    preg_replace('#^file://#', '', $datasetSubmission->getDatasetFileUri())
-                );
-                break;
-            case DatasetSubmission::TRANSFER_TYPE_HTTP:
-                $form->get('datasetFileUrl')->setData($datasetSubmission->getDatasetFileUri());
-                break;
-        }
+        if ($datasetSubmission instanceof DatasetSubmission) {
+            switch ($datasetSubmission->getDatasetFileTransferType()) {
+                case DatasetSubmission::TRANSFER_TYPE_UPLOAD:
+                    $form->get('datasetFileUpload')->setData(
+                        preg_replace('#^file://#', '', $datasetSubmission->getDatasetFileUri())
+                    );
+                    break;
+                case DatasetSubmission::TRANSFER_TYPE_SFTP:
+                    $form->get('datasetFilePath')->setData(
+                        preg_replace('#^file://#', '', $datasetSubmission->getDatasetFileUri())
+                    );
+                    break;
+                case DatasetSubmission::TRANSFER_TYPE_HTTP:
+                    $form->get('datasetFileUrl')->setData($datasetSubmission->getDatasetFileUri());
+                    break;
+            }
 
-        switch ($datasetSubmission->getMetadataFileTransferType()) {
-            case DatasetSubmission::TRANSFER_TYPE_UPLOAD:
-                $form->get('metadataFileUpload')->setData(
-                    preg_replace('#^file://#', '', $datasetSubmission->getMetadataFileUri())
-                );
-                break;
-            case DatasetSubmission::TRANSFER_TYPE_SFTP:
-                $form->get('metadataFilePath')->setData(
-                    preg_replace('#^file://#', '', $datasetSubmission->getMetadataFileUri())
-                );
-                break;
-            case DatasetSubmission::TRANSFER_TYPE_HTTP:
-                $form->get('metadataFileUrl')->setData($datasetSubmission->getMetadataFileUri());
-                break;
+            switch ($datasetSubmission->getMetadataFileTransferType()) {
+                case DatasetSubmission::TRANSFER_TYPE_UPLOAD:
+                    $form->get('metadataFileUpload')->setData(
+                        preg_replace('#^file://#', '', $datasetSubmission->getMetadataFileUri())
+                    );
+                    break;
+                case DatasetSubmission::TRANSFER_TYPE_SFTP:
+                    $form->get('metadataFilePath')->setData(
+                        preg_replace('#^file://#', '', $datasetSubmission->getMetadataFileUri())
+                    );
+                    break;
+                case DatasetSubmission::TRANSFER_TYPE_HTTP:
+                    $form->get('metadataFileUrl')->setData($datasetSubmission->getMetadataFileUri());
+                    break;
+            }
         }
 
         if ($this->getUser() instanceof Account) {
