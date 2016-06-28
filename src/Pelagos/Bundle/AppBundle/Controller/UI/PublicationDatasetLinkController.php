50fb421c55f7d910afb4a98ee76f881d983479ea
Michael Scott Williamson <michael.williamson@tamucc.edu>
2016-06-28 14:22:14 -0500

    Moved logic into Api, simplified UI controller.

--- src/Pelagos/Bundle/AppBundle/Controller/UI/PublicationDatasetLinkController.php
+++ src/Pelagos/Bundle/AppBundle/Controller/UI/PublicationDatasetLinkController.php
@@ -3,6 +3,7 @@
 namespace Pelagos\Bundle\AppBundle\Controller\UI;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -35,55 +36,4 @@ class PublicationDatasetLinkController extends UIController
         return $this->render('PelagosAppBundle:PublicationDatasetLink:index.html.twig');
     }
 
-    /**
-     * Generate citations for publications.
-     *
-     * @Route("/services/citation/publication/{doi}", requirements={"doi"=".+"})
-     * @Method("GET")
-     *
-     * @param string $doi A string containing a DOI
-     *
-     * @return array An array of Citation and status elements.
-     */
-    public function getCitationPublicationAction($doi)
-    {
-        $pubLinkUtil = $this->get('pelagos.util.publink');
-        $citationStruct = $pubLinkUtil->getCitationFromDoiDotOrg($doi);
-        if (200 == $citationStruct['status']) {
-            $entityHandler = $this->get('pelagos.entity.handler');
-
-            $publication = new Publication($doi);
-            $entityHandler->create($publication);
-
-            $publicationCitation = $citationStruct['citation'];
-            $publicationCitation->setPublication($publication);
-
-            $entityHandler->create($publicationCitation);
-            return new Response('');
-        }
-    }
-
-    /**
-     * Generate citations for datasets.
-     *
-     * @Route("/services/citation/dataset/{udi}")
-     * @Method("GET")
-     *
-     * @return Response A Symfony Response instance.
-     */
-    public function getCitationDatasetAction($udi)
-    {
-    }
-
-    /**
-     * Link Publications to Datasets.
-     *
-     * @Route("/services/plinker/{udi}/{doi}")
-     * @Method("LINK")
-     *
-     * @return Response A Symfony Response instance.
-     */
-    public function linkPublicationDatasetAction($udi, $doi)
-    {
-    }
 }
