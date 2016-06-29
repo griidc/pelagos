f493986c45c2593ac12dfe4fa0aa1d384379a0ca
James C. Davis <James.Davis@tamucc.edu>
2016-06-29 09:06:03 -0500

    Fixed up datasets template and added retrieval of datasets to controller.

--- src/Pelagos/Bundle/AppBundle/Controller/UI/DataDiscoveryController.php
+++ src/Pelagos/Bundle/AppBundle/Controller/UI/DataDiscoveryController.php
@@ -2,10 +2,15 @@
 
 namespace Pelagos\Bundle\AppBundle\Controller\UI;
 
+use Doctrine\ORM\Query;
+
 use Symfony\Component\HttpFoundation\Response;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+use Pelagos\Entity\Dataset;
+use Pelagos\Entity\DatasetSubmission;
 
 /**
  * The Data Discovery controller.
@@ -45,14 +50,76 @@ class DataDiscoveryController extends UIController
      */
     public function datasetsAction()
     {
+        $datasets = array();
+        $datasets['available'] = $this->getDatasets(
+            array(
+                'dataset.availabilityStatus' => array(
+                    DatasetSubmission::AVAILABILITY_STATUS_PUBLICLY_AVAILABLE,
+                    DatasetSubmission::AVAILABILITY_STATUS_PUBLICLY_AVAILABLE_REMOTELY_HOSTED,
+                )
+            )
+        );
+        $datasets['restricted'] = $this->getDatasets(
+            array(
+                'dataset.availabilityStatus' => array(
+                    DatasetSubmission::AVAILABILITY_STATUS_AVAILABLE_WITH_APPROVAL,
+                    DatasetSubmission::AVAILABILITY_STATUS_RESTRICTED,
+                    DatasetSubmission::AVAILABILITY_STATUS_AVAILABLE_WITH_APPROVAL_REMOTELY_HOSTED,
+                    DatasetSubmission::AVAILABILITY_STATUS_RESTRICTED_REMOTELY_HOSTED,
+                )
+            )
+        );
+        $datasets['inReview'] = $this->getDatasets(
+            array(
+                'dataset.availabilityStatus' => array(
+                    DatasetSubmission::AVAILABILITY_STATUS_PENDING_METADATA_APPROVAL,
+                )
+            )
+        );
+        $datasets['identified'] = $this->getDatasets(
+            array(
+                'dataset.availabilityStatus' => array(
+                    DatasetSubmission::AVAILABILITY_STATUS_NOT_AVAILABLE,
+                )
+            )
+        );
+
         return $this->render(
             'PelagosAppBundle:DataDiscovery:datasets.html.twig',
             array(
-                'unrestricted_datasets' => array(),
-                'restricted_datasets' => array(),
-                'md_under_review_datasets' => array(),
-                'identified_datasets' => array(),
+                'datasets' => $datasets,
             )
         );
+    }
+
+    /**
+     * Get datasets with properties matching any values specified by $criteria.
+     *
+     * @param array $criteria An array of criteria.
+     *
+     * @return array
+     */
+    protected function getDatasets(array $criteria)
+    {
+        $qb = $this->get('doctrine.orm.entity_manager')
+                   ->getRepository(Dataset::class)
+                   ->createQueryBuilder('dataset');
+        $qb->select('dataset, dif, datasetSubmission, metadata, researchGroup');
+        $qb->join('dataset.dif', 'dif');
+        $qb->join('dataset.datasetSubmission', 'datasetSubmission');
+        $qb->join('dataset.metadata', 'metadata');
+        $qb->join('dataset.researchGroup', 'researchGroup');
+        foreach ($criteria as $property => $values) {
+            foreach ($values as $value) {
+                $qb->orWhere(
+                    $qb->expr()->eq(
+                        $property,
+                        $qb->expr()->literal($value)
+                    )
+                );
+            }
+        }
+        $query = $qb->getQuery();
+        return $query->getResult(Query::HYDRATE_ARRAY);
     }
 }
