8cf8826867848fa7838f594c9e5b83ccfb4809ea
James C. Davis <James.Davis@tamucc.edu>
2016-07-15 15:13:26 -0500

    Added datasets, missing images, and fixed up template.

--- src/Pelagos/Bundle/AppBundle/Controller/UI/MdAppController.php
+++ src/Pelagos/Bundle/AppBundle/Controller/UI/MdAppController.php
@@ -6,6 +6,9 @@ use Symfony\Component\HttpFoundation\Response;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+use Pelagos\Entity\Dataset;
+use Pelagos\Entity\DatasetSubmission;
+
 /**
  * The MDApp controller.
  *
@@ -22,19 +25,33 @@ class MdAppController extends UIController
      */
     public function defaultAction()
     {
+        $entityHandler = $this->get('pelagos.entity.handler');
         return $this->render(
             'PelagosAppBundle:MdApp:main.html.twig',
             array(
                 'jiraBase' => 'https://triton.tamucc.edu/issues',
                 'm_dataset' => array(
-                    'submitted' => array(),
-                    'inreview' => array(),
-                    'secondcheck' => array(),
-                    'accepted' => array(),
-                    'backtosubmitter' => array(),
+                    'submitted' => $entityHandler->getBy(
+                        Dataset::class,
+                        array('metadataStatus' => DatasetSubmission::METADATA_STATUS_SUBMITTED)
+                    ),
+                    'inreview' => $entityHandler->getBy(
+                        Dataset::class,
+                        array('metadataStatus' => DatasetSubmission::METADATA_STATUS_IN_REVIEW)
+                    ),
+                    'secondcheck' => $entityHandler->getBy(
+                        Dataset::class,
+                        array('metadataStatus' => DatasetSubmission::METADATA_STATUS_SECOND_CHECK)
+                    ),
+                    'accepted' => $entityHandler->getBy(
+                        Dataset::class,
+                        array('metadataStatus' => DatasetSubmission::METADATA_STATUS_ACCEPTED)
+                    ),
+                    'backtosubmitter' => $entityHandler->getBy(
+                        Dataset::class,
+                        array('metadataStatus' => DatasetSubmission::METADATA_STATUS_BACK_TO_SUBMITTER)
+                    ),
                 ),
-                'baseUrl' => '',
-                'testPolygon' => '',
             )
         );
     }
