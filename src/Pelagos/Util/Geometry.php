c1e27c47ebdc1c3d49c7bc14fc1e265f13ecd633
Michael Scott Williamson <michael.williamson@tamucc.edu>
2016-07-18 13:31:23 -0500

    Original specification.

---
+++ src/Pelagos/Util/Geometry.php
@@ -0,0 +1,27 @@
+<?php
+namespace Pelagos\Util;
+
+class Geometry
+{
+    /**
+     * Calculate the envelope for a GML polygon.
+     *
+     * @param string $gml The GML polygon to calculate the envelope for.
+     *
+     * @return string WKT string for the envelope.
+     */
+    calculateEnvelopeFromGml($gml)
+    {
+    }
+
+    /**
+     * Calculate the GeographicBoundingBox for a GML polygon.
+     *
+     * @param string $gml The GML polygon to calculate the bounding box for.
+     *
+     * @return string XML String (gmd:EX_GeographicBoundingBox).
+     */
+    calculateGeographicBoundingBoxFromGml(string $gml)
+    {
+    }
+}
