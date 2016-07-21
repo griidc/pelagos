a2cb88649fb20217753636314ead379024f5e90b
James C. Davis <James.Davis@tamucc.edu>
2016-07-21 14:17:05 -0500

    Added download to dataland.

--- src/Pelagos/Bundle/AppBundle/Resources/public/js/dataland.js
+++ src/Pelagos/Bundle/AppBundle/Resources/public/js/dataland.js
@@ -49,15 +49,8 @@ var dlmap = new GeoViz();
         dlmap.initMap('dlolmap',{'onlyOneFeature':false,'allowModify':false,'allowDelete':false,'staticMap':false,'labelAttr':'udi'});
 
         $("#downloadds").button().click(function() {
-            // For now, just attempt to download.
-            window.location = Routing.generate("pelagos_app_ui_dataland_download", {"udi": udi});
-            /* Should eventually do this:
-            if ("dataset_download_status"  == "RemotelyHosted") {
-                showDatasetDownloadExternal(udi)
-            } else {
-                showDatasetDownload(udi)
-            }
-            */
+            var id = $("[datasetId]").attr("datasetId");
+            startDownload(id);
         });
 
         $("#download_dialog").dialog({
