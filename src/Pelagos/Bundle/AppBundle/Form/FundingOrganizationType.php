771a000f0840968713d460deb1b72118cd8e0df5
Michael Scott Williamson <michael.williamson@tamucc.edu>
2016-08-10 15:10:07 -0500

    Added int type to forms for sort order.

--- src/Pelagos/Bundle/AppBundle/Form/FundingOrganizationType.php
+++ src/Pelagos/Bundle/AppBundle/Form/FundingOrganizationType.php
@@ -4,6 +4,7 @@ namespace Pelagos\Bundle\AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
@@ -76,6 +77,11 @@ class FundingOrganizationType extends AbstractType
             ->add('country', TextType::class, array(
                 'label' => 'Country:',
                 'required' => false,
+            ))
+            ->add('sortOrder', IntegerType::class, array(
+                'label' => 'Sort Order:',
+                'required' => false,
+                'empty_data' => null,
             ));
     }
 
