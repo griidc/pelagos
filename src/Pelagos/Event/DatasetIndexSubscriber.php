5a16fb4c42038355359dff4264d2213edebf4afd
James C. Davis <James.Davis@tamucc.edu>
2016-09-16 06:27:51 -0500

    Added an event subscriber to populate calculated fields when indexing datasets

---
+++ src/Pelagos/Event/DatasetIndexSubscriber.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Pelagos\Event;
+
+use FOS\ElasticaBundle\Event\TransformEvent;
+use FOS\ElasticaBundle\Event\TypePopulateEvent;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+use Pelagos\Entity\Dataset;
+use Pelagos\Entity\Metadata;
+use Pelagos\Util\Geometry;
+
+/**
+ * An event subscriber for events related to the dataset index.
+ */
+class DatasetIndexSubscriber implements EventSubscriberInterface
+{
+    /**
+     * The Geometry utility service.
+     *
+     * @var Geometry
+     */
+    protected $geometryUtil;
+
+    /**
+     * Constructor.
+     *
+     * @param Geometry $geometryUtil The Geometry utility service.
+     */
+    public function __construct(Geometry $geometryUtil)
+    {
+        $this->geometryUtil = $geometryUtil;
+    }
+
+    /**
+     * Populate calculated fields in the Dataset index.
+     *
+     * @param TransformEvent $event The event that triggeref this.
+     *
+     * @return void
+     */
+    public function populateCalculatedFields(TransformEvent $event)
+    {
+        $dataset = $event->getObject();
+
+        if (!$dataset instanceof Dataset) {
+            return;
+        }
+
+        $document = $event->getDocument();
+
+        $wkt = null;
+        if ($dataset->hasMetadata()) {
+            $wkt = $dataset->getMetadata()->getGeometry();
+        } elseif ($dataset->hasDif() and null !== $dataset->getDif()->getSpatialExtentGeometry()) {
+            $wkt = $this->geometryUtil->convertGmlToWkt(
+                $dataset->getDif()->getSpatialExtentGeometry()
+            );
+        }
+        if (null !== $wkt) {
+            $document->set('geometry', $wkt);
+            $geometry = \geoPHP::load($wkt, 'wkt');
+            $simpleGeometry = $geometry->simplify(0.1);
+            // If the geometry couldn't be simplified.
+            if ($simpleGeometry->isEmpty()) {
+                // Set the original geometry as a GeoJSON array.
+                $document->set('simpleGeometry', json_decode($geometry->out('json'), true));
+            } else {
+                // Set the simpllified geometry as a GeoJSON array.
+                $document->set('simpleGeometry', json_decode($simpleGeometry->out('json'), true));
+            }
+        }
+
+        if (null !== $dataset->getDatasetSubmission()) {
+            $document->set('year', $dataset->getDatasetSubmission()->getCreationTimeStamp()->format('Y'));
+        }
+
+        if ($dataset->hasDif()) {
+            if (null !== $dataset->getDif()->getEstimatedStartDate()) {
+                $document->set('estimatedStartDate', $dataset->getDif()->getEstimatedStartDate()->format('Y-m-d'));
+            }
+            if (null !== $dataset->getDif()->getEstimatedEndDate()) {
+                $document->set('estimatedEndDate', $dataset->getDif()->getEstimatedEndDate()->format('Y-m-d'));
+            }
+        }
+    }
+
+    /**
+     * Get a list of the events this subscriber is subscribed to.
+     *
+     * @see EventSubscriberInterface::getSubscribedEvents
+     *
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            TransformEvent::POST_TRANSFORM => 'populateCalculatedFields',
+        );
+    }
+}
