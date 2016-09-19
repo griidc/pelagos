1836feae8642429a16bf8e6ac5cd55de92b052d9
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2016-09-19 14:04:18 -0500

    New listener to update dataset.
    This listener will update title and abstract when a dif, dataset submn, metadata are persisted.

---
+++ src/Pelagos/Event/DatasetListener.php
@@ -0,0 +1,38 @@
+<?php
+namespace Pelagos\Event;
+
+use Doctrine\ORM\Event\LifecycleEventArgs;
+
+use Pelagos\Entity\Dataset;
+use Pelagos\Entity\DatasetSubmission;
+use Pelagos\Entity\DIF;
+use Pelagos\Entity\Metadata;
+
+/**
+ * Listener class for Dataset related events.
+ */
+class DatasetListener
+{
+    /**
+     * Method to update dataset title and abstract when DIF dataset submission changes.
+     *
+     * @param EntityEvent $event Event being acted upon.
+     *
+     * @return void
+     */
+    public function postPersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        if ($entity instanceof DIF 
+            or $entity instanceof DatasetSubmission
+            or $entity instanceof Metadata
+            ) {
+            $dataset = $entity->getDataset();
+        
+            $dataset->updateTitle();
+            $dataset->updateAbstract();
+            $args->getEntityManager()->persist($dataset);
+            //$this->entityHandler->update($dataset);
+        }
+    }
+}
