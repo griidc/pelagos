811266a2dca1c8a382bac4f940e8ceb34ebfb71d
James C. Davis <James.Davis@tamucc.edu>
2016-10-27 16:38:40 -0500

    fix logic for determining when a new dataset file has been submitted

--- src/Pelagos/Bundle/AppBundle/Controller/UI/DatasetSubmissionController.php
+++ src/Pelagos/Bundle/AppBundle/Controller/UI/DatasetSubmissionController.php
@@ -243,6 +243,9 @@ class DatasetSubmissionController extends UIController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() and $form->isValid()) {
+
+            $this->processDatasetFileTransferDetails($form, $datasetSubmission);
+
             $datasetSubmission->submit();
 
             if ($datasetSubmission->getSequence() > 1) {
@@ -289,45 +292,27 @@ class DatasetSubmissionController extends UIController
      *
      * @param Form              $form              The submitted dataset submission form.
      * @param DatasetSubmission $datasetSubmission The Dataset Submission to update.
-     * @param string            $incomingDirectory The user's incoming directory.
      *
      * @return void
      */
     protected function processDatasetFileTransferDetails(
         Form $form,
-        DatasetSubmission $datasetSubmission,
-        $incomingDirectory
+        DatasetSubmission $datasetSubmission
     ) {
-        switch ($datasetSubmission->getDatasetFileTransferType()) {
-            case DatasetSubmission::TRANSFER_TYPE_UPLOAD:
-                $datasetFile = $form['datasetFile']->getData();
-                if ($datasetFile instanceof UploadedFile) {
-                    $originalFileName = $datasetFile->getClientOriginalName();
-                    $movedDatasetFile = $datasetFile->move($incomingDirectory, $originalFileName);
-                    $datasetSubmission->setDatasetFileUri('file://' . $movedDatasetFile->getRealPath());
-                    $this->newDatasetFile($datasetSubmission);
-                }
-                break;
-            case DatasetSubmission::TRANSFER_TYPE_SFTP:
-                $datasetFilePath = $form['datasetFilePath']->getData();
-                $newDatasetFileUri = empty($datasetFilePath) ? null : "file://$datasetFilePath";
-                if ($newDatasetFileUri !== $datasetSubmission->getDatasetFileUri()) {
-                    $datasetSubmission->setDatasetFileUri($newDatasetFileUri);
-                    $this->newDatasetFile($datasetSubmission);
-                } elseif ($form['datasetFileForceImport']->getData()) {
-                    $this->newDatasetFile($datasetSubmission);
-                }
-                break;
-            case DatasetSubmission::TRANSFER_TYPE_HTTP:
-                $datasetFileUrl = $form['datasetFileUrl']->getData();
-                $newDatasetFileUri = empty($datasetFileUrl) ? null : $datasetFileUrl;
-                if ($newDatasetFileUri !== $datasetSubmission->getDatasetFileUri()) {
-                    $datasetSubmission->setDatasetFileUri($newDatasetFileUri);
-                    $this->newDatasetFile($datasetSubmission);
-                } elseif ($form['datasetFileForceDownload']->getData()) {
-                    $this->newDatasetFile($datasetSubmission);
-                }
-                break;
+        // If there was a previous Dataset Submission.
+        if ($datasetSubmission->getDataset()->getDatasetSubmission() instanceof DatasetSubmission) {
+            // Get the previous datasetFileUri.
+            $previousDatasetFileUri = $datasetSubmission->getDataset()->getDatasetSubmission()->getDatasetFileUri();
+            // If the datasetFileUri has changed or the user has requested to force import or download.
+            if ($datasetSubmission->getDatasetFileUri() !== $previousDatasetFileUri
+                or $form['datasetFileForceImport']->getData()
+                or $form['datasetFileForceDownload']->getData()) {
+                // Assume the dataset file is new.
+                $this->newDatasetFile($datasetSubmission);
+            }
+        } else {
+            // This is the first submission so the dataset file is new.
+            $this->newDatasetFile($datasetSubmission);
         }
     }
 
