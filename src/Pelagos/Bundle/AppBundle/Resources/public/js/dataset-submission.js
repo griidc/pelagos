ce433cf7eb9c0fe3382e7fc282bf37bd2a66d5f6
James C. Davis <James.Davis@tamucc.edu>
2016-10-28 12:27:00 -0500

    use a 10 MB chunk size

--- src/Pelagos/Bundle/AppBundle/Resources/public/js/dataset-submission.js
+++ src/Pelagos/Bundle/AppBundle/Resources/public/js/dataset-submission.js
@@ -172,6 +172,7 @@ $(function() {
         },
         chunking: {
             enabled: true,
+            partSize: 10000000,
             concurrent: {
                 enabled: true
             },
