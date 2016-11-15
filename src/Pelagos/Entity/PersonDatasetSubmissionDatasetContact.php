1770181451b253ac095a9125f15e072324cff188
James C. Davis <James.Davis@tamucc.edu>
2016-11-15 10:09:42 -0600

    Add PersonDatasetSubmission to DatasetSubmission voter

--- src/Pelagos/Entity/PersonDatasetSubmissionDatasetContact.php
+++ src/Pelagos/Entity/PersonDatasetSubmissionDatasetContact.php
@@ -12,6 +12,11 @@ use Doctrine\ORM\Mapping as ORM;
 class PersonDatasetSubmissionDatasetContact extends PersonDatasetSubmission
 {
     /**
+     * A friendly name for this type of entity.
+     */
+    const FRIENDLY_NAME = 'Dataset Contact';
+
+    /**
      * The Dataset Submission for this association.
      *
      * @var DatasetSubmission
