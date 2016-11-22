23f7e222ea41f892c853a94e8b7298d35ed70fd3
Michael Scott Williamson <michael.williamson@tamucc.edu>
2016-11-22 16:14:12 -0600

    initial commit, added cmd line util w/hardcoded udi

---
+++ src/Pelagos/Bundle/AppBundle/Command/ManuallyMarkRemotelyHostedCommand.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Pelagos\Bundle\AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Pelagos\Entity\Dataset;
+use Pelagos\Entity\DatasetSubmission;
+
+/**
+ * Notify holders of accounts with soon to expire or expired passwords.
+ *
+ * @see ContainerAwareCommand
+ */
+class ManuallyMarkRemotelyHostedCommand extends ContainerAwareCommand
+{
+    /**
+     * The Symfony Console output object.
+     *
+     * @var OutputInterface
+     */
+    protected $output;
+
+    /**
+     * Configures the current command.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('dataset-submission:set-remotely-hosted')
+            ->setDescription('Manually set a dataset submission as Remotely Hosted.');
+    }
+
+    /**
+     * Executes the current command.
+     *
+     * @param InputInterface  $input  An InputInterface instance.
+     * @param OutputInterface $output An OutputInterface instance.
+     *
+     * @throws \Exception When dataset not found.
+     * @throws \Exception When datasetSubmission not found.
+     *
+     * @return integer Return 0 on success, or an error code otherwise.
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // This command takes no input.
+        unset($input);
+
+        // Save output object for use in other methods.
+        $this->output = $output;
+
+        $entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $datasets = $entityManager->getRepository('Pelagos\Entity\Dataset')->findBy(array('udi' => 'A1.x801.000:0014'));
+        $dataset = $datasets[0];
+
+        if (!($dataset instanceof Dataset)) {
+            throw new \Exception('Could not find dataset.');
+        }
+
+        $datasetSubmission = $dataset->getDatasetSubmission();
+        if (!($datasetSubmission instanceof DatasetSubmission)) {
+            throw new \Exception('Could not find Dataset Submission.');
+        }
+
+        $datasetSubmission->setDatasetFileTransferStatus(DatasetSubmission::TRANSFER_STATUS_REMOTELY_HOSTED);
+        $entityManager->persist($datasetSubmission);
+        $entityManager->persist($dataset);
+
+        return 0;
+    }
+}
