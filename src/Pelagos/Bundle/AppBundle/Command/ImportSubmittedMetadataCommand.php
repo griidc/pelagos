b258a1eafc3c6877a93af4541c2366a099b9900b
James C. Davis <James.Davis@tamucc.edu>
2017-01-30 12:26:54 -0600

    Mark metadata status as none when changing submission status to incomplete

--- src/Pelagos/Bundle/AppBundle/Command/ImportSubmittedMetadataCommand.php
+++ src/Pelagos/Bundle/AppBundle/Command/ImportSubmittedMetadataCommand.php
@@ -124,6 +124,7 @@ class ImportSubmittedMetadataCommand extends ContainerAwareCommand
                     $submissionTimeStampReflection = $datasetSubmissionReflection->getProperty('submissionTimeStamp');
                     $submissionTimeStampReflection->setAccessible(true);
                     $submissionTimeStampReflection->setValue($datasetSubmission, null);
+                    $datasetSubmission->setMetadataStatus(DatasetSubmission::METADATA_STATUS_NONE);
                     $output->writeln("  Marked dataset submission for $udi as incomplete.");
                     $counts['incompleted']++;
                 }
