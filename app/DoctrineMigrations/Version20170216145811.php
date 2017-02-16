e21c20bac5a79fdeae247d67789959623cef2f2b
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2017-02-16 15:06:51 -0600

    Removed Metadata Contact entity.
    Added migration to remove metadata contacts.

---
+++ app/DoctrineMigrations/Version20170216145811.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Pelagos\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170216145811 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DELETE FROM person_dataset_submission WHERE discr = \'persondatasetsubmissionmetadatacontact\'');
+
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+    }
+}
