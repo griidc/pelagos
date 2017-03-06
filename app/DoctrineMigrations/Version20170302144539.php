402d770d5bf10302c8e16860e6ca4ba9597acd52
Michael Scott Williamson <michael.williamson@tamucc.edu>
2017-03-06 14:42:40 -0600

    migrate existing DatasetContact persons into primaryDatasetContact

--- app/DoctrineMigrations/Version20170302144539.php
+++ app/DoctrineMigrations/Version20170302144539.php
@@ -6,12 +6,16 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
+ * Data migration that adds Primary Point of Contact to Dataset.
  */
 class Version20170302144539 extends AbstractMigration
 {
     /**
-     * @param Schema $schema
+     * The upgrade schema.
+     *
+     * @param Schema $schema The generated DB schema for DB upgrade.
+     *
+     * @return void
      */
     public function up(Schema $schema)
     {
@@ -22,10 +26,15 @@ class Version20170302144539 extends AbstractMigration
         $this->addSql('ALTER TABLE dataset_submission ADD CONSTRAINT FK_FEFE73FC8C423C05 FOREIGN KEY (primary_dataset_contact_id) REFERENCES person_dataset_submission (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_FEFE73FC8C423C05 ON dataset_submission (primary_dataset_contact_id)');
         $this->addSql('ALTER TABLE dataset_submission_audit ADD primary_dataset_contact_id INT DEFAULT NULL');
+        $this->addSql('UPDATE dataset_submission SET primary_dataset_contact_id = SELECT id FROM person_dataset_submission WHERE discr = \'persondatasetsubmissiondatasetcontact\' AND data_submission_id = dataset_submission.id');
     }
 
     /**
-     * @param Schema $schema
+     * The downgrade schema.
+     *
+     * @param Schema $schema The generated DB schema for DB downgrade.
+     *
+     * @return void
      */
     public function down(Schema $schema)
     {
