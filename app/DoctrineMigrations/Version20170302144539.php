3d3c1bbb4f073675e81c8223bc010263a844d5e5
Michael Scott Williamson <michael.williamson@tamucc.edu>
2017-03-07 11:38:34 -0600

    added constraints to migration subquery

--- app/DoctrineMigrations/Version20170302144539.php
+++ app/DoctrineMigrations/Version20170302144539.php
@@ -26,7 +26,7 @@ class Version20170302144539 extends AbstractMigration
         $this->addSql('ALTER TABLE dataset_submission ADD CONSTRAINT FK_FEFE73FC8C423C05 FOREIGN KEY (primary_dataset_contact_id) REFERENCES person_dataset_submission (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_FEFE73FC8C423C05 ON dataset_submission (primary_dataset_contact_id)');
         $this->addSql('ALTER TABLE dataset_submission_audit ADD primary_dataset_contact_id INT DEFAULT NULL');
-        $this->addSql('UPDATE dataset_submission SET primary_dataset_contact_id = SELECT id FROM person_dataset_submission WHERE discr = \'persondatasetsubmissiondatasetcontact\' AND data_submission_id = dataset_submission.id');
+        $this->addSql('UPDATE dataset_submission SET primary_dataset_contact_id = (SELECT id FROM person_dataset_submission WHERE discr = \'persondatasetsubmissiondatasetcontact\' AND person_dataset_submission.id IS NOT NULL AND dataset_submission_id = dataset_submission.id ORDER BY creation_time_stamp DESC LIMIT 1)');
     }
 
     /**
