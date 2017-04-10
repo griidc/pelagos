5a7f0a5e55c43c8122eab5bd91b987eac5ed7b09
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2017-04-10 15:38:17 -0500

    Added primaryContact to prototype, removed test CheckboxType

--- src/Pelagos/Bundle/AppBundle/Form/PersonDatasetSubmissionType.php
+++ src/Pelagos/Bundle/AppBundle/Form/PersonDatasetSubmissionType.php
@@ -8,7 +8,6 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
