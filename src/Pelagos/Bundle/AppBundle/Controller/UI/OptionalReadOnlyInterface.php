6d94f97079f608898407c09e515a289b1a6da0ce
Michael Scott Williamson <michael.williamson@tamucc.edu>
2017-05-11 14:11:48 -0500

    docblock on interface

--- src/Pelagos/Bundle/AppBundle/Controller/UI/OptionalReadOnlyInterface.php
+++ src/Pelagos/Bundle/AppBundle/Controller/UI/OptionalReadOnlyInterface.php
@@ -2,6 +2,9 @@
 
 namespace Pelagos\Bundle\AppBundle\Controller\UI;
 
+/**
+ * This empty interface simply serves as a designation of class needing selective read-only enforcement.
+ */
 interface OptionalReadOnlyInterface
 {
 }
