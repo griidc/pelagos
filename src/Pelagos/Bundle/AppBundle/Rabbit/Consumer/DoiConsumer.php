4f7e51bd0a5e66a2d53d8cd23d180021f5b298b1
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2017-06-07 12:17:54 -0500

    Added update function to DOI Consumer.

--- src/Pelagos/Bundle/AppBundle/Rabbit/Consumer/DoiConsumer.php
+++ src/Pelagos/Bundle/AppBundle/Rabbit/Consumer/DoiConsumer.php
@@ -31,21 +31,21 @@ class DoiConsumer implements ConsumerInterface
      * @var EntityManager
      */
     protected $entityManager;
-    
+
     /**
      * A Monolog logger.
      *
      * @var Logger
      */
     protected $logger;
-    
+
     /**
      * The entity event dispatcher.
      *
      * @var EntityEventDispatcher
      */
     protected $entityEventDispatcher;
-    
+
     /**
      * Constructor.
      *
@@ -62,7 +62,7 @@ class DoiConsumer implements ConsumerInterface
         $this->logger = $logger;
         $this->entityEventDispatcher = $entityEventDispatcher;
     }
-    
+
     /**
      * Process a filer message.
      *
@@ -87,7 +87,7 @@ class DoiConsumer implements ConsumerInterface
         if (null !== $dataset->getUdi()) {
             $loggingContext['udi'] = $dataset->getUdi();
         }
-        
+
         // @codingStandardsIgnoreStart
         $routingKey = $message->delivery_info['routing_key'];
         // @codingStandardsIgnoreEnd
@@ -96,7 +96,7 @@ class DoiConsumer implements ConsumerInterface
         } elseif (preg_match('/^publish/', $routingKey)) {
             //$this->publishDoi($dataset, $loggingContext);
         } elseif (preg_match('/^update/', $routingKey)) {
-            //$this->updateDoi($dataset, $loggingContext);
+            $this->updateDoi($dataset, $loggingContext);
         } else {
             $this->logger->warning("Unknown routing key: $routingKey", $loggingContext);
             return true;
@@ -105,7 +105,7 @@ class DoiConsumer implements ConsumerInterface
         $this->entityManager->flush();
         return true;
     }
-    
+
     /**
      * Issue a DOI for the dataset.
      *
@@ -118,35 +118,81 @@ class DoiConsumer implements ConsumerInterface
     {
         // Log processing start.
         $this->logger->info('Attempting to issue DOI', $loggingContext);
-        
-        $datasetSubmission = $dataset->getDatasetSubmission();
-        if (!$datasetSubmission instanceof DatasetSubmission) {
-            $this->logger->warning('No submission found for dataset', $loggingContext);
+
+        $doi = $dataset->getDoi();
+        if ($doi instanceof DOI) {
+            $this->logger->warning('The DOI already exist for dataset', $loggingContext);
+            //Update a the DOI instead.
+            $this->updateDoi($dataset, $loggingContext);
             return true;
         }
-        $loggingContext['dataset_submission_id'] = $datasetSubmission->getId();
         
         try {
             $doiUtil = new DOIutil();
             $issuedDoi = $doiUtil->createDOI(
                 'https://data.gulfresearchinitiative.org/data/' . $dataset->getUdi(),
-                $datasetSubmission->getAuthors(),
-                $datasetSubmission->getTitle(),
+                $dataset->getAuthors(),
+                $dataset->getTitle(),
                 'Harte Research Institute',
-                $datasetSubmission->getReferenceDate()->format('Y')
+                $dataset->getReferenceDateYear()
             );
         } catch (\Exception $exception) {
             $this->logger->error('Error requesting DOI: ' . $exception->getMessage(), $loggingContext);
             return;
         }
-        
+
         $doi = new DOI($issuedDoi);
         $dataset->setDoi($doi);
-        
+
         $loggingContext['doi'] = $doi;
         // Dispatch entity event.
         $this->entityEventDispatcher->dispatch($dataset, 'doi_issued');
         // Log processing complete.
         $this->logger->info('DOI Issued', $loggingContext);
+
+        return true;
+    }
+
+    /**
+     * Update information for the DOI of a dataset.
+     *
+     * @param Dataset $dataset        The Dataset.
+     * @param array   $loggingContext The logging context to use when logging.
+     *
+     * @return boolean True if success, false otherwise.
+     */
+    protected function updateDoi(Dataset $dataset, array $loggingContext)
+    {
+        // Log processing start.
+        $this->logger->info('Attempting to update DOI', $loggingContext);
+
+        $doi = $dataset->getDoi();
+        if (!$doi instanceof DOI) {
+            $this->logger->warning('No DOI found for dataset', $loggingContext);
+            //Create a new DOI instead.
+            $this->issueDoi($dataset, $loggingContext);
+            return true;
+        }
+
+        try {
+            $doiUtil = new DOIutil();
+            $issuedDoi = $doiUtil->updateDOI(
+                $doi->getDoi(),
+                'https://data.gulfresearchinitiative.org/data/' . $dataset->getUdi(),
+                $dataset->getAuthors(),
+                $dataset->getTitle(),
+                'Harte Research Institute',
+                $dataset->getReferenceDateYear()
+            );
+        } catch (\Exception $exception) {
+            $this->logger->error('Error requesting DOI: ' . $exception->getMessage(), $loggingContext);
+            return;
+        }
+
+        $loggingContext['doi'] = $doi;
+        // Dispatch entity event.
+        $this->entityEventDispatcher->dispatch($dataset, 'doi_updated');
+        // Log processing complete.
+        $this->logger->info('DOI Updated', $loggingContext);
     }
 }
