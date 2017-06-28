1179960f25f7a2041fdb8e5dabd45bca8bb100d8
Michael Scott Williamson <michael.williamson@tamucc.edu>
2017-06-28 11:27:14 -0500

    added cmd to set appr to published

---
+++ src/Pelagos/Bundle/AppBundle/Command/RabbitPublishAllApprovedNonRestrictedCommand.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Pelagos\Bundle\AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Pelagos\Entity\Dataset;
+use Pelagos\Entity\DatasetSubmission;
+use Pelagos\Entity\DIF;
+use Pelagos\Entity\DOI;
+
+/**
+ * This command publishes a rabbit message for every accepted dataset forcing update of DOI info.
+ *
+ * @see ContainerAwareCommand
+ */
+class RabbitPublishAllApprovedNonRestrictedCommand extends ContainerAwareCommand
+{
+    /**
+     * The Symfony Console output object.
+     *
+     * @var OutputInterface
+     */
+    protected $output;
+
+    /**
+     * Configures the current command.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('dataset-doi:pub-all-appr-nonres-datasets')
+            ->setDescription('Make DOI public for all approved, non-restricted datasets.');
+    }
+
+    /**
+     * Executes the current command.
+     *
+     * @param InputInterface  $input  An InputInterface instance.
+     * @param OutputInterface $output An OutputInterface instance.
+     *
+     * @throws \Exception When dataset not found.
+     * @throws \Exception When datasetSubmission not found.
+     *
+     * @return integer Return 0 on success, or an error code otherwise.
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $datasets = $entityManager->getRepository('Pelagos\Entity\Dataset')->findBy(array(
+            'identifiedStatus' => DIF::STATUS_APPROVED,
+            'availabilityStatus' => array(
+                DatasetSubmission::AVAILABILITY_STATUS_PUBLICLY_AVAILABLE,
+                DatasetSubmission::AVAILABILITY_STATUS_PUBLICLY_AVAILABLE_REMOTELY_HOSTED,
+            )
+        ));
+
+        $thumper = $this->getContainer()->get('old_sound_rabbit_mq.doi_issue_producer');
+        foreach ($datasets as $dataset) {
+            if ($dataset->getDoi()->getStatus() == DOI::STATUS_RESERVED) {
+                #$thumper->publish($dataset->getId(), 'pub');
+                echo 'Requesting DOI publish update for dataset ' . $dataset->getId() . ' (' . $dataset->getUdi() . ")\n";
+            }
+        }
+
+        return 0;
+    }
+}
