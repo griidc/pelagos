b2db804678717b971a0f03211914bdb1be3fb284
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2017-09-05 14:49:24 -0500

    Made where clause variables at constants with references to their classes.
    Also added "admin only" restriction.

--- src/Pelagos/Bundle/AppBundle/Controller/UI/GomriReportController.php
+++ src/Pelagos/Bundle/AppBundle/Controller/UI/GomriReportController.php
@@ -16,6 +16,7 @@ use \DateTime;
 use \DateInterval;
 
 const MONTH_DAY_FORMAT = 'M Y';
+const GOMRI_STRING = 'Gulf of Mexico Research Initiative (GoMRI)';
 
 /**
  * The GOMRI datasets report generator.
@@ -33,6 +34,10 @@ class GomriReportController extends UIController
      */
     public function defaultAction()
     {
+        if (!$this->isGranted('ROLE_DATA_REPOSITORY_MANAGER')) {
+            return $this->render('PelagosAppBundle:template:AdminOnly.html.twig');
+        }
+
         $container = $this->container;
         $response = new StreamedResponse(function () use ($container) {
             // final results array.
@@ -58,9 +63,15 @@ class GomriReportController extends UIController
                 'FROM ' . Dataset::class . ' dataset ' .
                 'JOIN dataset.dif dif ' .
                 'JOIN dataset.researchGroup researchgroup ' .
-                'WHERE researchgroup.fundingCycle < 700 ' .
-                'AND dif.status = 2';
+                'JOIN researchgroup.fundingCycle fundingCycle ' .
+                'JOIN fundingCycle.fundingOrganization fundingOrganization ' .
+                'WHERE fundingOrganization.name = :gomri ' .
+                'AND dif.status = :difStatusApproved';
             $query = $entityManager->createQuery($queryString);
+            $query->setParameters(array(
+                'difStatusApproved' => DIF::STATUS_APPROVED,
+                'gomri' => GOMRI_STRING,
+            ));
             $results = $query->getResult();
 
             foreach ($results as $result) {
@@ -73,13 +84,18 @@ class GomriReportController extends UIController
                 'FROM ' . DatasetSubmission::class . ' datasetsubmission ' .
                 'JOIN datasetsubmission.dataset dataset ' .
                 'JOIN dataset.researchGroup researchgroup ' .
+                'JOIN researchgroup.fundingCycle fundingCycle ' .
+                'JOIN fundingCycle.fundingOrganization fundingOrganization ' .
                 'WHERE datasetsubmission IN ' .
                 '   (SELECT MIN(subdatasetsubmission.id)' .
                 '   FROM ' . DatasetSubmission::class . ' subdatasetsubmission' .
                 '   WHERE subdatasetsubmission.datasetFileUri IS NOT null ' .
                 '   GROUP BY subdatasetsubmission.dataset)' .
-                'AND researchgroup.fundingCycle < 700 ';
+                'AND fundingOrganization.name = :gomri ';
             $query = $entityManager->createQuery($queryString);
+            $query->setParameters(array(
+                'gomri' => GOMRI_STRING,
+            ));
             $results = $query->getResult();
 
             foreach ($results as $result) {
@@ -92,17 +108,26 @@ class GomriReportController extends UIController
                 'FROM ' . DatasetSubmission::class . ' datasetsubmission ' .
                 'JOIN datasetsubmission.dataset dataset ' .
                 'JOIN dataset.researchGroup researchgroup ' .
+                'JOIN researchgroup.fundingCycle fundingCycle ' .
+                'JOIN fundingCycle.fundingOrganization fundingOrganization ' .
                 'WHERE datasetsubmission IN ' .
                 '(SELECT MIN(subdatasetsubmission.id) ' .
                 '   FROM ' . DatasetSubmission::class . ' subdatasetsubmission' .
                 '   WHERE subdatasetsubmission.datasetFileUri is not null ' .
-                "   AND subdatasetsubmission.metadataStatus = 'Accepted' " .
-                "   AND subdatasetsubmission.restrictions = 'None' " .
-                "   AND (subdatasetsubmission.datasetFileTransferStatus = 'Completed' " .
-                "       OR subdatasetsubmission.datasetFileTransferStatus = 'RemotelyHosted') " .
+                '   AND subdatasetsubmission.metadataStatus = :metadataStatus ' .
+                '   AND subdatasetsubmission.restrictions = :restrictions ' .
+                '   AND (subdatasetsubmission.datasetFileTransferStatus = :fileTransferStatusCompleted ' .
+                '       OR subdatasetsubmission.datasetFileTransferStatus = :fileTransferStatusRemotelyHosted) ' .
                 '   GROUP BY subdatasetsubmission.dataset)' .
-                'AND researchgroup.fundingCycle < 700 ';
+                'AND fundingOrganization.name = :gomri';
             $query = $entityManager->createQuery($queryString);
+            $query->setParameters(array(
+                'metadataStatus' => DatasetSubmission::METADATA_STATUS_ACCEPTED,
+                'restrictions' => DatasetSubmission::RESTRICTION_NONE,
+                'fileTransferStatusCompleted' => DatasetSubmission::TRANSFER_STATUS_COMPLETED,
+                'fileTransferStatusRemotelyHosted' => DatasetSubmission::TRANSFER_STATUS_REMOTELY_HOSTED,
+                'gomri' => GOMRI_STRING,
+            ));
             $results = $query->getResult();
 
             foreach ($results as $result) {
