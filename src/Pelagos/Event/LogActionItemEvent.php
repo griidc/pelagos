e26a34bf6cc2409da9674875e066311510628b60
Son Nguyen <Son.Nguyen@tamucc.edu>
2017-09-22 14:03:53 -0500

    Remove GenericDispatcher and DownloadEvent, Add LogActionItemEventDispatcher and LogActionItemEvent

---
+++ src/Pelagos/Event/LogActionItemEvent.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Pelagos\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Class for dataset download events.
+ */
+class LogActionItemEvent extends Event
+{
+    /**
+     * Name of the Action.
+     *
+     * @var Dataset
+     */
+    protected $actionName;
+
+    /**
+     * The name of this event's subject if it is an entity.
+     *
+     * @var string
+     */
+    protected $subjectEntityName;
+
+    /**
+     * The Entity Id if the subjectEntityName is not null.
+     *
+     * @var integer
+     */
+    protected $subjectEntityId;
+
+    /**
+     * The additional information for this event.
+     *
+     * @var array
+     */
+    protected $payLoad = array();
+
+    /**
+     * Constructor.
+     *
+     * @param string  $actionName        The name of the action this event is for.
+     * @param string  $subjectEntityName The subject name it is an entity.
+     * @param integer $subjectEntityId   The subject Id if subject entity name exists.
+     * @param array   $payLoad           The additional information for the event.
+     */
+    public function __construct($actionName, $subjectEntityName = null, $subjectEntityId = null, array $payLoad = null)
+    {
+        $this->$actionName = $actionName;
+        if ($subjectEntityName != null) {
+            $this->subjectEntityName = $subjectEntityName;
+            if ($subjectEntityId != null) {
+                $this->subjectEntityId = $subjectEntityId;
+            }
+        }
+        if ($payLoad != null) {
+            $this->payLoad = $payLoad;
+        }
+    }
+
+    /**
+     * Getter for actionName.
+     *
+     * @access public
+     *
+     * @return string Name of the action of the log action item event
+     */
+    public function getActionName()
+    {
+        return $this->actionName;
+    }
+
+    /**
+     * Getter for subjectEntityName.
+     *
+     * @access public
+     *
+     * @return string subject entity name of the log action item event
+     */
+    public function getSubjectEntityName()
+    {
+        return $this->subjectEntityName;
+    }
+
+    /**
+     * Getter for subjectEntityId.
+     *
+     * @access public
+     *
+     * @return integer subject entity Id of the log action item event
+     */
+    public function getSubjectEntityId()
+    {
+        return $this->subjectEntityId;
+    }
+
+    /**
+     * Getter for payLoad.
+     *
+     * @access public
+     *
+     * @return array additional information for the log action item event(json_array)
+     */
+    public function getPayLoad()
+    {
+        return $this->payLoad;
+    }
+}
---
+++ src/Pelagos/Event/LogActionItemEventDispatcher.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Pelagos\Event;
+
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+
+/**
+ * A generic dispatcher.
+ */
+class LogActionItemEventDispatcher
+{
+  /**
+   * The event dispatcher to use in this entity event dispatcher.
+   *
+   * @var EventDispatcherInterface
+   */
+    private $eventDispatcher;
+
+  /**
+   * Constructor.
+   *
+   * @param EventDispatcherInterface $eventDispatcher The event dispatcher to use.
+   */
+    public function __construct(EventDispatcherInterface $eventDispatcher)
+    {
+        $this->eventDispatcher = $eventDispatcher;
+    }
+
+  /**
+   * Dispatch an event.
+   *
+   * @param mixed  $data      The payload the event is for.
+   * @param string $eventName The name of the event.
+   *
+   * @return void
+   */
+    public function dispatch($data, $eventName)
+    {
+        $this->eventDispatcher->dispatch(
+            $eventName,
+            new LogActionItemEvent(
+                $data['actionName'],
+                $data['subjectEntityName'],
+                $data['subjectEntityId'],
+                $data['payLoad'],
+                $eventName
+            )
+        );
+        dump($data);
+    }
+}
