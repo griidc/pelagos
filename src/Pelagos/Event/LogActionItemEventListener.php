c6dbc2b2142e085744564d3bc883f08fa84a236e
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2017-09-28 14:30:24 -0500

    Changed entity handler to entity manager.

--- src/Pelagos/Event/LogActionItemEventListener.php
+++ src/Pelagos/Event/LogActionItemEventListener.php
@@ -1,13 +1,33 @@
 <?php
 namespace Pelagos\Event;
 
+use Doctrine\ORM\EntityManager;
+
 use Pelagos\Entity\LogActionItem;
+use Pelagos\Entity\Person;
 
 /**
  * Listener class for LogActionItem-related events.
  */
 class LogActionItemEventListener extends EventListener
 {
+    /**
+     * The entity manager.
+     *
+     * @var EntityManager
+     */
+    protected $entityManager;
+
+    /**
+     * This is the class constructor to handle dependency injections.
+     *
+     * @param EntityManager $entityManager The entity manager.
+     */
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * Method to persist a new LogAcionItem object to the database.
      *
@@ -23,6 +43,9 @@ class LogActionItemEventListener extends EventListener
             $event->getSubjectEntityID(),
             $event->getPayLoad()
         );
-        $this->entityHandler->create($logActionItem);
+        $systemPerson = $this->entityManager->find(Person::class, 0);
+        $logActionItem->setCreator($systemPerson);
+        $this->entityManager->persist($logActionItem);
+        $this->entityManager->flush();
     }
 }
