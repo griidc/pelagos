db7a0ed8c2aa7e483958252b0c74a99a168649fc
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2017-10-04 13:45:53 -0500

    Reverted uneeded use.

--- src/Pelagos/Bundle/AppBundle/Controller/UI/DataDiscoveryController.php
+++ src/Pelagos/Bundle/AppBundle/Controller/UI/DataDiscoveryController.php
@@ -4,7 +4,6 @@ namespace Pelagos\Bundle\AppBundle\Controller\UI;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\StreamedResponse;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
