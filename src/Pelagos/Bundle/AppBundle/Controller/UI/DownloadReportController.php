82504ac46cdd59f815b9ba717944ba4ec3c53ce8
Son Nguyen <Son.Nguyen@tamucc.edu>
2017-10-10 10:35:44 -0500

    Create ReportController abstract class, refactor downloadreportcontroller

--- src/Pelagos/Bundle/AppBundle/Controller/UI/DownloadReportController.php
+++ src/Pelagos/Bundle/AppBundle/Controller/UI/DownloadReportController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\StreamedResponse;
+
 use Pelagos\Exception\InvalidDateSelectedException;
 use Pelagos\Entity\Dataset;
 use Pelagos\Entity\DatasetSubmission;
@@ -17,16 +17,14 @@ use Pelagos\Entity\Metadata;
 
 use Pelagos\Util\ISOMetadataExtractorUtil;
 
-use \DateTime;
-
 /**
  * The dataset download report generator.
  *
  * @Route("/download-report")
  */
-class DownloadReportController extends UIController
+class DownloadReportController extends ReportController
 {
-    //Date Time format used in the csv report
+    //DateTime format used for date range
     const INREPORT_DATETIMEFORMAT = 'm-d-Y';
 
     /**
@@ -72,18 +70,35 @@ class DownloadReportController extends UIController
                     'attr' => array(
                         'placeholder' => 'yyyy-mm-dd',
                         'class' => 'endDate',
-                        )
+                    )
                 )
             )
             ->add('submit', SubmitType::class, array('label' => 'Generate Report'))->getForm();
-        $form->handleRequest($request);
+         $form->handleRequest($request);
 
         if ($form->isValid()) {
             $startDate = $form->getData()['startDate'];
             $endDate = $form->getData()['endDate'];
             if ($startDate && $endDate) {
                 if ($startDate <= $endDate) {
-                    return $this->downloadReport($startDate, $endDate);
+                    $optionalHeaders = [
+                        'Start Date' => $startDate->format(self::INREPORT_DATETIMEFORMAT),
+                        'End Date' => $endDate->format(self::INREPORT_DATETIMEFORMAT)
+                    ];
+                    $labels = [
+                        'UDI',
+                        'Title',
+                        'Primary Point Of Contact',
+                        'Primary Point Of Contact Email',
+                        'Total Downloads',
+                        '# of GoMRI downloads',
+                        '# of Google downloads'
+                    ];
+                    $data = $this->queryData([
+                        'startDate' => $startDate,
+                        'endDate' => $endDate
+                    ]);
+                    return $this->writeCsvResponse($labels, $data, null, $optionalHeaders);
                 }
             } else {
                 throw new InvalidDateSelectedException('The dates selected are invalid.');
@@ -96,51 +111,62 @@ class DownloadReportController extends UIController
     }
 
     /**
-     * Running query, doing calculation for number of download, and create the Streamed Response.
+     * This implements the abstract method from ReportController to get the data.
      *
-     * @param DateTime $startDate First day of the report.
-     * @param DateTime $endDate   Last day of the report (inclusively).
+     * @param array|NULL $options Additional parameters needed to run the query.
      *
-     * @return StreamedResponse A csv file.
+     * @return array  Return an indexed array.
      */
-    public function downloadReport(DateTime $startDate, DateTime $endDate)
+    protected function queryData(array $options = null)
     {
         $container = $this->container;
-        $response = new StreamedResponse(function () use ($container, $startDate, $endDate) {
-            $entityManager = $container->get('doctrine')->getManager();
-            $endDateInclusively = clone $endDate;
-            $endDateInclusively->add(new \DateInterval('P1D'));
-            //Query
-            $queryString = 'SELECT dataset.udi,log.payLoad from ' .
-                LogActionItem::class . ' log join ' . Dataset::class . ' dataset with
-                log.subjectEntityId = dataset.id where log.actionName = :actionName and
-                log.subjectEntityName = :subjectEntityName and 
-                log.creationTimeStamp >= :startDate 
-                and log.creationTimeStamp <= :endDate order by dataset.udi ASC';
+        $entityManager = $container->get('doctrine')->getManager();
+        //Query
+        $queryString = 'SELECT dataset.udi,log.payLoad from ' .
+            LogActionItem::class . ' log join ' . Dataset::class . ' dataset with
+                  log.subjectEntityId = dataset.id where log.actionName = :actionName and
+                  log.subjectEntityName = :subjectEntityName and 
+                  log.creationTimeStamp >= :startDate 
+                  and log.creationTimeStamp <= :endDate order by dataset.udi ASC';
 
-            $query = $entityManager->createQuery($queryString);
-            $query->setParameters([
-                'actionName' => 'File Download',
-                'subjectEntityName' => 'Pelagos\Entity\Dataset',
-                'startDate' => $startDate,
-                'endDate' => $endDateInclusively
-            ]);
-            $results = $query->getResult();
+        $query = $entityManager->createQuery($queryString);
+        $endDateInclusively = clone $options['endDate'];
+        $endDateInclusively = $endDateInclusively->add(new \DateInterval('P1D'));
+        $query->setParameters([
+            'actionName' => 'File Download',
+            'subjectEntityName' => 'Pelagos\Entity\Dataset',
+            'startDate' => $options['startDate'],
+            'endDate' => $endDateInclusively
+        ]);
+        $results = $query->getResult();
 
-            $finalArray = [];
-            foreach ($results as $result) {
-                //initialize array with key  = udi, set title and primary POC
-                if (!array_key_exists($result['udi'], $finalArray)) {
-                    $finalArray[$result['udi']] = [];
-                    $finalArray[$result['udi']]['totalCount'] = 0;
-                    $finalArray[$result['udi']]['GoMRI'] = 0;
-                    $finalArray[$result['udi']]['NonGoMRI'] = 0;
-                    $dataset = $this->entityHandler->getBy(Dataset::class, ['udi' => $result['udi']])[0];
-                    $finalArray[$result['udi']]['title'] = $dataset->getTitle();
-                    //get Primary point of contact from the XML
-                    $datasetSubmission = $dataset->getDatasetSubmission();
-                    if ($datasetSubmission instanceof DatasetSubmission
-                        and $dataset->getMetadata() instanceof Metadata) {
+        //process result query into an array with organized data
+        $finalArray = array();
+        $currentIndex = 0;
+        foreach ($results as $result) {
+            //initialize array with key  = udi, set title and primary POC
+            if (isset($finalArray[$currentIndex]['udi']) && $result['udi'] != $finalArray[$currentIndex]['udi']) {
+                $currentIndex++;
+            }
+            if (!isset($finalArray[$currentIndex])) {
+                $finalArray[$currentIndex] = array(
+                    'udi' => $result['udi'],
+                    'title' => null,
+                    'primaryPointOfContact' => null,
+                    'primaryPointOfContactEmail' => null,
+                    'totalCount' => 0,
+                    'GoMRI' => 0,
+                    'NonGoMRI' => 0,
+                );
+
+                $dataset = $this->container->get('doctrine')->getRepository(Dataset::class)
+                    ->findOneBy(array('udi' => $result['udi']));
+
+                $finalArray[$currentIndex]['title'] = $dataset->getTitle();
+                //get Primary point of contact from the XML
+                $datasetSubmission = $dataset->getDatasetSubmission();
+                if ($datasetSubmission instanceof DatasetSubmission
+                    and $dataset->getMetadata() instanceof Metadata) {
                         $datasetSubmission->getDatasetContacts()->clear();
                         ISOMetadataExtractorUtil::populateDatasetSubmissionWithXMLValues(
                             $dataset->getMetadata()->getXml(),
@@ -148,65 +174,23 @@ class DownloadReportController extends UIController
                             $this->getDoctrine()->getManager()
                         );
                         $dataset->setDatasetSubmission($datasetSubmission);
-                        $finalArray[$result['udi']]['primaryPointOfContact']
+                        $finalArray[$currentIndex]['primaryPointOfContact']
                             = $dataset->getPrimaryPointOfContact()->getLastName() .
                             ', ' . $dataset->getPrimaryPointOfContact()->getFirstName();
-                        $finalArray[$result['udi']]['primaryPointOfContactEmail']
-                            = $dataset->getPrimaryPointOfContact()->getEmailAddress();
-                    }
+                            $finalArray[$currentIndex]['primaryPointOfContactEmail']
+                                = $dataset->getPrimaryPointOfContact()->getEmailAddress();
                 }
-                //count user downloads and total download
-                if ($result['payLoad']['userType'] == 'GoMRI') {
-                    $finalArray[$result['udi']]['GoMRI']++;
-                } else {
-                    if ($result['payLoad']['userType'] == 'Non-GoMRI') {
-                        $finalArray[$result['udi']]['NonGoMRI']++;
-                    }
+            }
+        //count user downloads and total download
+            if ($result['payLoad']['userType'] == 'GoMRI') {
+                $finalArray[$currentIndex]['GoMRI']++;
+            } else {
+                if ($result['payLoad']['userType'] == 'Non-GoMRI') {
+                    $finalArray[$currentIndex]['NonGoMRI']++;
                 }
-                  $finalArray[$result['udi']]['totalCount']++;
             }
-
-            //write csv file
-            $handle = fopen('php://output', 'r+');
-
-            // Add header to CSV.
-            $createdTime = new DateTime('now');
-            fputcsv($handle, ['From', $startDate->format(self::INREPORT_DATETIMEFORMAT)]);
-            fputcsv($handle, ['To', $endDate->format(self::INREPORT_DATETIMEFORMAT)]);
-            fputcsv($handle, [
-                'Created at',
-                $createdTime->format(self::INREPORT_DATETIMEFORMAT)
-            ]);
-            fputcsv(
-                $handle,
-                [
-                    'UDI',
-                    'Title',
-                    'Primary Point Of Contact',
-                    'Primary Point Of Contact Email',
-                    'Total Downloads',
-                    '# of GoMRI downloads',
-                    '# of Google downloads'
-                ]
-            );
-            foreach ($finalArray as $udi => $value) {
-                fputcsv($handle, [
-                    $udi,
-                    $value['title'],
-                    $value['primaryPointOfContact'],
-                    $value['primaryPointOfContactEmail'],
-                    $value['totalCount'],
-                    $value['GoMRI'],
-                    $value['NonGoMRI']
-                ]);
-            }
-            fclose($handle);
-        });
-        //generate report filename
-        $now = new DateTime('now');
-        $fileName = 'downloadReport-' . $now->format('Y-m-d') . '.csv';
-        $response->headers->set('Content-Type', 'text/csv');
-        $response->headers->set('Content-Disposition', 'attachment; filename="' . $fileName . '"');
-        return $response;
+            $finalArray[$currentIndex]['totalCount']++;
+        }
+        return $finalArray;
     }
 }
---
+++ src/Pelagos/Bundle/AppBundle/Controller/UI/ReportController.php
@@ -0,0 +1,85 @@
+<?php
+namespace Pelagos\Bundle\AppBundle\Controller\UI;
+
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use \DateTime;
+
+/**
+ * This abstract controller provide a template for reports.
+ *
+ * @package Pelagos\Bundle\AppBundle\Controller\UI
+ */
+abstract class ReportController extends UIController
+{
+    //Datetime format used to put the date and time in the report file name
+    const FILENAME_DATETIMEFORMAT = 'Y-m-d';
+    //Timestamp format used to put the date and time in the report
+    const INREPORT_TIMESTAMPFORMAT = 'm-d-Y H:i';
+
+    /**
+     * This abstract function is where the data in the csv is generated.
+     *
+     * @param array|NULL $options Additional parameters needed to run the query.
+     *
+     * @return array  Return an indexed array.
+     */
+    abstract protected function queryData(array $options = null);
+
+    /**
+     * Write headers, labels, data to a csv response with a default or custom filename.
+     *
+     * @param array       $labels          Label row.
+     * @param array       $data            Data rows.
+     * @param string|NULL $customFileName  Non-generic csv output filename if needed.
+     * @param array|NULL  $optionalHeaders Additional information to add in the reports.
+     *
+     * @return \StreamedResponse The csv response.
+     */
+    protected function writeCsvResponse(
+        array $labels,
+        array $data,
+        $customFileName = null,
+        array $optionalHeaders = null
+    ) {
+        //generic report name extracted from the controller's name
+        $reportNameCamelCase = preg_replace('/Controller$/', '', (new \ReflectionClass($this))->getShortName());
+
+        $response = new StreamedResponse(function () use ($labels, $data, $optionalHeaders, $reportNameCamelCase) {
+            $handle = fopen('php://output', 'r+');
+            //default headers
+            fputcsv(
+                $handle,
+                array(strtoupper(preg_replace('/(?<!\ )[A-Z]/', ' $0', $reportNameCamelCase)))
+            );
+            fputcsv($handle, array(
+                'Created at', (new DateTime('now'))->format(self::INREPORT_TIMESTAMPFORMAT)));
+            fputcsv($handle, array());
+
+            // write additional options to the csv.
+            if ($optionalHeaders != null) {
+                foreach ($optionalHeaders as $key => $value) {
+                    fputcsv($handle, array($key, $value));
+                }
+            }
+            //write header
+            fputcsv($handle, array());
+            fputcsv($handle, $labels);
+            //write data
+            foreach ($data as $row) {
+                fputcsv($handle, $row);
+            }
+            fclose($handle);
+        });
+
+        //set filename: default file name extracted from the controller's name or custom filename
+        if ($customFileName == null) {
+            $defaultFileName = $reportNameCamelCase . '-' . (new DateTime('now'))
+                ->format(self::FILENAME_DATETIMEFORMAT) . '.csv';
+            $response->headers->set('Content-Disposition', 'attachment; filename="' . $defaultFileName . '"');
+        } else {
+            $response->headers->set('Content-Disposition', 'attachment; filename="' . $customFileName . '"');
+        }
+        $response->headers->set('Content-Type', 'text/csv');
+        return $response;
+    }
+}
