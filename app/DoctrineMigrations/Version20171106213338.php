7d2d3946a06bd4b925db06a1777fa6f123134586
Praneeth Pondicherry Ravendernath <Praneeeth.Pondicherry@tamucc.edu>
2017-11-06 16:43:04 -0600

    Deleted the unwanted migration and refactored the other one.

--- app/DoctrineMigrations/Version20171106213338.php
+++ app/DoctrineMigrations/Version20171106213338.php
@@ -23,14 +23,11 @@ class Version20171106213338 extends AbstractMigration
         $this->addSql('ALTER TABLE dataset_submission ADD harddrive_addressee_email TEXT DEFAULT NULL');
         $this->addSql('ALTER TABLE dataset_submission ADD harddrive_addressee_phone TEXT DEFAULT NULL');
         $this->addSql('ALTER TABLE dataset_submission ADD harddrive_delivery_address TEXT DEFAULT NULL');
-        $this->addSql('ALTER TABLE dataset_submission DROP hard_drive_delivery_info');
         $this->addSql('ALTER TABLE dataset_submission_audit ADD harddrive_file_size TEXT DEFAULT NULL');
         $this->addSql('ALTER TABLE dataset_submission_audit ADD harddrive_addressee_name TEXT DEFAULT NULL');
         $this->addSql('ALTER TABLE dataset_submission_audit ADD harddrive_addressee_email TEXT DEFAULT NULL');
         $this->addSql('ALTER TABLE dataset_submission_audit ADD harddrive_addressee_phone TEXT DEFAULT NULL');
         $this->addSql('ALTER TABLE dataset_submission_audit ADD harddrive_delivery_address TEXT DEFAULT NULL');
-        $this->addSql('ALTER TABLE dataset_submission_audit DROP hard_drive_delivery_info');
-        $this->addSql('COMMENT ON COLUMN metadata.geometry IS \'(DC2Type:geometry)\'');
     }
 
     /**
@@ -41,15 +38,11 @@ class Version20171106213338 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE dataset_submission_audit ADD hard_drive_delivery_info JSON DEFAULT NULL');
         $this->addSql('ALTER TABLE dataset_submission_audit DROP harddrive_file_size');
         $this->addSql('ALTER TABLE dataset_submission_audit DROP harddrive_addressee_name');
         $this->addSql('ALTER TABLE dataset_submission_audit DROP harddrive_addressee_email');
         $this->addSql('ALTER TABLE dataset_submission_audit DROP harddrive_addressee_phone');
         $this->addSql('ALTER TABLE dataset_submission_audit DROP harddrive_delivery_address');
-        $this->addSql('COMMENT ON COLUMN metadata.geometry IS \'(DC2Type:geometry)(DC2Type:geometry)\'');
-        $this->addSql('ALTER TABLE dataset_submission ADD hard_drive_delivery_info JSON DEFAULT NULL');
         $this->addSql('ALTER TABLE dataset_submission DROP harddrive_file_size');
         $this->addSql('ALTER TABLE dataset_submission DROP harddrive_addressee_name');
         $this->addSql('ALTER TABLE dataset_submission DROP harddrive_addressee_email');
