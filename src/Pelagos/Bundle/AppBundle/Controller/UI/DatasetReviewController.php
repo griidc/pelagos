6b05e6b03b80a1f5591faf2c2ce6a218185d57d0
Son Nguyen <Son.Nguyen@tamucc.edu>
2017-11-30 10:20:54 -0600

    Remove use statements in controller and some js, css libraries in twig

--- src/Pelagos/Bundle/AppBundle/Controller/UI/DatasetReviewController.php
+++ src/Pelagos/Bundle/AppBundle/Controller/UI/DatasetReviewController.php
@@ -3,36 +3,8 @@
 namespace Pelagos\Bundle\AppBundle\Controller\UI;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-
-use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-
-use Symfony\Component\Form\Form;
-
-use Symfony\Component\PropertyAccess\PropertyAccess;
-
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-
-use Pelagos\Bundle\AppBundle\Form\DatasetSubmissionType;
-use Pelagos\Bundle\AppBundle\Form\DatasetSubmissionXmlFileType;
-
-use Pelagos\Entity\Account;
-use Pelagos\Entity\DIF;
-use Pelagos\Entity\Dataset;
-use Pelagos\Entity\DatasetSubmission;
-use Pelagos\Entity\Metadata;
-use Pelagos\Entity\Person;
-use Pelagos\Entity\ResearchGroup;
-use Pelagos\Entity\PersonDatasetSubmission;
-use Pelagos\Entity\PersonDatasetSubmissionDatasetContact;
-
-use Pelagos\Exception\InvalidMetadataException;
-
-use Pelagos\Util\ISOMetadataExtractorUtil;
 
 /**
  * The Dataset Review controller for the Pelagos UI App Bundle.
