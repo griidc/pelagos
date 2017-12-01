4eea383b86f310fac697027907b15fb75dd895ab
Michael Scott Williamson <michael.williamson@tamucc.edu>
2017-12-01 15:15:03 -0600

    Retry writes to mdapp log. Complain to syslog.

--- src/Pelagos/Util/MdappLogger.php
+++ src/Pelagos/Util/MdappLogger.php
@@ -9,6 +9,16 @@ use Doctrine\ORM\EntityManager;
 class MdappLogger
 {
     /**
+     * Constant of the number of write attempts made to write to log.
+     */
+    const RETRIES = 5;
+
+    /**
+     * Constant of number fo seconds to sleep inbetween log write attempts.
+     */
+    const SLEEPYTIME = 1;
+
+    /**
      * String of full filepath to logfile.
      *
      * @var string
@@ -30,12 +40,34 @@ class MdappLogger
      *
      * @param string $message The text to log to file.
      *
+     * @throws \Exception If syslog cannot be written.
+     *
      * @return void
      */
     public function writeLog($message)
     {
         $tz = ini_get('date.timezone');
+        $attemptCount = 0;
         $timestamp = new \DateTime('now', new \DateTimeZone($tz));
-        file_put_contents($this->logfile, $timestamp->format('r') . ": $message\n", FILE_APPEND);
+        $successfulWrite = file_put_contents($this->logfile, $timestamp->format('r') . ": $message\n", FILE_APPEND);
+        while (false === $successfulWrite) {
+            if ($attemptCount < RETRIES) {
+                sleep(SLEEPYTIME);
+                $successfulWrite = file_put_contents(
+                    $this->logfile,
+                    $timestamp->format('r') . ": $message\n",
+                    FILE_APPEND
+                );
+                $attemptCount++;
+            } else {
+                $syslogSuccess = syslog(
+                    LOG_ERR,
+                    'Failed to write to Mdapp log: ' . $timestamp->format('r') . ": $message\n"
+                );
+                if (false == $syslogSuccess) {
+                    throw new \Exception('Could not write to syslog. Something is seriously wrong.');
+                }
+            }
+        }
     }
 }
