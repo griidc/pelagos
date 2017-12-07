097692a552d69b45fa958e7ed6bb91a80204552c
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2017-12-07 10:49:09 -0600

    Moved non - datatable code into new javascript file

--- src/Pelagos/Bundle/AppBundle/Resources/public/js/datasetReviewList.js
+++ src/Pelagos/Bundle/AppBundle/Resources/public/js/datasetReviewList.js
@@ -2,19 +2,6 @@ var $ = jQuery.noConflict();
 $(document).ready(function(){
   "use strict";
 
-    $("#udiLoadReviewform").bind("change keyup mouseout", function() {
-        var udiTextBox = $("#udiReview");
-        if($(this).valid() && udiTextBox.val() !== "" && udiTextBox.is(":disabled") === false) {
-            $("#loadReviewButton").button({
-                disabled: false
-            });
-        } else {
-            $("#loadReviewButton").button({
-                disabled: true
-            });
-        }
-    });
-
     var entityTable = $(".entityTable");
     if (entityTable.length) {
         entityTable.pelagosDataTable();
