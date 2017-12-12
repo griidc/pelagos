0bc1689127755bf1369e9c124c4af3b90bd72cec
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2017-12-12 16:17:56 -0600

    Removed animation, so not to have a dependency on Animate.css

--- src/Pelagos/Bundle/AppBundle/Resources/public/js/pelagosNoty.js
+++ src/Pelagos/Bundle/AppBundle/Resources/public/js/pelagosNoty.js
@@ -16,8 +16,8 @@
                 type: notyType,
                 theme: "relax",
                 animation: {
-                    open: "animated fadeIn", // Animate.css class names
-                    close: "animated fadeOut", // Animate.css class names
+                    open: {opacity: "toggle"},  // fadeIn
+                    close: {opacity: "toggle"}, // fadeOut
                 },
             }, options);
            
