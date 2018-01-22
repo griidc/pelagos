d73b331902890692835a1fd4370e6e3424c2acc2
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2018-01-22 13:38:22 -0600

    Added javascript for the file-uploader tab

--- src/Pelagos/Bundle/AppBundle/Resources/public/js/datasetReview.js
+++ src/Pelagos/Bundle/AppBundle/Resources/public/js/datasetReview.js
@@ -348,6 +348,238 @@ $(document).ready(function(){
             }
         });
     }
+
+    // Direct Upload
+    $("#fine-uploader").fineUploader({
+        template: "qq-template",
+        multiple: false,
+        request: {
+            endpoint: Routing.generate("pelagos_api_upload_post")
+        },
+        session: {
+            endpoint: Routing.generate("pelagos_api_dataset_submission_get_uploaded_files", { id: $("form[datasetsubmission]").attr("datasetsubmission") })
+        },
+        chunking: {
+            enabled: true,
+            partSize: 10000000,
+            concurrent: {
+                enabled: true
+            },
+            success: {
+                endpoint: Routing.generate("pelagos_api_upload_post") + "?done"
+            }
+        },
+        resume: {
+            enabled: true
+        },
+        retry: {
+            enableAuto: true
+        },
+        deleteFile: {
+            enabled: $("#submitButton").attr("disabled") != "disabled",
+            forceConfirm: true,
+            endpoint: Routing.generate("pelagos_api_upload_delete")
+        },
+        callbacks: {
+            onSessionRequestComplete: function (response, success, xhrOrXdr) {
+                if (response.length > 0) {
+                    $("#fine-uploader .qq-upload-button").hide();
+                }
+            },
+            onSubmit: function (id, name) {
+                setDatasetFileUri("");
+                $("#fine-uploader .qq-upload-button").hide();
+            },
+            onProgress: function (id, name, totalUploadedBytes, totalBytes) {
+                updateSpeedText(totalUploadedBytes, totalBytes);
+            },
+            onComplete: function (id, name, responseJSON, xhr) {
+                if (responseJSON.success) {
+                    setDatasetFileUri(responseJSON.path);
+                    saveDatasetSubmission();
+                }
+            },
+            onDelete: function (id) {
+                setDatasetFileUri("");
+                saveDatasetSubmission();
+            },
+            onStatusChange: function (id, oldStatus, newStatus) {
+                switch (newStatus) {
+                    case qq.status.CANCELED:
+                    case qq.status.DELETED:
+                    case qq.status.PAUSED:
+                    case qq.status.UPLOAD_SUCCESSFUL:
+                        resetSpeedText();
+                }
+                switch (newStatus) {
+                    case qq.status.CANCELED:
+                    case qq.status.DELETED:
+                        $("#fine-uploader .qq-upload-button").show();
+                }
+            }
+        }
+    });
+
+    // Request SFTP/GridFTP button
+    $("#sftpButton").click(function() {
+        $("#spinner").show();
+        $.ajax({
+            url: $("#sftpButton").attr("sftppath"),
+            type: "PATCH",
+            success: function() {
+                showDialog("SFTP Access", "SFTP Access has been granted.");
+                $(".sftpYes").show();
+                $(".sftpNo").hide();
+                // Enable file browse buttons..
+                $(".fileBrowserButton").prop("disabled", false);
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                showDialog("Problem with your request", jqXHR.responseJSON.message);
+            }
+        }).always(function() {
+            $("#spinner").hide();
+        });
+    });
+    // File browser for SFTP/GridFTP
+    $(".fileBrowserButton").fileBrowser({
+        url: Routing.generate("pelagos_api_account_get_incoming_directory", { id: "self" })
+    });
+
+    // SFTP/GridFTP and HTTP/FTP
+    $("#datasetFilePath, #datasetFileUrl").on("keyup change", function() {
+        $(this).valid();
+        setDatasetFileUri($(this).val());
+    });
+    $("#datasetFilePath, #datasetFileUrl").change(function() {
+        saveDatasetSubmission();
+    });
+
+    // set the datasetFileUri and datasetFileTransferType
+    function setDatasetFileUri(datasetFileUri) {
+        // get the datasetFileTransferType from the active tab
+        var datasetFileTransferType = $("#filetabs .ui-tabs-active").attr("datasetFileTransferType");
+        // set the datasetFileTransferType
+        $("#datasetFileTransferType").val(datasetFileTransferType);
+        if (datasetFileTransferType !== "upload") {
+            // clear uploaded files list (Direct Upload tab)
+            $(".qq-upload-list").html("")
+            // show upload button (Direct Upload tab)
+            $("#fine-uploader .qq-upload-button").show();
+        }
+        if (datasetFileTransferType != "SFTP") {
+            // clear datasetFilePath (Upload via SFTP/GridFTP tab)
+            $("#datasetFilePath").val("");
+        }
+        if (datasetFileTransferType != "HTTP") {
+            // clear datasetFileUrl (Request Pull from HTTP/FTP Server tab)
+            $("#datasetFileUrl").val("");
+            // if datasetFileUri is set
+            if (datasetFileUri != "") {
+                // prepend file uri prefix
+                datasetFileUri = "file://" + datasetFileUri;
+            }
+        }
+        // remove datasetFileUri error label
+        $('label.error[for="datasetFileUri"]').remove();
+        // set datasetFileUri
+        $("#datasetFileUri").val(datasetFileUri);
+    }
+
+    var uploadSpeeds = [];
+    var updateSpeeds = true;
+
+    function updateSpeedText(totalUploadedBytes, totalBytes) {
+        if (!updateSpeeds) {
+            return;
+        }
+        uploadSpeeds.push({
+            totalUploadedBytes: totalUploadedBytes,
+            currentTime: new Date().getTime()
+        });
+        var minSamples = 6;
+        var maxSamples = 20;
+        if (uploadSpeeds.length > maxSamples) {
+            uploadSpeeds.shift();
+        }
+        if (uploadSpeeds.length >= minSamples) {
+            try {
+                var firstSample = uploadSpeeds[0];
+                var lastSample = uploadSpeeds[uploadSpeeds.length - 1];
+                var progressBytes = lastSample.totalUploadedBytes - firstSample.totalUploadedBytes;
+                var progressTimeMS = lastSample.currentTime - firstSample.currentTime;
+                var bytesPerSecond = progressBytes / (progressTimeMS / 1000);
+                console.log(uploadSpeeds.length);
+                if (bytesPerSecond > 0) {
+                    var speedPrecision = 0;
+                    var MBps = bytesPerSecond / 1e6;
+                    if (MBps < 10) {
+                        speedPrecision = 1;
+                    }
+                    if (MBps < 1) {
+                        speedPrecision = 2;
+                    }
+                    if (MBps < 0.1) {
+                        speedPrecision = 3;
+                    }
+                    $("#uploader-speed").text("Transfer speed: " + MBps.toFixed(speedPrecision) + " MB per second");
+                    var remainingDays = 0;
+                    var remainingHours = 0;
+                    var remainingMinutes = 0;
+                    var remainingSeconds = ((totalBytes - totalUploadedBytes) / bytesPerSecond).toFixed(0);
+                    if (remainingSeconds >= 60) {
+                        remainingMinutes = Math.floor(remainingSeconds / 60);
+                        remainingSeconds %= 60;
+                    }
+                    if (remainingMinutes >= 60) {
+                        remainingHours = Math.floor(remainingMinutes / 60);
+                        remainingMinutes %= 60;
+                    }
+                    if (remainingHours >= 24) {
+                        remainingDays = Math.floor(remainingHours / 24);
+                        remainingHours %= 24;
+                    }
+                    var remainingText = "";
+                    if (remainingDays > 0) {
+                        remainingText += " " + remainingDays + " day";
+                        if (remainingDays > 1) {
+                            remainingText += "s";
+                        }
+                    }
+                    if (remainingHours > 0) {
+                        remainingText += " " + remainingHours + " hour";
+                        if (remainingHours > 1) {
+                            remainingText += "s";
+                        }
+                    }
+                    if (remainingMinutes > 0) {
+                        remainingText += " " + remainingMinutes + " minute";
+                        if (remainingMinutes > 1) {
+                            remainingText += "s";
+                        }
+                    }
+                    if (remainingSeconds > 0) {
+                        remainingText += " " + remainingSeconds + " second";
+                        if (remainingSeconds > 1) {
+                            remainingText += "s";
+                        }
+                    }
+                    $("#uploader-remaining").text("Time remaining:" + remainingText);
+                    updateSpeeds = false;
+                    setTimeout(function () {
+                        updateSpeeds = true;
+                    }, 500);
+                }
+            } catch (err) {
+            }
+        }
+    }
+
+    function resetSpeedText() {
+        $("#uploader-speed").text("");
+        $("#uploader-remaining").text("");
+        uploadSpeeds = [];
+        updateSpeeds = true;
+    }
 });
 
 function checkSpatial(isNonSpatial) {
