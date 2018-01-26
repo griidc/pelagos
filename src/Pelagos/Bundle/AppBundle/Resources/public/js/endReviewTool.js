94e5aeafc6816cbae95180840eadb0f19d41305e
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2018-01-26 12:43:03 -0600

    Added javascript and notification to show the flashbag messages

---
+++ src/Pelagos/Bundle/AppBundle/Resources/public/js/endReviewTool.js
@@ -0,0 +1,13 @@
+var $ = jQuery.noConflict();
+
+//FOUC preventor
+$("html").hide();
+
+$(document).ready(function () {
+    "use strict";
+
+    $("html").show();
+
+    $(".pelagosNoty").pelagosNoty({timeout: 0, showOnTop:false});
+
+});
