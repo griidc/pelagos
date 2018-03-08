10492929c1a1e9235369a906098e1e7499394083
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2018-03-08 16:31:27 -0600

    Removed the metaData check from Dataset entity in the Update methods

--- src/Pelagos/Entity/Dataset.php
+++ src/Pelagos/Entity/Dataset.php
@@ -314,10 +314,8 @@ class Dataset extends Entity
      */
     public function updateTitle()
     {
-        if ($this->hasMetadata()) {
-            // Copy Metadata title to Dataset.
-            $this->title = $this->getMetadata()->getTitle();
-        } elseif ($this->hasDatasetSubmission()) {
+        // After Dataset is accepted, the getDatasetSubmission pointer points to the accepted version.
+        if ($this->hasDatasetSubmission()) {
             // Copy DatasetSubmission title to Dataset.
             $this->title = $this->getDatasetSubmission()->getTitle();
         } elseif ($this->hasDif()) {
@@ -343,10 +341,8 @@ class Dataset extends Entity
      */
     public function updateAbstract()
     {
-        if ($this->hasMetadata()) {
-            // Copy Metadata abstract to Dataset.
-            $this->abstract = $this->getMetadata()->getAbstract();
-        } elseif ($this->hasDatasetSubmission()) {
+        // After Dataset is accepted, the getDatasetSubmission pointer points to the accepted version.
+        if ($this->hasDatasetSubmission()) {
             // Copy DatasetSubmission abstract to Dataset.
             $this->abstract = $this->getDatasetSubmission()->getAbstract();
         } elseif ($this->hasDif()) {
