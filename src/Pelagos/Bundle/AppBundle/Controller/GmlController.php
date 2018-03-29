fbac943ff0ac3891fe9aa768feb3a3d668dbf358
Son Nguyen <Son.Nguyen@tamucc.edu>
2018-03-29 15:09:31 -0500

    append namespace to GML string using regex

--- src/Pelagos/Bundle/AppBundle/Controller/GmlController.php
+++ src/Pelagos/Bundle/AppBundle/Controller/GmlController.php
@@ -162,25 +162,31 @@ class GmlController extends Controller
      * @param string $gml        Gml that needs namespace.
      * @param array  $namespaces Array of attributes and values.
      *
-     * @throws \Exception When Root node is not defined properly.
      * @return string GML string with namespace.
      */
     private function addNamespace($gml, array $namespaces)
     {
         $doc = new \DomDocument('1.0', 'UTF-8');
+
         $doc->loadXML($gml, LIBXML_NOERROR);
         $rootNode = $doc->documentElement;
-        if (null === $rootNode) {
-            throw new \Exception('Not a parsable GML!');
+        if (null !== $rootNode) {
+            foreach ($namespaces as $key => $value) {
+                $rootNode->setAttribute($key, $value);
+            }
+            $gml = $doc->saveXML();
+            $cleanXML = new \SimpleXMLElement($gml, LIBXML_NOERROR);
+            $dom = dom_import_simplexml($cleanXML);
+            $gml = $dom->ownerDocument->saveXML($dom->ownerDocument->documentElement);
+        } else {
+            //append namespaces to string using regex
+            $strNameSpaces = '';
+            foreach($namespaces as $key => $value) {
+                $strNameSpaces .= ' ' . $key . '="' . $value . '"';
+            }
+            $regEx = '/^<gml:\S*/';
+            $gml = preg_replace($regEx,"$0$strNameSpaces",$gml);
         }
-        foreach ($namespaces as $key => $value) {
-            $rootNode->setAttribute($key, $value);
-        }
-
-        $gml = $doc->saveXML();
-        $cleanXML = new \SimpleXMLElement($gml, LIBXML_NOERROR);
-        $dom = dom_import_simplexml($cleanXML);
-        $gml = $dom->ownerDocument->saveXML($dom->ownerDocument->documentElement);
         return $gml;
     }
 
@@ -210,18 +216,9 @@ class GmlController extends Controller
                 'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
                 'xsi:schemaLocation' => 'http://www.opengis.net/gml/3.2 ' . $schema
             );
-            try {
-                $gml = $this->addNamespace($gml, $namespaces);
-            } catch (\Exception $exception) {
-                return new JsonResponse(
-                    array(
-                        $isValid,
-                        array($exception->getMessage()),
-                        null
-                    ),
-                    JsonResponse::HTTP_OK
-                );
-            }
+
+            $gml = $this->addNamespace($gml, $namespaces);
+            
             $errors = [];
             $warnings = [];
             $metadataUtil = $this->get('pelagos.util.metadata');
