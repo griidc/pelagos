6a3e821555ad2774ec2efeb30114892494f90471
Son Nguyen <Son.Nguyen@tamucc.edu>
2018-04-26 09:37:34 -0500

    Create API for Data Center

--- src/Pelagos/Entity/DataCenter.php
+++ src/Pelagos/Entity/DataCenter.php
@@ -7,6 +7,48 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * This class represent data center entity information.
+ *
+ * @ORM\Entity
+ *
+ * @UniqueEntity(
+ *     fields={"organizationName"},
+ *     errorPath="organizationName",
+ *     message="A data center with this name already exists"
+ * )
+ *
+ * @UniqueEntity(
+ *     fields={"organizationUrl"},
+ *     errorPath="organizationUrl",
+ *     message="A data center with this Url already exists"
+ * )
+ *
+ * @Hateoas\Relation(
+ *   "self",
+ *   href = @Hateoas\Route(
+ *     "pelagos_api_data_center_get",
+ *     parameters = { "id" = "expr(object.getId())" }
+ *   )
+ * )
+ * @Hateoas\Relation(
+ *   "edit",
+ *   href = @Hateoas\Route(
+ *     "pelagos_api_data_center_put",
+ *     parameters = { "id" = "expr(object.getId())" }
+ *   ),
+ *   exclusion = @Hateoas\Exclusion(
+ *     excludeIf = "expr(not service('security.authorization_checker').isGranted(['CAN_EDIT'], object))"
+ *   )
+ * )
+ * @Hateoas\Relation(
+ *   "delete",
+ *   href = @Hateoas\Route(
+ *     "pelagos_api_data_center_delete",
+ *     parameters = { "id" = "expr(object.getId())" }
+ *   ),
+ *   exclusion = @Hateoas\Exclusion(
+ *     excludeIf = "expr(not object.isDeletable() or not service('security.authorization_checker').isGranted(['CAN_DELETE'], object))"
+ *   )
+ * )
  */
 
 /**
