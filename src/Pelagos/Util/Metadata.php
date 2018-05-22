4d6da797d5efb9c44ede83e97bd586c86e35f384
Son Nguyen <Son.Nguyen@tamucc.edu>
2018-05-22 08:24:22 -0500

    minor change

--- src/Pelagos/Util/Metadata.php
+++ src/Pelagos/Util/Metadata.php
@@ -36,6 +36,17 @@ class Metadata
      */
     public function getXmlRepresentation(Dataset $dataset, array $boundingBoxArray)
     {
+        //regex array storing tuple of an escape character and its text value
+        $regexArray = array();
+
+        $regexArray[] = array('/&amp;/', '&');
+        $regexArray[] = array('/&lt;/', '<');
+        $regexArray[] = array('/&gt;/', '>');
+        $regexArray[] = array('/&quot;/', '"');
+        //single quotes
+        $regexArray[] = array('/&apos;/', "'");
+        $regexArray[] = array('/&#039;/', "'");
+
         $xml = null;
         if ($dataset->getDatasetSubmission() instanceof DatasetSubmission) {
             $xml = $this->twig->render(
@@ -59,6 +70,12 @@ class Metadata
                 'utf8'
             );
             $xml = $tidyXml;
+
+            //remove all html escape special chars
+            foreach ($regexArray as $regex) {
+                $xml = preg_replace($regex[0], $regex[1], $xml);
+            }
+
             // Remove extra whitespace added around CDATA tags by tidy.
             $xml = preg_replace('/>[\s]+<\!\[CDATA\[/', '><![CDATA[', $xml);
             $xml = preg_replace('/]]>\s+</', ']]><', $xml);
