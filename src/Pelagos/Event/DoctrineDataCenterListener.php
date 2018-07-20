ab84645d9cffc5e1b15e13d9956a9fd170764b8d
Son Nguyen <Son.Nguyen@tamucc.edu>
2018-07-20 15:08:33 -0500

    Update dataCenter with doctrine listener

---
+++ src/Pelagos/Event/DoctrineDataCenterListener.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Pelagos\Event;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Event\OnFlushEventArgs;
+
+use Pelagos\Entity\DataCenter;
+use Pelagos\Entity\DataRepository;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+/**
+ * Doctrine Listener class for DataRepository update events.
+ */
+class DoctrineDataCenterListener
+{
+    /**
+     * On flush pass DataRepository object to updateDataCenter to update the related DataCenter, if necessary.
+     *
+     * @param OnFlushEventArgs $args The onFlush event arguments.
+     *
+     * @return void
+     */
+    public function onFlush(OnFlushEventArgs $args)
+    {
+        $entityManager = $args->getEntityManager();
+        $unitOfWork = $entityManager->getUnitOfWork();
+        foreach ($unitOfWork->getScheduledEntityInsertions() as $entity) {
+            $this->updateDataCenter($entity, $entityManager);
+        }
+        foreach ($unitOfWork->getScheduledEntityUpdates() as $entity) {
+            $this->updateDataCenter($entity, $entityManager);
+        }
+    }
+
+    /**
+     * Method to update dataCenter when the corresponding entity in dataRepository changes.
+     *
+     * @param mixed         $dataRepository A Doctrine entity (dataRepository).
+     * @param EntityManager $entityManager  The Doctrine entity manager.
+     *
+     * @return void
+     */
+    protected function updateDataCenter($dataRepository, EntityManager $entityManager)
+    {
+        //reflection class to invoke SET methods in DataCenter
+        $reflectionClass = new \ReflectionClass(DataCenter::class);
+
+        if ($dataRepository instanceof DataRepository) {
+            $originalDataRepository = $entityManager->getUnitOfWork()->getOriginalEntityData($dataRepository);
+
+            //get datacenter object by email (assume email address is unique)
+            $dataCenter = $entityManager->getRepository(DataCenter::class)
+                ->findOneBy(['emailAddress' => $originalDataRepository['emailAddress']]);
+
+            if (null !== $dataCenter) {
+
+                //changeSet structure: [ "key" => [ 0: old value, 1: new value] ]
+                $changeSet = $entityManager->getUnitOfWork()->getEntityChangeSet($dataRepository);
+
+                //get the array of changeset keys, loop through and updating datacenter by invoking its SET methods
+                $changeSetKeys = array_keys($changeSet);
+                foreach ($changeSetKeys as $key) {
+                    switch ($key) {
+                        case 'name':
+                        case 'description':
+                            $newOrganizationName = $dataRepository->getDescription()
+                                . ' (' . $dataRepository->getName() . ')';
+                            $dataCenter->setOrganizationName($newOrganizationName);
+                            break;
+                        case 'url':
+                            $dataCenter->setOrganizationUrl($dataRepository->getUrl());
+                            break;
+                        default:
+                            $reflectionClass->getMethod('set' . ucfirst($key))
+                                ->invoke($dataCenter, $changeSet[$key][1]);
+                            break;
+                    }
+                }
+
+                $dataCenter->setModifier($dataRepository->getModifier());
+
+                $entityManager->persist($dataCenter);
+                $classMetadata = $entityManager->getClassMetadata(DataCenter::class);
+                $entityManager->getUnitOfWork()->recomputeSingleEntityChangeSet($classMetadata, $dataCenter);
+            }
+        }
+    }
+}
