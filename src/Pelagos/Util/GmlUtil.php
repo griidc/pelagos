3e7de087a457538c63f7f80553769bba1b36cb2f
Michael Scott Williamson <michael.williamson@tamucc.edu>
2018-09-12 12:36:27 -0500

    Handle possible null GML

--- src/Pelagos/Util/GmlUtil.php
+++ src/Pelagos/Util/GmlUtil.php
@@ -16,35 +16,39 @@ class GmlUtil
      */
     public static function addNamespace($gml, array $namespaces = null)
     {
-        if ($namespaces == null) {
-            $namespaces = array(
-                'xmlns:gml' => 'http://www.opengis.net/gml/3.2',
-                'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
-                'xsi:schemaLocation' => 'http://www.opengis.net/gml/3.2 ' .
-                'http://schemas.opengis.net/gml/3.2.1/gml.xsd'
-            );
-        }
-        $doc = new \DomDocument('1.0', 'UTF-8');
+        if (($gml != null) and (1 == preg_match('/gml/', $gml))) {
+            if ($namespaces == null) {
+                $namespaces = array(
+                    'xmlns:gml' => 'http://www.opengis.net/gml/3.2',
+                    'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+                    'xsi:schemaLocation' => 'http://www.opengis.net/gml/3.2 ' .
+                    'http://schemas.opengis.net/gml/3.2.1/gml.xsd'
+                );
+            }
+            $doc = new \DomDocument('1.0', 'UTF-8');
 
-        $doc->loadXML($gml, LIBXML_NOERROR);
-        $rootNode = $doc->documentElement;
-        if (null !== $rootNode) {
-            foreach ($namespaces as $key => $value) {
-                $rootNode->setAttribute($key, $value);
+            $doc->loadXML($gml, LIBXML_NOERROR);
+            $rootNode = $doc->documentElement;
+            if (null !== $rootNode) {
+                foreach ($namespaces as $key => $value) {
+                    $rootNode->setAttribute($key, $value);
+                }
+                $gml = $doc->saveXML();
+                $cleanXML = new \SimpleXMLElement($gml, LIBXML_NOERROR);
+                $dom = dom_import_simplexml($cleanXML);
+                $gml = $dom->ownerDocument->saveXML($dom->ownerDocument->documentElement);
+            } else {
+                //append namespaces to string using regex
+                $strNameSpaces = '';
+                foreach ($namespaces as $key => $value) {
+                    $strNameSpaces .= ' ' . $key . '="' . $value . '"';
+                }
+                $regEx = '/^<gml:\S*/';
+                $gml = preg_replace($regEx, "$0$strNameSpaces", $gml);
             }
-            $gml = $doc->saveXML();
-            $cleanXML = new \SimpleXMLElement($gml, LIBXML_NOERROR);
-            $dom = dom_import_simplexml($cleanXML);
-            $gml = $dom->ownerDocument->saveXML($dom->ownerDocument->documentElement);
+            return $gml;
         } else {
-            //append namespaces to string using regex
-            $strNameSpaces = '';
-            foreach ($namespaces as $key => $value) {
-                $strNameSpaces .= ' ' . $key . '="' . $value . '"';
-            }
-            $regEx = '/^<gml:\S*/';
-            $gml = preg_replace($regEx, "$0$strNameSpaces", $gml);
+            return $gml;
         }
-        return $gml;
     }
 }
