c249cd21f4e8003ff6991b59919f52aaf30e9969
Michael Scott Williamson <michael.williamson@tamucc.edu>
2018-09-12 09:58:46 -0500

    Use standard namespaces from Util class

--- src/Pelagos/Util/Geometry.php
+++ src/Pelagos/Util/Geometry.php
@@ -101,14 +101,7 @@ class Geometry
      */
     public function convertGmlToWkt($gml)
     {
-        $namespaces = array(
-                'xmlns:gml' => 'http://www.opengis.net/gml/3.2',
-                'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
-                'xsi:schemaLocation' => 'http://www.opengis.net/gml/3.2 ' .
-                'http://schemas.opengis.net/gml/3.2.1/gml.xsd'
-            );
-
-        $gml = GmlUtil::addNamespace($gml, $namespaces);
+        $gml = GmlUtil::addNamespace($gml);
 
         $sql = 'SELECT ST_AsText(ST_GeomFromGML(:gml, :srid))';
         $connection = $this->entityManager->getConnection();
