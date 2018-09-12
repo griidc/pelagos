3c974875df9b9ff3fc8526cbdce784c542e77159
Michael Scott Williamson <michael.williamson@tamucc.edu>
2018-09-12 09:57:13 -0500

    add standard namespaces to GmlUtil class

--- src/Pelagos/Util/GmlUtil.php
+++ src/Pelagos/Util/GmlUtil.php
@@ -9,13 +9,21 @@ class GmlUtil
     /**
      * This function add namespace for validation to the given gml.
      *
-     * @param string $gml        Gml that needs namespace.
-     * @param array  $namespaces Array of attributes and values.
+     * @param string     $gml        Gml that needs namespace.
+     * @param array|null $namespaces Array of attributes and values, can be left null for std namespaces.
      *
      * @return string GML string with namespace.
      */
-    public static function addNamespace($gml, array $namespaces)
+    public static function addNamespace($gml, array $namespaces = null)
     {
+        if ($namespaces == null) {
+            $namespaces = array(
+                'xmlns:gml' => 'http://www.opengis.net/gml/3.2',
+                'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+                'xsi:schemaLocation' => 'http://www.opengis.net/gml/3.2 ' .
+                'http://schemas.opengis.net/gml/3.2.1/gml.xsd'
+            );
+        }
         $doc = new \DomDocument('1.0', 'UTF-8');
 
         $doc->loadXML($gml, LIBXML_NOERROR);
