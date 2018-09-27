af93b8927d8b25832deb7b6f79f8ffbd625e1968
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2018-09-27 16:10:40 -0500

    Added approvedDate attribute to the entity DIF

---
+++ app/DoctrineMigrations/Version20180927210830.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Pelagos\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180927210830 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE dif ADD approved_date TIMESTAMP(0) WITH TIME ZONE DEFAULT NULL');
+        $this->addSql('ALTER TABLE dif_audit ADD approved_date TIMESTAMP(0) WITH TIME ZONE DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE dif DROP approved_date');
+        $this->addSql('ALTER TABLE dif_audit DROP approved_date');
+    }
+}
