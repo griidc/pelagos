1ed920330fbe206e5b8c6c9631eee7fa3ab93e8c
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2018-10-02 17:24:54 -0500

    Changed the order of migrations, which was causing the udi prefix to fail

--- app/DoctrineMigrations/Version20181002221946.php
+++ app/DoctrineMigrations/Version20181002221946.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180726174556 extends AbstractMigration
+class Version20181002221946 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -31,10 +31,10 @@ class Version20180726174556 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
-        
-        $this->addSql('ALTER TABLE dataset_submission RENAME COLUMN dataset_status TO metadata_status');
+
         $this->addSql('ALTER TABLE dataset RENAME COLUMN dataset_status TO metadata_status');
         $this->addSql('ALTER TABLE dataset_audit RENAME COLUMN dataset_status TO metadata_status');
+        $this->addSql('ALTER TABLE dataset_submission RENAME COLUMN dataset_status TO metadata_status');
         $this->addSql('ALTER TABLE dataset_submission_audit RENAME COLUMN dataset_status TO metadata_status');
     }
 }
