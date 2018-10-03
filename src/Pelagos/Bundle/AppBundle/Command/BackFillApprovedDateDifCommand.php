d9a5d0a28a1845353e20f90449f519b10642eb04
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2018-10-03 10:55:57 -0500

    Added backfill script to fill approvedDate attribute in DIF

---
+++ src/Pelagos/Bundle/AppBundle/Command/BackFillApprovedDateDifCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Pelagos\Bundle\AppBundle\Command;
+
+use Pelagos\Entity\Dataset;
+use Pelagos\Entity\DIF;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Back fill Script for approved date attribute in DIF entity.
+ *
+ * @see ContainerAwareCommand
+ */
+class BackFillApprovedDateDifCommand extends ContainerAwareCommand
+{
+    /**
+     * Configures the current command.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('dataset:backfill-dif-approved-date')
+            ->setDescription('Backfill DIF approved date for approved datasets.');
+    }
+
+    /**
+     * Executes the current command.
+     *
+     * @param InputInterface  $input  An InputInterface instance.
+     * @param OutputInterface $output An OutputInterface instance.
+     *
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
+
+        $datasets = $entityManager->getRepository(Dataset::class)->findBy(array('identifiedStatus' => DIF::STATUS_APPROVED));
+        $count = 0;
+        foreach ($datasets as $dataset) {
+            $dif = $dataset->getDif();
+
+            if ($dif->getStatus() === DIF::STATUS_APPROVED) {
+                $dif->setApprovedDate($dif->getModificationTimeStamp());
+                $output->writeln('Approved date back-filled for dataset: ' . $dataset->getId());
+                $entityManager->persist($dataset);
+                $entityManager->flush();
+                $count++;
+            }
+        }
+        $output->writeln('Total number of datasets which got back-filled: ' . $count);
+    }
+}
