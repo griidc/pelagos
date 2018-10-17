20f92c1abddd1c9fd4205ed14e1d9edd72e05c7c
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2018-10-17 12:36:10 -0500

    Working side by side view, map doesn't work.

--- src/Pelagos/Bundle/AppBundle/Controller/UI/SideBySideController.php
+++ src/Pelagos/Bundle/AppBundle/Controller/UI/SideBySideController.php
@@ -16,7 +16,6 @@ use Pelagos\Entity\DIF;
 use Pelagos\Entity\PersonDatasetSubmissionDatasetContact;
 
 use Pelagos\Response\TerminateResponse;
- 
 
 /**
  * The DIF controller for the Pelagos UI App Bundle.
@@ -26,10 +25,20 @@ use Pelagos\Response\TerminateResponse;
 class SideBySideController extends UIController
 {
     /**
-     * The default action for the DIF.
+     * Valid values for $datasetFileTransferType and $metadataFileTransferType.
+     */
+    const SUBMISSIONS_STATES = array(
+        DatasetSubmission::STATUS_UNSUBMITTED => 'Unsubmitted',
+        DatasetSubmission::STATUS_INCOMPLETE => 'Draft',
+        DatasetSubmission::STATUS_COMPLETE => 'Submitted',
+        DatasetSubmission::STATUS_IN_REVIEW => 'In Review',
+    );
+
+    /**
+     * The default action for Side by Side.
      *
      * @param Request     $request The Symfony request object.
-     * @param string|null $id      The id of the DIF to load.
+     * @param string|null $udi     The UDI of the Dataset to load.
      *
      * @Route("/{udi}")
      *
@@ -37,50 +46,35 @@ class SideBySideController extends UIController
      */
     public function defaultAction(Request $request, $udi = null)
     {
-        // if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
-            // return $this->redirect('/user/login?destination=' . $request->getPathInfo());
-        // }
-        
-        $datasets = $this->entityHandler->getBy(Dataset::class, array('udi' => $udi));
-
-        if (count($datasets) == 0) {
-            throw $this->createNotFoundException("No dataset found for UDI: $udi");
+        if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirect('/user/login?destination=' . $request->getPathInfo());
         }
 
-        if (count($datasets) > 1) {
-            throw new \Exception("Got more than one return for UDI: $udi");
-        }
+        $datasetSubmissionHistory = $this->getDatasetSubmissionHistory($udi);
 
-        $dataset = $datasets[0];
-        
-        $datasetSubmissionHistory = $dataset->getDatasetSubmissionHistory();
-        
         $submissions = array();
-        
-        foreach($datasetSubmissionHistory->getIterator() as $i => $submission) {
+
+        foreach ($datasetSubmissionHistory->getIterator() as $i => $submission) {
             $data = array();
-            $data["version"] = $i;
-            $data["udi"] = $submission->getDataset()->getUdi();
-            $data["sequence"] = $submission->getSequence();
-            $data["status"] = $submission->getDataset()->getStatus();
-            $data["modifier"] = $submission->getModifier();
-            $data["modificationtimestamp"] = $submission->getModificationTimeStamp();
+            $data['version'] = $i;
+            $data['udi'] = $submission->getDataset()->getUdi();
+            $data['sequence'] = $submission->getSequence();
+            $data['status'] = self::SUBMISSIONS_STATES[$submission->getStatus()];
+            $data['modifier'] = $submission->getModifier()->getLastName() .
+                ', ' . $submission->getModifier()->getFirstName();
+            $data['modificationtimestamp'] = $submission->getModificationTimeStamp()->format('c');
             $submissions[] = $data;
         }
-        
-        dump($submissions);
 
         return $this->render(
             'PelagosAppBundle:SideBySide:index.html.twig',
             array(
-                'udi' => $dataset->getUdi(),
+                'udi' => $datasetSubmissionHistory->first()->getDataset()->getUdi(),
                 'submissions' => $submissions,
-                
-                
             )
         );
     }
-    
+
     /**
      * The get submission form action for the Side By Side controller.
      *
@@ -88,40 +82,30 @@ class SideBySideController extends UIController
      * @param string|null $udi      The UDI of the Dataset to load.
      * @param string|null $revision The revision number of the Submission to load.
      *
+     * @throws \Exception If revision does not exists.
+     *
      * @Route("/getForm/{udi}/{revision}")
      *
      * @return Response A Response instance.
      */
     public function getSubmissionFormAction(Request $request, $udi = null, $revision = null)
     {
-        // if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
-            // return $this->redirect('/user/login?destination=' . $request->getPathInfo());
-        // }
-        
-        $datasets = $this->entityHandler->getBy(Dataset::class, array('udi' => $udi));
-
-        if (count($datasets) == 0) {
-            throw $this->createNotFoundException("No dataset found for UDI: $udi");
+        if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirect('/user/login?destination=' . $request->getPathInfo());
         }
 
-        if (count($datasets) > 1) {
-            throw new \Exception("Got more than one return for UDI: $udi");
-        }
+        $datasetSubmissionHistory = $this->getDatasetSubmissionHistory($udi);
 
-        $dataset = $datasets[0];
-
-        $datasetSubmissionHistory = $dataset->getDatasetSubmissionHistory();
-        
         if ($datasetSubmissionHistory->count() < $revision and $revision !== null) {
             throw new \Exception("Revision $revision does not exist for UDI: $udi");
         }
-        
+
         if ($revision !== null) {
             $datasetSubmission = $datasetSubmissionHistory[$revision];
         } else {
             $datasetSubmission = $datasetSubmissionHistory->first();
         }
-        
+
         $researchGroupList = array();
         $account = $this->getUser();
         if (null !== $account) {
@@ -135,9 +119,9 @@ class SideBySideController extends UIController
                 $researchGroups
             );
         }
-        
+
         $form = $this->get('form.factory')->createNamed(null, DatasetSubmissionType::class, $datasetSubmission);
-        
+
         $terminateResponse = new TerminateResponse();
 
         return $this->render(
@@ -149,8 +133,35 @@ class SideBySideController extends UIController
                 'showForceDownload' => false,
                 'researchGroupList' => $researchGroupList,
                 'mode' => 'view',
-            ), 
+            ),
             $terminateResponse
         );
+    }
+
+    /**
+     * Get the dataset submission history from UDI.
+     *
+     * @param string|null $udi The UDI of the Dataset to load.
+     *
+     * @throws \Exception If dataset if not found.
+     * @throws \Exception If more than one dataset is returned.
+     *
+     * @return DatasetSubmissionHistory An array collection of submissions.
+     */
+    private function getDatasetSubmissionHistory($udi)
+    {
+        $datasets = $this->entityHandler->getBy(Dataset::class, array('udi' => $udi));
+
+        if (count($datasets) == 0) {
+            throw \Exception("No dataset found for UDI: $udi");
+        }
+
+        if (count($datasets) > 1) {
+            throw new \Exception("Got more than one return for UDI: $udi");
+        }
+
+        $dataset = $datasets[0];
+
+        return $dataset->getDatasetSubmissionHistory();
     }
 }
--- src/Pelagos/Bundle/AppBundle/Resources/public/js/sidebyside.js
+++ src/Pelagos/Bundle/AppBundle/Resources/public/js/sidebyside.js
@@ -4,28 +4,41 @@ $(document).ready(function()
 {
     "use strict";
     
-    
-    // $("#left").load("/pelagos-symfony/dev/mvde/sidebyside/getForm/R1.x135.120:0003/0");
-    // $("#right").load("/pelagos-symfony/dev/mvde/sidebyside/getForm/R1.x135.120:0003/13");
-    
-    
-    $("#left-version").change(function() {
-        var version = $(this).val();
+    $(".left-version").find("select").change(function() {
         var version = $(this).find("option:selected").data("version");
         var udi = $(this).find("option:selected").data("udi");
+        
+        $(this).parents("div.left-version")
+            .find(".submission-status")
+            .text($(this).find("option:selected").data("status"));
+        $(this).parents("div.left-version")
+            .find(".submission-modificationtimestamp")
+            .text($(this).find("option:selected").data("modificationtimestamp"));
+        $(this).parents("div.left-version")
+            .find(".submission-modifier")
+            .text($(this).find("option:selected").data("modifier"));
+        
         $("#left").load("/pelagos-symfony/dev/mvde/sidebyside/getForm/" + udi + "/" + version);
-        //
     });
     
-    $("#right-version").change(function() {
-        var version = $(this).val();
+    $(".right-version").find("select").change(function() {
         var version = $(this).find("option:selected").data("version");
         var udi = $(this).find("option:selected").data("udi");
+        
+        $(this).parents("div.right-version")
+            .find(".submission-status")
+            .text($(this).find("option:selected").data("status"));
+        $(this).parents("div.right-version")
+            .find(".submission-modificationtimestamp")
+            .text($(this).find("option:selected").data("modificationtimestamp"));
+        $(this).parents("div.right-version")
+            .find(".submission-modifier")
+            .text($(this).find("option:selected").data("modifier"));
+        
         $("#right").load("/pelagos-symfony/dev/mvde/sidebyside/getForm/" + udi + "/" + version);
-        //
     });
     
-    $("#left-version").change();
-    $("#right-version").change();
+    $(".left-version select").change();
+    $(".right-version select").change();
     
 });
