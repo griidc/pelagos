2dfddcaef18791603e62cc1ee539ff223fb98d59
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2018-10-30 15:00:32 -0500

    Changed double quotes to single quotes

--- src/Pelagos/Bundle/AppBundle/Rabbit/Consumer/DoiConsumer.php
+++ src/Pelagos/Bundle/AppBundle/Rabbit/Consumer/DoiConsumer.php
@@ -410,7 +410,7 @@ class DoiConsumer implements ConsumerInterface
             $status = DOI::STATUS_UNAVAILABLE;
         } elseif ($dataset->getDatasetStatus() === Dataset::DATASET_STATUS_ACCEPTED
             and $restriction === DatasetSubmission::RESTRICTION_NONE) {
-            if ($doiStatus === DOI::STATUS_RESERVED or (preg_match("/^unavailable/", $doiStatus) === 1)) {
+            if ($doiStatus === DOI::STATUS_RESERVED or (preg_match('/^unavailable/', $doiStatus) === 1)) {
                 $status = DOI::STATUS_PUBLIC;
             }
         }
