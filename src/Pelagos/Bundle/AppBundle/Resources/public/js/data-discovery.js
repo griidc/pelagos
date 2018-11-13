b44d35e692df04c1675fd1d0412a4f25731881db
Son Nguyen <Son.Nguyen@tamucc.edu>
2018-11-13 14:01:16 -0600

    change to base 10

--- src/Pelagos/Bundle/AppBundle/Resources/public/js/data-discovery.js
+++ src/Pelagos/Bundle/AppBundle/Resources/public/js/data-discovery.js
@@ -102,10 +102,10 @@ function resizeLeftRight() {
     $("#left").height(lh);
     $("#right").height(rh);
 }
-
+//base 10
 function formatBytes(bytes,decimals) {
     if(bytes == 0) return "0 Bytes";
-    var k = 1024,
+    var k = 1000,
         dm = decimals <= 0 ? 0 : decimals || 2,
         sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"],
         i = Math.floor(Math.log(bytes) / Math.log(k));
