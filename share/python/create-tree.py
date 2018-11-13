69ec88f69e06044d1df0660f45b35f270be1f4aa
Michael Scott Williamson <michael.williamson@tamucc.edu>
2018-11-13 15:02:01 -0600

    Per-subdirectory summing working

--- share/python/create-tree.py
+++ share/python/create-tree.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-import re
 import csv
+import getopt
+from collections import OrderedDict
+import os
+import re
 import sys
-
-filename = '/home/users/mwilliamson/R1.x137.108.0001.hashes.txt'
 
 def check_header(filename):
     # This function checks for a valid hashdeep header
@@ -35,20 +37,75 @@ def check_header(filename):
     else:
         return None
 
+# https://www.oreilly.com/library/view/python-cookbook/0596001673/ch04s16.html
+def splitall(path):
+    allparts = []
+    while 1:
+        parts = os.path.split(path)
+        if parts[0] == path:  # sentinel for absolute paths
+            allparts.insert(0, parts[0])
+            break
+        elif parts[1] == path: # sentinel for relative paths
+            allparts.insert(0, parts[1])
+            break
+        else:
+            path = parts[0]
+            allparts.insert(0, parts[1])
+    return allparts
 
-path = check_header(filename)
-if (path is not None):
-    with open(filename, 'rb') as f:
-        reader = csv.reader(f)
-        rownum = 1
-        for row in reader:
-            if (rownum > 5):
-                object_filename = re.sub(path + '/', '', row[3])
-                print object_filename
+def generate_tree(filename, short):
+    path = check_header(filename)
+    if (path is not None):
+        sizes = OrderedDict()
+        with open(filename, 'rb') as f:
+            reader = csv.reader(f)
+            rownum = 1
+            last = None
+            for row in reader:
+                # skip header
+                if (rownum > 5):
+                    object_filename = re.sub(path + '/', '', row[3])
+                    object_size = row[0]
+                    parts = splitall(object_filename)
+                    for i in range (0, len(parts)-1, 1):
+                        if (i == 0):
+                            my_str = parts[i]
+                        else:
+                            my_str = my_str + '/' + parts[i]
+                        try:
+                            sizes[my_str] += int(object_size)
+                        except KeyError:
+                            sizes[my_str] = int(object_size)
+                rownum += 1
+            # ASCII-art generation
+            last_path = ''
+            for path, size in sizes.iteritems():
+                if (path.startswith(last_path)):
+                    print "├──" + path, size
+                else:
+                    print '└──' + path, size
+                last_path = path
+    else:
+        print("Error in header. Stopping")
 
-            rownum = rownum+1
-else:
-    print("Error in header. Stopping")
+def main(argv, script_name):
+    hashfile = ''
+    short_report = False;
+    try:
+        opts, args = getopt.getopt(argv,"hdi:",["ifile=",])
+    except getopt.GetoptError:
+        print script_name + ' -i <hashfile>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print script_name + ' -i <hashfile> [-d <show directories only>]'
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            hashfile = arg
+        elif opt in ("-d"):
+            short_report = True
+    generate_tree(hashfile, short_report)
 
-
+if __name__ == "__main__":
+    main(sys.argv[1:], sys.argv[0])
 
