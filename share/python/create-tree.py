e4978de139e21f29dc8ca379a1925857d696c732
Michael Scott Williamson <michael.williamson@tamucc.edu>
2018-11-28 15:42:24 -0600

    large data message and type summary in short mode

--- share/python/create-tree.py
+++ share/python/create-tree.py
@@ -7,6 +7,7 @@ from collections import OrderedDict
 import os
 import re
 import sys
+import textwrap
 
 def check_header(filename):
     # This function checks for a valid hashdeep header
@@ -68,6 +69,7 @@ def intToSize(size):
 
 def generate_tree(filename, short):
     path = check_header(filename)
+    filetypes = {}
     if (path is not None):
         sizes = OrderedDict()
         with open(filename, 'rb') as f:
@@ -79,6 +81,15 @@ def generate_tree(filename, short):
                 if (rownum > 5):
                     object_filename = re.sub(path + '/', '', row[3])
                     object_size = row[0]
+
+                    # Find file's filetype, add to count by filetype.
+                    filetype = os.path.splitext(row[3])[1]
+                    try:
+                        filetypes[filetype] += 1
+                    except KeyError:
+                        filetypes[filetype] = 1
+
+                    # Split out paths to keep track, by dir, of totals.
                     parts = splitall(object_filename)
                     for i in range (0, len(parts), 1):
                         if (i == 0):
@@ -92,10 +103,34 @@ def generate_tree(filename, short):
                         except KeyError:
                             sizes[my_str] = int(object_size)
                 rownum += 1
-            # ASCII-art generation
+            # Output Section
+
+            # Display filetype summary in short mode.
+            if (short):
+                print textwrap.dedent("""\
+
+                    GRIIDC LARGE DATASET NOTICE:
+
+                    GRIIDC stores extremely large data offline, but they may be requested by
+                    contacting GRIIDC via email at griidc@gomri.org to make arragements to make
+                    this data temporarily available for direct download, by shipping physical
+                    media or hard drives, or by using other methods best suited to the user's
+                    particular use case and capabilities. Please also understand some of
+                    these datasets are multiple TB in size, so it may take time to ship this
+                    data. GRIIDC will work with users to ensure that they are able to get
+                    these large data.
+                    """)
+                print
+                for file_type, type_count in filetypes.iteritems():
+                    if(file_type == ''):
+                        file_type = '(no extension)'
+                    print(str(type_count) + ', ' + file_type)
+                print
+            print("Directories in dataset file:\n")
             for path, size in sizes.iteritems():
                 if (short):
-                    # Short form, directories only
+
+                    # Display directories only in short mode.
                     if(re.search("\|EOL:$", path)):
                         pass
                     else:
