409d311f06788e8a29dc84cdca04931e1918ee43
Son Nguyen <Son.Nguyen@tamucc.edu>
2019-01-07 15:40:43 -0600

    add List

---
+++ src/Pelagos/Bundle/AppBundle/Controller/UI/RemotelyHostedDatasetsController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace Pelagos\Bundle\AppBundle\Controller\UI;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * The Remotely Hosted Datasets list controller.
+ *
+ * @Route("/remotelyhosted-datasets")
+ */
+class RemotelyHostedDatasetsController extends UIController
+{
+    /**
+     * Remotely Hosted Datasets UI.
+     *
+     * @Route("")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function defaultAction()
+    {
+        // Checks authorization of users
+        if (!$this->isGranted('ROLE_DATA_REPOSITORY_MANAGER')) {
+            return $this->render('PelagosAppBundle:template:AdminOnly.html.twig');
+        }
+
+        $GLOBALS['pelagos']['title'] = 'Remotely Hosted Datasets';
+        return $this->render('PelagosAppBundle:List:RemotelyHostedDatasets.html.twig');
+    }
+
+}
