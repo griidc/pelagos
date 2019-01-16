6bbdc00622dce9169da0bfb0a65f86cdd19adba8
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2019-01-16 11:54:07 -0600

    Removed if statement which is unnecessary

--- src/Pelagos/Bundle/AppBundle/Rabbit/Consumer/DoiConsumer.php
+++ src/Pelagos/Bundle/AppBundle/Rabbit/Consumer/DoiConsumer.php
@@ -307,9 +307,7 @@ class DoiConsumer implements ConsumerInterface
             $status = DOI::STATUS_UNAVAILABLE;
         } elseif ($dataset->getDatasetStatus() === Dataset::DATASET_STATUS_ACCEPTED
             and $restriction === DatasetSubmission::RESTRICTION_NONE) {
-            if (in_array($doiStatus, [DOI::STATUS_RESERVED, DOI::STATUS_PUBLIC]) or (preg_match('/^unavailable/', $doiStatus) === 1)) {
-                $status = DOI::STATUS_PUBLIC;
-            }
+            $status = DOI::STATUS_PUBLIC;
         }
 
         return $status;
