e09e7a5725bf7ecc3e517d2031f7b960b198ef79
Michael Scott Williamson <michael.williamson@tamucc.edu>
2019-02-01 11:00:25 -0600

    Added database migration.

---
+++ app/DoctrineMigrations/Version20190201165954.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Pelagos\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20190201165954 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE dataset_submission ADD dataset_file_cold_storage_archive_size BIGINT DEFAULT NULL');
+        $this->addSql('ALTER TABLE dataset_submission ADD dataset_file_cold_storage_archive_sha256hash TEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE dataset_submission_audit ADD dataset_file_cold_storage_archive_size BIGINT DEFAULT NULL');
+        $this->addSql('ALTER TABLE dataset_submission_audit ADD dataset_file_cold_storage_archive_sha256hash TEXT DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE dataset_submission DROP dataset_file_cold_storage_archive_size');
+        $this->addSql('ALTER TABLE dataset_submission DROP dataset_file_cold_storage_archive_sha256hash');
+        $this->addSql('ALTER TABLE dataset_submission_audit DROP dataset_file_cold_storage_archive_size');
+        $this->addSql('ALTER TABLE dataset_submission_audit DROP dataset_file_cold_storage_archive_sha256hash');
+    }
+}
