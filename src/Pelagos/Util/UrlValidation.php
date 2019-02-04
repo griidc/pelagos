a059cb17a2dbb9bac4999ef053ad24f6cd771a23
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2019-02-04 12:08:37 -0600

    Removed curl cookiefile which wasnt required and added curl header and no body

--- src/Pelagos/Util/UrlValidation.php
+++ src/Pelagos/Util/UrlValidation.php
@@ -20,19 +20,20 @@ class UrlValidation
     public function validateUrl($url)
     {
         $ch = curl_init();
-        $cookies = tempnam('/tmp','cookie.txt');
+        //php://memory is a read-write streams that allow temporary data to be stored in a file-like wrapper.
+        $cookies = tempnam('/tmp','php://memory');
         curl_setopt($ch, CURLOPT_COOKIEJAR, $cookies);
-        curl_setopt($ch, CURLOPT_COOKIEFILE, $cookies);
         curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_NOBODY, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        $output = curl_exec($ch);
+        curl_exec($ch);
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
 
         //check to see if it worked.
         if (200 !== $httpCode) {
-
             return "Could not get URL, returned HTTP code $httpCode";
         }
 
