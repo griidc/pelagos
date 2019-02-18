da61697003a6fee0e754ec9a893e8c64478c5b47
Michael Scott Williamson <michael.williamson@tamucc.edu>
2019-02-18 14:07:18 -0600

    GRIIDC code standards cleanup.

--- src/Pelagos/Bundle/AppBundle/Command/ColdStorageFlagCommand.php
+++ src/Pelagos/Bundle/AppBundle/Command/ColdStorageFlagCommand.php
@@ -10,11 +10,28 @@ use Pelagos\Entity\Dataset;
 use Pelagos\Entity\DatasetSubmission;
 use Pelagos\Entity\Person;
 
+/**
+ * This command marks a dataset as cold-stored.
+ *
+ * This command saves the original hash and size, replaces the datafile with a
+ * supplied manifest file, then re-triggers filer/hasher in an expected way for
+ * a replaced datafile.
+ */
 class ColdStorageFlagCommand extends ContainerAwareCommand
 {
 
+    /**
+     * Doctrine EntityManager, as provided in container.
+     *
+     * @var mixed entityManager
+     */
     protected $entityManager;
 
+    /**
+     * Symfony command config section.
+     *
+     * @return void
+     */
     protected function configure()
     {
         $this
@@ -23,6 +40,21 @@ class ColdStorageFlagCommand extends ContainerAwareCommand
             ->addArgument('infofile', InputArgument::REQUIRED, 'Filename of the coldinfo-formatted textfile required.');
     }
 
+    /**
+     * Symfony command execution section.
+
+     * @param InputInterface  $input  Command args.
+     * @param OutputInterface $output Output txt.
+     *
+     * @throws \Exception If cannot find dataset with provided UDI.
+     * @throws \Exception If cannot find dataset submission in dataset.
+     * @throws \Exception If new submission already somehow exists.
+     * @throws \ReflectionException If reflection class fails.
+     * @throws \Exception If stubfile not readable/accessible.
+     * @throws \Exception If infofile not readable/accessible.
+     *
+     * @return integer
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
@@ -30,17 +62,17 @@ class ColdStorageFlagCommand extends ContainerAwareCommand
         $systemPerson = $entityManager->find(Person::class, 0);
 
         $infoFileName = $input->getArgument('infofile');
-        if(file_exists($infoFileName)){
-            $infoFileContents=file($infoFileName);
+        if (file_exists($infoFileName)) {
+            $infoFileContents = file($infoFileName);
             $udi = trim($infoFileContents[0]);
 
             $nudi = preg_replace('/:/', '.', $udi);
             $infoPath = pathinfo($infoFileName)['dirname'];
-            $size = preg_replace('/size \(bytes\): /','', trim($infoFileContents[1]));
-            $hash = preg_replace('/orig sha256: /','', trim($infoFileContents[2]));
+            $size = preg_replace('/size \(bytes\): /', '', trim($infoFileContents[1]));
+            $hash = preg_replace('/orig sha256: /', '', trim($infoFileContents[2]));
             $stubFileName = "$infoPath/$nudi-manifest.zip";
 
-            if(file_exists($stubFileName)){
+            if (file_exists($stubFileName)) {
                 $output->writeln("UDI: ($udi)");
                 $output->writeln("Size: ($size)");
                 $output->writeln("Hash: ($hash)");
@@ -53,7 +85,7 @@ class ColdStorageFlagCommand extends ContainerAwareCommand
                 if (count($datasets) == 0) {
                     throw new \Exception('Could not find a dataset with the udi provided.');
                 } else {
-                    $output->writeln("Dataset Found.");
+                    $output->writeln('Dataset Found.');
                 }
 
                 $dataset = $datasets[0];
@@ -62,14 +94,14 @@ class ColdStorageFlagCommand extends ContainerAwareCommand
                 if (!($datasetSubmission instanceof DatasetSubmission)) {
                     throw new \Exception('Could not find Dataset Submission.');
                 } else {
-                    $output->writeln("Submission Found.");
+                    $output->writeln('Submission Found.');
                     // Create a new submission using latest via constructor.
                     $newDatasetSubmission = new DatasetSubmission($datasetSubmission);
                     if ($entityManager->contains($newDatasetSubmission)) {
                         throw new \Exception('Attempted to create a dataset submission that is already tracked');
                     }
-                    $output->writeln("original submission ID is: " . $dataset->getDatasetSubmission()->getId());
-                    $output->writeln("new submission ID is initially: " . $newDatasetSubmission->getId());
+                    $output->writeln('original submission ID is: ' . $dataset->getDatasetSubmission()->getId());
+                    $output->writeln('new submission ID is initially: ' . $newDatasetSubmission->getId());
 
                     // This method sets submission status to: DatasetSubmission::STATUS_IN_REVIEW (3).
                     $newDatasetSubmission->setDatasetSubmissionReviewStatus();
@@ -87,6 +119,8 @@ class ColdStorageFlagCommand extends ContainerAwareCommand
                     $newDatasetSubmission->setDatasetFileSha256Hash(null);
                     $newDatasetSubmission->setDatasetFileTransferType(DatasetSubmission::TRANSFER_TYPE_SFTP);
                     $newDatasetSubmission->setDatasetFileUri($stubFileName);
+                    $newDatasetSubmission->setDistributionFormatName('Cold Storage archive - ' .
+                        $datasetSubmission->getDistributionFormatName());
 
                     // This is normally null, unless manually set. Handle that gracefully.
                     $newDatasetSubmissionId = $newDatasetSubmission->getId();
@@ -121,11 +155,13 @@ class ColdStorageFlagCommand extends ContainerAwareCommand
                     $entityManager->persist($newDatasetSubmission);
                     $entityManager->flush($newDatasetSubmission);
 
-                    $output->writeln("Original DatasetSubmission Status: " . $datasetSubmission->getStatus());
-                    $output->writeln("New submission ID is now: " . $newDatasetSubmission->getId());
+                    $output->writeln('Original DatasetSubmission Status: ' . $datasetSubmission->getStatus());
+                    $output->writeln('New submission ID is now: ' . $newDatasetSubmission->getId());
 
-                    $output->writeln("New DatasetSubmission Status referenced in dataset: " . $dataset->getDatasetSubmission()->getStatus());
-                    $output->writeln("New submission ID in dataset is now: " . $dataset->getDatasetSubmission()->getId());
+                    $output->writeln('New DatasetSubmission Status referenced in dataset: '
+                        . $dataset->getDatasetSubmission()->getStatus());
+                    $output->writeln('New submission ID in dataset is now: '
+                        . $dataset->getDatasetSubmission()->getId());
 
                     //Use rabbitmq to process dataset file and persist the file details. This will
                     //Trigger filer and hasher (via filer) to complete the process.
@@ -134,7 +170,6 @@ class ColdStorageFlagCommand extends ContainerAwareCommand
                         'dataset.SFTP'
                     );
                 }
-
             } else {
                 throw new \Exception("Could not open $stubFileName, expected to be at same location as $infoFileName.");
             }
