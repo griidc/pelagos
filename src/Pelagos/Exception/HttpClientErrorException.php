090a236ad0940d7272c9c3608d0add79e5a844fe
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2019-02-21 15:16:30 -0600

    Added custom exception classes for client and server errors and changed util class to use them

---
+++ src/Pelagos/Exception/HttpClientErrorException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Pelagos\Exception;
+
+/**
+ * Class HttpClientErrorException.
+ *
+ * An exception to be thrown when client errors are thrown (4xx).
+ *
+ * @package Exception
+ */
+class HttpClientErrorException extends \Exception
+{
+}
---
+++ src/Pelagos/Exception/HttpServerErrorException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Pelagos\Exception;
+
+/**
+ * Class HttpServerErrorException.
+ *
+ * An exception to be thrown when server errors are thrown (5xx).
+ *
+ * @package Exception
+ */
+class HttpServerErrorException extends \Exception
+{
+}
