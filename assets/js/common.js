38c5ef34b5ea50bbb184f03042132794384e7de5
Son Nguyen <Son.Nguyen@tamucc.edu>
2019-02-28 11:27:33 -0600

    fix css

--- assets/js/common.js
+++ assets/js/common.js
@@ -10,6 +10,10 @@ require('../css/pelagos-module.css');
 
 $( document ).ready(function() {
     $("#pelagos-menu-1").hoverIntent(hoverIn, hoverOut, 'li');
+
+    $(window).resize(function(){
+        setContentHeight();
+    }).resize();
 });
 
 function hoverIn() {
@@ -21,3 +25,23 @@ function hoverOut() {
     $(this).find("ul").addClass("sf-hidden");
     $(this).removeClass("sfHover");
 }
+
+function setContentHeight() {
+    var winHeight = $(window).height();
+    var adminMenuHeight = $("#admin-menu").outerHeight(true);
+    if (adminMenuHeight) {
+        $('body').height($(window).height() - adminMenuHeight);
+    }
+    else {
+        adminMenuHeight = 0;
+    }
+    var headerHeight = $("#header").outerHeight(true);
+    var navHeight = $("#navigation").outerHeight(true);
+    var messagesHeight = $("#page > .messages").length ? $("#page > .messages").outerHeight(true) : 0;
+    var footerHeight = $('#footer').length ? $("#footer").outerHeight(true) : 0;
+
+    var newheight = winHeight - (adminMenuHeight + headerHeight + navHeight + messagesHeight + footerHeight);
+
+    $(".page-pelagos-full #main-wrapper").height(newheight);
+}
+
