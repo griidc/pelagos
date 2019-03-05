b4cce1f42a958d2662bfc3e4fca8ba554af255f2
Michael Scott Williamson <michael.williamson@tamucc.edu>
2019-03-05 14:45:09 -0600

    Remove unneeded utility class.

--- src/Pelagos/Entity/DatasetSubmission.php
+++ src/Pelagos/Entity/DatasetSubmission.php
@@ -5,7 +5,6 @@ namespace Pelagos\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Pelagos\Util\PelagosUtilities;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
