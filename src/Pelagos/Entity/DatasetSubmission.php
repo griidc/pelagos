089586a4a3f4f600b3850b480bec258a01d5effd
Michael Scott Williamson <michael.williamson@tamucc.edu>
2019-03-07 15:47:36 -0600

    Add getter for ErddapUrl protocol.

--- src/Pelagos/Entity/DatasetSubmission.php
+++ src/Pelagos/Entity/DatasetSubmission.php
@@ -2670,6 +2670,21 @@ class DatasetSubmission extends Entity
     }
 
     /**
+     * Getter for the erddap url protocol.
+     *
+     * @return string|null
+     */
+    public function getErddapUrlProtocol()
+    {
+        if ($this->erddapUrl !== null) {
+            preg_match( '/^(.*):.*$/', $this->erddapUrl, $matches );
+            return $matches[1];
+        } else {
+            return null;
+        }
+    }
+
+    /**
      * Setter for the erddap url.
      *
      * @param string|null $erddapUrl Erddap url.
