cb2f41529cca6f01f610e34c5944fe88770ada85
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2019-03-19 11:02:24 -0500

    Added jquery plugin for pagination

--- src/Pelagos/Bundle/AppBundle/Resources/public/js/search.js
+++ src/Pelagos/Bundle/AppBundle/Resources/public/js/search.js
@@ -8,21 +8,17 @@ $(document).ready(function () {
     if (count > pageSize) {
         var pageCount = Math.ceil(count / pageSize);
 
-        for (var i = 0; i < 3; i++) {
-            if (i === 0)
-                $("#paginate").append('<li class="page-item disabled">\n' +
-                    '      <a class="page-link" href="#" tabindex="-1">Previous</a>\n' +
-                    '    </li><li class="page-item active"><a class="page-link" href="#">' + (i + 1) + '' +
-                    '<span class="sr-only">(current)</span></a></li>');
-            else
-                $("#paginate").append('<li class="page-item"><a class="page-link" href="#">' + (i + 1) + '</a></li>');
-        }
-
-        $("#paginate").append('<li class="page-item"><a class="page-link" href="#">Next</a></li>');
-
-        $("#paginate li a").click(function() {
-            $("#paginate li").removeClass("active");
-            $(this).parent().addClass("active");
+        $("#search-pagination").twbsPagination({
+            totalPages: pageCount,
+            visiblePages: 6,
+            next: 'Next',
+            prev: 'Prev',
+            href: true,
+            onPageClick: function (event, page) {
+                //fetch content and render here
+                var url = Routing.generate("pelagos_app_ui_searchpage_default") + "?query=" + $("#search").val();
+                $(this).find("li a").attr("href", url);
+            }
         });
     }
 });
