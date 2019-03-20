129b12de13fc48af7aa53be2f904f7d088b3438c
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2019-03-20 09:15:57 -0500

    Changed jquery pagination plugin

--- src/Pelagos/Bundle/AppBundle/Resources/public/js/search.js
+++ src/Pelagos/Bundle/AppBundle/Resources/public/js/search.js
@@ -8,19 +8,24 @@ $(document).ready(function () {
     var startPage = Number(urlParts[1]);
     if (count > pageSize) {
         var pageCount = Math.ceil(count / pageSize);
+        var url = Routing.generate("pelagos_app_ui_searchpage_default") + "?query=" + $("#search").val() + "&page=";
 
-        $("#search-pagination").twbsPagination({
-            startPage: startPage,
-            totalPages: pageCount,
-            visiblePages: 6,
-            next: 'Next',
-            prev: 'Prev',
-            href: true,
-            onPageClick: function (event, page) {
-                //fetch content and render here
-                var url = Routing.generate("pelagos_app_ui_searchpage_default") + "?query=" + $("#search").val();
-                $(this).find("li a").attr("href", url);
-            }
+        $('#search-pagination').bootpag({
+            total: pageCount,
+            page: startPage,
+            maxVisible: 5,
+            leaps: true,
+            firstLastUse: true,
+            first: "←",
+            last: "→",
+            wrapClass: "pagination",
+            activeClass: "active",
+            disabledClass: "disabled",
+            nextClass: "next",
+            prevClass: "prev",
+            lastClass: "last",
+            firstClass: "first",
+            href: url + "{{number}}"
         });
     }
 });
