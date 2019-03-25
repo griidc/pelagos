521c34946ccd6871d1174443a87f281adbf700bb
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2019-03-25 15:02:26 -0500

    Looping around with different URL if return is not biblio.

--- src/Pelagos/Util/PubLinkUtil.php
+++ src/Pelagos/Util/PubLinkUtil.php
@@ -34,23 +34,44 @@ class PubLinkUtil
      */
     public function fetchCitation($doi, $style = PublicationCitation::CITATION_STYLE_APA, $locale = 'en-US')
     {
-        $statusCodes = array(
-            200 => 'The request was OK.',
-            204 => 'The request was OK but there was no metadata available.',
-            404 => 'The DOI requested doesn\'t exist.',
-            406 => 'Can\'t serve any requested content type.',
-        );
+        $curlCitation = $this->curlCitation($doi);
+        $curlResponse = $curlCitation['curlResponse'];
+        $status = $curlCitation['status'];
+        $errorText = $curlCitation['errorText'];
 
+        $citation = new PublicationCitation($curlResponse, $style, $locale);
+
+        return array('citation' => $citation, 'status' => $status, 'errorText' => $errorText);
+    }
+
+    /**
+     * This method looks up a citation string at doi.org.
+     *
+     * @param string       $doi         The DOI of the publication.
+     * @param string|null  $style       The textual style desired.
+     * @param string|null  $locale      The character representation desired.
+     * @param boolean|null $useCrossRef Use the alternative way to retrieve the citation.
+     *
+     * @return array
+     */
+    private function curlCitation($doi, $style = PublicationCitation::CITATION_STYLE_APA, $locale = 'en-US', $useCrossRef = false)
+    {
         $ch = curl_init();
-        $url = 'http://dx.doi.org/' . $doi;
-        $header = array("Accept: text/bibliography; style=$style; locale=$locale");
 
+        if (!$useCrossRef) {
+            $url = 'http://dx.doi.org/' . $doi;
+            $header = array("Accept: text/bibliography; style=$style; locale=$locale");
+        } else {
+            $url = "https://api.crossref.org/works/$doi/transform/text/x-bibliography";
+        }
 
         curl_setopt($ch, CURLOPT_URL, $url);
         // Since the request 303's forward, we have to turn follow on.
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        if (!empty($header)) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        }
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
         $curlResponse = curl_exec($ch);
@@ -58,13 +79,12 @@ class PubLinkUtil
         $curlInfo = curl_getinfo($ch);
         curl_close($ch);
         $status = $curlInfo['http_code'];
+        $contentType = $curlInfo['content_type'];
 
-        if ($status == 200) {
-            $citation = new PublicationCitation($curlResponse, $style, $locale);
-        } else {
-            $citation = null;
+        if ($status == 200 and !preg_match('/text\/bibliography/', $contentType) and !$useCrossRef) {
+            return $this->curlCitation($doi, $style, $locale, true);
         }
 
-        return array('citation' => $citation, 'status' => $status, 'errorText' => $errorText);
+        return array('curlResponse' => $curlResponse, 'status' => $status, 'errorText' => $errorText);
     }
 }
