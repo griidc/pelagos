331add855264f9c12581f4954fcf070bacc8d9a9
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2019-04-08 14:25:02 -0500

    Added js function get facet ids and check them if present in the url

--- src/Pelagos/Bundle/AppBundle/Resources/public/js/search.js
+++ src/Pelagos/Bundle/AppBundle/Resources/public/js/search.js
@@ -6,9 +6,10 @@ $(document).ready(function () {
     var count = $("#count").attr("data-content");
     var urlParts = window.location.search.split("&");
     var startPage = getPageNo(urlParts);
-    if (urlParts[2]) {
-        var rgId = urlParts[2].split("resGrp=")[1];
-        var foId = urlParts[2].split("fundOrg=")[1];
+    if (urlParts.length > 2) {
+        var facetIds = getFacetIds(urlParts);
+        var rgId = facetIds["rgId"];
+        var foId = facetIds["foId"];
     }
     var url = Routing.generate("pelagos_app_ui_searchpage_default") + "?query=" + $("#searchBox").val() + "&page=" + startPage;
 
@@ -18,11 +19,24 @@ $(document).ready(function () {
         $("#pageNo").attr("disabled", true);
     });
 
+    if (rgId) {
+        rgId = rgId.split(',');
+        if (rgId.length > 0) {
+            $.each(rgId, function (k, v) {
+                $("#" + rgId[k]).attr("checked", true);
+            });
+        }
+    }
 
-    $("#" + rgId).attr("checked", true);
-    $("#" + foId).attr("checked", true);
+    if (foId) {
+        foId = foId.split(',');
+        if(foId.length > 0) {
+            $.each(foId, function (k, v){
+                $("#" + foId[k]).attr("checked", true);
+            });
+        }
+    }
 
-    $("#resgrp-facet :checkbox:checked");
     var rgIdsArray = [];
     var foIdsArray = [];
     var rgIds = "";
@@ -34,13 +48,13 @@ $(document).ready(function () {
             rgIdsArray.push($(this).attr("id"));
         });
 
-        if (rgIdsArray.length !== 0) {
+        if (rgIdsArray.length > 0) {
             rgIds = rgIdsArray.join(",");
         }
         $("#fundorg-facet :checkbox:checked").each(function () {
             foIdsArray.push($(this).attr("id"));
         });
-        if (foIdsArray.length !== 0) {
+        if (foIdsArray.length > 0) {
             foIds = foIdsArray.join(',');
         }
 
@@ -95,4 +109,15 @@ function getPageNo(urlParts) {
         pageNo = Number(urlParts[1].split("page=")[1]);
     }
     return pageNo;
+}
+
+function getFacetIds(urlParts) {
+    if (urlParts.length > 3) {
+        var foId = urlParts[2].split("fundOrg=")[1];
+        var rgId = urlParts[3].split("resGrp=")[1];
+    } else {
+        var rgId = urlParts[2].split("resGrp=")[1];
+        var foId = urlParts[2].split("fundOrg=")[1];
+    }
+    return {"foId": foId, "rgId": rgId};
 }
