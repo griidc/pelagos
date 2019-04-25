ca61df690ece7082451158561bfef1f3f4e4937b
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2019-04-25 14:47:07 -0500

    Removed unnecessary html show func

--- src/Pelagos/Bundle/AppBundle/Resources/public/js/external-download-log.js
+++ src/Pelagos/Bundle/AppBundle/Resources/public/js/external-download-log.js
@@ -3,8 +3,6 @@ var $ = jQuery.noConflict();
 $(document).ready(function () {
     "use strict";
 
-    $("html").show();
-
     $(".pelagosNoty").pelagosNoty({timeout: 0, showOnTop:false});
 
     let userType = $("#externalDownloadLog_userType");
