0bc1a038d1a52d571c771a43ae9c6142a01a88bf
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2019-05-06 13:21:25 -0500

    Undo webpack changes.

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ Encore
     .setOutputPath('web/build/')
 
     // the public path used by the web server to access the previous directory
-    .setPublicPath('/build')
+    .setPublicPath(Encore.isProduction() ? '/pelagos-symfony/build' : '/build')
 
     // will create web/build/app.js and web/build/app.css
     .createSharedEntry('common', './assets/js/common.js')
