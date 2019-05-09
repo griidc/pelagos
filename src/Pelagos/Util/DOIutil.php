12e99330b30c817e6b58ee29fbc7c7835147b80e
Michael Scott Williamson <michael.williamson@tamucc.edu>
2019-05-09 16:33:39 -0500

    DOI source for minting to GRIIDC-generated.

--- src/Pelagos/Util/DOIutil.php
+++ src/Pelagos/Util/DOIutil.php
@@ -62,7 +62,7 @@ class DOIutil
     }
 
     /**
-     * This function will mint a DOI.
+     * This function will mint a DOI, generating the unique DOI on-site.
      *
      * @param string $url             URL for DOI.
      * @param string $creator         Creator for DOI.
@@ -71,9 +71,6 @@ class DOIutil
      * @param string $publicationYear Published Date for DOI.
      * @param string $status          Status of the DOI, by default is reserved.
      * @param string $resourcetype    Type for DOI Request, by default Dataset.
-     *
-     * @throws HttpClientErrorException When there was an 4xx error negotiating with EZID.
-     * @throws HttpServerErrorException When there was an 5xx error negotiating with EZID.
      *
      * @return string The DOI issued by EZID.
      */
@@ -86,50 +83,10 @@ class DOIutil
         $status = 'reserved',
         $resourcetype = 'Dataset'
     ) {
-        $input = '_target:' . $this->escapeSpecialCharacters($url) . "\n";
-        $input .= "_profile:datacite\n";
-        $input .= "_status:$status\n";
-        $input .= 'datacite.creator:'
-            . $this->escapeSpecialCharacters($creator)
-            . "\n";
-        $input .= 'datacite.title:'
-            . $this->escapeSpecialCharacters($title)
-            . "\n";
-        $input .= 'datacite.publisher:' . $this->escapeSpecialCharacters($publisher) . "\n";
-        $input .= "datacite.publicationyear:$publicationYear\n";
-        $input .= "datacite.resourcetype:$resourcetype";
+        $doi = $this->generateDoi();
+        $this->createDOI($doi, $url, $creator, $title, $publisher, $publicationYear, $status, $resourcetype);
 
-        utf8_encode($input);
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->url . '/shoulder/' . $this->doishoulder);
-        curl_setopt($ch, CURLOPT_USERPWD, $this->doiusername . ':' . $this->doipassword);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt(
-            $ch,
-            CURLOPT_HTTPHEADER,
-            array('Content-Type: text/plain; charset=UTF-8','Content-Length: ' . strlen($input))
-        );
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $input);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $output = curl_exec($ch);
-        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-
-        //check to see if it worked.
-        //using in array because EZID API returns 201 and EZ Datacite API returns 200.
-        if (!in_array($httpCode, [200, 201])) {
-            $expMsg = "ezid failed with:$httpCode($output)";
-            if ($httpCode >= 400 and $httpCode <= 499) {
-                throw new HttpClientErrorException($expMsg, $httpCode);
-            } elseif ($httpCode >= 500 or $httpCode == 0) {
-                throw new HttpServerErrorException($expMsg, $httpCode);
-            }
-        }
-
-        $doi = preg_match('/^success: (doi:\S+)/', $output, $matches);
-
-        return $matches[1];
+        return $doi;
     }
 
     /**
