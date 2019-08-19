e7ddbcf6ddb3315c3bee647623832e913e450ede
Michael Scott Williamson <michael.williamson@tamucc.edu>
2019-08-19 15:35:46 -0500

    Add DB migration, with default FALSE for lock status.

---
+++ app/DoctrineMigrations/Version20190819203038.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace Pelagos\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190819203038 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE research_group ADD lock_status BOOLEAN NOT NULL DEFAULT FALSE');
+        $this->addSql('ALTER TABLE research_group_audit ADD lock_status BOOLEAN DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE research_group DROP lock_status');
+        $this->addSql('ALTER TABLE research_group_audit DROP lock_status');
+    }
+}
