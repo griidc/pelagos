b1a75eedacd0ac56bd23143f69aef00475b71809
Michael Scott Williamson <michael.williamson@tamucc.edu>
2019-08-21 10:30:06 -0500

    Replaced migration for new col names

--- app/DoctrineMigrations/Version20190821152614.php
+++ app/DoctrineMigrations/Version20190821152614.php
@@ -8,15 +8,15 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190819203038 extends AbstractMigration
+final class Version20190821152614 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER TABLE research_group ADD lock_status BOOLEAN NOT NULL DEFAULT FALSE');
-        $this->addSql('ALTER TABLE research_group_audit ADD lock_status BOOLEAN DEFAULT NULL');
+        $this->addSql('ALTER TABLE research_group ADD locked BOOLEAN NOT NULL DEFAULT FALSE');
+        $this->addSql('ALTER TABLE research_group_audit ADD locked BOOLEAN DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -24,7 +24,7 @@ final class Version20190819203038 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER TABLE research_group DROP lock_status');
-        $this->addSql('ALTER TABLE research_group_audit DROP lock_status');
+        $this->addSql('ALTER TABLE research_group DROP locked');
+        $this->addSql('ALTER TABLE research_group_audit DROP locked');
     }
 }
