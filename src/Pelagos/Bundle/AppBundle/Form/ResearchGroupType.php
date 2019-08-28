baec7e9ac228ffd0c26781a8f62b7a0d91b2523b
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2019-08-28 13:16:01 -0500

    Removed un needed use statements.

--- src/Pelagos/Bundle/AppBundle/Form/ResearchGroupType.php
+++ src/Pelagos/Bundle/AppBundle/Form/ResearchGroupType.php
@@ -4,8 +4,6 @@ namespace Pelagos\Bundle\AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
