ac84642342a2260e70b703af0f829606927bad8f
Michael Scott Williamson <michael.williamson@tamucc.edu>
2019-10-14 15:16:53 -0500

    Removed un-needed JS router.

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
-    .setPublicPath('/build')
+    .setPublicPath('/mwilliamson/build')
     // only needed for CDN's or sub-directory deploy
     .setManifestKeyPrefix('build/')
 
@@ -35,7 +35,7 @@ Encore
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
     //.enableSingleRuntimeChunk()
-    
+
     // No runtime.js needed.
     .disableSingleRuntimeChunk()
 
