389122de0b619fded8b764482a54ba4ad28f2a06
Michael Scott Williamson <michael.williamson@tamucc.edu>
2019-10-15 15:34:03 -0500

    Add MdappLogger utility. Handle injected parameter.

---
+++ src/Util/MdappLogger.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Util;
+
+use Doctrine\ORM\EntityManager;
+
+/**
+ * This is a Metadata utility class.
+ */
+class MdappLogger
+{
+    /**
+     * String of full filepath to logfile.
+     *
+     * @var string
+     */
+    protected $logfile;
+
+    /**
+     * Constructor for dependency injection.
+     *
+     * @param string $logfile String with filepath.
+     */
+    public function __construct(string $mdappLogfile)
+    {
+        $this->logfile = $mdappLogfile;
+    }
+
+    /**
+     * Writes MDAPP-related events to logfile.
+     *
+     * @param string $message The text to log to file.
+     *
+     * @return void
+     */
+    public function writeLog($message)
+    {
+        $tz = ini_get('date.timezone');
+        $timestamp = new \DateTime('now', new \DateTimeZone($tz));
+        $successfulWrite = file_put_contents(
+            $this->logfile,
+            $timestamp->format('r') . ": $message\n",
+            (FILE_APPEND | LOCK_EX)
+        );
+    }
+}
