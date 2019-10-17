04d92a1dfd4775b70fdb49793acc174f9631dd5f
Michael Scott Williamson <michael.williamson@tamucc.edu>
2019-10-17 10:40:29 -0500

    Update namespace in controller.

--- src/Controller/UI/DatasetSummaryController.php
+++ src/Controller/UI/DatasetSummaryController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Pelagos\Bundle\AppBundle\Controller\UI;
+namespace App\Controller\UI;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
