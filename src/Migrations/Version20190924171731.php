6b16ed98c48f19114c1ae76cd78ab369316fa986
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2019-11-07 14:43:55 -0600

    Migrations folder

--- src/Migrations/Version20190924171731.php
+++ src/Migrations/Version20190924171731.php
@@ -8,16 +8,18 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190417162649 extends AbstractMigration
+final class Version20190924171731 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER TABLE funding_organization ADD short_name citext DEFAULT NULL');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_2A1BCCA03EE4B093 ON funding_organization (short_name)');
-        $this->addSql('ALTER TABLE funding_organization_audit ADD short_name citext DEFAULT NULL');
+        $this->addSql('SET SESSION IntervalStyle TO ISO_8601');
+        $this->addSql('ALTER DATABASE '. $_ENV['DATABASE_NAME'] . ' SET timezone TO \'UTC\'');
+        $this->addSql('ALTER TABLE person_token ALTER valid_for TYPE VARCHAR(255)');
+        $this->addSql('ALTER TABLE person_token ALTER valid_for DROP DEFAULT');
+
     }
 
     public function down(Schema $schema) : void
@@ -25,8 +27,8 @@ final class Version20190417162649 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('DROP INDEX UNIQ_2A1BCCA03EE4B093');
-        $this->addSql('ALTER TABLE funding_organization DROP short_name');
-        $this->addSql('ALTER TABLE funding_organization_audit DROP short_name');
+        $this->addSql('ALTER DATABASE '. $_ENV['DATABASE_NAME'] . ' SET timezone TO \'US/Central\'');
+        $this->addSql('ALTER TABLE person_token ALTER valid_for TYPE INTERVAL  USING valid_for::interval');
+        $this->addSql('ALTER TABLE person_token ALTER valid_for DROP DEFAULT');
     }
 }
