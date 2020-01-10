ad801030ea2b9d7f7e6256b8077e351f8a56b91e
Michael Scott Williamson <michael.williamson@tamucc.edu>
2020-01-10 09:20:41 -0600

    Revert "Make methods field mandatory."
    This reverts commit 1216cbc8a47baa0a7d5aa78a5ce5ff6e4dbc7c2f.

--- src/Form/DatasetSubmissionType.php
+++ src/Form/DatasetSubmissionType.php
@@ -132,7 +132,7 @@ class DatasetSubmissionType extends AbstractType
             ))
             ->add('suppMethods', Type\TextareaType::class, array(
                 'label' => 'Supplemental Information - Methods',
-                'required' => true,
+                'required' => false,
                 'attr' => array('rows' => '5'),
             ))
             ->add('suppInstruments', Type\TextareaType::class, array(
