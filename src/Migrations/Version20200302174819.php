35a70ca5d9ccd910bbe4010621a4f7464d247e34
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2020-03-02 11:49:24 -0600

    Added migration, this is due to new doctrine directive.

---
+++ src/Migrations/Version20200302174819.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200302174819 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DROP SEQUENCE revisions_id_seq CASCADE');
+        $this->addSql('DROP TABLE revisions');
+        $this->addSql('ALTER TABLE dataset_submission ADD dataset_file_sha256_hash TEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE dataset_submission ADD dataset_file_cold_storage_archive_sha256_hash TEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE dataset_submission ADD metadata_file_sha256_hash TEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE dataset_submission DROP dataset_file_sha256hash');
+        $this->addSql('ALTER TABLE dataset_submission DROP metadata_file_sha256hash');
+        $this->addSql('ALTER TABLE dataset_submission DROP dataset_file_cold_storage_archive_sha256hash');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('CREATE SEQUENCE revisions_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE revisions (id SERIAL NOT NULL, "timestamp" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, username VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('ALTER TABLE dataset_submission ADD dataset_file_sha256hash TEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE dataset_submission ADD metadata_file_sha256hash TEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE dataset_submission ADD dataset_file_cold_storage_archive_sha256hash TEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE dataset_submission DROP dataset_file_sha256_hash');
+        $this->addSql('ALTER TABLE dataset_submission DROP dataset_file_cold_storage_archive_sha256_hash');
+        $this->addSql('ALTER TABLE dataset_submission DROP metadata_file_sha256_hash');
+    }
+}
