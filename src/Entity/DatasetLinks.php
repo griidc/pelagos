9c3501d7b88852e92790f506952fda92cfe6fd42
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2020-03-03 13:25:28 -0600

    Cleaned up class.

--- src/Entity/DatasetLinks.php
+++ src/Entity/DatasetLinks.php
@@ -5,64 +5,109 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
+ * Dataset Links Entity class.
+ *
  * @ORM\Entity(repositoryClass="App\Repository\DatasetLinksRepository")
  */
 class DatasetLinks extends Entity
 {
     /**
-     * Valid values for self::$temporalExtent.
+     * Valid values for self::$functionCode.
      *
-     * The array keys are the values to be set in self::temporalExtent.
+     * The array keys are the values to be set in self::functionCode.
      */
-    const TEMPORAL_EXTENT_DESCRIPTIONS = [
-        'ground condition' => [
-            'name' => 'Ground Condition',
-            'description' => 'Data represent the actual condition of things on the ground during ' .
-                             'the time period specified and may also be used to characterize data ' .
-                             'generated from a sample collection in the field when samples are subsequently ' .
-                             'analyzed in a laboratory.'
+    const ONLINE_FUNCTION_CODES = [
+        'download' => [
+            'name' => 'Download',
+            'description' => 'online instructions for transferring data from one storage device or system to another',
+            'code' => '001',
         ],
-        'modeled period' => [
-            'name' => 'Modeled Period',
-            'description' => 'Data represents simulated conditions during the time period, ' .
-                             'and may be used to characterize data generated using a computational model.'
+        'information' => [
+            'name' => 'Information',
+            'description' => 'online information about the resource',
+            'code' => '002',
         ],
-        'ground condition and modeled period' => [
-            'name' => 'Ground Condition and Modeled Period',
-            'description' => 'Both choices apply.'
+        'offlineAccess' => [
+            'name' => 'Offline Access',
+            'description' => 'online instructions for requesting the resource from the provider',
+            'code' => '003',
+        ],
+        'order' => [
+            'name' => 'Order',
+            'description' => 'online order process for obtaining the resource',
+            'code' => '004',
+        ],
+        'search' => [
+            'name' => 'Search',
+            'description' => 'online search interface for seeking out information about the resource',
+            'code' => '005',
         ],
     ];
     
     /**
+     * Url for the Dataset Link.
+     *
+     * @var string
+     *
      * @ORM\Column(type="text")
      */
-    private $url;
+    protected $url;
 
     /**
+     * Name for the Dataset Link.
+     *
+     * @var string
+     *
      * @ORM\Column(type="text")
      */
-    private $name;
+    protected $name;
 
     /**
+     * Description for the Dataset Link.
+     *
+     * @var string
+     *
      * @ORM\Column(type="text")
      */
-    private $description;
+    protected $description;
 
     /**
-     * @ORM\Column(type="integer")
+     * Function code for the Dataset Link.
+     *
+     * @see ONLINE_FUNCTION_CODES
+     
+     * @var string
+     *
+     * @ORM\Column(type="text")
      */
-    private $functionCode;
+    protected $functionCode;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * Protocol for the Dataset Link.
+     *
+     * @var string
+     *
+     * @ORM\Column(type="text", nullable=true)
      */
-    private $protocol;
+    protected $protocol;
 
-    public function getUrl(): ?string
+    /**
+     * Get the URL for the Dataset Link.
+     *
+     * @return string
+     */
+    public function getUrl(): string
     {
         return $this->url;
     }
 
+    /**
+     * Set the URL for this Dataset Link.
+     *
+     * @param string $url The URL for this Dataset Link.
+     *
+     * @return void
+     */
     public function setUrl(string $url): self
     {
         $this->url = $url;
@@ -70,11 +115,23 @@ class DatasetLinks extends Entity
         return $this;
     }
 
+    /**
+     * Get the name for the Dataset Link.
+     *
+     * @return string
+     */
     public function getName(): ?string
     {
         return $this->name;
     }
 
+    /**
+     * Set the name for this Dataset Link.
+     *
+     * @param string $name The name for this Dataset Link.
+     *
+     * @return self
+     */
     public function setName(string $name): self
     {
         $this->name = $name;
@@ -82,35 +139,71 @@ class DatasetLinks extends Entity
         return $this;
     }
 
+    /**
+     * Get the description for the Dataset Link.
+     *
+     * @return string
+     */
     public function getDescription(): ?string
     {
         return $this->description;
     }
 
-    public function setDescription(string $description): self
+    /**
+     * Set the description for the Dataset Link.
+     *
+     * @param string $string The description for the Dataset Link.
+     *
+     * @return self
+     */
+    public function setDescription(string $string): self
     {
         $this->description = $description;
 
         return $this;
     }
 
-    public function getfunctionCode(): ?int
+    /**
+     * Get the function code for the Dataset Link.
+     *
+     * @return string
+     */
+    public function getfunctionCode(): string
     {
         return $this->functionCode;
     }
 
-    public function setfunctionCode(int $functionCode): self
+    /**
+     * Set the function code for the Dataset Link.
+     *
+     * @param string $functionCode The function code for the Dataset Link.
+     *
+     * @return self
+     */
+    public function setFunctionCode(string $functionCode): self
     {
         $this->functionCode = $functionCode;
 
         return $this;
     }
 
+    /**
+     * Get the choice list for restrictions.
+     *
+     * @return string|null
+     */
     public function getProtocol(): ?string
     {
         return $this->protocol;
     }
 
+    /**
+     * Set the protocol for the Dataset Link.
+     *
+     * @param string|null $protocol The protocol for the Dataset Link.
+     *
+     * @return self
+     */
     public function setProtocol(?string $protocol): self
     {
         $this->protocol = $protocol;
