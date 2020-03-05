516ce31a4fd493d708008cb9b8736a5f4176b347
Michael Scott Williamson <michael.williamson@tamucc.edu>
2020-03-05 12:46:59 -0600

    Added data migrations.

--- src/Migrations/Version20200305163604.php
+++ src/Migrations/Version20200305163604.php
@@ -15,9 +15,15 @@ final class Version20200305163604 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
+        $this->addSql('ALTER TABLE publication ADD citation_text citext');
+
+        // Migrate the attribute in: (we can safely assume there was only 1 Citation per PublicationCitation)
+        $this->addSql('UPDATE publication SET citation_text = (SELECT citation_text FROM publication_citation WHERE publication_id = publication.id)');
+
         $this->addSql('DROP SEQUENCE publication_citation_id_seq CASCADE');
         $this->addSql('DROP TABLE publication_citation');
-        $this->addSql('ALTER TABLE publication ADD citation_text citext NOT NULL');
+
+
     }
 
     public function down(Schema $schema) : void
@@ -33,6 +39,10 @@ final class Version20200305163604 extends AbstractMigration
         $this->addSql('ALTER TABLE publication_citation ADD CONSTRAINT fk_d5da692f38b217a7 FOREIGN KEY (publication_id) REFERENCES publication (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE publication_citation ADD CONSTRAINT fk_d5da692f61220ea6 FOREIGN KEY (creator_id) REFERENCES person (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE publication_citation ADD CONSTRAINT fk_d5da692fd079f553 FOREIGN KEY (modifier_id) REFERENCES person (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+
+        // Migrate attribute back. Creator/modifier and associated timestamps should have been the same anyway, even as separate entity.
+        // Since only 1 Citation per Publication, and it's a new table, works without calling the sequence.
+        $this->addSql('INSERT INTO publication_citation (id, publication_id, creator_id, modifier_id, citation_text, style, locale, creation_time_stamp, modification_time_stamp) SELECT id, id, creator_id, modifier_id, citation_text, \'apa\', \'UTF-8\', creation_time_stamp, modification_time_stamp FROM publication');
         $this->addSql('ALTER TABLE publication DROP citation_text');
     }
 }
