120286546606f6349023076f6dbb3147d736bb1f
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2020-03-05 09:03:07 -0600

    Changed from add/remove column, to rename.

--- src/Migrations/Version20200302174819.php
+++ src/Migrations/Version20200302174819.php
@@ -17,12 +17,9 @@ final class Version20200302174819 extends AbstractMigration
 
         $this->addSql('DROP SEQUENCE revisions_id_seq CASCADE');
         $this->addSql('DROP TABLE revisions');
-        $this->addSql('ALTER TABLE dataset_submission ADD dataset_file_sha256_hash TEXT DEFAULT NULL');
-        $this->addSql('ALTER TABLE dataset_submission ADD dataset_file_cold_storage_archive_sha256_hash TEXT DEFAULT NULL');
-        $this->addSql('ALTER TABLE dataset_submission ADD metadata_file_sha256_hash TEXT DEFAULT NULL');
-        $this->addSql('ALTER TABLE dataset_submission DROP dataset_file_sha256hash');
-        $this->addSql('ALTER TABLE dataset_submission DROP metadata_file_sha256hash');
-        $this->addSql('ALTER TABLE dataset_submission DROP dataset_file_cold_storage_archive_sha256hash');
+        $this->addSql('ALTER TABLE dataset_submission RENAME COLUMN dataset_file_sha256hash TO dataset_file_sha256_hash');
+        $this->addSql('ALTER TABLE dataset_submission RENAME COLUMN dataset_file_cold_storage_archive_sha256hash TO dataset_file_cold_storage_archive_sha256_hash');
+        $this->addSql('ALTER TABLE dataset_submission RENAME COLUMN metadata_file_sha256hash TO metadata_file_sha256_hash');
     }
 
     public function down(Schema $schema) : void
@@ -33,11 +30,9 @@ final class Version20200302174819 extends AbstractMigration
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('CREATE SEQUENCE revisions_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE revisions (id SERIAL NOT NULL, "timestamp" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, username VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
-        $this->addSql('ALTER TABLE dataset_submission ADD dataset_file_sha256hash TEXT DEFAULT NULL');
-        $this->addSql('ALTER TABLE dataset_submission ADD metadata_file_sha256hash TEXT DEFAULT NULL');
-        $this->addSql('ALTER TABLE dataset_submission ADD dataset_file_cold_storage_archive_sha256hash TEXT DEFAULT NULL');
-        $this->addSql('ALTER TABLE dataset_submission DROP dataset_file_sha256_hash');
-        $this->addSql('ALTER TABLE dataset_submission DROP dataset_file_cold_storage_archive_sha256_hash');
-        $this->addSql('ALTER TABLE dataset_submission DROP metadata_file_sha256_hash');
+
+        $this->addSql('ALTER TABLE dataset_submission RENAME COLUMN dataset_file_sha256_hash TO dataset_file_sha256hash');
+        $this->addSql('ALTER TABLE dataset_submission RENAME COLUMN dataset_file_cold_storage_archive_sha256_hash TO dataset_file_cold_storage_archive_sha256hash');
+        $this->addSql('ALTER TABLE dataset_submission RENAME COLUMN metadata_file_sha256_hash TO metadata_file_sha256hash');
     }
 }
