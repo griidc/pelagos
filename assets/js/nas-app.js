496f2d92c20a2277f8d1f1d1e747e271eacb20df
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2020-04-08 14:55:37 -0500

    Changed Jira buttons to theme.

--- assets/js/nas-app.js
+++ assets/js/nas-app.js
@@ -7,13 +7,13 @@
 const $ = require('jquery');
 global.$ = global.jQuery = $;
 
-
+require('../css/nas-grp.css');
 require('../css/template.css');
+require('../css/jira-buttons.css');
 require('../css/superfish-navbar.css');
 require('../css/superfish.css');
 require('../css/pelagos-module.css');
 require('../css/messages.css');
-require('../css/nas-grp.css');
 
 const routes = require('../../public/js/fos_js_routes.json');
 import Routing from '../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
