9eea8ca615f2bda4904717df33ef53bd189d2c33
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2020-04-17 12:20:31 -0500

    Removed unused plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 var Encore = require('@symfony/webpack-encore');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
