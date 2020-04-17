44b2f5d3d42d3b28f45bc227cdcb9e012367253a
Michael Scott Williamson <michael.williamson@tamucc.edu>
2020-04-17 15:26:05 -0500

    Fixed test code.

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ Encore
     .setOutputPath('public/build/')
 
     // public path used by the web server to access the output path
-    .setPublicPath(Encore.isProduction() ? '/pelagos-symfony/build' : '/mwilliamson/build')
+    .setPublicPath(Encore.isProduction() ? '/pelagos-symfony/build' : '/build')
 
     // only needed for CDN's or sub-directory deploy
     .setManifestKeyPrefix('build/')
