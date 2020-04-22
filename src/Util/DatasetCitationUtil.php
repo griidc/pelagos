8fbbefb58a375d752962cd79111b12f37487ac8a
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2020-04-22 10:35:17 -0500

    Inital dataset Util needs work.

---
+++ src/Util/DatasetCitationUtil.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Util;
+
+use App\Entity\Dataset;
+
+/**
+ * A utility to create a Dataset Citation Text.
+ */
+class DatasetCitationUtil
+{
+
+    /**
+     * Creates the and return the Dataset Citation Text.
+     *
+     * @param Dataset $dataset The Dataset that has the data.
+     *
+     * @return string The Dataset Citation string.
+     */
+    public static function getCitation(Dataset $dataset) :string
+    {
+        $title = $dataset->getTitle();
+        $title = preg_replace('/\.$/', '', $title);
+        $udi = $dataset->getUdi();
+        $author = $this->getAuthors();
+        $year = null;
+        if ($dataset->getAcceptedDate() instanceof \Datetime) {
+            $year = $dataset->getAcceptedDate()->format('Y');
+        }
+        $doi = $dataset->getDoi();
+
+        $citationString = $author . ' (' . $year . ') ' . $title . '.' .
+            ' Distributed by: GRIIDC '
+            . '(GRIIDC), Harte Research Institute, Texas A&M University-Corpus Christi. ';
+
+        if ($doi instanceof DOI) {
+            $citationString .= 'doi:' . $doi->getDoi();
+        } else {
+            $citationString .= "Available from: http://data.gulfresearchinitiative.org/data/$udi";
+        }
+        return $citationString;
+    }
+}
