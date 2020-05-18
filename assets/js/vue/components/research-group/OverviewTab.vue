019ebe3c6ca21ad752385d6b750c4824ef7ef9bc
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2020-05-18 08:50:30 -0500

    Updated the overview tab

--- assets/js/vue/components/research-group/OverviewTab.vue
+++ assets/js/vue/components/research-group/OverviewTab.vue
@@ -1,27 +1,51 @@
 <template>
     <div>
-        <h4>{{ info.name }}</h4>
+        <h4>{{ overview.name }}</h4>
         <hr>
-        <h6 style="font-weight: bold">Description</h6>
-        <p>{{ info.description }}</p>
-        <h6 style="font-weight: bold">Funding Organization</h6>
-        <p>{{ info.fundingCycle.fundingOrganization.name }}</p>
-        <h6 style="font-weight: bold">Funding Cycle</h6>
-        <p>{{ info.fundingCycle.name }}</p>
+        <h6 class="overview-label">Project Director(s): </h6>
+        <p v-for="director in getDirectors()">
+            {{ director.person.firstName + director.person.lastName }}
+            ({{ director.person.organization }})
+        </p>
+        <h6 class="overview-label">Funding Organization</h6>
+        <p>
+            <a :href=overview.fundingCycle.fundingOrganization.url>
+                {{ overview.fundingCycle.fundingOrganization.name }}
+            </a>
+        </p>
+        <h6 class="overview-label">Funding Cycle</h6>
+        <p>
+            <a :href="overview.fundingCycle.url">
+                {{ overview.fundingCycle.name }}
+            </a>
+        </p>
+        <h6 class="overview-label">Description</h6>
+        <p>{{ overview.description }}</p>
     </div>
 </template>
 
 <script>
     export default {
-        name: "InfoTab",
+        name: "OverviewTab",
         props: {
-            info: {
+            overview: {
                 type: Object
             }
         },
+        methods: {
+            getDirectors: function () {
+                return this.overview.personResearchGroups.filter(person => {
+                    if (person.role.name === "Leadership") {
+                        return person;
+                    }
+                });
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    .overview-label {
+        font-weight: bold;
+    }
 </style>
