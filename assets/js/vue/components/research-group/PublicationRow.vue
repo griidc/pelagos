c5e77057529932d8137b8801a22b84a50b4792f9
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2020-05-22 11:23:39 -0500

    Added publications tab and publication row vue components

---
+++ assets/js/vue/components/research-group/PublicationRow.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <b-card class="card-product" @click="openUrl(publication.doi)" v-for="publication in publicationRow.publications" v-bind:key="publication.id">
+            <b-card-text class="d-flex justify-content-between" >
+                {{ publication.citationText }}
+            </b-card-text>
+        </b-card>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PublicationRow",
+        props: {
+            publicationRow: {
+                type: Object
+            }
+        },
+        methods: {
+            openUrl: function(doi) {
+                if ("" === window.getSelection().toString()) {
+                    window.open("https://dx.doi.org/" + doi, '_blank');
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .card-product {
+        margin-bottom: 1rem;
+    }
+
+    .card-product:hover .btn-overlay {
+        opacity: 1;
+    }
+
+    .card-product:hover {
+        box-shadow: 0 4px 15px rgba(153, 153, 153, 0.3);
+        transition: .5s;
+        cursor: pointer;
+    }
+</style>
---
+++ assets/js/vue/components/research-group/PublicationsTab.vue
@@ -0,0 +1,20 @@
+<template>
+    <b-card-group>
+        <PublicationRow :publicationRow="dataset" v-for="dataset in datasets" v-bind:key="dataset.id"/>
+    </b-card-group>
+</template>
+
+<script>
+    import PublicationRow from "./PublicationRow";
+    export default {
+        name: "PublicationsTab",
+        components: { PublicationRow },
+        props: {
+            datasets: {}
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
