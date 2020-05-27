9ddb0df2ecae6c2195d756d376cceb191335afbf
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2020-05-27 14:37:18 -0500

    Added UTC converstion. Removed UTC conversion in Dataland.

--- src/Migrations/Version20200521165709.php
+++ src/Migrations/Version20200521165709.php
@@ -15,6 +15,10 @@ final class Version20200521165709 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
+        $this->addSql("UPDATE dataset_submission SET temporal_extent_begin_position = temporal_extent_begin_position AT TIME ZONE 'UTC'");
+
+        $this->addSql("UPDATE dataset_submission SET temporal_extent_end_position = temporal_extent_end_position AT TIME ZONE 'UTC'");
+
         $this->addSql('ALTER TABLE dataset_submission ALTER temporal_extent_begin_position TYPE DATE');
         $this->addSql('ALTER TABLE dataset_submission ALTER temporal_extent_begin_position DROP DEFAULT');
         $this->addSql('ALTER TABLE dataset_submission ALTER temporal_extent_end_position TYPE DATE');
