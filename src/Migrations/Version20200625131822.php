f52c74690468a9163a2964b2be089047f8dbc960
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2020-06-25 16:34:19 -0500

    Using the less precise timestamp(0) instead. With timezone.

--- src/Migrations/Version20200625131822.php
+++ src/Migrations/Version20200625131822.php
@@ -25,9 +25,9 @@ final class Version20200625131822 extends AbstractMigration
                 
         foreach ($datasetSubmissions as $submission) {
             $id = $submission['id'];
-            $this->addSql("INSERT INTO fileset (id, creator_id, modifier_id, creation_time_stamp, modification_time_stamp) VALUES (nextval('public.fileset_id_seq'), 0, 0, LOCALTIMESTAMP, LOCALTIMESTAMP)");
+            $this->addSql("INSERT INTO fileset (id, creator_id, modifier_id, creation_time_stamp, modification_time_stamp) VALUES (nextval('public.fileset_id_seq'), 0, 0, CURRENT_TIMESTAMP(0), CURRENT_TIMESTAMP(0))");
             $this->addSql("UPDATE dataset_submission SET fileset_id = currval('public.fileset_id_seq') WHERE id = $id");
-            $this->addSql("INSERT INTO file (id, fileset_id, creation_time_stamp, creator_id, modification_time_stamp, modifier_id, file_name, file_size, file_sha256_hash, file_path) SELECT nextval('file_id_seq'), currval('public.fileset_id_seq'), LOCALTIMESTAMP, 0, LOCALTIMESTAMP, 0, dataset_file_name, dataset_file_size, dataset_file_sha256_hash, SUBSTRING(dataset_file_uri FROM '\/[^\/].*$') FROM dataset_submission WHERE id = $id");
+            $this->addSql("INSERT INTO file (id, fileset_id, creation_time_stamp, creator_id, modification_time_stamp, modifier_id, file_name, file_size, file_sha256_hash, file_path) SELECT nextval('file_id_seq'), currval('public.fileset_id_seq'), CURRENT_TIMESTAMP(0), 0, CURRENT_TIMESTAMP(0), 0, dataset_file_name, dataset_file_size, dataset_file_sha256_hash, SUBSTRING(dataset_file_uri FROM '\/[^\/].*$') FROM dataset_submission WHERE id = $id");
         }
     }
 
