e79e4d3b699bf8371e3ed9c93329142c7f5690fb
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2020-06-30 09:47:11 -0500

    Added empty research Group

--- assets/js/vue/components/FundingCyclesList.vue
+++ assets/js/vue/components/FundingCyclesList.vue
@@ -5,6 +5,11 @@
             <option v-for="fundingCycle in fundingCycles" :value="fundingCycle.id" :key="fundingCycle.id">{{ fundingCycle.name }}</option>
         </select>
         
+        <select @change="doSomething" class="form-control">
+            <option value="" selected>[Please select a Funding Cycle]</option>
+            <option v-for="researchGroup in researchGroups" :value="researchGroup.id" :key="researchGroup.id">{{ researchGroup.name }}</option>
+        </select>
+        
     </div>
 </template>
 
@@ -14,6 +19,11 @@
         props: {
             fundingCycles: {
                 type: Object,
+            }
+        },
+        data() {
+            return {
+                researchGroups: []
             }
         },
         methods: {
