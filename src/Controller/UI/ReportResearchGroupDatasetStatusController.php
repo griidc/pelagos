94f9b037a0453c81ffd8fcf898de65dc81735818
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2020-07-22 14:07:20 -0500

    Added second report for research group dataset status

--- src/Controller/UI/ReportResearchGroupDatasetStatusController.php
+++ src/Controller/UI/ReportResearchGroupDatasetStatusController.php
@@ -6,6 +6,7 @@ use App\Form\ReportResearchGroupDatasetStatusType;
 use App\Entity\ResearchGroup;
 
 use App\Handler\EntityHandler;
+use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Routing\Annotation\Route;
 
 use Symfony\Component\HttpFoundation\Request;
@@ -78,69 +79,89 @@ class ReportResearchGroupDatasetStatusController extends ReportController
     }
 
     /**
+     * Research group dataset status report for dataset monitoring.
+     *
+     * @param Request       $request         Message information for this Request.
+     * @param EntityHandler $entityHandler   The entity handler.
+     *
+     * @Route(
+     *     "/report-researchgroup/dataset-monitoring",
+     *     name="pelagos_app_ui_reportresearchgroupdatasetstatus_datasetmonitoringreport",
+     *     methods={"GET"}
+     *     )
+     *
+     * @return Response|StreamedResponse A Symfony Response instance.
+     */
+    public function datasetMonitoringReportAction(Request $request, EntityHandler $entityHandler)
+    {
+        // Checks authorization of users
+        if (!$this->isGranted('ROLE_DATA_REPOSITORY_MANAGER')) {
+            return $this->render('template/AdminOnly.html.twig');
+        }
+        //  fetch all the Research Groups
+        $allResearchGroups = $entityHandler->getAll(ResearchGroup::class, array('name' => 'ASC'));
+        //  put all the names in an array with the associated doctrine id
+        $researchGroupNames = array();
+        foreach ($allResearchGroups as $rg) {
+            $researchGroupNames[$rg->getName()] = $rg->getId();
+        }
+        $form = $this->get('form.factory')->createNamed(
+            null,
+            ReportResearchGroupDatasetStatusType::class,
+            $researchGroupNames
+        );
+
+        $form->handleRequest($request);
+
+        return $this->render(
+            'Reports/ReportResearchGroupDatasetStatus.html.twig',
+            array('form' => $form->createView())
+        );
+    }
+
+    /**
+     * The post action for Dataset Submission.
+     *
+     * @param Request       $request         The Symfony request object.
+     *
+     * @Route(
+     *     "/report-researchgroup/dataset-monitoring",
+     *     name="pelagos_app_ui_datasetsubmission_post",
+     *     methods={"POST"}
+     *     )
+     *
+     * @return Response A Response instance.
+     */
+    public function postAction(Request $request)
+    {
+        $researchGroupId = $request->get('ResearchGroupSelector');
+        $researchGroup = $this->container->get('doctrine')->getRepository(ResearchGroup::class)
+            ->findOneBy(array('id' => $researchGroupId));
+
+        return $this->writeCsvResponse(
+            $this->getData(['researchGroup' => $researchGroup, 'version' => 2]),
+            $this->createCsvReportFileName($researchGroup->getName(), $researchGroupId)
+        );
+    }
+
+    /**
      * This method gets data for the report.
      *
-     * @param array|NULL $options Additional parameters needed to run the query.
+     * @param array $options Additional parameters needed to run the query.
      *
      * @return array  Return the data array
      */
-    protected function getData(array $options = null)
+    protected function getData(array $options)
     {
-        $datasetCountString = 'No datasets';
         $datasets = $options['researchGroup']->getDatasets();
-        $datasetCount = count($datasets);
-        if ($datasetCount > 0) {
-            $datasetCountString = ' [ ' . (string) count($datasets) . ' ]';
+        $reportData = array();
+
+        if (isset($options['version'])) {
+            $reportData = $this->getVersionTwoReport($datasets, $options);
+        } else {
+            $reportData = $this->getVersionOneReport($datasets, $options);
         }
-
-        //extra headers to be put in the report
-        $additionalHeaders = array(
-          array('RESEARCH GROUP',$options['researchGroup']->getName()),
-          array('DATASET COUNT', $datasetCountString),
-          array(parent::BLANK_LINE));
-
-        //prepare label array
-        $labels = array('labels' => array('DATASET UDI',
-          'TITLE',
-          'PRIMARY POINT OF CONTACT',
-          'STATUS',
-          'DATE APPROVED',
-          'DATE REGISTERED'));
-
-        //prepare data array
-        $dataArray = array();
-        if ($datasetCount > 0) {
-            foreach ($datasets as $dataset) {
-                $datasetStatus = $dataset->getStatus();
-                //  exclude datasets that don't have an approved DIF
-                if ($datasetStatus != 'NoDif') {
-                    $datasetTimeStampString = 'N/A';
-                    if ($dataset->getDatasetSubmission() != null &&
-                      $dataset->getDatasetSubmission()->getSubmissionTimeStamp() != null) {
-                        $datasetTimeStampString = $dataset->getDatasetSubmission()->getSubmissionTimeStamp()
-                        ->format(self::REPORTDATETIMEFORMAT);
-                    }
-                    $dif = $dataset->getDif();
-                    $ppoc = $dif->getPrimaryPointOfContact();
-                        $ppocString = $ppoc->getLastName() . ', ' .
-                        $ppoc->getFirstName();
-                    $difTimeStampString = 'N/A';
-                    if ($dif->getApprovedDate() != null) {
-                        $difTimeStampString = $dif->getApprovedDate()->format(self::REPORTDATETIMEFORMAT);
-                    }
-                    $dataRow = array(
-                        'udi' => $dataset->getUdi(),
-                        'title' => $dataset->getTitle(),
-                        'primaryPointOfContact' => $ppocString,
-                        'datasetStatus' => $datasetStatus,
-                        'dateIdentified' => $difTimeStampString,
-                        'dateRegistered' => $datasetTimeStampString
-                    );
-                    $dataArray[] = $dataRow;
-                }
-            }
-        }
-        return array_merge($this->getDefaultHeaders(), $additionalHeaders, $labels, $dataArray);
+        return array_merge($this->getDefaultHeaders(), $reportData['additionalHeaders'], $reportData['labels'], $reportData['dataArray']);
     }
 
     /**
@@ -160,5 +181,140 @@ class ReportResearchGroupDatasetStatusController extends ReportController
             . $nowDateTimeString
             . '.csv';
         return str_replace(' ', '_', $tempFileName);
+    }
+
+    /**
+     * @param $datasets
+     * @param $options
+     *
+     * @return array
+     */
+    private function getVersionOneReport($datasets, $options): array
+    {
+        $datasetCount = $this->getDatasetCount($datasets);
+        //extra headers to be put in the report
+        $additionalHeaders = array(
+            array('RESEARCH GROUP',$options['researchGroup']->getName()),
+            array('DATASET COUNT', $datasetCount['string']),
+            array(parent::BLANK_LINE));
+
+        //prepare label array
+        $labels = array('labels' => array('DATASET UDI',
+            'TITLE',
+            'PRIMARY POINT OF CONTACT',
+            'STATUS',
+            'DATE APPROVED',
+            'DATE REGISTERED'));
+
+        //prepare data array
+        $dataArray = array();
+        if ($datasetCount['number'] > 0) {
+            foreach ($datasets as $dataset) {
+                $datasetStatus = $dataset->getStatus();
+                //  exclude datasets that don't have an approved DIF
+                if ($datasetStatus != 'NoDif') {
+                    $datasetTimeStampString = 'N/A';
+                    if ($dataset->getDatasetSubmission() != null &&
+                        $dataset->getDatasetSubmission()->getSubmissionTimeStamp() != null) {
+                        $datasetTimeStampString = $dataset->getDatasetSubmission()->getSubmissionTimeStamp()
+                            ->format(self::REPORTDATETIMEFORMAT);
+                    }
+                    $dif = $dataset->getDif();
+                    $ppoc = $dif->getPrimaryPointOfContact();
+                    $ppocString = $ppoc->getLastName() . ', ' .
+                        $ppoc->getFirstName();
+                    $difTimeStampString = 'N/A';
+                    if ($dif->getApprovedDate() != null) {
+                        $difTimeStampString = $dif->getApprovedDate()->format(self::REPORTDATETIMEFORMAT);
+                    }
+                    $dataRow = array(
+                        'udi' => $dataset->getUdi(),
+                        'title' => $dataset->getTitle(),
+                        'primaryPointOfContact' => $ppocString,
+                        'datasetStatus' => $datasetStatus,
+                        'dateIdentified' => $difTimeStampString,
+                        'dateRegistered' => $datasetTimeStampString
+                    );
+                    $dataArray[] = $dataRow;
+                }
+            }
+        }
+
+        return array(
+            'additionalHeaders' => $additionalHeaders,
+            'labels' => $labels,
+            'dataArray' => $dataArray
+        );
+    }
+
+    /**
+     * @param Collection $datasets
+     *
+     * @return array
+     */
+    private function getDatasetCount(Collection $datasets): array
+    {
+        $datasetCount = array();
+        $datasetCount['string'] = 'No datasets';
+
+        $datasetCount['number'] = count($datasets);
+        if ($datasetCount['number'] > 0) {
+            $datasetCount['string'] = ' [ ' . (string) count($datasets) . ' ]';
+        }
+        return $datasetCount;
+    }
+
+    /**
+     * @param $datasets
+     * @param $options
+     *
+     * @return array
+     */
+    private function getVersionTwoReport($datasets, $options): array
+    {
+        $datasetCount = $this->getDatasetCount($datasets);
+        //extra headers to be put in the report
+        $additionalHeaders = array(
+            array('DATASET REPORT FOR RESEARCH GROUP:',$options['researchGroup']->getName()),
+            array('DATASET COUNT', $datasetCount['string']),
+            array(parent::BLANK_LINE));
+
+        //prepare label array
+        $labels = array(
+            'labels' => array(
+                'DATASET UDI',
+                'DATASET DOI',
+                'TITLE',
+                'PRIMARY POINT OF CONTACT',
+                'STATUS',
+                'RESTRICTED',
+            )
+        );
+
+        //prepare data array
+        $dataArray = array();
+        if ($datasetCount['number'] > 0) {
+            foreach ($datasets as $dataset) {
+                $datasetStatus = $dataset->getStatus();
+                $ppoc = $dataset->getPrimaryPointOfContact();
+                $ppocString = $ppoc->getLastName() . ', ' .
+                    $ppoc->getFirstName();
+                $dataRow = array(
+                    'udi' => $dataset->getUdi(),
+                    'doi'=> $dataset->getDoi(),
+                    'title' => $dataset->getTitle(),
+                    'primaryPointOfContact' => $ppocString,
+                    'datasetStatus' => $datasetStatus,
+                    'restricted' => ($dataset->isRestricted()) ? 'YES' : 'NO',
+                );
+                $dataArray[] = $dataRow;
+            }
+        }
+
+        return array(
+            'additionalHeaders' => $additionalHeaders,
+            'labels' => $labels,
+            'dataArray' => $dataArray
+        );
     }
 }
