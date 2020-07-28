48b7b570b9c9663ed7084eda23f586fcf00f5ed6
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2020-07-28 09:06:48 -0500

    Palette test.

--- assets/js/vue/components/stats/DatasetOverTime.vue
+++ assets/js/vue/components/stats/DatasetOverTime.vue
@@ -3,7 +3,7 @@
     <DxChart
       id="chart"
       :data-source="dataSource"
-      palette="Ocean"
+      palette=myPalette
     >
       <DxCommonSeriesSettings
         :type="type"
@@ -53,6 +53,15 @@ import {
 import DxSelectBox from 'devextreme-vue/select-box';
 
 import service from './data.js';
+
+var myPalette = {
+    // Applies in the BarGauge, Chart, Funnel, PieChart, PolarChart, Sankey, and TreeMap with a discrete colorizer
+    simpleSet: ['#60a69f', '#78b6d9', '#6682bb', '#a37182', '#eeba69'], 
+    // Applies in the CircularGauge and LinearGauge
+    indicatingSet: ['#90ba58', '#eeba69', '#a37182'], 
+    // Applies in the VectorMap and TreeMap with a gradient or range colorizer 
+    gradientSet: ['#78b6d9', '#eeba69'] 
+};
 
 export default {
 
