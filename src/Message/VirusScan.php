557bf7ab1a9cf59fddf0cc62e364d9a5d1b17afc
Michael Scott Williamson <michael.williamson@tamucc.edu>
2020-09-21 15:19:53 -0500

    Add message class for virus scan.

---
+++ src/Message/VirusScan.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Message;
+
+/**
+ * Message for virus-scanning files.
+ */
+class ScanFile
+{
+    /**
+     * The file ID of the file to be scanned.
+     *
+     * @var string
+     */
+    protected $fileId;
+
+    /**
+     * Constructor.
+     *
+     * @param int $fileId The file ID of the file to be scanned.
+     */
+    public function __construct(int $fileId)
+    {
+        $this->fileId = $fileId;
+    }
+
+    /**
+     * The file ID getter.
+     *
+     * @return integer The file ID.
+     */
+    public function getFileId(): int
+    {
+        return $this->fileId;
+    }
+}
