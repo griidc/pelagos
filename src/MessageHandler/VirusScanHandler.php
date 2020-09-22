981b637306c102ff42fe01b3a3a0b07c6ac89d33
Michael Scott Williamson <michael.williamson@tamucc.edu>
2020-09-22 14:05:10 -0500

    Separate logs, remove test code.

--- src/MessageHandler/VirusScanHandler.php
+++ src/MessageHandler/VirusScanHandler.php
@@ -38,13 +38,13 @@ class VirusScanHandler implements MessageHandlerInterface
      *
      * @param EntityManagerInterface   $entityManager           The entity handler.
      * @param FileRepository           $fileRepository          The file Repository.
-     * @param LoggerInterface          $datasetFileHasherLogger Name hinted dataset_file_hasher logger.
+     * @param LoggerInterface          $virusScanLogger         Name hinted virus_scan logger.
      */
-    public function __construct(EntityManagerInterface $entityManager, FileRepository $fileRepository, LoggerInterface $datasetFileHasherLogger)
+    public function __construct(EntityManagerInterface $entityManager, FileRepository $fileRepository, LoggerInterface $virusScanLogger)
     {
         $this->entityManager = $entityManager;
         $this->fileRepository = $fileRepository;
-        $this->logger = $datasetFileHasherLogger;
+        $this->logger = $virusScanLogger;
     }
 
     /**
@@ -55,7 +55,6 @@ class VirusScanHandler implements MessageHandlerInterface
     public function __invoke(VirusScan $virusScan)
     {
         $fileId = $virusScan->getFileId();
-        $this->logger->info(sprintf('Scanning File with ID: %d', $fileId));
         $file = $this->fileRepository->find($fileId);
         if ($file instanceof File) {
             $filePath = $file->getFilePath();
@@ -63,15 +62,15 @@ class VirusScanHandler implements MessageHandlerInterface
                 $socket = (new \Socket\Raw\Factory())->createClient('unix:///run/clamd.scan/clamd.sock');
                 $quahog = new \Xenolope\Quahog\Client($socket);
                 $result = $quahog->scanFile($filePath);
-                dump($result);
-                //$this->logger->info(sprintf('Virus scanned file: %s. Status: %s. Reason: %s.', $result['Filename'], $result['Status'], $result['Reason']));
+                $this->logger->info(sprintf('Virus scanned file id:%s filename: %s. Status: %s. Reason: %s.', $fileId, $result['filename'], $result['status'], $result['reason']));
             } catch (\Exception $e) {
                 $this->logger->error(sprintf('Unable to scan file. Message: %s', $e->getMessage()));
                 // On catch, furter execution prevented, so attributes will not be updated.
                 return;
             }
-            //$file->setVirusScanDate($fileHash, '1974-12-02' );
-            //$file->setVirusScanResult($result['Status']);
+            // In the future, we'll likely set an attribute of the scan results, or an email for identified viruses, etc.
+            //$file->setVirusLastScanDate();
+            //$file->setVirusLastScanResult($result['status']);
             //$this->entityManager->flush();
         } else {
             $this->logger->alert(sprintf('File with ID: %d was not found to scan!', $fileId));
