36ea3755c639b1dd0fbd05d4aef847b5b0ba8aba
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2020-10-08 10:26:41 -0500

    Changed from processed to done again.

--- src/Migrations/Version20200625131822.php
+++ src/Migrations/Version20200625131822.php
@@ -27,7 +27,7 @@ final class Version20200625131822 extends AbstractMigration
             $id = $submission['id'];
             $this->addSql("INSERT INTO fileset (id, creator_id, modifier_id, creation_time_stamp, modification_time_stamp) VALUES (nextval('public.fileset_id_seq'), 0, 0, CURRENT_TIMESTAMP(0), CURRENT_TIMESTAMP(0))");
             $this->addSql("UPDATE dataset_submission SET fileset_id = currval('public.fileset_id_seq') WHERE id = $id");
-            $this->addSql("INSERT INTO file (id, fileset_id, creation_time_stamp, creator_id, modification_time_stamp, modifier_id, file_name, file_size, file_sha256_hash, file_path, status) SELECT nextval('file_id_seq'), currval('public.fileset_id_seq'), CURRENT_TIMESTAMP(0), 0, CURRENT_TIMESTAMP(0), 0, dataset_file_name, dataset_file_size, dataset_file_sha256_hash, SUBSTRING(dataset_file_uri FROM '\/[^\/].*$'), 'processed' FROM dataset_submission WHERE id = $id");
+            $this->addSql("INSERT INTO file (id, fileset_id, creation_time_stamp, creator_id, modification_time_stamp, modifier_id, file_name, file_size, file_sha256_hash, file_path, status) SELECT nextval('file_id_seq'), currval('public.fileset_id_seq'), CURRENT_TIMESTAMP(0), 0, CURRENT_TIMESTAMP(0), 0, dataset_file_name, dataset_file_size, dataset_file_sha256_hash, SUBSTRING(dataset_file_uri FROM '\/[^\/].*$'), 'done' FROM dataset_submission WHERE id = $id");
         }
     }
 
