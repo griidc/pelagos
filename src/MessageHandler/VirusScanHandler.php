7778261e72e6b246501d9c6dafeed7c1b50d6185
Michael Scott Williamson <michael.williamson@tamucc.edu>
2020-10-19 07:45:26 -0500

    Cleanup per pier-review.

--- src/MessageHandler/VirusScanHandler.php
+++ src/MessageHandler/VirusScanHandler.php
@@ -4,8 +4,9 @@ namespace App\MessageHandler;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
+use Socket\Raw\Factory as SocketFactory;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
-
+use Xenolope\Quahog\Client as QuahogClient;
 use App\Message\VirusScan;
 use App\Entity\File;
 use App\Repository\FileRepository;
@@ -34,17 +35,26 @@ class VirusScanHandler implements MessageHandlerInterface
     private $logger;
 
     /**
+     * The ClamAV socket file.
+     *
+     * @var string clamSock
+     */
+    private $clamSock;
+
+    /**
      * Constructor for this Controller, to set up default services.
      *
      * @param EntityManagerInterface   $entityManager           The entity handler.
      * @param FileRepository           $fileRepository          The file Repository.
      * @param LoggerInterface          $virusScanLogger         Name hinted virus_scan logger.
+     * @param string                   $clamdSock               String pointing to socket to clamav daemon.
      */
-    public function __construct(EntityManagerInterface $entityManager, FileRepository $fileRepository, LoggerInterface $virusScanLogger)
+    public function __construct(EntityManagerInterface $entityManager, FileRepository $fileRepository, LoggerInterface $virusScanLogger, string $clamdSock)
     {
         $this->entityManager = $entityManager;
         $this->fileRepository = $fileRepository;
         $this->logger = $virusScanLogger;
+        $this->clamdSock = $clamdSock;
     }
 
     /**
@@ -59,8 +69,8 @@ class VirusScanHandler implements MessageHandlerInterface
         if ($file instanceof File) {
             $filePath = $file->getFilePath();
             try {
-                $socket = (new \Socket\Raw\Factory())->createClient('unix:///run/clamd.scan/clamd.sock');
-                $quahog = new \Xenolope\Quahog\Client($socket);
+                $socket = (new SocketFactory())->createClient('unix:///run/clamd.scan/clamd.sock');
+                $quahog = new QuahogClient($socket);
                 $result = $quahog->scanFile($filePath);
                 $this->logger->info(sprintf('Virus scanned file id:%s filename: %s. Status: %s. Reason: %s.', $fileId, $result['filename'], $result['status'], $result['reason']));
             } catch (\Exception $e) {
