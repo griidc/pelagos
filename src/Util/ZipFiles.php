4591e30deea9ae2dff97fc6f29dfd5086d78286d
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2020-10-20 11:56:13 -0500

    Added zip file utility class

---
+++ src/Util/ZipFiles.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Util;
+
+use App\Entity\File;
+
+use phpDocumentor\Reflection\Types\Collection;
+
+use ZipArchive;
+
+class ZipFiles
+{
+
+    /**
+     * ZipArchive class instance.
+     *
+     * @var ZipArchive
+     */
+    private $zip;
+
+    /**
+     * ZipFiles constructor.
+     */
+    public function __construct()
+    {
+        $this->zip = new ZipArchive();
+    }
+
+    /**
+     * Creates a zip file from collection of files.
+     *
+     * @param Collection|File $files   Files that need to be zipped.
+     * @param string          $zipFile Filename of the zipfile.
+     *
+     * @return string
+     */
+    public function createZipFile(Collection $files, string $zipFile) : string
+    {
+        $flag = (file_exists($zipFile))? ZIPARCHIVE::OVERWRITE : ZIPARCHIVE::CREATE;
+        if ($this->zip->open($zipFile, $flag) === true) {
+            foreach ($files as $file) {
+                $this->zip->addFile($file->getFilePath(), $file->getFileName());
+            }
+            $this->zip->close();
+        } else{
+            echo "Error";
+        }
+
+        return $zipFile;
+    }
+}
