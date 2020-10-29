5b5710c6c4e30a8d8620d3f49c04d37938bf3e69
Michael Scott Williamson <michael.williamson@tamucc.edu>
2020-10-29 09:51:50 -0500

    Removed unneeded dependency injections.

--- src/MessageHandler/VirusScanHandler.php
+++ src/MessageHandler/VirusScanHandler.php
@@ -3,31 +3,14 @@
 namespace App\MessageHandler;
 
 use App\Util\VirusScanUtil;
-use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Socket\Raw\Factory as SocketFactory;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use Xenolope\Quahog\Client as QuahogClient;
 use App\Message\ScanFileForVirus;
-use App\Entity\File;
-use App\Repository\FileRepository;
 
 class VirusScanHandler implements MessageHandlerInterface
 {
-    /**
-     * The Entity Manager.
-     *
-     * @var EntityManagerInterface
-     */
-    private $entityManager;
-
-    /**
-     * The File Repository.
-     *
-     * @var FileRepository
-     */
-    private $fileRepository;
-
     /**
      * The monolog logger.
      *
@@ -46,15 +29,11 @@ class VirusScanHandler implements MessageHandlerInterface
     /**
      * Constructor for this Controller, to set up default services.
      *
-     * @param EntityManagerInterface $entityManager   The entity handler.
-     * @param FileRepository         $fileRepository  The file Repository.
      * @param LoggerInterface        $virusScanLogger Name hinted virus_scan logger.
      * @param VirusScanUtil          $scanner         A stream-based virus-scanning utility instance.
     */
-    public function __construct(EntityManagerInterface $entityManager, FileRepository $fileRepository, LoggerInterface $virusScanLogger, VirusScanUtil $scanner)
+    public function __construct(LoggerInterface $virusScanLogger, VirusScanUtil $scanner)
     {
-        $this->entityManager = $entityManager;
-        $this->fileRepository = $fileRepository;
         $this->logger = $virusScanLogger;
         $this->scanner = $scanner;
     }
