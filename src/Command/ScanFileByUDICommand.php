e29f47bf336664ac52c1b4de73cb3ee55dd26d53
Michael Scott Williamson <michael.williamson@tamucc.edu>
2020-10-29 15:55:36 -0500

    PSR12 compliance.

--- src/Command/ScanFileByUDICommand.php
+++ src/Command/ScanFileByUDICommand.php
@@ -2,16 +2,15 @@
 
 namespace App\Command;
 
+use App\Entity\Dataset;
+use App\Entity\DatasetSubmission;
+use App\Message\ScanFileForVirus;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
-
-use App\Entity\DatasetSubmission;
-use App\Entity\Dataset;
-use App\Message\ScanFileForVirus;
 
 /**
  * This command triggers a file virus scan, by UDI.
