3d6c5b592604e043517258e408e4a78b0614bb9c
Praneeth Pondicherry Ravendernath <Praneeth.Pondicherry@tamucc.edu>
2020-11-19 14:27:55 -0600

    Replaced object file provider with custom file provider

--- assets/js/file-manager.js
+++ assets/js/file-manager.js
@@ -8,32 +8,15 @@ const fileManagerElement = document.getElementById("file-manager-app");
 
 if (fileManagerElement.dataset.id) {
     const datasetSubmissionId = Number(fileManagerElement.dataset.id);
-
         new Vue({
         el: '#file-manager-app',
         data() {
             return {
-                fileItems: this.getFileItems(),
-                showFileManager: false,
                 datasetSubmissionId: datasetSubmissionId
             }
         },
         components: { FileManager },
-        template: `<FileManager v-if="showFileManager" :files="fileItems"/>`,
-        methods: {
-            getFileItems: function () {
-                const axiosInstance = axios.create({});
-                axiosInstance
-                    .get(Routing.generate('pelagos_api_get_files_dataset_submission') + "/" + datasetSubmissionId)
-                    .then(response => {
-                        // this.fileItems = Object.assign({}, response.data)
-                        this.fileItems = response.data;
-                        this.showFileManager = true;
-                    }).catch(error => {
-                        console.log(error);
-                });
-            }
-        },
+        template: `<FileManager :datasetSubId="datasetSubmissionId"/>`
     });
 
     let myDropzone = new Dropzone("div#dropzone-uploader", {
