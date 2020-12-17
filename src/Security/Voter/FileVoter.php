9ec63568fb719921d58bc390823371ba8fd38b2b
Michael Scott Williamson <michael.williamson@tamucc.edu>
2020-12-17 08:08:25 -0600

    Query for associated submission having fileset.

--- src/Security/Voter/FileVoter.php
+++ src/Security/Voter/FileVoter.php
@@ -8,6 +8,7 @@ use App\Entity\Account;
 use App\Entity\File as PelagosFile;
 use App\Entity\Person;
 use App\Entity\ResearchGroup;
+use Doctrine\ORM\EntityManager;
 
 /**
  * A voter to determine if CRUD actions possible on a File.
@@ -16,6 +17,23 @@ use App\Entity\ResearchGroup;
  */
 class FileVoter extends PelagosEntityVoter
 {
+    /**
+     * Class constructor.
+     *
+     * @param EntityManager $em
+     */
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * Entity manager for QB.
+     *
+     * @var EntityManager
+     */
+    protected $em;
+
     /**
      * Determine if the attribute and subject are supported by this voter.
      *
@@ -26,7 +44,7 @@ class FileVoter extends PelagosEntityVoter
      */
     protected function supports($attribute, $subject) //phpcs:ignore
     {
-        // Make sure the subject is an instance of Dataset
+        // Make sure the subject is an instance of File
         if (!$subject instanceof PelagosFile) {
             return false;
         }
@@ -41,15 +59,15 @@ class FileVoter extends PelagosEntityVoter
     }
 
     /**
-     * Perform a authorization test on an attribute, Dataset subject and authentication token.
+     * Perform a authorization test on an attribute, File subject and authentication token.
      *
      * The Symfony calling security framework calls supports before calling voteOnAttribute.
      *
      * @param string         $attribute The action to be considered.
-     * @param mixed          $subject   A Dataset.
+     * @param mixed          $subject   A File.
      * @param TokenInterface $token     A security token containing user authentication information.
      *
-     * @return boolean True If the user has one of the target roles for any of the subject's DataRepositories.
+     * @return boolean True If the user has one of the target roles for any of the subject's targets.
      */
     protected function voteOnAttribute($attribute, $subject, TokenInterface $token) //phpcs:ignore
     {
@@ -62,7 +80,30 @@ class FileVoter extends PelagosEntityVoter
 
         // Return TRUE if Account is of a user who's a member of the same RG as the Dataset.
         $person = $user->getPerson();
-        if ($person instanceof Person and in_array($dataset->getResearchGroup(), $person->getResearchGroups())) {
+        $personsResearchGroups = $person->getResearchGroups();
+        $datasetsResearchGroup = $dataset->getResearchGroup();
+        $fileset = $subject->getFileset();
+
+        // Find the linked dataset submission. Vote based on it's dataset's RG.
+        $qb = $em->createQueryBuilder();
+
+        $query = $qb
+            ->SELECT('datasetsubmission', 'ds')
+            ->FROM('\App\Entity\DatasetSubmission')
+            ->WHERE(
+                $qb->expr()->eq('ds.fileset', '?1')
+            )
+            ->setParameter(1, $fileset)
+            ->getQuery();
+
+        $datasetSubmission = $query->getSingleResult();
+
+        if (!$datasetSubmission instanceof DatasetSubmission) {
+            return false;
+        }
+        $dataset = $datasetSubmission->getDataset();
+
+        if ($person instanceof Person and in_array($datasetsResearchGroup, $personsResearchGroups)) {
             return true;
         }
 
