5c28e07f843f1e6dc1225edf5c06e347e8409988
Pondicherry Ravendernath, Praneeth <praneeth.pondicherry@tamucc.edu>
2021-01-05 15:05:55 -0600

    Added move and rename for files

--- src/Controller/Api/FileController.php
+++ src/Controller/Api/FileController.php
@@ -8,7 +8,9 @@ use App\Util\Datastore;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -25,7 +27,7 @@ class FileController extends AbstractFOSRestController
      * @param EntityManagerInterface $entityManager Entity manager interface instance.
      * @param Datastore              $datastore     Datastore to manipulate the file on disk.
      *
-     * @Route("/api/file/{id}", name="pelagos_api_datasets_delete", methods={"DELETE"}, defaults={"_format"="json"})
+     * @Route("/api/file/{id}", name="pelagos_api_file_delete", methods={"DELETE"}, defaults={"_format"="json"})
      *
      * @IsGranted("IS_AUTHENTICATED_FULLY")
      * @IsGranted("CAN_DELETE", subject="file")
@@ -51,7 +53,43 @@ class FileController extends AbstractFOSRestController
 
         return new Response(
             null,
-            Response::HTTP_NO_CONTENT,
+            Response::HTTP_OK,
+            array(
+                'Content-Type' => 'application/x-empty',
+            )
+        );
+    }
+
+    /**
+     * Update a file entity.
+     *
+     * @param File                   $file          File entity instance.
+     * @param EntityManagerInterface $entityManager Entity manager interface instance.
+     * @param Request                $request       The request body sent with destination directory.
+     *
+     * @Route("/api/file/{id}", name="pelagos_api_file_update_filename", methods={"PUT"}, defaults={"_format"="json"})
+     *
+     * @IsGranted("IS_AUTHENTICATED_FULLY")
+     * @IsGranted("CAN_EDIT", subject="file")
+     *
+     * @throws BadRequestHttpException When the destination file name already exists.
+     *
+     * @return Response A response object with an empty body and a "no content" status code.
+     */
+    public function updateFileName(File $file, EntityManagerInterface $entityManager, Request $request) : Response
+    {
+        $newFileName = $request->get('destinationDir');
+        $fileset = $file->getFileset();
+        if (!$fileset->doesFileExist($newFileName)) {
+            $file->setFilePathName($newFileName);
+            $entityManager->flush();
+        } else {
+            throw new BadRequestHttpException('File with same name and folder already exists');
+        }
+
+        return new Response(
+            null,
+            Response::HTTP_OK,
             array(
                 'Content-Type' => 'application/x-empty',
             )
