1fd8ba83c01d1fd1f81cd49064a156a8ab65cab6
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2021-02-10 16:34:11 -0600

    Added stream info util.

---
+++ src/Util/StreamInfo.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Util;
+
+/**
+ * A utility class to get various information from a file stream.
+ */
+class StreamInfo
+{
+    /**
+     * Get the hash for file stream.
+     *
+     * @param array $fileStream The file stream array.
+     *
+     * @return string The hash calculated from the stream.
+     */
+    public static function calculateHash(array $inputFileStream, string $algo = 'sha256') :string
+    {
+        $fileStream = $inputFileStream['fileStream'] ?? null;
+        $context = hash_init($algo);
+        hash_update_stream($context, $fileStream);
+        return hash_final($context);
+    }
+
+    /**
+     * Get the file size for the file stream.
+     *
+     * @param array $fileStream The file stream array.
+     *
+     * @return integer The file size of the stream.
+     */
+    public static function getFileSize(array $inputFileStream) :int
+    {
+        $fileStream = $inputFileStream['fileStream'] ?? null;
+        $fstat = fstat($fileStream);
+        return (int) $fstat[size];
+    }
+}
