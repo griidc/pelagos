f9cd89f54b75d6ee3c78202f5545c3c426fcf42c
Michael Scott Williamson <michael.williamson@tamucc.edu>
2021-02-26 08:07:38 -0600

    Added startup/shutdown messagemq script.

---
+++ share/bash/control-pelagos-messagemq-consumer.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+#
+# chkconfig: 35 98 02
+# description: Pelagos MessageMQ Consumer Supervisor
+#
+# This script to be copied to or symlinked at /usr/local/bin and set to executable.
+#
+# This script is called by the pelagos-messagemq-consumer.service service with either a
+# start or stop argument.
+
+# Get function from functions library
+. /etc/init.d/functions
+
+prog="pelagos-messagemq-consumer-supervisor"
+runuser=pelagos
+
+success=true;
+
+# Start the service
+start() {
+    echo -n $"Starting $prog: "
+	cd /opt/pelagos
+
+    if su - $runuser -c "supervisord --configuration=/opt/pelagos/config/supervisor/supervisord.conf --identifier=pelagos" ; then
+        echo "started supervisord"
+    else
+        echo "failed to start supervisord"
+        success=false
+    fi
+
+    if su - $runuser -c "supervisorctl --serverurl unix:///opt/pelagos/var/supervisor/supervisor.sock update" ; then
+        echo "updated supervisor"
+    else
+        echo "failed to update supervisor"
+        success=false
+    fi
+
+    if su - $runuser -c "supervisorctl --serverurl unix:///opt/pelagos/var/supervisor/supervisor.sock reload" ; then
+        echo "reloaded supervisor"
+    else
+        echo "failed to reload supervisord"
+        success=false
+    fi
+
+    if su - $runuser -c "supervisorctl --serverurl unix:///opt/pelagos/var/supervisor/supervisor.sock start messenger-consume:*" ; then
+        echo "started messenger consumer"
+    else
+        echo "failed to start the actual messenger consumer"
+        success=false
+    fi
+
+    if $success ; then
+        success
+    else
+        failure
+    fi
+
+    echo
+}
+
+# Stop the service
+stop() {
+	cd /opt/pelagos
+
+    if su - $runuser -c "supervisorctl --serverurl unix:///opt/pelagos/var/supervisor/supervisor.sock stop messenger-consume:*" ; then
+        echo "stopped consumer"
+    else
+        success=false
+    fi
+
+    if su - $runuser -c "killall supervisord" ; then
+        echo "stopped supervisord"
+    else
+        success=false
+    fi
+
+    if $success ; then
+        success
+    else
+        failure
+    fi
+
+    echo
+}
+
+### main logic ###
+case "$1" in
+  start)
+        start
+        ;;
+  stop)
+        stop
+        ;;
+  restart)
+        stop
+        start
+        ;;
+  *)
+        echo $"Usage: $0 {start|stop|restart}"
+        exit 1
+esac
+
+exit 0
