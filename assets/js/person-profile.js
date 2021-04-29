2e4b1e227a68d869b434d54c65697623c482563a
Pondicherry Ravendernath, Praneeth <praneeth.pondicherry@tamucc.edu>
2021-04-29 15:32:11 -0500

    Added profile page vue app

--- assets/js/person-profile.js
+++ assets/js/person-profile.js
@@ -1,15 +1,21 @@
 import Vue from "vue";
 import PersonProfile from "@/vue/PersonProfile";
+import Loading from "vue-loading-overlay";
+import {BootstrapVue, IconsPlugin} from "bootstrap-vue";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.use(Loading);
 new Vue({
     components: { PersonProfile },
     data() {
         return {
-            person: {}
+            person: 0
         }
     },
     beforeMount() {
         this.person = Number(this.$el.attributes['data-name'].value);
     },
-    template: `<PersonProfile :person='person'/>`,
+    template: `<div class="bootstrap"><PersonProfile :personId='person'/></div>`,
 }).$mount("#person-profile");
