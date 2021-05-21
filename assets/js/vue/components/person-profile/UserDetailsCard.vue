6de5056c3ec1fe092b12cbf1ab3e6553b1e4466a
Michael Scott Williamson <michael.williamson@tamucc.edu>
2021-05-21 16:36:57 -0500

    Add POSIX info into User card.

--- assets/js/vue/components/person-profile/UserDetailsCard.vue
+++ assets/js/vue/components/person-profile/UserDetailsCard.vue
@@ -7,6 +7,7 @@
                 </div>
                 <div class="col-sm-9 text-secondary">
                     {{ `${personDetails.firstName}  ${personDetails.lastName}` }}
+
                 </div>
             </div>
             <hr>
@@ -40,12 +41,28 @@
                 </div>
             </div>
             <hr>
-            <div class="row">
+            <div class="row"  v-if="personDetails.isMe === true">
                 <div class="col-sm-3">
                     <h6 class="mb-0">GridFTP</h6>
                 </div>
+                <div class="col-sm-9 text-secondary" v-if="personDetails.isposix === true">
+                  This account has Globus/SFTP enabled. Your username is <i>{{ personDetails.posixUsername }}</i>
+                </div>
+                <div class="col-sm-9 text-secondary" v-else>
+                  Globus/SFTP is not currently enabled on this account. <button onClick=@requestposix>Request Access</button>
+                </div>
+                </div>
+              </div>
+            <div class="row"  v-if="personDetails.isMe === true">
+                <div class="col-sm-3">
+                    <h6 class="mb-0">Details</h6>
+                </div>
                 <div class="col-sm-9 text-secondary">
-                    {{ personDetails.isposix }}
+                  If enabled, this option enables use of griidc-ingest.griidc.org globus or SFTP endpoint
+                  to upload large files (including multi TB files) to GRIIDC. For more information on
+                  Globus GridFTP and SFTP transfers, please visit
+                  <a href="https://data.gulfresearchinitiative.org/bulk-transfers">
+                    https://data.gulfresearchinitiative.org/PLACEHOLDER-TBD</a>.
                 </div>
             </div>
         </b-card-body>
