d869922d6343338ecd1e93075e306a7d555b560d
Michael Scott Williamson <michael.williamson@tamucc.edu>
2021-05-25 14:12:23 -0500

    Set spacing back to 4.

--- assets/js/vue/components/person-profile/UserDetailsCard.vue
+++ assets/js/vue/components/person-profile/UserDetailsCard.vue
@@ -42,31 +42,31 @@
             </div>
             <hr>
             <div class="row"  v-show="personDetails.isMe">
-              <div class="col-sm-3">
-                  <h6 class="mb-0">GridFTP</h6>
-              </div>
-              <div class="col-sm-9 text-secondary" v-if="personDetails.isposix === true">
-                This account has Globus/SFTP enabled. Your username is <i>{{ personDetails.posixUsername }}</i>
-              </div>
-              <div class="col-sm-9 text-secondary" v-else>
-                Globus/SFTP is not currently enabled on this account.
-                <b-button variant="primary" @click="requestposix"><b-spinner small v-if="loading"></b-spinner>
-                  Request Globus/SFTP Access
-                </b-button>
-              </div>
+                <div class="col-sm-3">
+                    <h6 class="mb-0">GridFTP</h6>
+                </div>
+                <div class="col-sm-9 text-secondary" v-if="personDetails.isposix === true">
+                    This account has Globus/SFTP enabled. Your username is <i>{{ personDetails.posixUsername }}</i>
+                </div>
+                <div class="col-sm-9 text-secondary" v-else>
+                    Globus/SFTP is not currently enabled on this account.
+                    <b-button variant="primary" @click="requestposix"><b-spinner small v-if="loading"></b-spinner>
+                        Request Globus/SFTP Access
+                    </b-button>
+                </div>
             </div>
             <hr v-show="personDetails.isMe === true">
             <div class="row"  v-show="personDetails.isMe">
-              <div class="col-sm-3">
-                <h6 class="mb-0">Details</h6>
-              </div>
-              <div class="col-sm-9 text-secondary">
-                If enabled, this option enables use of griidc-ingest.griidc.org globus or SFTP endpoint
-                to upload large files (including multi TB files) to GRIIDC. For more information on
-                Globus GridFTP and SFTP transfers, please visit
-                <a href="https://data.gulfresearchinitiative.org/bulk-transfers">
-                  https://data.gulfresearchinitiative.org/PLACEHOLDER-TBD</a>.
-              </div>
+                <div class="col-sm-3">
+                    <h6 class="mb-0">Details</h6>
+                </div>
+                <div class="col-sm-9 text-secondary">
+                    If enabled, this option enables use of griidc-ingest.griidc.org globus or SFTP endpoint
+                    to upload large files (including multi TB files) to GRIIDC. For more information on
+                    Globus GridFTP and SFTP transfers, please visit
+                    <a href="https://data.gulfresearchinitiative.org/bulk-transfers">
+                    https://data.gulfresearchinitiative.org/PLACEHOLDER-TBD</a>.
+                </div>
             </div>
         </b-card-body>
     </b-card>
@@ -75,28 +75,28 @@
 <script>
 import {patchApi} from "@/vue/utils/axiosService";
 export default {
-  name: "UserDetailsCard",
-  props: {
-    personDetails: {
-        type: Object
-      },
-  loading: false
-  },
-  methods: {
-    requestposix:function() {
-      this.loading = true;
-      patchApi(
-      Routing.generate('pelagos_api_account_make_posix'),
-        {}
-        ).then(response => {
-          window.location.reload()
-      }).catch(error => {
-        console.log(error);
-      }).finally(() => {
-        this.loading = false;
-      });
+    name: "UserDetailsCard",
+    props: {
+        personDetails: {
+            type: Object
+        },
+    loading: false
+    },
+    methods: {
+        requestposix:function() {
+            this.loading = true;
+            patchApi(
+            Routing.generate('pelagos_api_account_make_posix'),
+                {}
+            ).then(response => {
+                window.location.reload()
+            }).catch(error => {
+                console.log(error);
+            }).finally(() => {
+                this.loading = false;
+            });
+        }
     }
-  }
 }
 </script>
 
