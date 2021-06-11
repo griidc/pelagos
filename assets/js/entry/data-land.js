a825aa893ba593aa65211b361cc2031b261d6621
Pondicherry Ravendernath, Praneeth <praneeth.pondicherry@tamucc.edu>
2021-06-11 12:24:57 -0500

    Added  bootstrap button

--- assets/js/entry/data-land.js
+++ assets/js/entry/data-land.js
@@ -3,10 +3,11 @@ import FileManager from "@/vue/FileManager";
 import '/assets/css/file-manager.css'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import DownloadZipBtn from "@/vue/components/data-land/DownloadZipBtn";
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue';
 
 const fileManagerElement = document.getElementById("file-manager-app");
+const datasetSubmissionId = Number(fileManagerElement.dataset.submission);
 
-const datasetSubmissionId = Number(fileManagerElement.dataset.id);
 new Vue({
     el: '#file-manager-app',
     data() {
@@ -18,8 +19,17 @@ new Vue({
     template: `<FileManager :datasetSubId="datasetSubmissionId" :writeMode="false"/>`
 });
 
+const downloadZipElement = document.getElementById("download-zip");
+const udi = downloadZipElement.dataset.udi;
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 new Vue({
     el: '#download-zip',
     components: { DownloadZipBtn },
-    template: `<DownloadZipBtn/>`
+    data() {
+        return {
+            udi: udi,
+        }
+    },
+    template: `<DownloadZipBtn :udi="udi"/>`
 });
--- assets/js/vue/components/data-land/DownloadZipBtn.vue
+++ assets/js/vue/components/data-land/DownloadZipBtn.vue
@@ -1,17 +1,35 @@
 <template>
     <div>
-        <button @click="randomBtn">CLick me</button>
+        <b-button size="sm" class="mb-2" @click="randomBtn" variant="primary">
+            <b-icon icon="arrow-down" aria-hidden="true"></b-icon> Download Zip
+        </b-button>
         <DxPopup
-            title="Duplicate Filenames"
+            title="Download Zip"
             :visible.sync="showPopup"
             :close-on-outside-click="true"
             :show-title="true"
             :width="500"
             :height="200">
             <template>
-                <p>
-                    Hello Wrld!
-                </p>
+                <div id="dataset_download_content">
+                    <div>
+                        <p>
+                            All materials on this website are made available to GRIIDC and in turn to you "as-is."
+                            By downloading files, you agree to the
+                            <a href=https://data.gulfresearchinitiative.org/terms-and-conditions>
+                                GRIIDC Terms of Service
+                            </a>.
+                        </p>
+                    </div>
+
+<!--                    <div>-->
+<!--                        <strong>UDI:</strong> ${data.dataset.udi}<br>-->
+<!--                        <strong>File name:</strong> ${data.dataset.filename}<br>-->
+<!--                        <strong>File size:</strong> ${data.dataset.fileSize}<br>-->
+<!--                        <strong>SHA256 Checksum:</strong> ${data.dataset.checksum}<br>-->
+<!--                        <strong>Estimated Download Time:</strong> <span id="dl_time">${testDownload(data.dataset.fileSizeRaw)}</span><br>-->
+<!--                    </div>-->
+                </div>
             </template>
         </DxPopup>
     </div>
@@ -26,6 +44,11 @@ export default {
     name: "DownloadZipBtn",
     components: {
         DxPopup
+    },
+    props: {
+        udi: {
+            required: true
+        }
     },
     data() {
         return {
