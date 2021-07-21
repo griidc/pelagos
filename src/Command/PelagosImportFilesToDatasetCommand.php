cb2f92b3cfdc686eed74aa65c250f9524ecf830b
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2021-07-21 10:04:26 -0500

    Use this messagebus

--- src/Command/PelagosImportFilesToDatasetCommand.php
+++ src/Command/PelagosImportFilesToDatasetCommand.php
@@ -110,7 +110,7 @@ class PelagosImportFilesToDatasetCommand extends Command
             $deleteFile = $fileset->getProcessedFiles()->first();
             if ($deleteFile instanceof File) {
                 $deleteFileMessage = new DeleteFile($deleteFile->getFilePathName(), false);
-                $messageBus->dispatch($deleteFileMessage);
+                $this->messageBus->dispatch($deleteFileMessage);
                 $fileset->removeFile($deleteFile);
             }
         }
