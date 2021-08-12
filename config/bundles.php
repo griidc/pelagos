3585c02dac911ad71ecd7bbd814915d39642b9c1
Michael van den Eijnden <Michael.vandenEijnden@tamucc.edu>
2021-08-12 12:57:35 -0500

    Fixed debug handler for kernel. Doctrine repo manager use statement. Added warmup to router modifier. Removed web server bundle.

--- config/bundles.php
+++ config/bundles.php
@@ -17,7 +17,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Http\HttplugBundle\HttplugBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
