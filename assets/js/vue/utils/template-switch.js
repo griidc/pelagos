06bc8773b5148af5fdd11f3e4108a5e1942836bd
Pondicherry Ravendernath, Praneeth <praneeth.pondicherry@tamucc.edu>
2021-08-24 21:30:52 -0500

    Removed research areas for HRI home page

--- assets/js/vue/utils/template-switch.js
+++ assets/js/vue/utils/template-switch.js
@@ -24,20 +24,7 @@ const templateVariables = {
     displayTextBlock: true,
     researchAwardOption: 'Grant Award',
     cardHeadingText: 'View Project Overview By',
-  },
-  HRI: {
-    name: 'HRI',
-    status: 'Dataset Status',
-    fundingCycle: '',
-    researchGroup: 'Research Areas',
-    fundingOrg: 'Funding Organizations',
-    showFundingCycles: false,
-    showProjectDirector: false,
-    showFundingOrgFacet: false,
-    displayTextBlock: true,
-    researchAwardOption: '',
-    cardHeadingText: 'Research Areas',
-  },
+  }
 };
 
 export default {
