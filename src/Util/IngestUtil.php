13a868616b3114bc7b23e9106979687273e81816
Michael Scott Williamson <michael.williamson@tamucc.edu>
2021-09-02 13:56:40 -0500

    Suggestions implemented per peer-review.

--- src/Util/IngestUtil.php
+++ src/Util/IngestUtil.php
@@ -3,12 +3,6 @@
 namespace App\Util;
 
 use GuzzleHttp\Client;
-use GuzzleHttp\Exception\ClientException;
-use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\Exception\ServerException;
-
-use App\Exception\HttpClientErrorException;
-use App\Exception\HttpServerErrorException;
 
 /**
  * A utility query the Ingest server API.
@@ -36,25 +30,22 @@ class IngestUtil
     /**
      * This function will get a listing of ingest folders.
      *
-     * @param string $user Username to get ingest/incoming/* folder listing for.
+     * @param string $username Username to get ingest/incoming/* folder listing for.
      *
      * @throws HttpClientErrorException Exception thrown when response code is 4xx negotiating with Ingest server REST API.
      * @throws HttpServerErrorException Exception thrown when response code is 5xx negotiating with Ingest server REST API.
      *
      * @return array
      */
-    public function getUsersIngestFoldersInIncomingDir(string $username)
+    public function getUsersIngestFoldersInIncomingDir(string $username): array
     {
         $client = new Client();
-        $metadata = array();
         $url = $this->ingestApiUrl . '/' . $username;
         try {
             $response = $client->request('GET', $url);
-        } catch (ClientException $exception) {
-            throw new HttpClientErrorException($exception->getMessage(), $exception->getCode());
-        } catch (ServerException $exception) {
-            throw new HttpServerErrorException($exception->getMessage(), $exception->getCode());
-        }
+        } catch (\Exception $exception) {
+            throw new \Exception($exception->getMessage(), $exception->getCode());
+        } 
         return(json_decode($response->getBody()->getContents(), true));
     }
 }
