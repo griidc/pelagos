3c9bc8c697b6e8ca97bb501f26e3781f93eadcc1
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2021-12-15 13:04:13 -0600

    some cleanup, correct migration

--- src/Entity/InformationProduct.php
+++ src/Entity/InformationProduct.php
@@ -6,62 +6,122 @@ use App\Repository\InformationProductRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
+ * Information Product Entity class.
+ *
  * @ORM\Entity(repositoryClass=InformationProductRepository::class)
  */
 class InformationProduct extends Entity
 {
     /**
+     * A friendly name for this type of entity.
+     */
+    const FRIENDLY_NAME = 'Information Product';
+
+    /**
+     * The title of the Information Product.
+     *
+     * @var string
+     *
      * @ORM\Column(type="text")
+     *
+     * @Assert\NotBlank(
+     *     message="A title is required."
+     * )
      */
     private $title;
 
     /**
+     * The creators of the Information Product.
+     *
+     * @var string
+     *
      * @ORM\Column(type="text")
+     *
+     * @Assert\NotBlank(
+     *     message="A creator is required."
+     * )
      */
     private $creators;
 
     /**
+     * The publisher of the Information Product.
+     *
+     * @var string
+     *
      * @ORM\Column(type="text")
+     *
+     * @Assert\NotBlank(
+     *     message="A publisher is required."
+     *
      */
     private $publisher;
 
     /**
+     * An external DOI for the Information Product.
+     *
+     * @var string
+     *
      * @ORM\Column(type="text", nullable=true)
      */
     private $externalDoi;
 
     /**
+     * Is the Information Product published.
+     *
+     * @var boolean
+     *
      * @ORM\Column(type="boolean")
      */
-    private $published;
+    private $published = false;
 
     /**
+     * Is the Information Product remotely hosted.
+     *
+     * @var boolean
+     *
      * @ORM\Column(type="boolean")
      */
-    private $remoteResource;
+    private $remoteResource = false;
 
     /**
+     * The research groups this Information Product is associated with.
+     *
+     * @var Collection
+     *
      * @ORM\ManyToMany(targetEntity=ResearchGroup::class)
      */
     private $researchGroups;
 
+    /**
+     * Constructor.
+     *
+     * Initializes collections to empty collections.
+     */
     public function __construct()
     {
         $this->researchGroups = new ArrayCollection();
     }
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
+    /**
+     * Get the title for this Information Product.
+     *
+     * @return string|null
+     */
     public function getTitle(): ?string
     {
         return $this->title;
     }
 
+    /**
+     * Set the title for this Information Product.
+     *
+     * @param string|null $shortTitle The title for this Information Product.
+     *
+     * @return self
+     */
     public function setTitle(string $title): self
     {
         $this->title = $title;
@@ -69,6 +129,11 @@ class InformationProduct extends Entity
         return $this;
     }
 
+    /**
+     * Get the creators for this Information Product.
+     *
+     * @return string|null
+     */
     public function getCreators(): ?string
     {
         return $this->creators;
