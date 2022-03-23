24a68a50803e451bbc5ec35491f0dedeb30ee22b
Michael Scott Williamson <michael.williamson@tamucc.edu>
2022-03-23 15:27:19 -0500

    Simplify per discussion.

--- src/Command/PelagosClearErrorAndFileCommand.php
+++ src/Command/PelagosClearErrorAndFileCommand.php
@@ -2,13 +2,8 @@
 
 namespace App\Command;
 
-use App\Entity\Dataset;
-use App\Entity\DatasetSubmission;
 use App\Entity\File;
-use App\Entity\Fileset;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
-use FFI;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -28,23 +23,14 @@ class PelagosClearErrorAndFileCommand extends Command
     protected $entityManager;
 
     /**
-     * String with .env homedir prefix path.
-     *
-     * @var String $homedirPrefix;
-     */
-    protected $homedirPrefix;
-
-    /**
      * Class constructor for dependency injection.
      *
      * @param EntityManagerInterface $entityManager A Doctrine EntityManager.
      */
     public function __construct(
-        EntityManagerInterface $entityManager,
-        string $homedirPrefix
+        EntityManagerInterface $entityManager
     ) {
         $this->entityManager = $entityManager;
-        $this->homedirPrefix = $homedirPrefix;
 
         // It is required to call parent constructor if
         // using a constructon in a Symfony command.
@@ -59,7 +45,6 @@ class PelagosClearErrorAndFileCommand extends Command
         $this
             ->setDescription(self::$defaultDescription)
             ->addArgument('fileId', InputArgument::REQUIRED, 'ID of file in error to delete')
-            ->addArgument('udi', InputArgument::REQUIRED, 'UDI of the dataset containing file to delete.')
         ;
     }
 
@@ -75,56 +60,27 @@ class PelagosClearErrorAndFileCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
         $fileId = $input->getArgument('fileId');
-        $udi = $input->getArgument('udi');
-        $newUdi = preg_replace('/:/', '.', $udi);
 
-        $dataset = $this->entityManager->getRepository(Dataset::class)->findOneBy(
-            array('udi' => $udi)
+        $fileToDelete = $this->entityManager->getRepository(File::class)->findOneBy(
+        // Only identify files in an error state.
+            array(
+                'id' => $fileId,
+                'status' => File::FILE_ERROR
+            )
         );
 
-        if ($dataset instanceof Dataset) {
-            $fileToDelete = $this->entityManager->getRepository(File::class)->findOneBy(
-            // Only identify files in an error state.
-                array(
-                    'id' => $fileId,
-                    'fileset' => $dataset->getDatasetSubmission()->getFileset(),
-                    'status' => File::FILE_ERROR
-                )
-            );
-
-            if ($fileToDelete instanceof File) {
-                // This section is just added for extra safety. Don't delete
-                // unless the file is represented in the datastore. Don't delete
-                // the only copy of a file.
-                $dqlSafe = "SELECT f FROM \App\Entity\File f
-                    WHERE f.id <> :fileId
-                    AND f.fileset = :filesetId
-                    AND f.physicalFilePath like :newUdi
-                    AND f.physicalFilePath not like :uploadDir";
-
-                $query = $this->entityManager->createQuery($dqlSafe);
-                $query->setParameter('filesetId', $dataset->getDatasetSubmission()->getFileset());
-                $query->setParameter('fileId', $fileId);
-                $query->setParameter('newUdi', preg_replace('/:/', '.', $udi) . '%');
-                $query->setParameter('uploadDir', $this->homedirPrefix . '/upload/files/%');
-                if (count($query->getResult()) > 0) {
-                    try {
-                        $delId = $fileToDelete->getId();
-                        $this->entityManager->remove($fileToDelete);
-                        $this->entityManager->flush();
-                        unlink($fileToDelete->getPhysicalFilePath());
-                        $io->note("Removed file id: $delId at: " . $fileToDelete->getPhysicalFilePath());
-                    } catch (\Exception $e) {
-                        $io->error("Could not delete file." . $e->getMessage());
-                    }
-                } else {
-                    $io->warning("Not deleting the only copy of file!");
-                }
-            } else {
-                $io->warning("File specified ($fileId) is not in error or not found in fileset for dataset ($udi).");
+        if ($fileToDelete instanceof File) {
+            try {
+                $delId = $fileToDelete->getId();
+                $this->entityManager->remove($fileToDelete);
+                $this->entityManager->flush();
+                unlink($fileToDelete->getPhysicalFilePath());
+                $io->note("Removed file id: $delId at: " . $fileToDelete->getPhysicalFilePath());
+            } catch (\Exception $e) {
+                $io->error("Could not delete file." . $e->getMessage());
             }
         } else {
-            $io->warning("Could not find dataset: ($udi).");
+            $io->warning("File specified ($fileId) is not in error or not found.");
         }
         return 0;
     }
