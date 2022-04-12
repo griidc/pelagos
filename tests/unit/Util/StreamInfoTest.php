dc4e955902bb41de9edf41ddc574837e0732036f
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2022-04-12 15:09:24 -0500

    Fix test

--- tests/unit/Util/StreamInfoTest.php
+++ tests/unit/Util/StreamInfoTest.php
@@ -2,11 +2,10 @@
 
 namespace App\Tests\Util;
 
-use PHPUnit\Framework\TestCase;
-use org\bovigo\vfs\vfsStream,
-    org\bovigo\vfs\vfsStreamDirectory;
-
 use App\Util\StreamInfo;
+use Laminas\Diactoros\Stream;
+use PHPUnit\Framework\TestCase;
+use org\bovigo\vfs\vfsStream;
 
 /**
  * Unit tests for App\Util\StreamInfo.php
@@ -33,7 +32,7 @@ class StreamInfoTest extends TestCase
         $file = vfsStream::newFile('test.txt')
                 ->withContent('test')
                 ->at(vfsStream::setup());
-        $this->fileStream = fopen($file->url(), 'r');
+        $this->fileStream = new Stream(fopen($file->url(), 'r'));
     }
 
     /**
@@ -41,7 +40,7 @@ class StreamInfoTest extends TestCase
      */
     public function testFileSize()
     {
-        $size = StreamInfo::getFileSize(array('fileStream' => $this->fileStream));
+        $size = StreamInfo::getFileSize($this->fileStream);
         $this->assertEquals($size, 4);
     }
 
@@ -50,16 +49,16 @@ class StreamInfoTest extends TestCase
      */
     public function testCalculateHashSha256()
     {
-        $hash = StreamInfo::calculateHash(array('fileStream' => $this->fileStream));
+        $hash = StreamInfo::calculateHash($this->fileStream);
         $this->assertEquals($hash, '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08');
     }
-    
+
     /**
      * Test calculating the hash md5.
      */
     public function testCalculateHashMd5()
     {
-        $hash = StreamInfo::calculateHash(array('fileStream' => $this->fileStream), 'md5');
+        $hash = StreamInfo::calculateHash($this->fileStream, 'md5');
         $this->assertEquals($hash, '098f6bcd4621d373cade4e832627b4f6');
     }
 }
