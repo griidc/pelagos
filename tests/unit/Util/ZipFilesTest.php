779142b4e4faf243ee89756b3a7203488433fe7a
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2022-04-18 12:57:01 -0500

    Added zipper test

---
+++ tests/unit/Util/ZipFilesTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Tests\Util;
+
+use App\Util\ZipFiles;
+use Laminas\Diactoros\Stream;
+use org\bovigo\vfs\content\LargeFileContent;
+use org\bovigo\vfs\vfsStream,
+    org\bovigo\vfs\vfsStreamDirectory;
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * Unit tests for App\Util\ZipFiles.php
+ */
+class ZipFilesTest extends KernelTestCase
+{
+    /**
+     * Test adding large file.
+     */
+    public function testAddFileToZip()
+    {
+        self::bootKernel();
+        /** @var ZipFiles $zipFiles */
+        $zipFiles = static::$container->get(ZipFiles::class);
+        $root = vfsStream::setup();
+        $zipFile = vfsStream::newFile('test.zip')
+            ->at($root);
+        $resource = fopen($zipFile->url(), 'w+');
+
+        $outputStream = new Stream($resource);
+        $zipFiles->start($outputStream, 'test.zip');
+
+        $largeFile = vfsStream::newFile('large.txt')
+            ->withContent(LargeFileContent::withMegaBytes(100))
+            ->at($root);
+
+        $largeFileResource = fopen($largeFile->url(), 'r');
+        $largeFileStream = new Stream($largeFileResource);
+        $zipFiles->addFile('large.txt', $largeFileStream);
+
+        $testFile = vfsStream::newFile('test.txt')
+            ->withContent('test')
+            ->at($root);
+
+        $testFileResource = fopen($testFile->url(), 'r');
+        $testFileStream = new Stream($testFileResource);
+        $zipFiles->addFile('large.txt', $testFileStream);
+
+        $zipFiles->finish();
+
+        $outputStream->close();
+
+        $resource = fopen($zipFile->url(), 'r');
+        $stream = new Stream($resource);
+
+        $this->assertEquals(102140, $stream->getSize());
+    }
+}
