4dc52748b505eda191eaeb8da97154227ab9b9e3
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2022-04-19 17:49:04 -0500

    Use guzzle for hashing

--- src/Util/StreamInfo.php
+++ src/Util/StreamInfo.php
@@ -3,6 +3,7 @@
 namespace App\Util;
 
 use App\Entity\DatasetSubmission;
+use GuzzleHttp\Psr7\Utils as guzzlePsr7Utils;
 use Psr\Http\Message\StreamInterface;
 
 /**
@@ -13,23 +14,20 @@ class StreamInfo
     /**
      * Get the hash for file stream.
      *
-     * @param array $inputFileStream The file stream array.
-     * @param array $algo            The algorithm for the hash function (default SHA256).
+     * @param StreamInterface $inputFileStream The file stream.
+     * @param array           $algo            The algorithm for the hash function (default SHA256).
      *
      * @return string The hash calculated from the stream.
      */
     public static function calculateHash(StreamInterface $inputFileStream, string $algo = DatasetSubmission::SHA256) :string
     {
-        $fileStream = $inputFileStream->detach() ?? null;
-        $context = hash_init($algo);
-        hash_update_stream($context, $fileStream);
-        return hash_final($context);
+        return guzzlePsr7Utils::hash($inputFileStream, $algo);
     }
 
     /**
      * Get the file size for the file stream.
      *
-     * @param array $inputFileStream The file stream array.
+     * @param StreamInterface $inputStream The file stream.
      *
      * @return integer The file size of the stream.
      */
