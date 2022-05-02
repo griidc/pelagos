96a82c4bf6f16bb02e0196a1111a012468269f64
Praneeth Pondicherry Ravendernath <praneeth.pondicherry@tamucc.edu>
2022-05-01 22:09:53 -0500

    Added check for descriptors

--- src/Controller/Api/InformationProductController.php
+++ src/Controller/Api/InformationProductController.php
@@ -87,11 +87,13 @@ class InformationProductController extends AbstractFOSRestController
         }
         $productTypeDescriptorIds = $request->get('selectedProductTypes');
         $productTypeDescriptors = $entityManager->getRepository(ProductTypeDescriptor::class)->findBy(['id' => $productTypeDescriptorIds]);
+        $this->checkIfDescriptorExists($productTypeDescriptorIds, $productTypeDescriptors);
         foreach ($productTypeDescriptors as $productTypeDescriptor) {
             $informationProduct->addProductTypeDescriptor($productTypeDescriptor);
         }
         $digitalResourceTypeDescriptorIds = $request->get('selectedDigitalResourceTypes');
         $digitalResourceTypeDescriptors = $entityManager->getRepository(DigitalResourceTypeDescriptor::class)->findBy(['id' => $digitalResourceTypeDescriptorIds]);
+        $this->checkIfDescriptorExists($digitalResourceTypeDescriptorIds, $digitalResourceTypeDescriptors);
         foreach ($digitalResourceTypeDescriptors as $digitalResourceTypeDescriptor) {
             $informationProduct->addDigitalResourceTypeDescriptor($digitalResourceTypeDescriptor);
         }
@@ -154,6 +156,7 @@ class InformationProductController extends AbstractFOSRestController
         $productTypeDescriptorIds = $request->get('selectedProductTypes');
         $productTypeDescriptorsToBeDeleted = $entityManager->getRepository(ProductTypeDescriptor::class)->findBy(['id' => $informationProduct->getProductTypeDescriptorList()]);
         $productTypeDescriptorsToBeAdded = $entityManager->getRepository(ProductTypeDescriptor::class)->findBy(['id' => $productTypeDescriptorIds]);
+        $this->checkIfDescriptorExists($productTypeDescriptorIds, $productTypeDescriptorsToBeAdded);
         // Remove previously added product type descriptors
         foreach ($productTypeDescriptorsToBeDeleted as $productTypeDescriptor) {
             $informationProduct->removeProductTypeDescriptor($productTypeDescriptor);
@@ -165,6 +168,7 @@ class InformationProductController extends AbstractFOSRestController
         $digitalResourceTypeDescriptorIds = $request->get('selectedDigitalResourceTypes');
         $digitalResourceTypeDescriptorsToBeDeleted = $entityManager->getRepository(DigitalResourceTypeDescriptor::class)->findBy(['id' => $informationProduct->getDigitalResourceTypeDescriptorList()]);
         $digitalResourceTypeDescriptorsToBeAdded = $entityManager->getRepository(DigitalResourceTypeDescriptor::class)->findBy(['id' => $digitalResourceTypeDescriptorIds]);
+        $this->checkIfDescriptorExists($digitalResourceTypeDescriptorIds, $digitalResourceTypeDescriptorsToBeAdded);
         // Remove previously added digital resource type descriptors
         foreach ($digitalResourceTypeDescriptorsToBeDeleted as $digitalResourceTypeDescriptor) {
             $informationProduct->removeDigitalResourceTypeDescriptor($digitalResourceTypeDescriptor);
@@ -480,6 +484,25 @@ class InformationProductController extends AbstractFOSRestController
         } elseif ($file->getStatus() === File::FILE_DONE) {
             $deleteMessage = new DeleteFile($file->getPhysicalFilePath());
             $messageBus->dispatch($deleteMessage);
+        }
+    }
+
+    /**
+     * Check if descriptor type or product type exists.
+     *
+     * @param array $idsSelected
+     * @param array $descriptorsToBeAdded
+     *
+     * @return void
+     */
+    private function checkIfDescriptorExists(array $idsSelected, array $descriptorsToBeAdded)
+    {
+        if (count($descriptorsToBeAdded) !== count($idsSelected)) {
+            foreach ($descriptorsToBeAdded as $descriptor) {
+                if (in_array($descriptor->getId(), $idsSelected)) {
+                    throw new BadRequestHttpException(get_class($descriptor) . ' id:'. $descriptor->getId());
+                }
+            }
         }
     }
 }
