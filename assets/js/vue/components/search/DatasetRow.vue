511356db83682ffae067da5e32909eb279d37e8d
Praneeth Pondicherry Ravendernath <praneeth.pondicherry@tamucc.edu>
2022-06-02 09:27:34 -0500

    Added coldstorage unpacked size

--- assets/js/vue/components/search/DatasetRow.vue
+++ assets/js/vue/components/search/DatasetRow.vue
@@ -28,12 +28,7 @@
                     <div>
                         UDI: {{ datasetRowData.udi }}
                     </div>
-                  <div v-if="datasetRowData.coldStorage && datasetRowData.availabilityStatus !== 7
-                       && datasetRowData.datasetSubmission
-                       && datasetRowData.datasetSubmission.coldStorageTotalUnpackedSize">
-                    File Size: {{ humanSize(datasetRowData.datasetSubmission.coldStorageTotalUnpackedSize) }}
-                  </div>
-                    <div v-else-if="datasetRowData.availabilityStatus !== 7 && datasetRowData.fileSize">
+                    <div v-if="datasetRowData.availabilityStatus !== 7 && datasetRowData.fileSize">
                       File Size: {{ datasetRowData.fileSize }}
                     </div>
                 </div>
