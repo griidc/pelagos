d21ad31121599a14e7b89df79b0d6c7757517f97
Michael Scott Williamson <michael.williamson@tamucc.edu>
2022-08-05 15:33:32 -0500

    Move constant up.

--- src/Util/Base32Generator.php
+++ src/Util/Base32Generator.php
@@ -10,7 +10,17 @@ use Hidehalo\Nanoid\GeneratorInterface;
  */
 class Base32Generator
 {
+    /**
+     * @string Alaphabet by Douglass Crockford for ease of human communication of strings over voice and
+     * minimizing the possibility of generation of incidental curse words.
+     */
     const CROCKFORDALPHABET='0123456789abcdefghjkmnpqrstvwxyz';
+
+    /**
+     * Method that returns an 8 char cryptographically-sound pseudorandom string using the Crockford 32 alphabet.
+     *
+     * @return string
+     */
     public static function generateId(): string
     {
         $client = new Client();
