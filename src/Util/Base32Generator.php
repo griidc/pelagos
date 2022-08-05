348a62895de5cf848952fb9bac6f8b8ce2c9eea0
Michael Scott Williamson <michael.williamson@tamucc.edu>
2022-08-05 15:59:02 -0500

    Code standards cleanup.

--- src/Util/Base32Generator.php
+++ src/Util/Base32Generator.php
@@ -14,7 +14,7 @@ class Base32Generator
      * @string Alaphabet by Douglass Crockford for ease of human communication of strings over voice and
      * minimizing the possibility of generation of incidental curse words.
      */
-    const CROCKFORDALPHABET='0123456789abcdefghjkmnpqrstvwxyz';
+    const CROCKFORDALPHABET = '0123456789abcdefghjkmnpqrstvwxyz';
 
     /**
      * Method that returns an 8 char cryptographically-sound pseudorandom string using the Crockford 32 alphabet.
@@ -24,6 +24,6 @@ class Base32Generator
     public static function generateId(): string
     {
         $client = new Client();
-        return $client->formattedId(self::CROCKFORDALPHABET, $size=8);
+        return $client->formattedId(self::CROCKFORDALPHABET, $size = 8);
     }
 }
