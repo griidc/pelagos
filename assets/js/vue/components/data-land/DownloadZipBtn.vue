962f04fe2360c9029fcaa1b740df18eeb140744a
Praneeth Pondicherry Ravendernath <praneeth.pondicherry@tamucc.edu>
2022-09-28 11:02:46 -0500

    Refactored to use bootstrap modal

--- assets/js/vue/components/data-land/DownloadZipBtn.vue
+++ assets/js/vue/components/data-land/DownloadZipBtn.vue
@@ -1,92 +1,83 @@
 <template>
     <div>
-        <b-button size="sm" class="mb-2" @click="downloadBtn" variant="primary">
-          <i class="fas fa-external-link-alt" v-if="datasetInfo.remotelyHosted"></i>
-          <i class="fas fa-download" v-else></i>
-          {{ buttonTitle }}
-        </b-button>
-        <DxPopup
-            :title="buttonTitle"
-            :visible.sync="showDownloadDialog"
-            :close-on-outside-click="true"
-            :show-title="true"
-            :width="500"
-            :height="500">
-            <template>
-                <div v-if="datasetInfo.remotelyHosted">
-                    <h3>The dataset you selected is hosted by an external repository.</h3>
-                    <div>
-                        <p>
-                            All materials on this website are made available to
-                            GRIIDC and in turn to you "as-is." By downloading files,
-                            you agree to the <a href=https://data.gulfresearchinitiative.org/terms-and-conditions>GRIIDC Terms of
-                            Service</a>.
-                        </p>
-                        <p>
-                            This particular dataset is not hosted directly by GRIIDC, so additional
-                            terms and conditions may be
-                            imposed by the hosting entity.
-                        </p>
-                    </div>
-                    <div>
-                        <strong>UDI:</strong> {{ datasetInfo.dataset.udi }}<br>
-                        <strong>Location:</strong>
-                        <a :href=datasetInfo.fileUri target=_BLANK>
-                            {{ datasetInfo.fileUri }}
-                        </a>
-                        <b-icon
-                                icon="arrow-up-right"
-                                aria-hidden="true"
-                                v-if="datasetInfo.remotelyHosted">
-                        </b-icon>
-                        <br>
-                        <p>
-                            {{ additionalInfo }}
-                        </p>
-                    </div>
-                </div>
-                <div v-else>
-                    <div>
-                        <p>
-                            All materials on this website are made available to GRIIDC and in turn
-                            to you "as-is." By downloading files, you agree to the
-                            <a href="https://data.gulfresearchinitiative.org/terms-and-conditions" target="_blank">
-                                GRIIDC Terms of Service
-                            </a>
-                        </p>
-                    </div>
-                    <hr>
-                    <div>
-                        <strong>UDI:</strong> {{ datasetInfo.dataset.udi }}<br>
-                        <strong>File name:</strong> {{ datasetInfo.dataset.filename }}<br>
-                        <strong>File size:</strong> {{ datasetInfo.dataset.fileSize }}<br>
-                        <strong>SHA256 Checksum:</strong> {{ datasetInfo.dataset.checksum }}<br>
-                        <strong>Estimated Download Time:</strong> {{ estimatedDownloadTime }}<br>
-                    </div>
-                </div>
-                <DxToolbarItem v-if="!datasetInfo.remotelyHosted"
-                               widget="dxButton"
-                               toolbar="bottom"
-                               location="center"
-                               :options="downloadButtonOptions"
-                />
-            </template>
-        </DxPopup>
+      <b-button size="sm" class="mb-2" @click="downloadBtn" variant="primary">
+        <i class="fas fa-external-link-alt" v-if="datasetInfo.remotelyHosted"></i>
+        <i class="fas fa-download" v-else></i>
+        {{ buttonTitle }}
+      </b-button>
+      <b-modal
+          id="modal-1"
+          :title="buttonTitle"
+          v-model="showDownloadDialog"
+          :static="staticValue"
+          ok-variant="primary"
+          hide-footer
+          header-text-variant="dark">
+        <div v-if="datasetInfo.remotelyHosted">
+          <h3>The dataset you selected is hosted by an external repository.</h3>
+          <div>
+            <p>
+              All materials on this website are made available to
+              GRIIDC and in turn to you "as-is." By downloading files,
+              you agree to the <a href=https://data.gulfresearchinitiative.org/terms-and-conditions>GRIIDC Terms of
+              Service</a>.
+            </p>
+            <p>
+              This particular dataset is not hosted directly by GRIIDC, so additional
+              terms and conditions may be
+              imposed by the hosting entity.
+            </p>
+          </div>
+          <div>
+            <div>
+              <strong>UDI:</strong> {{ datasetInfo.dataset.udi }}<br>
+            </div>
+            <strong>Location:</strong>
+            <a :href=datasetInfo.fileUri target=_BLANK>
+              {{ datasetInfo.fileUri }}
+            </a>
+            <b-icon
+                icon="arrow-up-right"
+                aria-hidden="true"
+                v-if="datasetInfo.remotelyHosted">
+            </b-icon>
+            <br>
+            <p>
+              {{ additionalInfo }}
+            </p>
+          </div>
+        </div>
+        <div v-else>
+          <div>
+            <p>
+              All materials on this website are made available to GRIIDC and in turn
+              to you "as-is." By downloading files, you agree to the
+              <a href="https://data.gulfresearchinitiative.org/terms-and-conditions" target="_blank">
+                GRIIDC Terms of Service
+              </a>
+            </p>
+          </div>
+          <hr>
+          <div>
+            <strong>UDI:</strong> {{ datasetInfo.dataset.udi }}<br>
+            <strong>File name:</strong> {{ datasetInfo.dataset.filename }}<br>
+            <strong>File size:</strong> {{ datasetInfo.dataset.fileSize }}<br>
+            <strong>SHA256 Checksum:</strong> {{ datasetInfo.dataset.checksum }}<br>
+            <strong>Estimated Download Time:</strong> {{ estimatedDownloadTime }}<br>
+          </div>
+        </div>
+        <b-button v-if="!datasetInfo.remotelyHosted" class="mt-3" variant="primary" @click="downloadDataset"><i class="fas fa-download"></i> Download</b-button>
+      </b-modal>
     </div>
 </template>
 
 <script>
 import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.light.css';
-import { DxPopup, DxToolbarItem } from 'devextreme-vue/popup';
 import { getApi } from '../../utils/axiosService';
 
 export default {
   name: 'DownloadZipBtn',
-  components: {
-    DxPopup,
-    DxToolbarItem,
-  },
   props: {
     id: {
       required: true,
@@ -94,6 +85,7 @@ export default {
   },
   data() {
     return {
+      staticValue: true,
       showDownloadDialog: false,
       datasetInfo: {
         remotelyHosted: false,
@@ -102,31 +94,6 @@ export default {
       },
       buttonTitle: '',
       additionalInfo: '',
-      downloadButtonOptions: {
-        icon: 'download',
-        text: 'Download',
-        onClick: () => {
-          // eslint-disable-next-line no-undef
-          const url = `${Routing.generate('pelagos_app_download_http', { id: this.id })}`;
-          const link = document.createElement('a');
-          link.href = url;
-          document.body.appendChild(link);
-          setTimeout(() => {
-            document.body.removeChild(link);
-            window.URL.revokeObjectURL(url);
-          }, 0);
-          link.click();
-          // eslint-disable-next-line no-undef
-          notify({
-            // eslint-disable-next-line no-undef
-            message,
-            position: {
-              my: 'center top',
-              at: 'center top',
-            },
-          }, 'success', 3000);
-        },
-      },
       estimatedDownloadTime: 'Calculating...',
     };
   },
@@ -153,6 +120,27 @@ export default {
   methods: {
     downloadBtn() {
       this.showDownloadDialog = true;
+    },
+    downloadDataset() {
+      // eslint-disable-next-line no-undef
+      const url = `${Routing.generate('pelagos_app_download_http', { id: this.id })}`;
+      const link = document.createElement('a');
+      link.href = url;
+      document.body.appendChild(link);
+      setTimeout(() => {
+        document.body.removeChild(link);
+        window.URL.revokeObjectURL(url);
+      }, 0);
+      link.click();
+      // eslint-disable-next-line no-undef
+      notify({
+        // eslint-disable-next-line no-undef
+        message,
+        position: {
+          my: 'center top',
+          at: 'center top',
+        },
+      }, 'success', 3000);
     },
   },
   watch: {
@@ -186,5 +174,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
