16a7c521eb21e9f2b8b3f06b1c490e8d7b305620
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2022-11-11 12:00:18 -0600

    Experimental force to float

--- src/Event/DatasetIndexSubscriber.php
+++ src/Event/DatasetIndexSubscriber.php
@@ -34,7 +34,7 @@ class DatasetIndexSubscriber implements EventSubscriberInterface
     /**
      * Populate calculated fields in the Dataset index.
      *
-     * @param TransformEvent $event The event that triggeref this.
+     * @param PostTransformEvent $event The event that triggeref this.
      *
      * @return void
      */
@@ -61,7 +61,6 @@ class DatasetIndexSubscriber implements EventSubscriberInterface
             $wkt = null;
         }
 
-
         if (null !== $wkt) {
             $document->set('geometry', $wkt);
             $geometry = \geoPHP::load($wkt, 'wkt');
@@ -69,11 +68,43 @@ class DatasetIndexSubscriber implements EventSubscriberInterface
             // If the geometry couldn't be simplified.
             if (null == $simpleGeometry or $simpleGeometry->isEmpty()) {
                 // Set the original geometry as a GeoJSON array.
-                $document->set('simpleGeometry', json_decode($geometry->out('json'), true));
+                $json = $geometry->out('json');
             } else {
                 // Set the simpllified geometry as a GeoJSON array.
-                $document->set('simpleGeometry', json_decode($simpleGeometry->out('json'), true));
+                $json = $simpleGeometry->out('json');
             }
+            $array = json_decode($json, true);
+
+            // var_dump($array["type"]);
+            // var_dump($array['coordinates']);
+
+            if (key_exists('coordinates', $array)) {
+
+                $array['coordinates'] = array_map(array($this, 'coordinatesToFloat'), $array['coordinates']);
+
+                // $array['coordinates'] = array_map(function ($coordinates) {
+                //     if (!is_array($coordinates)) {
+                //         return floatval($coordinates);
+                //     }
+                //     $floatCoordinates = [];
+                //     foreach ($coordinates as $pair) {
+                //         if (!is_array($pair)) {
+                //             $floatCoordinates[] = floatval($pair);
+                //         } else {
+                //             $floatPoints = [];
+                //             foreach ($pair as $point) {
+                //                 $floatPoints[] = floatval($point);
+                //             }
+                //             $floatCoordinates[] = $floatPoints;
+                //         }
+                //     }
+                //     return $floatCoordinates;
+                // }, $array['coordinates']);
+            }
+
+            // dump($array);
+
+            $document->set('simpleGeometry', $array);
         }
 
         if (null !== $dataset->getDatasetSubmission()) {
@@ -122,6 +153,28 @@ class DatasetIndexSubscriber implements EventSubscriberInterface
         } else {
             $document->set('updatedDateTime', $dataset->getModificationTimeStamp()->format('Ymd\THis\Z'));
         }
+    }
+
+    /**
+     * Forces array elements to float.
+     *
+     * @param array|float $coordinates
+     */
+    private function coordinatesToFloat($coordinates): array|float
+    {
+        if (!is_array($coordinates)) {
+            return floatval($coordinates);
+        }
+        $floatCoordinates = [];
+        foreach ($coordinates as $pair) {
+            if (!is_array($pair)) {
+                $floatCoordinates[] = floatval($pair);
+            } else {
+                $floatCoordinates[] = $this->coordinatesToFloat($pair);
+            }
+        }
+
+        return $floatCoordinates;
     }
 
     /**
