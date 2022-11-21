7fa1ea89d7add9e5209f73e4d0e04b877b17adf6
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2022-11-21 16:07:41 -0600

    Modified template switcher

--- assets/js/vue/utils/template-switch.js
+++ assets/js/vue/utils/template-switch.js
@@ -1,7 +1,7 @@
+global.baseTemplateName = 'default';
+
 const templateVariables = {
-  default: 'GRIIDC',
-  GRIIDC: {
-    name: 'GRIIDC',
+  default: {
     status: 'Dataset Status',
     fundingCycle: 'Funding Cycles',
     researchGroup: 'Research Groups',
@@ -14,50 +14,37 @@ const templateVariables = {
     productTypeDesc: 'Product Type',
     tags: 'Tags',
   },
+  GRIIDC: {
+  },
   GRP: {
-    name: 'GRP',
-    status: 'Dataset Status',
     fundingCycle: 'Grant Awards',
     researchGroup: 'Projects',
-    fundingOrg: 'Funding Organizations',
     showFundingCycles: true,
     showProjectDirector: true,
     showFundingOrgFacet: false,
     researchAwardOption: 'Grant Award',
     cardHeadingText: 'View Project Overview By',
-    digitalTypeDesc: 'Resource Type',
-    productTypeDesc: 'Product Type',
-    tags: 'Tags',
   },
   HRI: {
-    name: 'HRI',
-    status: 'Dataset Status',
-    fundingCycle: 'Funding Cycles',
-    researchGroup: 'Research Groups',
-    fundingOrg: 'Funding Organizations',
     showFundingCycles: false,
     showProjectDirector: false,
     showFundingOrgFacet: false,
-    researchAwardOption: 'Research Award',
-    digitalTypeDesc: 'Resource Type',
-    productTypeDesc: 'Product Type',
-    tags: 'Tags',
   },
 };
 
 export default {
   getProperty: (name) => {
     if (
-    // eslint-disable-next-line no-undef
-      PELAGOS_TEMPLATE_PROPS !== undefined
-      // eslint-disable-next-line no-undef
-            && PELAGOS_TEMPLATE_PROPS.BaseTemplateName !== undefined
-      // eslint-disable-next-line no-undef
-            && templateVariables[PELAGOS_TEMPLATE_PROPS.BaseTemplateName] !== undefined
+      global.baseTemplateName !== undefined
+            && global.baseTemplateName !== undefined
+            && templateVariables[global.baseTemplateName] !== undefined
+            && name in templateVariables[global.baseTemplateName]
     ) {
-      // eslint-disable-next-line no-undef
-      return templateVariables[PELAGOS_TEMPLATE_PROPS.BaseTemplateName][name];
+      return templateVariables[global.baseTemplateName][name];
     }
-    return templateVariables[templateVariables.default][name];
+    return templateVariables.default[name];
+  },
+  setTemplate: (template) => {
+    global.baseTemplateName = template;
   },
 };
