9552a3b83f68610e3a576c39822a2aac55968550
Michael Scott Williamson <michael.williamson@tamucc.edu>
2023-01-10 10:55:31 -0600

    Added Funder entity.

---
+++ src/Entity/Funder.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FunderRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Funder class.
+ */
+#[ORM\Entity(repositoryClass: FunderRepository::class)]
+class Funder extends Entity
+{
+    /**
+     * Name of the Funder.
+     */
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $name = null;
+
+    /**
+     * Reference URI of the Funder.
+     */
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $referenceUri = null;
+
+    /**
+     * Gets name of the funder.
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Sets name of the funder.
+     *
+     * @param string $name name of the funder
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Gets the reference URI for the Funder.
+     */
+    public function getReferenceUri(): ?string
+    {
+        return $this->referenceUri;
+    }
+
+    /**
+     * Sets the reference URI for the Funder.
+     *
+     * @param string $referenceUri the reference URI for the Funder
+     */
+    public function setReferenceUri(?string $referenceUri): self
+    {
+        $this->referenceUri = $referenceUri;
+
+        return $this;
+    }
+}
