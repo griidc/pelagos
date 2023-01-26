f02fcaa0911a7acbf6abcf13725c4191c4547a96
Praneeth Pondicherry Ravendernath <praneeth.pondicherry@tamucc.edu>
2023-01-26 11:43:31 -0600

    Moved to separate funder component file

--- assets/static/js/dataset-submission.js
+++ assets/static/js/dataset-submission.js
@@ -33,62 +33,6 @@ $(function() {
 
     $("html").show();
 
-    const selectedFunders = [];
-
-    function removeItemFromList(arr, value) {
-        var index = arr.indexOf(value);
-        if (index > -1) {
-          arr.splice(index, 1);
-        }
-        return arr;
-    }
-
-    const addedFunders = [];
-
-    $.ajax({
-        url: Routing.generate('pelagos_api_datasets_get_collection') + '?udi=' + $("#regForm").attr("udi") + '&_properties=funders.id',
-        success: function(data){
-            if (data && data.length > 0) {
-                let associatedFunders = data[0].funders;
-                associatedFunders.forEach((funder) => {
-                  addedFunders.push(funder.id);
-                });
-            }
-        },
-    }).always(function() {
-        $("#funderTagBox").dxTagBox({
-            placeholder: 'Choose Funder...',
-            width: "90%",
-            dataSource: Routing.generate('app_api_funders_by_name'),
-            displayExpr: 'name',
-            value: addedFunders,
-            valueExpr: 'id',
-            searchEnabled: true,
-            acceptCustomValue: true,
-            onSelectionChanged(event) {
-                event.addedItems.forEach(addedItem => {
-                    selectedFunders.push(addedItem.id);
-                });
-                event.removedItems.forEach(removedItem => {
-                    removeItemFromList(selectedFunders, removedItem.id);
-                });
-                $("#funders").val(selectedFunders);
-            },
-            onCustomItemCreating(args) {
-                const newItem = {
-                    id: -1,
-                    name: args.text.trim(),
-                }
-                const { component } = args;
-                const currentItems = component.option('items');
-                currentItems.unshift(newItem);
-                component.option('items', currentItems);
-                args.customItem = newItem;
-                selectedFunders.push(newItem.name);
-            },
-        });
-    });
-
     $("label").next("input[required],textarea[required],select[required]").prev().addClass("emRequired");
     //Setup qTip
     $.fn.qtip.defaults = $.extend(true, {}, $.fn.qtip.defaults, {
