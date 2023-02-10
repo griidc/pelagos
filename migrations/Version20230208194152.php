9cd51c100b837e408025a707bbd83b9ab6144331
Michael Scott Williamson <michael.williamson@tamucc.edu>
2023-02-10 08:16:15 -0600

    Cleaned up migration and added description.

--- migrations/Version20230208194152.php
+++ migrations/Version20230208194152.php
@@ -14,7 +14,7 @@ final class Version20230208194152 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return '';
+        return 'This migration changes FOs unique index to a normal index.';
     }
 
     public function up(Schema $schema) : void
@@ -31,7 +31,6 @@ final class Version20230208194152 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP INDEX IDX_2A1BCCA0FDAAAD2F');
         $this->addSql('CREATE UNIQUE INDEX uniq_2a1bcca0fdaaad2f ON funding_organization (default_funder_id)');
     }
