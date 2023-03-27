30cec9743ce5fa5b645d495eb4c09f1fccac67cc
Michael Scott Williamson <michael.williamson@tamucc.edu>
2023-03-27 12:37:49 -0500

    Add callback to MDE, also fixes whitespace issue.

--- web/applications/metadata-editor/index.php
+++ web/applications/metadata-editor/index.php
@@ -19,61 +19,54 @@ drupal_add_library('system', 'ui.widget');
 drupal_add_library('system', 'ui.dialog');
 drupal_add_library('system', 'ui.tooltip');
 
-drupal_add_css('includes/css/metadata.css',array('type'=>'external'));
-drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.11.1/jquery.validate.min.js',array('type'=>'external'));
+drupal_add_css('includes/css/metadata.css', array('type'=>'external'));
+drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.11.1/jquery.validate.min.js', array('type'=>'external'));
 //drupal_add_css('misc/ui/jquery.ui.button.css');
 //drupal_add_css('misc/ui/jquery.ui.datepicker.css');
 //drupal_add_css('misc/ui/jquery.ui.tabs.css');
 //drupal_add_css('misc/ui/jquery.ui.dialog.css');
 
-drupal_add_js('//cdn.jsdelivr.net/openlayers/2.13.1/OpenLayers.js',array('type'=>'external'));
+drupal_add_js('//cdn.jsdelivr.net/openlayers/2.13.1/OpenLayers.js', array('type'=>'external'));
 
-drupal_add_js('//maps.google.com/maps/api/js?v=3&sensor=false&key='.$config['google_maps_api_key'],array('type'=>'external'));
+drupal_add_js('//maps.google.com/maps/api/js?v=3&sensor=false&key='.$config['google_maps_api_key'].'&callback=gmapsCallback', array('type'=>'external'));
 
-drupal_add_js('includes/geoviz/geoviz.js',array('type'=>'external'));
-drupal_add_js('includes/geoviz/mapWizard.js',array('type'=>'external'));
+drupal_add_js('includes/geoviz/geoviz.js', array('type'=>'external'));
+drupal_add_js('includes/geoviz/mapWizard.js', array('type'=>'external'));
 
-if (array_key_exists('action',$_GET) and $_GET['action'] == 'help') {
+if (array_key_exists('action', $_GET) and $_GET['action'] == 'help') {
     require 'help.html';
     exit;
 }
 
 $xmldoc = null;
 
-if (isset($_FILES["file"]))
-{
-	if ($_FILES["file"]["error"] > 0)
-	{
-		//echo "Error: " . $_FILES["file"]["error"] . "<br>";
-		$dMessage = 'Error while loading file: ' .  $_FILES["file"]["error"];
-		drupal_set_message($dMessage, 'error', false);
-	}
-	else
-	{
-		//echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-		//echo "Type: " . $_FILES["file"]["type"] . "<br>";
-		//echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-		//echo "Stored in: " . $_FILES["file"]["tmp_name"];
-		$thefile = $_FILES["file"]["tmp_name"];
-	}
+if (isset($_FILES["file"])) {
+    if ($_FILES["file"]["error"] > 0) {
+        //echo "Error: " . $_FILES["file"]["error"] . "<br>";
+        $dMessage = 'Error while loading file: ' .  $_FILES["file"]["error"];
+        drupal_set_message($dMessage, 'error', false);
+    } else {
+        //echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+        //echo "Type: " . $_FILES["file"]["type"] . "<br>";
+        //echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+        //echo "Stored in: " . $_FILES["file"]["tmp_name"];
+        $thefile = $_FILES["file"]["tmp_name"];
+    }
 }
 
-if (isset($_POST))
-{
-	if (count($_POST)>1)
-	{
-		makeXML($_POST);
-	}
+if (isset($_POST)) {
+    if (count($_POST)>1) {
+        makeXML($_POST);
+    }
 }
 
-if (isset($_GET["dataUrl"]) and !isset($_FILES["file"]))
-{
-	$xmlURL = $_GET["dataUrl"];
-	$xmldoc = loadXMLFromURL($xmlURL);
+if (isset($_GET["dataUrl"]) and !isset($_FILES["file"])) {
+    $xmlURL = $_GET["dataUrl"];
+    $xmldoc = loadXMLFromURL($xmlURL);
 
     if ($xmldoc != null and gettype($xmldoc) == 'object') {
         $dMessage = 'Successfully loaded XML from URL: ' .  $xmlURL;
-        drupal_set_message($dMessage, 'status',false);
+        drupal_set_message($dMessage, 'status', false);
     } elseif ($xmldoc != null and $xmldoc == 415) {
         $dMessage =  'Sorry, the GRIIDC Metadata Editor is unable to load ';
         $dMessage .= 'the submitted metadata file because it is not valid ';
@@ -84,19 +77,18 @@ if (isset($_GET["dataUrl"]) and !isset($_FILES["file"]))
     } elseif ($xmldoc != null and is_array($xmldoc)) {
         $dMessage = 'Error while loading data from: ' .  $xmlURL;
         $dMessage .= "<br>This does not appear to be a valid ISO 19115-2 metadata file.";
-		drupal_set_message($dMessage, 'error', false);
+        drupal_set_message($dMessage, 'error', false);
         $xmldoc = null;
     } else {
-		$dMessage = 'Error while loading data from: ' .  $xmlURL;
-		drupal_set_message($dMessage, 'error', false);
+        $dMessage = 'Error while loading data from: ' .  $xmlURL;
+        drupal_set_message($dMessage, 'error', false);
         $xmldoc = null;
-	}
+    }
 }
 
-if (isset($thefile))
-{
-	if ($_FILES["file"]["type"] == "text/xml") {
-		$xmldoc = loadXMLFromFile($thefile);
+if (isset($thefile)) {
+    if ($_FILES["file"]["type"] == "text/xml") {
+        $xmldoc = loadXMLFromFile($thefile);
         if ($xmldoc != null and (is_array($xmldoc) or 'gmi:MI_Metadata' !== $xmldoc->documentElement->tagName)) {
             $dMessage = 'Unable to load file: ' .  $_FILES["file"]["name"];
             $dMessage .= "<br>This does not appear to be a valid ISO 19115-2 metadata file.";
@@ -110,17 +102,16 @@ if (isset($thefile))
             $dMessage = 'Successfully loaded file: ' .  $_FILES["file"]["name"];
             drupal_set_message($dMessage, 'status', false);
         }
-	} else {
-		$dMessage = 'Sorry.' .  $_FILES["file"]["name"] . ', is not an XML file!';
-		drupal_set_message($dMessage, 'warning', false);
-	}
+    } else {
+        $dMessage = 'Sorry.' .  $_FILES["file"]["name"] . ', is not an XML file!';
+        drupal_set_message($dMessage, 'warning', false);
+    }
 }
 
 $mMD = new metaData();
 
-if (isset($xmldoc))
-{
-	$mMD->xmldoc = $xmldoc;
+if (isset($xmldoc)) {
+    $mMD->xmldoc = $xmldoc;
 }
 
 include 'MI_Metadata.php';
@@ -182,7 +173,9 @@ Click OK to download.
 			</div>
 			<form name="metadata" id="metadata" method="post">
             <input type="hidden" name="__validated" value="0">
-			<input type="hidden" id="__ldxmldoc" name="__ldxmldoc" value="<?php if(isset($mMD->xmldoc)){echo base64_encode($mMD->xmldoc->saveXML());};?>">
+			<input type="hidden" id="__ldxmldoc" name="__ldxmldoc" value="<?php if (isset($mMD->xmldoc)) {
+			    echo base64_encode($mMD->xmldoc->saveXML());
+			};?>">
 			<fieldset>
 				<?php echo $myMImeta->getHTML(); ?>
 			</fieldset>
