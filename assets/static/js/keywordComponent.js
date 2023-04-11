e1b3094ae5c7a906b778a16d8e5dba09352780c6
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2023-04-11 15:16:04 -0500

    GCMD importer WIP

--- assets/static/js/keywordComponent.js
+++ assets/static/js/keywordComponent.js
@@ -1,61 +1,25 @@
 $(() => {
-
-    stuff =  $.ajax({
-        url: '/api/standard/keyword',
-        dataType: 'json',
-      }).then((result) => {
-        console.log(result);
-        $('#treelist').dxTreeList({
-            dataSource: result,
-            rootValue: -1,
-            keyExpr: 'notation',
-            parentIdExpr: 'parentId',
-            columns: [{
-                dataField: 'label',
-                caption: 'Keyword',
-              }],
-            expandedRowKeys: [1],
-            showRowLines: true,
-            showBorders: true,
-            columnAutoWidth: true,
-            sorting: {
-                mode: "none",
-            },
-            searchPanel: { visible: true },
-            selection: {
-                mode: 'multiple',
-                recursive: false,
-              },
-          });
-      });
-
-    // $('#treelist').dxTreeList({
-    //   dataSource: {
-    //     load(options) {
-    //       return $.ajax({
-    //         url: 'https://localhost:8082/api/standard/keyword',
-    //         dataType: 'json',
-    //         data: { parentIds: options.parentIds },
-    //       }).then((result) => ({
-    //         data: result,
-    //       }));
-    //     },
-    //   },
-    //   scrolling: {
-    //     mode: 'standard',
-    //   },
-    //   showColumnHeaders: false,
-    //   allowSearch: true,
-    //   remoteOperations: {
-    //     filtering: true,
-    //   },
-    //   keyExpr: 'notation',
-    //   parentIdExpr: 'parentId',
-    //   hasItemsExpr: 'hasItems',
-    //   rootValue: '',
-    //   showBorders: true,
-    //   columns: [
-    //     { dataField: 'label' },
-    //   ],
-    // });
+  $.ajax({
+    url: '/api/standard/keyword',
+    dataType: 'json',
+  }).then((result) => {
+    $('#treelist').dxTreeList({
+      dataSource: result,
+      rootValue: -1,
+      keyExpr: 'key',
+      parentIdExpr: 'parent',
+      columns: [{
+          dataField: 'label',
+          caption: 'Keywords',
+        }],
+      expandedRowKeys: [1],
+      showRowLines: true,
+      showBorders: true,
+      columnAutoWidth: true,
+      sorting: {
+          mode: "none",
+      },
+      searchPanel: { visible: true },
+    });
   });
+});
--- src/Enum/KeywordType.php
+++ src/Enum/KeywordType.php
@@ -9,4 +9,27 @@ enum KeywordType: string
 {
     case TYPE_ANZSRC = 'anzsrc';
     case TYPE_GCMD = 'gcmd';
+    case TYPE_GCMD_SCIENCE = 'gcmd:science';
+    case TYPE_GCMD_DISCIPLINE = 'gcmd:discipline';
+
+    static public function fromString(?string $keyword): ?KeywordType
+    {
+        switch ($keyword)
+        {
+            case 'anzsrc':
+                return KeywordType::TYPE_ANZSRC;
+                break;
+            case 'gcmd':
+                return KeywordType::TYPE_GCMD;
+                break;
+            case 'gcmd:science':
+                return KeywordType::TYPE_GCMD_SCIENCE;
+                break;
+            case 'gcmd:discipline':
+                return KeywordType::TYPE_GCMD_DISCIPLINE;
+                break;
+            default:
+                return null;
+        }
+    }
 }
