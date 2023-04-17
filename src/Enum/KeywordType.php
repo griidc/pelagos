c60d8e570bcb7b30c3ac85264225475eff658027
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2023-04-17 08:17:15 -0500

    Fixed enum

--- src/Enum/KeywordType.php
+++ src/Enum/KeywordType.php
@@ -2,21 +2,11 @@
 
 namespace App\Enum;
 
-use BackedEnum;
-
 /**
  * Types for Keywords.
  */
-enum KeywordType: string implements BackedEnum
+enum KeywordType: string
 {
     case TYPE_ANZSRC = 'anzsrc';
     case TYPE_GCMD = 'gcmd';
-
-    static public function tryFrom(int|string $keyword): ?static
-    {
-        return match ($keyword) {
-            KeywordType::TYPE_ANZSRC->value => KeywordType::TYPE_ANZSRC,
-            KeywordType::TYPE_GCMD->value => KeywordType::TYPE_GCMD,
-        };
-    }
 }
