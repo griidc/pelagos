77c5ab78da25a79d4d779165c8fc15f99fedeb70
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2023-04-19 11:31:23 -0500

    Think mostly done.

--- tests/unit/Util/StreamInfoTest.php
+++ tests/unit/Util/StreamInfoTest.php
@@ -3,10 +3,9 @@
 namespace App\Tests\Util;
 
 use PHPUnit\Framework\TestCase;
-use org\bovigo\vfs\vfsStream,
-    org\bovigo\vfs\vfsStreamDirectory;
-
-use App\Util\StreamInfo;
+use org\bovigo\vfs\vfsStream;
+use GuzzleHttp\Psr7\Utils;
+use Psr\Http\Message\StreamInterface;
 
 /**
  * Unit tests for App\Util\StreamInfo.php
@@ -16,10 +15,9 @@ class StreamInfoTest extends TestCase
     /**
      * The file stream.
      *
-     * @var Stream
+     * @var StreamInterface
      */
     private $fileStream;
-
 
     /**
      * Unit test setup.
@@ -28,12 +26,10 @@ class StreamInfoTest extends TestCase
      */
     public function setUp()
     {
-        $this->root = vfsStream::setup();
-
         $file = vfsStream::newFile('test.txt')
                 ->withContent('test')
                 ->at(vfsStream::setup());
-        $this->fileStream = fopen($file->url(), 'r');
+        $this->fileStream = Utils::streamFor(fopen($file->url(), 'r'));
     }
 
     /**
@@ -41,7 +37,7 @@ class StreamInfoTest extends TestCase
      */
     public function testFileSize()
     {
-        $size = StreamInfo::getFileSize(array('fileStream' => $this->fileStream));
+        $size = $this->fileStream->getSize();
         $this->assertEquals($size, 4);
     }
 
@@ -50,7 +46,7 @@ class StreamInfoTest extends TestCase
      */
     public function testCalculateHashSha256()
     {
-        $hash = StreamInfo::calculateHash(array('fileStream' => $this->fileStream));
+        $hash = utils::hash($this->fileStream, 'sha256');
         $this->assertEquals($hash, '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08');
     }
     
@@ -59,7 +55,7 @@ class StreamInfoTest extends TestCase
      */
     public function testCalculateHashMd5()
     {
-        $hash = StreamInfo::calculateHash(array('fileStream' => $this->fileStream), 'md5');
+        $hash = utils::hash($this->fileStream, 'md5');
         $this->assertEquals($hash, '098f6bcd4621d373cade4e832627b4f6');
     }
 }
