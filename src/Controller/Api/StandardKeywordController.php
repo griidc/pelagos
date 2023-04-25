c65dbf80f23b1cd8cfb87980af32b5960bbf7f3b
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2023-04-25 14:01:50 -0500

    New API serializer

--- src/Controller/Api/StandardKeywordController.php
+++ src/Controller/Api/StandardKeywordController.php
@@ -4,8 +4,8 @@ namespace App\Controller\Api;
 
 use App\Enum\KeywordType;
 use App\Repository\KeywordRepository;
+use App\Util\JsonSerializer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,36 +13,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class StandardKeywordController extends AbstractController
 {
     #[Route('/api/standard/keyword', name: 'app_api_standard_keyword')]
-    public function index(Request $request, KeywordRepository $keywordRepository): Response
+    public function index(Request $request, KeywordRepository $keywordRepository, JsonSerializer $jsonSerializer): Response
     {
-        $type = $request->query->get('type') ?? [KeywordType::TYPE_GCMD, KeywordType::TYPE_ANZSRC];
+        $type = $request->query->get('type') ?? KeywordType::cases();
         $keywords = $keywordRepository->findBy(
-            ['type' => $type]
+            criteria: ['type' => $type],
+            orderBy: ['label' => 'ASC']
         );
 
-        $data = [];
-
-        foreach ($keywords as $keyword) {
-            $uri = $keyword->getReferenceUri();
-            $label = $keyword->getLabel();
-            $parentId = $keyword->getParentUri();
-            $definition = $keyword->getDefinition();
-            $displayPath = $keyword->getDisplayPath();
-
-            $data[] = [
-                'key' => $uri,
-                'label' => $label,
-                'hasItems' => !(empty($parentId)),
-                'parent' => $parentId,
-                'definition' => $definition,
-                'displayPath' => $displayPath,
-            ];
-        }
-
-        usort($data, function ($a, $b) {
-            return strcmp($a['label'], $b['label']);
-        });
-
-        return new JsonResponse($data);
+        return $jsonSerializer->serialize($keywords, ['api'])->createJsonResponse();
     }
 }
