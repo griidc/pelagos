4d5ba31b09e02a84c4024b1e09c244dd527e79be
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2023-04-26 12:20:03 -0500

    added selection list

--- assets/static/js/keywordComponent.js
+++ assets/static/js/keywordComponent.js
@@ -1,25 +1,55 @@
 $(() => {
+  var selectedKeywords = [];
+
   $.ajax({
     url: '/api/standard/keyword?type=gcmd',
     dataType: 'json',
   }).then((result) => {
-    $('#treelist').dxTreeList({
+    const treeList = $('#treelist').dxTreeList({
       dataSource: result,
       rootValue: -1,
-      keyExpr: 'key',
-      parentIdExpr: 'parent',
+      keyExpr: 'referenceUri',
+      parentIdExpr: 'parentUri',
       columns: [{
-          dataField: 'label',
-          caption: 'Keywords',
-        }],
+        dataField: 'label',
+        caption: 'Keywords',
+      }],
       expandedRowKeys: [1],
       showRowLines: true,
       showBorders: true,
       columnAutoWidth: true,
-      sorting: {
-          mode: "none",
+      showColumnHeaders: false,
+      selection: {
+        mode: 'single',
       },
+      // onSelectionChanged() {
+      //   const selectedData = treeList.getSelectedRowsData();
+      //   console.log(selectedData);
+      // },
       searchPanel: { visible: true },
+    }).dxTreeList('instance');
+
+    const listWidget = $('#simpleList').dxList({
+      dataSource: selectedKeywords,
+      allowItemDeleting: true,
+      itemDeleteMode: 'static',
+    }).dxList('instance');
+
+    $('#add-button').dxButton({
+      text: 'Add',
+      icon: 'add',
+      width: 120,
+      onClick() {
+        const selectedRow = treeList.getSelectedRowsData();
+        const displayPath = selectedRow.map((keyword) => keyword.displayPath).toString();
+
+        if (!selectedKeywords.includes(displayPath)) {
+          selectedKeywords.push(displayPath);
+        }
+
+        listWidget.reload();
+        listWidget.repaint();
+      },
     });
   });
 });
