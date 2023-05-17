278dfc265391516bc9320fdab0bc559d43b73287
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2023-05-17 09:45:58 -0500

    Have an empty collection for keywords

--- src/Entity/DIF.php
+++ src/Entity/DIF.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation as Serializer;
@@ -395,6 +396,7 @@ class DIF extends Entity
         if (null !== $dataset) {
             $this->setDataset($dataset);
         }
+        $this->keywords = new ArrayCollection();
     }
 
     /**
