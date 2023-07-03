635e6f272252c52039273c70d13dd72635d5b9ca
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2023-07-03 08:46:09 -0500

    Initial new keywords on DS

--- assets/static/js/dataset-submission.js
+++ assets/static/js/dataset-submission.js
@@ -36,6 +36,23 @@ $(function() {
     $("html").show();
 
     $("#funderList").trigger("fundersAdded", {"disabled": defaultFunderTagBoxDisabled});
+    $("#keywordList").trigger("keywordsAdded", {"disabled": false});
+
+    $("#keywordList").on("change", function(event){
+        $('[id^="keywords_"]').remove();
+        var maxKeywordId = 0;
+        $.each(($("#keywordList").val().split(',')), function(key, value) {
+            if (value === "") { return; }
+            var newElement = document.createElement("input");
+            var keywordId = value;
+            newElement.id = `keywords_${maxKeywordId}`;
+            newElement.name = `keywords[${maxKeywordId}]`;
+            newElement.value = keywordId;
+            newElement.type = "hidden";
+            $('[id="keyword-items"]').append(newElement);
+            maxKeywordId++;
+        })
+    });
 
     $("label").next("input[required],textarea[required],select[required]").prev().addClass("emRequired");
     //Setup qTip
