261911127187baf1279c863feca6e39109c69210
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2023-09-11 09:27:06 -0500

    Using null value instead of setting in listener

--- src/Event/ElasticIndexSubscriber.php
+++ src/Event/ElasticIndexSubscriber.php
@@ -142,7 +142,6 @@ class ElasticIndexSubscriber implements EventSubscriberInterface
         } else {
             $document->set('updatedDateTime', $dataset->getModificationTimeStamp()->format('Ymd\THis\Z'));
         }
-        $document->set('published', true);
     }
 
     /**
