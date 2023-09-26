99a9ae8f888087ce9a0965ce3feffbd96e930a53
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2023-09-26 09:16:17 -0500

    WIP edit page

--- src/Controller/UI/KeywordDatasetController.php
+++ src/Controller/UI/KeywordDatasetController.php
@@ -2,23 +2,26 @@
 
 namespace App\Controller\UI;
 
+use App\Entity\Dataset;
+use App\Entity\DatasetSubmission;
 use App\Repository\DatasetRepository;
 use App\Util\JsonSerializer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\DatasetSubmissionType;
+use Symfony\Component\Form\FormFactoryInterface;
 
 class KeywordDatasetController extends AbstractController
 {
-    #[Route('/keywordToDataset', name: 'pelagos_app_ui_keyword_dataset')]
-    public function index(): Response
+    #[Route('/keyword-dataset', name: 'pelagos_app_ui_list_keyword_dataset')]
+    public function list(): Response
     {
-        return $this->render('KeywordDataset/list.html.twig', [
-        ]);
+        return $this->render('KeywordDataset/list.html.twig');
     }
 
-    #[Route('/api/KeywordDataset', name: 'pelagos_app_api_keyword_dataset')]
+    #[Route('/api/keyword_dataset', name: 'pelagos_app_api_keyword_dataset')]
     public function getList(DatasetRepository $datasetRepository, JsonSerializer $jsonSerializer): Response
     {
         $datasets = $datasetRepository->getListOfApprovedDatasetWithoutKeywords();
@@ -26,5 +29,28 @@ class KeywordDatasetController extends AbstractController
         return $jsonSerializer->serialize(
             data: $datasets,
         )->createJsonResponse();
+    }
+
+    #[Route('/keyword-dataset/{udi}', name: 'pelagos_app_ui_edit_keyword_dataset')]
+    public function editDataset(string $udi, DatasetRepository $datasetRepository, FormFactoryInterface $formFactory): Response
+    {
+        $dataset = $datasetRepository->findOneBy(['udi' => $udi]);
+
+        if (!$dataset instanceof Dataset) {
+            throw new NotFoundHttpException("Dataset with $udi not found!");
+        }
+
+        $datasetSubmission = $dataset->getDatasetSubmission();
+
+        if (!$datasetSubmission instanceof DatasetSubmission) {
+            throw new NotFoundHttpException('This Dataset does not have an Submission!');
+        }
+
+        $form = $formFactory->create(DatasetSubmissionType::class, $datasetSubmission);
+
+        return $this->render('KeywordDataset/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+
     }
 }
