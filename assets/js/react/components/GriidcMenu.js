38c19d102685644f475720755c3ec17a1a19f4a5
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2024-01-24 09:34:20 -0600

    fixed menu order

--- assets/js/react/components/GriidcMenu.js
+++ assets/js/react/components/GriidcMenu.js
@@ -25,62 +25,7 @@ const GriidcMenu = ({ mainsite }) => (
           About Us
         </a>
       </Popover>
-      <Popover className="relative">
-        {({ open }) => (
-          <Fragment>
-            <Popover.Button
-              className={clsx(
-                'flex items-center text-base font-medium ml-8 hover:text-blue-600',
-                open ? 'text-blue-600' : 'text-gray-600',
-              )}
-              onClick={() => avoidFocusOnClick()}
-            >
-              Help
-              <ChevronDownIcon
-                className={clsx(
-                  'h-5 w-5 flex-none text-blue-600',
-                  open && 'rotate-180',
-                )}
-                aria-hidden="true"
-              />
-            </Popover.Button>
 
-            <Transition
-              as={React.Fragment}
-              enter="transition ease-out duration-200"
-              enterFrom="opacity-0 translate-y-1"
-              enterTo="opacity-100 translate-y-0"
-              leave="transition ease-in duration-150"
-              leaveFrom="opacity-100 translate-y-0"
-              leaveTo="opacity-0 translate-y-1"
-            >
-              <Popover.Panel
-                static
-                className="absolute left-8 top-full z-10 mt-3 w-56 rounded bg-white shadow-md ring-1 ring-gray-100"
-              >
-               <a className={clsx(
-                 'm-1.5 block hover:text-blue-600 p-2 text-sm leading-[1.25rem] text-gray-500',
-               )}
-               href={`${mainsite}/faq`}>
-                Frequently Asked Questions
-              </a>
-              <a className={clsx(
-                'm-1.5 block hover:text-blue-600 p-2 text-sm leading-[1.25rem] text-gray-500',
-              )}
-               href={`${mainsite}/training-and-user-guides`}>
-                Training and User Guides
-              </a>
-              <a className={clsx(
-                'm-1.5 block hover:text-blue-600 p-2 text-sm leading-[1.25rem] text-gray-500',
-              )}
-               href={`${mainsite}/webinar-recordings`}>
-                Webinar Recordings
-              </a>
-              </Popover.Panel>
-            </Transition>
-          </Fragment>
-        )}
-      </Popover>
       <Popover className="relative">
         {({ open }) => (
           <Fragment>
@@ -100,7 +45,6 @@ const GriidcMenu = ({ mainsite }) => (
                 aria-hidden="true"
               />
             </Popover.Button>
-
             <Transition
               as={React.Fragment}
               enter="transition ease-out duration-200"
@@ -169,6 +113,62 @@ const GriidcMenu = ({ mainsite }) => (
                 href="/dataset-monitoring">
                   Monitor Data
                 </a>
+              </Popover.Panel>
+            </Transition>
+          </Fragment>
+        )}
+      </Popover>
+      <Popover className="relative">
+        {({ open }) => (
+          <Fragment>
+            <Popover.Button
+              className={clsx(
+                'flex items-center text-base font-medium ml-8 hover:text-blue-600',
+                open ? 'text-blue-600' : 'text-gray-600',
+              )}
+              onClick={() => avoidFocusOnClick()}
+            >
+              Help
+              <ChevronDownIcon
+                className={clsx(
+                  'h-5 w-5 flex-none text-blue-600',
+                  open && 'rotate-180',
+                )}
+                aria-hidden="true"
+              />
+            </Popover.Button>
+
+            <Transition
+              as={React.Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel
+                static
+                className="absolute left-8 top-full z-10 mt-3 w-56 rounded bg-white shadow-md ring-1 ring-gray-100"
+              >
+               <a className={clsx(
+                 'm-1.5 block hover:text-blue-600 p-2 text-sm leading-[1.25rem] text-gray-500',
+               )}
+               href={`${mainsite}/faq`}>
+                Frequently Asked Questions
+              </a>
+              <a className={clsx(
+                'm-1.5 block hover:text-blue-600 p-2 text-sm leading-[1.25rem] text-gray-500',
+              )}
+               href={`${mainsite}/training-and-user-guides`}>
+                Training and User Guides
+              </a>
+              <a className={clsx(
+                'm-1.5 block hover:text-blue-600 p-2 text-sm leading-[1.25rem] text-gray-500',
+              )}
+               href={`${mainsite}/webinar-recordings`}>
+                Webinar Recordings
+              </a>
               </Popover.Panel>
             </Transition>
           </Fragment>
