9619a4f8f4ad5e60aa00ffe9b26d8ba60a687fb1
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2024-02-13 08:41:05 -0600

    More last minute updates
    Updated header menu
    updated footer (harte link)

--- assets/js/react/components/GriidcMenu.js
+++ assets/js/react/components/GriidcMenu.js
@@ -17,15 +17,68 @@ const GriidcMenu = (props) => (
       <Popover className="relative">
         <a className="text-base font-medium text-gray-600 ml-8 hover:text-blue-600" href="/">Home</a>
       </Popover>
-      <Popover className="relative">
-        <a className={clsx(
-          'text-base font-medium text-gray-600 ml-8 hover:text-blue-600 font-bold text-blue-200',
-        )}
-        href={`${props.mainsite}/about`}>
-          About Us
-        </a>
-      </Popover>
 
+      <Popover className="relative">
+        {({ open }) => (
+          <Fragment>
+            <Popover.Button
+              className={clsx(
+                'flex items-center text-base font-medium ml-8 hover:text-blue-600',
+                open ? 'text-blue-600' : 'text-gray-600',
+              )}
+              onClick={() => avoidFocusOnClick()}
+            >
+              About Us
+              <ChevronDownIcon
+                className={clsx(
+                  'h-5 w-5 flex-none text-blue-600',
+                  open && 'rotate-180',
+                )}
+                aria-hidden="true"
+              />
+            </Popover.Button>
+            <Transition
+              as={React.Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel
+                static
+                className="absolute left-8 top-full z-10 mt-3 w-56 rounded bg-white shadow-md ring-1 ring-gray-100"
+              >
+              <a className={clsx(
+                'm-1.5 block hover:text-blue-600 p-2 text-sm leading-[1.25rem] text-gray-500',
+              )}
+                href={`${props.mainsite}/about`}>
+                About Us
+              </a>
+              <a className={clsx(
+                'm-1.5 block hover:text-blue-600 p-2 text-sm leading-[1.25rem] text-gray-500',
+              )}
+                href={`${props.mainsite}/program-overview`}>
+                Program Overview
+              </a>
+              <a className={clsx(
+                'm-1.5 block hover:text-blue-600 p-2 text-sm leading-[1.25rem] text-gray-500',
+              )}
+               href={`${props.mainsite}/team`}>
+                Our Team
+              </a>
+              <a className={clsx(
+                'm-1.5 block hover:text-blue-600 p-2 text-sm leading-[1.25rem] text-gray-500',
+              )}
+               href={`${props.mainsite}/griidc-services`}>
+                Services
+              </a>
+              </Popover.Panel>
+            </Transition>
+          </Fragment>
+        )}
+      </Popover>
       <Popover className="relative">
         {({ open }) => (
           <Fragment>
