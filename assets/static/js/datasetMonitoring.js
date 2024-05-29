6a56859ac660bdfeef377b90904f4a44e16fbed0
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2024-05-28 20:18:00 -0500

    linting

--- assets/static/js/datasetMonitoring.js
+++ assets/static/js/datasetMonitoring.js
@@ -22,12 +22,11 @@ $(() => {
 
   var groupStore;
 
-  const loadGroups = () =>
-  {
+  const loadGroups = () => {
     $.ajax({
       url: Routing.generate('app_api_dataset_monitoring_groups'),
       method: 'GET',
-      dataType : 'json',
+      dataType: 'json',
     }).then(function (response) {
       groupStore = new DevExpress.data.DataSource({
         paginate: false,
@@ -42,13 +41,12 @@ $(() => {
       });
       dsmTreeList.option("dataSource", groups);
       dsmTreeList.repaint();
-    }).always(function() {
+    }).always(function () {
       dsmTreeList.option("disabled", false);
     });
   }
 
-  const filterGroups = (datasetFilter) =>
-  {
+  const filterGroups = (datasetFilter) => {
     switch (datasetFilter) {
       case 'only':
         dsmTreeList.filter(["datasets", ">", 0]);
@@ -66,8 +64,7 @@ $(() => {
     }
   }
 
-  const collapseAll = () =>
-  {
+  const collapseAll = () => {
     dsmTreeList.beginUpdate();
     dsmTreeList.forEachNode((node) => {
       dsmTreeList.collapseRow(node.key);
@@ -78,35 +75,35 @@ $(() => {
   const dsmToolbar = $('#dsm-toolbar').dxToolbar({
     items:
       [
-      {
-        location: 'before',
-        widget: 'dxSelectBox',
-        locateInMenu: 'never',
-        options: {
-          width: 'auto',
-          items: datasetFilters,
-          valueExpr: 'id',
-          displayExpr: 'text',
-          value: datasetFilters[0].id,
-          onValueChanged(e) {
-            datasetFilter = e.value;
-            filterGroups(datasetFilter);
+        {
+          location: 'before',
+          widget: 'dxSelectBox',
+          locateInMenu: 'never',
+          options: {
+            width: 'auto',
+            items: datasetFilters,
+            valueExpr: 'id',
+            displayExpr: 'text',
+            value: datasetFilters[0].id,
+            onValueChanged(e) {
+              datasetFilter = e.value;
+              filterGroups(datasetFilter);
+            },
           },
         },
-      },
-      {
-        location: 'after',
-        widget: 'dxButton',
-        locateInMenu: 'never',
-        options: {
-          hint: 'Collape All',
-          icon: 'collapse',
-          onClick() {
-            collapseAll();
+        {
+          location: 'after',
+          widget: 'dxButton',
+          locateInMenu: 'never',
+          options: {
+            hint: 'Collape All',
+            icon: 'collapse',
+            onClick() {
+              collapseAll();
+            },
           },
         },
-      },
-    ]
+      ]
   }).dxToolbar('instance');
 
   const dsmLoadPanel = $('.dsm-loadpanel').dxLoadPanel({
@@ -141,7 +138,7 @@ $(() => {
       visible: false,
       allowSearch: false,
     }
-  ],
+    ],
     disabled: true,
     showRowLines: false,
     showBorders: false,
@@ -166,28 +163,28 @@ $(() => {
   const loadGroupHtml = (selectedItem) => {
     dsmLoadPanel.toggle(true);
     dsmTreeList.option("disabled", true);
-    var parameters = {"datasetFilter": datasetFilter};
-      if (selectedItem.fundingOrganization !== undefined) {
-        parameters = Object.assign(parameters,{fundingOrganization : selectedItem.fundingOrganization});
-      }
-      if (selectedItem.fundingCycle !== undefined) {
-        parameters = Object.assign(parameters,{fundingCycle : selectedItem.fundingCycle});
-      }
-      if (selectedItem.researchGroup !== undefined) {
-        parameters = Object.assign(parameters,{researchGroup : selectedItem.researchGroup});
-      }
+    var parameters = { "datasetFilter": datasetFilter };
+    if (selectedItem.fundingOrganization !== undefined) {
+      parameters = Object.assign(parameters, { fundingOrganization: selectedItem.fundingOrganization });
+    }
+    if (selectedItem.fundingCycle !== undefined) {
+      parameters = Object.assign(parameters, { fundingCycle: selectedItem.fundingCycle });
+    }
+    if (selectedItem.researchGroup !== undefined) {
+      parameters = Object.assign(parameters, { researchGroup: selectedItem.researchGroup });
+    }
 
-      $.ajax({
-        url: Routing.generate("app_api_dataset_monitoring_datasets",
-          parameters
-        ),
-        method: "GET",
-        accept: "text/html",
-      }).then(function(html) {
-        $("#dsm-datasets").html(html);
-      }).done(function(){
-        dsmLoadPanel.toggle(false);
-        dsmTreeList.option("disabled", false);
-      });
+    $.ajax({
+      url: Routing.generate("app_api_dataset_monitoring_datasets",
+        parameters
+      ),
+      method: "GET",
+      accept: "text/html",
+    }).then(function (html) {
+      $("#dsm-datasets").html(html);
+    }).done(function () {
+      dsmLoadPanel.toggle(false);
+      dsmTreeList.option("disabled", false);
+    });
   }
 });
