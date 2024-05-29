bda783c7fae1dd44b1f11a20a69a576fd389890f
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2024-05-28 20:15:54 -0500

    Change from treeView to TreeList

--- assets/static/js/datasetMonitoring.js
+++ assets/static/js/datasetMonitoring.js
@@ -37,9 +37,13 @@ $(() => {
           data: response
         },
       });
-      filterGroups(datasetFilter);
+      groupStore.load().done(function (data) {
+        groups = data;
+      });
+      dsmTreeList.option("dataSource", groups);
+      dsmTreeList.repaint();
     }).always(function() {
-      dsmTreeview.option("disabled", false);
+      dsmTreeList.option("disabled", false);
     });
   }
 
@@ -47,31 +51,28 @@ $(() => {
   {
     switch (datasetFilter) {
       case 'only':
-        groupStore.filter(["datasets", ">", 0]);
+        dsmTreeList.filter(["datasets", ">", 0]);
         break;
       case 'without':
-        groupStore.filter([
-          [ "fundingOrganization", ">", 0 ],
-          "or",
-          [ "fundingCycle", ">", 0 ],
-          "or",
-          [ ["researchGroup", ">", 0] , "and",  ["datasets", "=", 0] ],
-      ]
-    );
+        dsmTreeList.filter(["datasets", "=", 0]);
         break;
       default:
-        groupStore.filter(null);
+        dsmTreeList.filter(null);
         break;
     }
 
-    groupStore.load().done(function (data) {
-      groups = data;
-    });
-    dsmTreeview.option("dataSource", groups);
-    dsmTreeview.repaint();
     if (typeof selectedItem !== 'undefined') {
       loadGroupHtml(selectedItem);
     }
+  }
+
+  const collapseAll = () =>
+  {
+    dsmTreeList.beginUpdate();
+    dsmTreeList.forEachNode((node) => {
+      dsmTreeList.collapseRow(node.key);
+    })
+    dsmTreeList.endUpdate();
   }
 
   const dsmToolbar = $('#dsm-toolbar').dxToolbar({
@@ -101,9 +102,7 @@ $(() => {
           hint: 'Collape All',
           icon: 'collapse',
           onClick() {
-            dsmTreeview.unselectAll();
-            dsmTreeview.collapseAll();
-            dsmTreeview.repaint();
+            collapseAll();
           },
         },
       },
@@ -118,57 +117,63 @@ $(() => {
     hideOnOutsideClick: false,
   }).dxLoadPanel('instance');
 
-  const dsmTreeview = $('#simple-treeview').dxTreeView({
-    dataStructure: 'plain',
-    items: groups,
-    parentIdExpr: 'parent',
+  const dsmTreeList = $('#dsm-treelist').dxTreeList({
+    dataSource: groups,
     keyExpr: 'id',
-    displayExpr: 'name',
-    searchEnabled: true,
-    searchExpr: ["name"],
+    parentIdExpr: 'parent',
+    filterRow: {
+      visible: true
+    },
+    headerFilter: {
+      visible: false,
+    },
+    searchPanel: {
+      visible: false,
+    },
+    columns: [{
+      dataField: 'name',
+      caption: 'Name',
+      dataType: "string",
+    },
+    {
+      dataType: "number",
+      dataField: 'datasets',
+      visible: false,
+      allowSearch: false,
+    }
+  ],
     disabled: true,
-    SearchEditorOptions: {
-      placeholder: "Search",
-      showClearButton: true,
-      buttons: [
-        "clear",
-        {
-          name: 'collapse',
-          location: 'after',
-          options: {
-            icon: 'revert',
-            stylingMode: 'contained',
-            hint: 'Collape All',
-            onClick() {
-              dsmTreeview.option("searchValue", "");
-              dsmTreeview.unselectAll();
-              dsmTreeview.collapseAll();
-              dsmTreeview.repaint();
-            },
-          },
-        },
-      ],
+    showRowLines: false,
+    showBorders: false,
+    showColumnHeaders: false,
+    columnAutoWidth: false,
+    wordWrapEnabled: true,
+    selection: {
+      mode: 'single',
     },
     onInitialized() {
       loadGroups();
     },
-    onItemClick(item) {
-      selectedItem = item.itemData;
+    onEditorPrepared(event) {
+      event.editorOptions.showClearButton = true;
+    },
+    onSelectionChanged(event) {
+      selectedItem = event.selectedRowsData[0];
       loadGroupHtml(selectedItem);
     },
-  }).dxTreeView('instance');
+  }).dxTreeList('instance');
 
   const loadGroupHtml = (selectedItem) => {
     dsmLoadPanel.toggle(true);
-    dsmTreeview.option("disabled", true);
+    dsmTreeList.option("disabled", true);
     var parameters = {"datasetFilter": datasetFilter};
-      if (selectedItem["fundingOrganization"] !== undefined) {
+      if (selectedItem.fundingOrganization !== undefined) {
         parameters = Object.assign(parameters,{fundingOrganization : selectedItem.fundingOrganization});
       }
-      if (selectedItem["fundingCycle"] !== undefined) {
+      if (selectedItem.fundingCycle !== undefined) {
         parameters = Object.assign(parameters,{fundingCycle : selectedItem.fundingCycle});
       }
-      if (selectedItem["researchGroup"] !== undefined) {
+      if (selectedItem.researchGroup !== undefined) {
         parameters = Object.assign(parameters,{researchGroup : selectedItem.researchGroup});
       }
 
@@ -179,10 +184,10 @@ $(() => {
         method: "GET",
         accept: "text/html",
       }).then(function(html) {
-        $("#datasets-here").html(html);
+        $("#dsm-datasets").html(html);
       }).done(function(){
         dsmLoadPanel.toggle(false);
-        dsmTreeview.option("disabled", false);
+        dsmTreeList.option("disabled", false);
       });
   }
 });
