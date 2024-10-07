51a848d1ae4d5e48b58b0de9c4689616a57987a9
Michael Scott Williamson <michael.williamson@tamucc.edu>
2024-10-07 10:39:31 -0500

    Got annotation converter working.

--- share/python/custom-validator-to-annotation.py
+++ share/python/custom-validator-to-annotation.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
 import re
+import sys
 
-infile = open("src/Entity/DataCenter.php", "r")
+filename = sys.argv[1]
+
+try:
+    infile = open(filename, "r")
+except OSError:
+    print("Could not open/read file: ", filename)
+    sys.exit()
+
 msg = ''
+removed = False
 
 for line in infile:
     if '@CustomAssert\\NoAngleBrackets' in line:
+        removed = True;
         msgline = next(infile)
         msg = re.sub('^.*message=\"', '', msgline)
         msg = re.sub('\"\n', '', msg)
         next(infile)
     elif '*/' not in line:
         print(line, end="")
+
     if '*/' in line:
         print(line, end="")
         if msg:
             print('    #[Assert\Regex(\'/<>/\', \''+msg+'\')]')
+            msg = ''
