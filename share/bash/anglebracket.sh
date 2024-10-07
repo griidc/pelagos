c3b0b2bd4f2ff8236cf7a440ba25802fd351327e
Michael Scott Williamson <michael.williamson@tamucc.edu>
2024-10-07 11:49:02 -0500

    Remove bash wrapper.

--- share/bash/anglebracket.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for file in $(grep -ir 'CustomAssert\\NoAngleBrackets' src/ | sed "s/:.*$//" | sort -u)
-do
-    share/python/custom-validator-to-annotation.py $file > $file.new;
-    mv $file.new $file;
-done
