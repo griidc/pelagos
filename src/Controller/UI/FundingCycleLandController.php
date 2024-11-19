7b0035f2923c3a2b1c11a1e58785339746a98e2e
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2024-11-19 09:55:35 -0600

    Added some tabs templates

--- src/Controller/UI/FundingCycleLandController.php
+++ src/Controller/UI/FundingCycleLandController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller\UI;
 
+use App\Entity\FundingCycle;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class FundingCycleLandController extends AbstractController
 {
-    #[Route('/fcland', name: 'app_funding_cycle_land')]
-    public function index(): Response
+    #[Route('/funding-cycle/about/{fundingCycle}', name: 'app_funding_cycle_land')]
+    public function index(FundingCycle $fundingCycle): Response
     {
-        return $this->render('FundingCycleLand/index.html.twig');
+        return $this->render('FundingCycleLand/index.html.twig',
+            [
+                'fundingCycle' => $fundingCycle,
+            ]
+        );
     }
 }
