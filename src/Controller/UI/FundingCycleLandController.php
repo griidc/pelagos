8f5d24810b5ea1262bff2a3c80a2ba5e15d58fd6
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2024-11-20 16:43:49 -0600

    More updates

--- src/Controller/UI/FundingCycleLandController.php
+++ src/Controller/UI/FundingCycleLandController.php
@@ -7,40 +7,24 @@ use App\Repository\InformationProductRepository;
 use App\Repository\DatasetRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FundingCycleLandController extends AbstractController
 {
-    private $informationProductRepository;
-    private $datasetRepository;
-
-    public function __construct(InformationProductRepository $informationProductRepository, DatasetRepository $datasetRepository)
-    {
-        $this->informationProductRepository = $informationProductRepository;
-        $this->datasetRepository = $datasetRepository;
-    }
-
     #[Route('/funding-cycle/about/{fundingCycle}', name: 'app_funding_cycle_land')]
-    public function index(FundingCycle $fundingCycle, string $tab = null): Response
+    public function index(FundingCycle $fundingCycle, InformationProductRepository $informationProductRepository, DatasetRepository $datasetRepository, #[MapQueryParameter] ?int $tab = null): Response
     {
-        $tab = $this->container->get('request_stack')->getCurrentRequest()->query->get('tab', $tab);
+        $informationProducts = $informationProductRepository->findByFundingCycle($fundingCycle);
 
-        $researchGroupList = [];
-        foreach ($fundingCycle->getResearchGroups() as $researchGroup) {
-            /* @var $researchGroup \App\Entity\ResearchGroup */
-            $researchGroupList[] = $researchGroup->getId();
-        }
-
-        $informationProducts = $this->informationProductRepository->findByResearchGroupIds($researchGroupList);
-        $datasets = $this->datasetRepository->getDatasetsByFundingCycle($fundingCycle->getId());
+        // $datasets = $datasetRepository->getDatasetsByFundingCycle($fundingCycle);
 
         return $this->render(
             'FundingCycleLand/index.html.twig',
             [
                 'fundingCycle' => $fundingCycle,
-                'researchGroupList' => $researchGroupList,
                 'informationProducts' => $informationProducts,
-                'datasets' => $datasets,
+                // 'datasets' => $datasets,
                 'tab' => $tab,
             ]
         );
