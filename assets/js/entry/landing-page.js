2319c14e601535277b4981caf1a5c7aa3d93adb4
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2024-12-11 11:08:49 -0600

    Made dataset card selectable, some rewrites

--- assets/js/entry/landing-page.js
+++ assets/js/entry/landing-page.js
@@ -1,6 +1,7 @@
-import '../../scss/funding-cycle-land.scss';
+import '../../scss/landing-pages.scss';
 import '../../css/activeInactive.css';
 import Alpine from 'alpinejs';
+import '../components/cardClick';
 
 window.Alpine = Alpine;
 Alpine.start();
