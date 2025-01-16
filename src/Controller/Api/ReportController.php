192d4c9d95b9c98d2e62793fc6dbea676b0d29e0
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2025-01-16 13:31:43 -0600

    Added to admin list

--- src/Controller/Api/ReportController.php
+++ src/Controller/Api/ReportController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Api;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 use App\Entity\FundingOrganization;
 use App\Entity\ResearchGroup;
 use App\Repository\FundingOrganizationRepository;
@@ -14,14 +15,22 @@ use App\Repository\FundingOrganizationRepository;
 use App\Repository\ResearchGroupRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 >>>>>>> b88d625f4 (Working CSV)
+=======
+use App\Entity\FundingOrganization;
+use App\Entity\ResearchGroup;
+use App\Repository\FundingOrganizationRepository;
+use App\Repository\ResearchGroupRepository;
+>>>>>>> 146c8b57e (Added to admin list)
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\HeaderUtils;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+
 class ReportController extends AbstractController
 {
+<<<<<<< HEAD
 <<<<<<< HEAD
     #[Route(path: '/api/grp-datasets-people-report', name: 'pelagos_api_grp_datasets_people_report', methods: ['GET'])]
     public function getGrpDatasetAndPeopleReport(ResearchGroupRepository $researchGroupRepository, FundingOrganizationRepository $fundingOrganizationRepository, SerializerInterface $serialzer): Response
@@ -29,26 +38,37 @@ class ReportController extends AbstractController
     #[Route(path: '/api/stuff')]
     public function getStuff(ResearchGroupRepository $researchGroupRepository,  FundingOrganizationRepository $fundingOrganizationRepository, SerializerInterface $serialzer) : Response
 >>>>>>> b88d625f4 (Working CSV)
+=======
+    #[Route(path: '/api/grp-datasets-people-report', name: 'pelagos_api_grp_datasets_people_report', methods: ['GET'])]
+    public function getStuff(ResearchGroupRepository $researchGroupRepository, FundingOrganizationRepository $fundingOrganizationRepository, SerializerInterface $serialzer): Response
+>>>>>>> 146c8b57e (Added to admin list)
     {
         $fundingOrganization = $fundingOrganizationRepository->findOneBy(['shortName' => 'NAS']);
 
         $researchGroupIds = [];
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 146c8b57e (Added to admin list)
         if ($fundingOrganization instanceof FundingOrganization) {
             foreach ($fundingOrganization->getFundingCycles() as $fundingCycle) {
                 foreach ($fundingCycle->getResearchGroups() as $researchGroup) {
                     $researchGroupIds[] = $researchGroup->getId();
                 }
+<<<<<<< HEAD
 =======
         foreach ($fundingOrganization->getFundingCycles() as $fundingCycle) {
             foreach ($fundingCycle->getResearchGroups() as $researchGroup) {
                 $researchGroupIds[] = $researchGroup->getId();
 >>>>>>> b88d625f4 (Working CSV)
+=======
+>>>>>>> 146c8b57e (Added to admin list)
             }
         }
 
         $researchGroups = $researchGroupRepository->findBy(['id' => $researchGroupIds], ['name' => 'ASC']);
 
+<<<<<<< HEAD
 <<<<<<< HEAD
         usort($researchGroups, function (ResearchGroup $a, ResearchGroup $b) {
             return $a->getFundingCycle()->getName() <=> $b->getFundingCycle()->getName();
@@ -63,6 +83,9 @@ class ReportController extends AbstractController
                     'fundingCycle.name',
 =======
         usort($researchGroups, function(ResearchGroup $a, ResearchGroup $b) {
+=======
+        usort($researchGroups, function (ResearchGroup $a, ResearchGroup $b) {
+>>>>>>> 146c8b57e (Added to admin list)
             return $a->getFundingCycle()->getName() <=> $b->getFundingCycle()->getName();
         });
 
@@ -70,13 +93,18 @@ class ReportController extends AbstractController
             [
                 'groups' => 'grp-dp-report',
                 'csv_headers' => [
+<<<<<<< HEAD
                     'Funding Cycle' => 'fundingCycle.name',
 >>>>>>> b88d625f4 (Working CSV)
+=======
+                    'fundingCycle.name',
+>>>>>>> 146c8b57e (Added to admin list)
                     'ResearchGroupName',
                     'approvedDifsCount',
                     'submittedDatasets',
                     'availableDatasets',
                     'restrictedDataset',
+<<<<<<< HEAD
 <<<<<<< HEAD
                     'peopleCount',
                 ],
@@ -172,16 +200,25 @@ class ReportController extends AbstractController
         '.csv';
 =======
                     'peopleCount'
+=======
+                    'peopleCount',
+>>>>>>> 146c8b57e (Added to admin list)
                 ],
                 'output_utf8_bom' => true,
             ]);
 
+<<<<<<< HEAD
             $csvFilename = 'DatasetMonitoringReport-' .
             (new \DateTime('now'))->format('Ymd\THis') .
             '.csv';
 
             dd($data);
 >>>>>>> b88d625f4 (Working CSV)
+=======
+        $csvFilename = 'GRP-Datasets-People-Report-' .
+        (new \DateTime('now'))->format('Ymd\THis') .
+        '.csv';
+>>>>>>> 146c8b57e (Added to admin list)
 
         $response = new Response($data);
 
