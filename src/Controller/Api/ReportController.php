936514c0c3cb6615e337525c51f12378995e57fa
Michael Scott Williamson <michael.williamson@tamucc.edu>
2025-01-16 08:43:44 -0600

    fixed tabbing issue in file.

--- src/Controller/Api/ReportController.php
+++ src/Controller/Api/ReportController.php
@@ -15,28 +15,28 @@ class ReportController extends AbstractController
     #[Route(path: '/api/grp-people-accounts-report', name: 'pelagos_api_grp_accounts_people_report', methods: ['GET'])]
     public function getGrpPeopleAndAccountsReport(FundingOrganizationRepository $fundingOrganizationRepository): Response
     {
-		$fundingOrganization = $fundingOrganizationRepository->findOneBy(['shortName' => 'NAS']);
-		$fundingCycles = $fundingOrganization->getFundingCycles();
+        $fundingOrganization = $fundingOrganizationRepository->findOneBy(['shortName' => 'NAS']);
+        $fundingCycles = $fundingOrganization->getFundingCycles();
 
-		foreach ($fundingCycles as $fundingCycle) {
-			$researchGroups = $fundingCycle->getResearchGroups();
-			foreach ($researchGroups as $researchGroup) {
-				$people = $researchGroup->getPeople();
-				foreach ($people as $person) {
-					$data[] = [
-						'Funding Cycle' => $fundingCycle->getName(),
-						'Research Group' => $researchGroup->getName(),
-						'Last Name' => $person->getLastName(),
-						'First Name' => $person->getFirstName(),
-						'Account' => $person->getAccount() ? True : False,
-						'Account Creation' => $person->getAccount()?->getCreationTimeStamp()->format('Y-m-d H:i:s'),
-					];
-				}
-			}
-		}
+        foreach ($fundingCycles as $fundingCycle) {
+            $researchGroups = $fundingCycle->getResearchGroups();
+            foreach ($researchGroups as $researchGroup) {
+                $people = $researchGroup->getPeople();
+                foreach ($people as $person) {
+                    $data[] = [
+                        'Funding Cycle' => $fundingCycle->getName(),
+                        'Research Group' => $researchGroup->getName(),
+                        'Last Name' => $person->getLastName(),
+                        'First Name' => $person->getFirstName(),
+                        'Account' => $person->getAccount() ? True : False,
+                        'Account Creation' => $person->getAccount()?->getCreationTimeStamp()->format('Y-m-d H:i:s'),
+                    ];
+                }
+            }
+        }
 
-		var_dump($data);
-		die();
+        var_dump($data);
+        die();
 
         $csvFilename = 'GRP-People-Accounts-Report-' .
         (new \DateTime('now'))->format('Ymd\THis') .
