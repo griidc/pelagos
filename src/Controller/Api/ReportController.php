53f1022ae39e6a2adab1c47aa5bef459be15b64f
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2025-01-16 13:30:56 -0600

    Working CSV
    Still needs admin entry

--- src/Controller/Api/ReportController.php
+++ src/Controller/Api/ReportController.php
@@ -2,36 +2,54 @@
 
 namespace App\Controller\Api;
 
+<<<<<<< HEAD
 use App\Entity\FundingOrganization;
 use App\Entity\ResearchGroup;
 use App\Repository\FundingOrganizationRepository;
 use App\Repository\PersonResearchGroupRepository;
 use App\Repository\ResearchGroupRepository;
+=======
+use App\Entity\ResearchGroup;
+use App\Repository\FundingOrganizationRepository;
+use App\Repository\ResearchGroupRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+>>>>>>> b88d625f4 (Working CSV)
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\HeaderUtils;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
-
 class ReportController extends AbstractController
 {
+<<<<<<< HEAD
     #[Route(path: '/api/grp-datasets-people-report', name: 'pelagos_api_grp_datasets_people_report', methods: ['GET'])]
     public function getGrpDatasetAndPeopleReport(ResearchGroupRepository $researchGroupRepository, FundingOrganizationRepository $fundingOrganizationRepository, SerializerInterface $serialzer): Response
+=======
+    #[Route(path: '/api/stuff')]
+    public function getStuff(ResearchGroupRepository $researchGroupRepository,  FundingOrganizationRepository $fundingOrganizationRepository, SerializerInterface $serialzer) : Response
+>>>>>>> b88d625f4 (Working CSV)
     {
         $fundingOrganization = $fundingOrganizationRepository->findOneBy(['shortName' => 'NAS']);
 
         $researchGroupIds = [];
+<<<<<<< HEAD
         if ($fundingOrganization instanceof FundingOrganization) {
             foreach ($fundingOrganization->getFundingCycles() as $fundingCycle) {
                 foreach ($fundingCycle->getResearchGroups() as $researchGroup) {
                     $researchGroupIds[] = $researchGroup->getId();
                 }
+=======
+        foreach ($fundingOrganization->getFundingCycles() as $fundingCycle) {
+            foreach ($fundingCycle->getResearchGroups() as $researchGroup) {
+                $researchGroupIds[] = $researchGroup->getId();
+>>>>>>> b88d625f4 (Working CSV)
             }
         }
 
         $researchGroups = $researchGroupRepository->findBy(['id' => $researchGroupIds], ['name' => 'ASC']);
 
+<<<<<<< HEAD
         usort($researchGroups, function (ResearchGroup $a, ResearchGroup $b) {
             return $a->getFundingCycle()->getName() <=> $b->getFundingCycle()->getName();
         });
@@ -43,11 +61,23 @@ class ReportController extends AbstractController
                 'groups' => 'grp-dp-report',
                 'csv_headers' => [
                     'fundingCycle.name',
+=======
+        usort($researchGroups, function(ResearchGroup $a, ResearchGroup $b) {
+            return $a->getFundingCycle()->getName() <=> $b->getFundingCycle()->getName();
+        });
+
+        $data = $serialzer->serialize($researchGroups, 'csv',
+            [
+                'groups' => 'grp-dp-report',
+                'csv_headers' => [
+                    'Funding Cycle' => 'fundingCycle.name',
+>>>>>>> b88d625f4 (Working CSV)
                     'ResearchGroupName',
                     'approvedDifsCount',
                     'submittedDatasets',
                     'availableDatasets',
                     'restrictedDataset',
+<<<<<<< HEAD
                     'peopleCount',
                 ],
                 'output_utf8_bom' => true,
@@ -140,6 +170,18 @@ class ReportController extends AbstractController
         $csvFilename = 'GRP-People-Accounts-Report-' .
         (new \DateTime('now'))->format('Ymd\THis') .
         '.csv';
+=======
+                    'peopleCount'
+                ],
+                'output_utf8_bom' => true,
+            ]);
+
+            $csvFilename = 'DatasetMonitoringReport-' .
+            (new \DateTime('now'))->format('Ymd\THis') .
+            '.csv';
+
+            dd($data);
+>>>>>>> b88d625f4 (Working CSV)
 
         $response = new Response($data);
 
