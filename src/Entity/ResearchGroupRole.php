d65466720c6ebd994c3b47ada0333b4d77e0cfcd
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2025-02-27 08:46:39 -0600

    Remove redundant array definition for roles in ResearchGroupRole entity

--- src/Entity/ResearchGroupRole.php
+++ src/Entity/ResearchGroupRole.php
@@ -20,11 +20,4 @@ class ResearchGroupRole extends AbstractRole implements RoleInterface
     const ADMIN = 'Administration';
     const DATA = 'Data';
     const RESEARCHER = 'Researcher';
-
-    public const array ROLES = [
-        self::LEADERSHIP => self::LEADERSHIP,
-        self::ADMIN => self::ADMIN,
-        self::DATA => self::DATA,
-        self::RESEARCHER => self::RESEARCHER,
-    ];
 }
