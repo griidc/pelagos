6c6b6b564642b03ac1801c707126f0bb3ea999c3
Michael van den Eijnden <michael.vandeneijnden@tamucc.edu>
2025-03-18 14:49:06 -0500

    Refactor map search script to streamline variable declarations and improve readability

--- assets/js/entry/map-search.js
+++ assets/js/entry/map-search.js
@@ -11,55 +11,52 @@ import * as EsriLeafletVector from 'esri-leaflet-vector';
 import '../../css/leaflet-custom.css';
 import Routing from '../../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min';
 
-const leafletMap = document.getElementById('leaflet-map');
+const esriApiKey = process.env.ESRI_API_KEY;
+
+const GRIIDCStyle = {
+  color: 'orange',
+  weight: 4,
+  opacity: 0,
+  fillOpacity: 0,
+};
+
+const geojsonMarkerOptions = {
+  radius: 12,
+  fill: false,
+  weight: 4,
+  opacity: 1,
+};
+
+const map = Leaflet.map('leaflet-map', {
+  preferCanvas: true,
+  minZoom: 2,
+  maxZoom: 14,
+  attributionControl: true,
+  worldCopyJump: true,
+});
+
+const basemapEnum = 'ArcGIS:Imagery';
+EsriLeafletVector.vectorBasemapLayer(basemapEnum, {
+  apiKey: esriApiKey,
+}).addTo(map);
+
+Leaflet.featureGroup().addTo(map);
+map.setView([27.5, -97.5], 3);
+
 let geojsonLayer = null;
 
-if (typeof (leafletMap) !== 'undefined' && leafletMap != null) {
-  const esriApiKey = process.env.ESRI_API_KEY;
-
-  const GRIIDCStyle = {
-    color: 'orange',
-    weight: 4,
-    opacity: 0,
-    fillOpacity: 0,
-  };
-
-  const geojsonMarkerOptions = {
-    radius: 12,
-    fill: false,
-    weight: 4,
-    opacity: 1,
-  };
-
-  const map = Leaflet.map('leaflet-map', {
-    preferCanvas: true,
-    minZoom: 2,
-    maxZoom: 14,
-    attributionControl: true,
-    worldCopyJump: true,
-  });
-
-  const basemapEnum = 'ArcGIS:Imagery';
-  EsriLeafletVector.vectorBasemapLayer(basemapEnum, {
-    apiKey: esriApiKey,
+const url = `${Routing.generate('pelagos_api_datasets_all_geojson')}`;
+fetch(url).then((response) => response.json()).then((response) => {
+  geojsonLayer = Leaflet.geoJSON(response, {
+    pointToLayer(feature, latlng) {
+      return Leaflet.circleMarker(latlng, geojsonMarkerOptions);
+    },
+    style: GRIIDCStyle,
   }).addTo(map);
 
-  Leaflet.featureGroup().addTo(map);
-  map.setView([27.5, -97.5], 3);
-
-  const url = `${Routing.generate('pelagos_api_datasets_all_geojson')}`;
-  fetch(url).then((response) => response.json()).then((response) => {
-    geojsonLayer = Leaflet.geoJSON(response, {
-      pointToLayer(feature, latlng) {
-        return Leaflet.circleMarker(latlng, geojsonMarkerOptions);
-      },
-      style: GRIIDCStyle,
-    }).addTo(map);
-
-    // const bounds = geojsonLayer.getBounds();
-    // map.fitBounds(bounds, { padding: [20, 20] });
-  });
-}
+  // const bounds = geojsonLayer.getBounds();
+  // map.fitBounds(bounds, { padding: [20, 20] });
+});
 
 function showGeometryByUDI(id) {
   if (geojsonLayer === null) {
