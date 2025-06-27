cd3910e94ea4fc68b2edb79417af4b87a4d14874
Michael van den Eijnden <mickel1138@gmail.com>
2025-06-27 09:42:37 -0500

    Add FundingCycleCrudController and update DashboardController to include Funding Cycle menu item

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Account;
 use App\Entity\DigitalResourceTypeDescriptor;
 use App\Entity\Funder;
+use App\Entity\FundingCycle;
 use App\Entity\FundingOrganization;
 use App\Entity\LogActionItem;
 use App\Entity\PersonResearchGroup;
@@ -56,6 +57,7 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::section('PODS');
         yield MenuItem::linkToCrud('Funding Organization', 'fas fa-list-alt', FundingOrganization::class);
         yield MenuItem::linkToCrud('Research Groups', 'fas fa-list-alt', ResearchGroup::class);
+        yield MenuItem::linkToCrud('Funding Cycle', 'fas fa-list-alt', FundingCycle::class);
         // yield MenuItem::linkToCrud('Person to Research Groups', 'fas fa-list-alt', PersonResearchGroup::class);
         yield MenuItem::section('Lists');
         yield MenuItem::linkToUrl('Information Products', 'fas fa-list-alt', $this->generateUrl('pelagos_app_ui_information_products'));
