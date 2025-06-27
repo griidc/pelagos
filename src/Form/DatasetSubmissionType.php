bbfc8a764745931227a0dcc62c2edb9633ce8cac
Michael van den Eijnden <mickel1138@gmail.com>
2025-06-27 13:00:04 -0500

    Remove timezone settings from date fields in DatasetSubmissionType form

--- src/Form/DatasetSubmissionType.php
+++ src/Form/DatasetSubmissionType.php
@@ -189,8 +189,8 @@ class DatasetSubmissionType extends AbstractType
                 'html5' => false,
                 'format' => 'yyyy-MM-dd',
                 'required' => true,
-                'model_timezone' => 'UTC',
-                'view_timezone' => 'UTC',
+                // 'model_timezone' => 'UTC',
+                // 'view_timezone' => 'UTC',
             ])
             ->add('temporalExtentEndPosition', Type\DateType::class, [
                 'label' => 'End Date',
@@ -199,8 +199,8 @@ class DatasetSubmissionType extends AbstractType
                 'html5' => false,
                 'format' => 'yyyy-MM-dd',
                 'required' => true,
-                'model_timezone' => 'UTC',
-                'view_timezone' => 'UTC',
+                // 'model_timezone' => 'UTC',
+                // 'view_timezone' => 'UTC',
             ])
             ->add('distributionFormatName', Type\TextType::class, [
                 'label' => 'Distribution Format Name',
