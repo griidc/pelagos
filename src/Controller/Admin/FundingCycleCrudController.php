e5575e16b58d5a38dab4d2a95d7c196786438552
Michael van den Eijnden <mickel1138@gmail.com>
2025-07-01 09:50:48 -0500

    Refactor FundingCycleCrudController to enhance action configuration and field management

--- src/Controller/Admin/FundingCycleCrudController.php
+++ src/Controller/Admin/FundingCycleCrudController.php
@@ -3,7 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\FundingCycle;
+use App\Form\ResearchGroupType;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -14,23 +21,58 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
  */
 class FundingCycleCrudController extends AbstractCrudController
 {
+    use EasyAdminCrudTrait;
+
     #[\Override]
     public static function getEntityFqcn(): string
     {
         return FundingCycle::class;
     }
 
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Funding Cycles')
+            ->setEntityLabelInSingular('Funding Cycle')
+            ->setPageTitle(Crud::PAGE_INDEX, 'Funding Cycles')
+            ->setPageTitle(Crud::PAGE_EDIT, 'Edit Funding Cycle')
+            ->setPageTitle(Crud::PAGE_NEW, 'Create Funding Cycle')
+            ->setPageTitle(Crud::PAGE_DETAIL, 'Funding Cycle Details')
+            ->showEntityActionsInlined();
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->update(Crud::PAGE_INDEX, Action::DETAIL, function (Action $action) {
+                return $action
+                    ->setIcon('fa fa-eye')
+                    ->setLabel('View');
+            })
+            ->update(Crud::PAGE_INDEX, Action::EDIT, function (Action $action) {
+                return $action
+                    ->setIcon('fa fa-edit')
+                    ->setLabel('Edit');
+            });
+    }
 
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+            IdField::new('id')->hideOnForm(),
             TextField::new('name'),
-            TextEditorField::new('description'),
-            UrlField::new('url')
-                ->setRequired(false)
-                ->setHelp('URL to the funding cycle documentation or related information.'),
-
+            TextField::new('udiPrefix'),
+            AssociationField::new('fundingOrganization'),
+            TextEditorField::new('description')->hideOnIndex(),
+            UrlField::new('url'),
+            DateField::new('startDate')->hideOnIndex(),
+            DateField::new('endDate')->hideOnIndex(),
+            CollectionField::new('researchGroups')
+                ->setDisabled()
+                ->setEntryType(ResearchGroupType::class)
+                ->setEntryIsComplex(true)
+                ->hideOnIndex(),
         ];
     }
 }
