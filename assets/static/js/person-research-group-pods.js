463ffd8a926f19c8daff1354d0dbb378f2d0ebc4
Michael van den Eijnden <mickel1138@gmail.com>
2025-07-21 09:27:53 -0500

    Refactor delete button event listener to prevent default action only on confirmation

--- assets/static/js/person-research-group-pods.js
+++ assets/static/js/person-research-group-pods.js
@@ -3,11 +3,10 @@ var deleteButtons = document.querySelectorAll('.field-collection-delete-button')
 // add event listener to each delete button
 deleteButtons.forEach(function(deleteButton) {
     deleteButton.addEventListener('click', function(e) {
-        e.preventDefault();
         var confirmation = confirm('Are you sure you want to delete this item?');
         if (!confirmation) {
             e.preventDefault();
             e.stopImmediatePropagation();
         }
     });
-}
+});
