7b7ae706692093f8b89fe8acbd239d1ae774120f
Michael van den Eijnden <mickel1138@gmail.com>
2025-08-04 15:37:44 -0500

    Add Dataset and DatasetSubmission CRUD controllers to the admin panel

---
+++ src/Controller/Admin/DatasetSubmissionCrudController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\DatasetSubmission;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class DatasetSubmissionCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return DatasetSubmission::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+
+    #[\Override]
+    public function configureActions(Actions $actions): Actions
+    {
+        $exportAction = Action::new('export')
+            ->setLabel('Export')
+            ->setIcon('fa fa-file-export')
+            ->linkToCrudAction('export')
+            ->createAsGlobalAction();
+
+        return parent::configureActions($actions)
+            ->add(Crud::PAGE_INDEX, $exportAction)
+            ->remove(Crud::PAGE_INDEX, Action::BATCH_DELETE)
+            ->remove(Crud::PAGE_INDEX, Action::NEW)
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->update(Crud::PAGE_INDEX, Action::DETAIL, function (Action $action) {
+                return $action
+                    ->setIcon('fa fa-eye')
+                    ->setLabel('View');
+            })
+            ->remove(Crud::PAGE_INDEX, Action::EDIT)
+            ->remove(Crud::PAGE_INDEX, Action::DELETE)
+            ->remove(Crud::PAGE_DETAIL, Action::DELETE)
+            ->remove(Crud::PAGE_DETAIL, Action::EDIT);
+    }
+}
