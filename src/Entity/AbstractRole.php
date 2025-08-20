64e0023cc4050b97e27de6c80890a9bc3eac23c2
Michael van den Eijnden <mickel1138@gmail.com>
2025-08-20 08:59:38 -0500

    refactor: update AbstractRole and RoleInterface for improved type consistency and clarity

--- src/Entity/AbstractRole.php
+++ src/Entity/AbstractRole.php
@@ -2,9 +2,9 @@
 
 namespace App\Entity;
 
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
-use App\Validator\Constraints as CustomAssert;
 
 /**
  * Base entity class for the representation of typed Role classes.
@@ -14,17 +14,17 @@ use App\Validator\Constraints as CustomAssert;
 #[ORM\MappedSuperclass]
 abstract class AbstractRole extends Entity implements RoleInterface
 {
+    use IdTrait;
+
     /**
      * A friendly name for this type of entity.
      */
-    const FRIENDLY_NAME = 'Role';
+    public const FRIENDLY_NAME = 'Role';
 
     /**
      * The name of this role.
      *
      * @var string
-     *
-     *
      */
     #[Assert\Regex(pattern: '/[<>]/', match: false, message: 'Name cannot contain angle brackets (< or >)')]
     #[ORM\Column(type: 'citext')]
@@ -34,17 +34,16 @@ abstract class AbstractRole extends Entity implements RoleInterface
     /**
      * The weight associated with this role.
      *
-     * @var integer
-     *
+     * @var int
      */
-    #[ORM\Column(type: 'integer')]
+    #[ORM\Column(type: Types::INTEGER)]
     #[Assert\NotBlank(message: 'Weight is required')]
     protected $weight;
 
     /**
      * Setter for Name.
      *
-     * @param string $name The name of this role.
+     * @param string $name the name of this role
      *
      * @return void
      */
@@ -56,7 +55,7 @@ abstract class AbstractRole extends Entity implements RoleInterface
     /**
      * Getter for Name.
      *
-     * @return string The name of this role.
+     * @return string the name of this role
      */
     public function getName()
     {
@@ -66,28 +65,25 @@ abstract class AbstractRole extends Entity implements RoleInterface
     /**
      * Setter for Weight.
      *
-     * @param integer $weight The weight associated with this role.
-     *
-     * @throws \InvalidArgumentException When provided weight is not an integer or integer string.
+     * @param int $weight the weight associated with this role
      *
      * @return void
+     *
+     * @throws \InvalidArgumentException when provided weight is not an integer or integer string
      */
     public function setWeight(int $weight)
     {
-        if (
-            is_int($weight) and $weight > 0 or
-            is_string($weight) and ctype_digit($weight) and (int) $weight > 0
-        ) {
-            $this->weight = (int) $weight;
-        } else {
+        if ($weight <= 0) {
             throw new \InvalidArgumentException('Weight must be a positive integer');
         }
+
+        $this->weight = $weight;
     }
 
     /**
      * Getter for Weight.
      *
-     * @return integer The weight associated with this role.
+     * @return int the weight associated with this role
      */
     public function getWeight()
     {
