35ab226ada3d8597c1a4e26580e77e4c5d5eaf24
Michael van den Eijnden <mickel1138@gmail.com>
2025-08-27 10:47:34 -0500

    fix: update migration description and adjust SQL commands for research_group_id_seq

--- migrations/Version20250820141406.php
+++ migrations/Version20250820141406.php
@@ -14,18 +14,20 @@ final class Version20250820141406 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Doctrine 3 migration';
     }
 
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP SEQUENCE research_group_id_seq CASCADE');
         $this->addSql('COMMENT ON COLUMN person_token.valid_for IS \'\'');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE research_group_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('COMMENT ON COLUMN person_token.valid_for IS \'(DC2Type:dateinterval)\'');
     }
 }
