a54aa4372752d0b3b742060aeabdc86db4218986
Michael van den Eijnden <mickel1138@gmail.com>
2025-10-08 14:24:45 -0500

    Initialize dashboard with Alpine.js and related styles

--- assets/js/entry/dashboard.js
+++ assets/js/entry/dashboard.js
@@ -1,1 +1,10 @@
+import '../../scss/landing-pages.scss';
+import '../../css/activeInactive.css';
 import '../../scss/dashboard.scss';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import Alpine from 'alpinejs';
+import collapse from '@alpinejs/collapse';
+
+window.Alpine = Alpine;
+Alpine.plugin(collapse);
+Alpine.start();
