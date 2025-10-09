f5310a641a46ad77096580f9028248718f35f8f3
Michael van den Eijnden <mickel1138@gmail.com>
2025-10-09 13:51:14 -0500

    Import cardClick module in dashboard.js for enhanced interactivity

--- assets/js/entry/dashboard.js
+++ assets/js/entry/dashboard.js
@@ -4,6 +4,7 @@ import '../../scss/dashboard.scss';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Alpine from 'alpinejs';
 import collapse from '@alpinejs/collapse';
+import '../modules/cardClick';
 
 window.Alpine = Alpine;
 Alpine.plugin(collapse);
