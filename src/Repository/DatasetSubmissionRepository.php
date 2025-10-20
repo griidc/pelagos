dbcd63dec859230c0bffd75244a9b42d32b8c32f
Michael Scott Williamson <michael.williamson@tamucc.edu>
2025-10-20 15:35:17 -0500

    Use array collection for params.

--- src/Repository/DatasetSubmissionRepository.php
+++ src/Repository/DatasetSubmissionRepository.php
@@ -5,6 +5,8 @@ namespace App\Repository;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\ORM\Query;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Query\Parameter;
 use App\Entity\Dataset;
 use App\Entity\DatasetSubmission;
 use App\Util\FundingOrgFilter;
@@ -85,11 +87,11 @@ class DatasetSubmissionRepository extends ServiceEntityRepository
                 GROUP BY subDatasetSubmission.dataset
             )')
             ->setParameters(
-                array(
-                    'metadatastatus' => Dataset::DATASET_STATUS_ACCEPTED,
-                    'restrictedstatus' => DatasetSubmission::RESTRICTION_NONE,
-                    'transerstatuscompleted' => DatasetSubmission::TRANSFER_STATUS_COMPLETED,
-                )
+                new ArrayCollection(array(
+                    new Parameter('metadatastatus', Dataset::DATASET_STATUS_ACCEPTED),
+                    new Parameter('restrictedstatus', DatasetSubmission::RESTRICTION_NONE),
+                    new Parameter('transerstatuscompleted', DatasetSubmission::TRANSFER_STATUS_COMPLETED),
+                ))
             )
             ->orderBy('datasetSubmission.creationTimeStamp');
 
