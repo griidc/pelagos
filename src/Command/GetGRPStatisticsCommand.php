f3ede6390ad66cd52f8aba39ec41291bb30bb611
Michael Scott Williamson <michael.williamson@tamucc.edu>
2026-02-09 13:28:29 -0600

    Initial GRP report, just extends the GoMRI report class.

---
+++ src/Command/GetGRPStatisticsCommand.php
@@ -0,0 +1,207 @@
+<?php
+namespace App\Command;
+
+use App\Entity\Dataset;
+use App\Entity\DatasetSubmission;
+use App\Entity\DIF;
+use App\Entity\LogActionItem;
+use App\Repository\LogActionItemRepository;
+use App\Util\FundingOrgFilter;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[\Symfony\Component\Console\Attribute\AsCommand(name: 'pelagos:get-grp-statistics', description: 'Produce GRP report artifacts.')]
+class GetGRPStatisticsCommand extends GetGoMRIStatisticsCommand
+{
+    #[Override]
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $datasets = $this->getEntityManager()->getRepository(Dataset::class)->findAll();
+
+        $datasetCount = 0;
+        $grpDatasetCount = 0;
+        $grpDatasetColdStorageCount = 0;
+        $grpDatasetTotalSize = 0;
+        $grpColdStorageDatasetTotalSize = 0;
+        $totalPostGrpDatasetsSubmittedByQuarter = [];
+        $skipCount = 0;
+
+        foreach ($datasets as $dataset) {
+            if (DIF::STATUS_UNSUBMITTED === $dataset->getDif()->getStatus()) {
+                continue;
+            }
+            ++$datasetCount;
+            $datasetSubmission = $dataset->getDatasetSubmission();
+            if ('NAS' === $dataset->getResearchGroup()->getFundingCycle()->getFundingOrganization()->getShortName()) {
+                ++$grpDatasetCount;
+                if ($datasetSubmission instanceof DatasetSubmission) {
+                    $this->quarterize($datasetSubmission->getSubmissionTimeStamp(), $totalPostGrpDatasetsSubmittedByQuarter);
+
+
+                    if ($datasetSubmission->isDatasetFileInColdStorage()) {
+                        ++$grpDatasetColdStorageCount;
+                        $grpColdStorageDatasetTotalSize += $datasetSubmission->getColdStorageTotalUnpackedSize() ?? 0;
+                    } else {
+                        $grpDatasetTotalSize += $datasetSubmission->getFileset()?->getFileSize() ?? 0;
+                    }
+                }
+            }
+
+        }
+
+        $io->writeln("Total number of GRP Datasets: $grpDatasetCount");
+        $io->writeln('Total Size of GRP Datasets: ' . round(($grpDatasetTotalSize + $grpColdStorageDatasetTotalSize) / 1000000000000, 1) . ' TB');
+        $io->writeln("Number of GRP Datasets in cold storage: $grpDatasetColdStorageCount");
+        $io->writeln('Total size of GRP Datasets in cold storage: ' . round($grpColdStorageDatasetTotalSize / 1000000000000, 1) . ' TB');
+
+
+        // GRP submissions by quarter:
+        $years = array_keys($totalPostGrpDatasetsSubmittedByQuarter);
+        sort($years);
+        $minYear = $years[0];
+        $maxYear = $years[sizeof($years) - 1];
+        $io->writeln("\nGRP Uploads (submissions)\n");
+        for ($i = $minYear; $i <= $maxYear; $i++) {
+            if (in_array($i, $years)) {
+                $io->writeln("Number of GRP datasets submitted $i"
+                . ' Q1:' . $totalPostGrpDatasetsSubmittedByQuarter[$i][0]
+                . ' Q2:' . $totalPostGrpDatasetsSubmittedByQuarter[$i][1]
+                . ' Q3:' . $totalPostGrpDatasetsSubmittedByQuarter[$i][2]
+                . ' Q4:' . $totalPostGrpDatasetsSubmittedByQuarter[$i][3]);
+            }
+        }
+
+        $io->writeln("\nGRP Downloads:\n");
+        $downloadSizeByYearAndQuarter = [];
+        $anonymousDownloadCountByYearAndQuarter = [];
+        $loggedInDownloadCountByYearAndQuarter = [];
+
+        foreach ($this->getDownloads() as $datasetDownload) {
+            $id = $datasetDownload[0];
+            $timestamp = $datasetDownload[1];
+            $loginType = $datasetDownload[2];
+
+            $yearQuarter = $this->determineQuarter($timestamp);
+            $year = $yearQuarter['year'];
+            $quarter = $yearQuarter['quarter'];
+
+            if (!array_key_exists($year, $anonymousDownloadCountByYearAndQuarter)) {
+                $anonymousDownloadCountByYearAndQuarter[$year][0] = 0;
+                $anonymousDownloadCountByYearAndQuarter[$year][1] = 0;
+                $anonymousDownloadCountByYearAndQuarter[$year][2] = 0;
+                $anonymousDownloadCountByYearAndQuarter[$year][3] = 0;
+                $anonymousDownloadCountByYearAndQuarter[$year][4] = 0;
+            }
+
+            if (!array_key_exists($year, $loggedInDownloadCountByYearAndQuarter)) {
+                $loggedInDownloadCountByYearAndQuarter[$year][0] = 0;
+                $loggedInDownloadCountByYearAndQuarter[$year][1] = 0;
+                $loggedInDownloadCountByYearAndQuarter[$year][2] = 0;
+                $loggedInDownloadCountByYearAndQuarter[$year][3] = 0;
+                $loggedInDownloadCountByYearAndQuarter[$year][4] = 0;
+            }
+
+            if (!array_key_exists($year, $downloadSizeByYearAndQuarter)) {
+                $downloadSizeByYearAndQuarter[$year][0] = 0;
+                $downloadSizeByYearAndQuarter[$year][1] = 0;
+                $downloadSizeByYearAndQuarter[$year][2] = 0;
+                $downloadSizeByYearAndQuarter[$year][3] = 0;
+                $downloadSizeByYearAndQuarter[$year][4] = 0;
+            }
+
+            $dataset = $this->getEntityManager()->find(Dataset::class, $id);
+            // We may also need to consider using the right size for the external-logged download requests
+            // that also get counted here, but end up getting the stub sizes. There aren't many though.
+            if ($dataset instanceof Dataset) {
+                $size = $dataset->getTotalFileSize();
+            } else {
+                $size = 0;
+                $skipCount++;
+            }
+
+            if ($loginType === 'anonymous') {
+                $anonymousDownloadCountByYearAndQuarter[$year][$quarter]++;
+            } else {
+                $loggedInDownloadCountByYearAndQuarter[$year][$quarter]++;
+            }
+            $downloadSizeByYearAndQuarter[$year][$quarter] += $size / 1000000000;
+        }
+
+        // array_keys still onlys dump first level, years, for $array[year][quarter].
+        $firstYear = min(array_keys($anonymousDownloadCountByYearAndQuarter));
+        $lastYear = max(array_keys($anonymousDownloadCountByYearAndQuarter));
+
+        $totalDownloads = 0;
+        $totalDownloadSize = 0;
+        for ($year = $firstYear; $year <= $lastYear; $year++) {
+            $yearCountTotal = 0;
+            $yearSizeTotal = 0;
+            for ($quarter = 1; $quarter <= 4; $quarter++) {
+                $popularDownloads = $this->getTopDatasetsDownloadedByYearAndQuarter(self::NUMBEROFTOPDOWNLOADSTOSHOW, $year, $quarter);
+                $io->writeln(
+                    $year
+                    . '/Q'
+                    . $quarter
+                    . ' Logged-in Download Count: '
+                    . $loggedInDownloadCountByYearAndQuarter[$year][$quarter]
+                    . ' Anonymous DL Count: '
+                    . $anonymousDownloadCountByYearAndQuarter[$year][$quarter]
+                    . ', ' . 'Total Size (GB): '
+                    . round($downloadSizeByYearAndQuarter[$year][$quarter])
+                );
+
+                $yearCountTotal += $loggedInDownloadCountByYearAndQuarter[$year][$quarter];
+                $yearCountTotal += $anonymousDownloadCountByYearAndQuarter[$year][$quarter];
+                $totalDownloads += $loggedInDownloadCountByYearAndQuarter[$year][$quarter];
+                $totalDownloads += $anonymousDownloadCountByYearAndQuarter[$year][$quarter];
+
+                $yearSizeTotal += $downloadSizeByYearAndQuarter[$year][$quarter];
+                $totalDownloadSize += $downloadSizeByYearAndQuarter[$year][$quarter];
+
+                $popular = "Top: ";
+                foreach ($popularDownloads as $udi => $count) {
+                    $popular .= "$udi:$count, ";
+                }
+                $popular = substr($popular, 0, strlen($popular) - 2);
+                $io->writeln($popular);
+            }
+            $io->newLine();
+
+            $io->writeln('Totals for: ' . $year . ':');
+            $io->writeln('Download count: ' . $yearCountTotal);
+            $io->writeln('Data Downloaded: ' . round($yearSizeTotal) . ' GB');
+            $io->writeln('--------------------------------------------------------------------------------');
+
+            $io->newLine();
+        }
+
+        // Total Download stats:
+        $io->writeln('All Time Stats:');
+        $io->writeln('Download count: ' . $totalDownloads);
+        $io->writeln('Data Downloaded: ' . round($totalDownloadSize) . ' GB');
+
+        // Show most popular downloads of all time.
+        $popularDownloadsOfAllTime = $this->getTopDatasetsDownloadedByYearAndQuarter(self::NUMBEROFTOPDOWNLOADSTOSHOW);
+        $allTimePopular = '';
+        $io->writeln('Top ' . self::NUMBEROFTOPDOWNLOADSTOSHOW . ' downloads of all time:');
+        foreach ($popularDownloadsOfAllTime as $udi => $count) {
+            $allTimePopular .= "$udi:$count, ";
+        }
+        $allTimePopular = substr($allTimePopular, 0, strlen($allTimePopular) - 2);
+        $io->writeln($allTimePopular);
+
+        // Add disclaimer if any datasets had been deleted. (This doesn't apply to gomri currently, but other FOs)
+        if ($skipCount > 0) {
+            $io->warning("Skipped $skipCount entries as these datasets are no longer available.");
+        }
+
+        return Command::SUCCESS;
+    }
+}
