2b2afb28f484bb7966dd78724459ddc50d926639
Michael van den Eijnden <mickel1138@gmail.com>
2026-05-13 08:21:48 -0500

    feat(dataset-submission): add form fields and validation for dataset contacts and information and keywords

--- assets/js/entry/dataset-submission.js
+++ assets/js/entry/dataset-submission.js
@@ -1,1 +1,62 @@
 import '../../scss/dataset-submission.scss';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+import TomSelect from 'tom-select';
+import 'tom-select/dist/css/tom-select.css';
+
+import JustValidate from 'just-validate';
+import JustValidatePluginDate from 'just-validate-plugin-date';
+
+import Routing from '../../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min';
+
+import GeoViz from '../modules/geoViz';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const geoViz = new GeoViz(document.getElementById('leaflet-map'), {
+    loadWizard: true,
+  });
+
+  const datasetContacts = document.getElementsByClassName('contactperson');
+
+  Array.from(datasetContacts).forEach((contact) => {
+    const contactSelect = new TomSelect(contact, {
+      maxOptions: null,
+    });
+  });
+
+  const funders = document.getElementById('funders_0');
+  const fundersSelect = new TomSelect(funders, {
+    maxOptions: null,
+    plugins: {
+      clear_button: {
+        title: 'Remove all selected options',
+      },
+    },
+  });
+
+  const themeKeywords = document.getElementById('theme-keywords');
+  const themeKeywordsSelect = new TomSelect(themeKeywords, {
+    plugins: ['remove_button'],
+    searchField: null,
+    maxOptions: null,
+    create: true,
+    persist: false,
+  });
+
+  const placeKeywords = document.getElementById('place-keywords');
+  const placeKeywordsSelect = new TomSelect(placeKeywords, {
+    plugins: ['remove_button'],
+    searchField: null,
+    maxOptions: null,
+    create: true,
+    persist: false,
+  });
+
+  const topicKeywords = document.getElementById('topic-keyword-select');
+  const topicKeywordsSelect = new TomSelect(topicKeywords, {
+    plugins: ['remove_button'],
+    maxOptions: null,
+    create: false,
+    persist: false,
+  });
+});
