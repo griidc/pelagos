6daef66a733d9b3246f15f64d6bdade29193fff6
Michael Scott Williamson <michael.williamson@tamucc.edu>
2026-06-12 12:47:01 -0500

    Add method to get tags associated with an Information Product, as a string.

--- src/Entity/InformationProduct.php
+++ src/Entity/InformationProduct.php
@@ -612,4 +612,29 @@ class InformationProduct extends Entity
 
         return $this;
     }
+
+    /**
+     * Show strings of tags used - this is the product type descriptor and digital resource type descriptor names.
+     */
+    #[Serializer\VirtualProperty]
+    public function getTags(): string
+    {        $tagString = '';
+        foreach ($this->getProductTypeDescriptors() as $productTypeDescriptor) {
+            if ($tagString) {
+                $tagString = $tagString . ', ' . $productTypeDescriptor->getDescription();
+            } else {
+                $tagString = $productTypeDescriptor->getDescription();
+            }
+        }
+
+        foreach ($this->getDigitalResourceTypeDescriptors() as $digitalResourceTypeDescriptor) {
+            if ($tagString) {
+                $tagString = $tagString . ', ' . $digitalResourceTypeDescriptor->getDescription();
+            } else {
+                $tagString = $digitalResourceTypeDescriptor->getDescription();
+            }
+        }
+
+        return $tagString;
+    }
 }
