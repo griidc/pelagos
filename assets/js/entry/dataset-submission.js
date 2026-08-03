509655a5cf2e21e3bb95d9f4ab728aa21075f93b
Michael van den Eijnden <mickel1138@gmail.com>
2026-08-03 08:22:41 -0500

    Update form field locking condition to check for STATUS_INCOMPLETE instead of STATUS_UNSUBMITTED

--- assets/js/entry/dataset-submission.js
+++ assets/js/entry/dataset-submission.js
@@ -529,7 +529,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  if (status !== DATASET_SUBMISSION_STATES.STATUS_UNSUBMITTED && !isDrpm) {
+  if (status !== DATASET_SUBMISSION_STATES.STATUS_INCOMPLETE && !isDrpm) {
     const formFields = form.querySelectorAll('input, select, textarea, button');
     formFields.forEach((field) => {
       const formField = field;
